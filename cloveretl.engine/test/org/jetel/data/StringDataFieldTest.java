05658bd85951e616e8ef56efb737a8ddc58b0fd5
wesm <wesm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2003-04-10 18:47:58 +0000

    Work-in-progress
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@13 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/test/org/jetel/data/StringDataFieldTest.java
@@ -0,0 +1,58 @@
+/*
+ *  jETeL/Clover - Java based ETL application framework.
+ *  Created on Apr 10, 2003
+ *  Copyright (C) 2003, 2002  David Pavlis, Wes Maciorowski
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package test.org.jetel.data;
+
+import junit.framework.TestCase;
+
+/**
+ * @author maciorowski
+ *
+ */
+public class StringDataFieldTest  extends TestCase {
+
+	
+
+protected void setUp() { 
+
+}
+
+
+
+protected void tearDown() {
+
+
+}
+
+
+
+
+
+/**
+
+ *  Test for @link org.jetel.metadata.DataRecordMetadata.getField(int _fieldNum)
+
+ *
+
+ */
+
+public void test_1_getField() {
+
+	}
+}
