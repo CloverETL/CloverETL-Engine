b6842549f25bab4715ca03fff980a23b3a438119
dpavlis <info@cloveretl.com>
2003-05-25 20:24:35 +0000

    added functionality to create DataRecordMetadata based on SQLMetadata
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@96 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/database/SQLUtil.java
+++ cloveretl.engine/src/org/jetel/database/SQLUtil.java
@@ -22,6 +22,7 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.DatabaseMetaData;
 import java.sql.ParameterMetaData;
+import java.sql.ResultSetMetaData;
 import java.util.List;
 import java.util.LinkedList;
 import org.jetel.metadata.DataFieldMetadata;
@@ -35,6 +36,10 @@ import org.jetel.metadata.DataRecordMetadata;
  *@since      September 25, 2002
  */
 public class SQLUtil {
+
+	private final static String DEFAULT_DELIMITER = ";";
+	private final static String END_RECORD_DELIMITER = "\n";
+
 
 	/**
 	 *  Gets the FieldTypes attribute of the SQLUtil class
@@ -55,13 +60,22 @@ public class SQLUtil {
 		return fieldTypes;
 	}
 
+
+	/**
+	 *  Gets the fieldTypes attribute of the SQLUtil class
+	 *
+	 *@param  metadata          Description of the Parameter
+	 *@return                   The fieldTypes value
+	 *@exception  SQLException  Description of the Exception
+	 */
 	public static List getFieldTypes(ParameterMetaData metadata) throws SQLException {
 		List fieldTypes = new LinkedList();
-		for(int i=1;i<=metadata.getParameterCount();i++){
+		for (int i = 1; i <= metadata.getParameterCount(); i++) {
 			fieldTypes.add(new Integer(metadata.getParameterType(i)));
 		}
 		return fieldTypes;
 	}
+
 
 	/**
 	 *  Converts SQL data type into Jetel data type
@@ -72,29 +86,30 @@ public class SQLUtil {
 	 */
 	public static char sqlType2jetel(int sqlType) {
 		switch (sqlType) {
-			case Types.INTEGER:
-			case Types.SMALLINT:
-				return DataFieldMetadata.INTEGER_FIELD;
-			case Types.BIGINT:
-			case Types.DECIMAL:
-			case Types.DOUBLE:
-			case Types.FLOAT:
-			case Types.NUMERIC:
-			case Types.REAL:
-				return DataFieldMetadata.NUMERIC_FIELD;
-			//------------------
-			case Types.CHAR:
-			case Types.LONGVARCHAR:
-			case Types.VARCHAR:
-				return DataFieldMetadata.STRING_FIELD;
-			//------------------
-			case Types.DATE:
-			case Types.TIME:
-			case Types.TIMESTAMP:
-				return DataFieldMetadata.DATE_FIELD;
-			default:
-				return (char) -1;
-			// unknown or not possible to translate
+						case Types.INTEGER:
+						case Types.SMALLINT:
+							return DataFieldMetadata.INTEGER_FIELD;
+						case Types.BIGINT:
+						case Types.DECIMAL:
+						case Types.DOUBLE:
+						case Types.FLOAT:
+						case Types.NUMERIC:
+						case Types.REAL:
+							return DataFieldMetadata.NUMERIC_FIELD;
+						//------------------
+						case Types.CHAR:
+						case Types.LONGVARCHAR:
+						case Types.VARCHAR:
+						case Types.OTHER:
+							return DataFieldMetadata.STRING_FIELD;
+						//------------------
+						case Types.DATE:
+						case Types.TIME:
+						case Types.TIMESTAMP:
+							return DataFieldMetadata.DATE_FIELD;
+						default:
+							return (char) -1;
+						// unknown or not possible to translate
 		}
 	}
 
@@ -107,16 +122,16 @@ public class SQLUtil {
 	 */
 	public static String jetelType2Str(char fieldType) {
 		switch (fieldType) {
-			case DataFieldMetadata.NUMERIC_FIELD:
-				return "numeric";
-			case DataFieldMetadata.INTEGER_FIELD:
-				return "integer";
-			case DataFieldMetadata.STRING_FIELD:
-				return "string";
-			case DataFieldMetadata.DATE_FIELD:
-				return "date";
-			default:
-				throw new RuntimeException("Unsupported data type " + fieldType);
+						case DataFieldMetadata.NUMERIC_FIELD:
+							return "numeric";
+						case DataFieldMetadata.INTEGER_FIELD:
+							return "integer";
+						case DataFieldMetadata.STRING_FIELD:
+							return "string";
+						case DataFieldMetadata.DATE_FIELD:
+							return "date";
+						default:
+							throw new RuntimeException("Unsupported data type " + fieldType);
 		}
 	}
 
@@ -134,7 +149,7 @@ public class SQLUtil {
 	public static String assembleInsertSQLStatement(DataRecordMetadata metadata, String tableName) {
 		StringBuffer strBuf = new StringBuffer();
 		StringBuffer strBuf2 = new StringBuffer();
-		
+
 		strBuf.append(") values(");
 		for (int i = 0; i < metadata.getNumFields(); i++) {
 			strBuf2.append(metadata.getField(i).getName());
@@ -144,13 +159,53 @@ public class SQLUtil {
 				strBuf2.append(",");
 			}
 		}
-		strBuf.insert(0,strBuf2.toString());
-		strBuf.insert(0," (");
-		strBuf.insert(0,tableName);
-		strBuf.insert(0,"insert into ");
+		strBuf.insert(0, strBuf2.toString());
+		strBuf.insert(0, " (");
+		strBuf.insert(0, tableName);
+		strBuf.insert(0, "insert into ");
 		strBuf.append(")");
 		System.out.println(strBuf.toString());
 		return strBuf.toString();
 	}
+
+
+	/**
+	 *  Converts SQL metadata into Clover's DataRecordMetadata
+	 *
+	 *@param  dbMetadata        SQL ResultSet metadata describing which columns are returned by query
+	 *@return                   DataRecordMetadata which correspond to the SQL ResultSet 
+	 *@exception  SQLException  Description of the Exception
+	 */
+	public static DataRecordMetadata dbMetadata2jetel(ResultSetMetaData dbMetadata) throws SQLException {
+		DataFieldMetadata fieldMetadata;
+		DataRecordMetadata jetelMetadata = new DataRecordMetadata(dbMetadata.getTableName(1),
+				DataRecordMetadata.DELIMITED_RECORD);
+
+		for (int i = 1; i <= dbMetadata.getColumnCount(); i++) {
+			try {
+				fieldMetadata = new DataFieldMetadata(dbMetadata.getColumnName(i),DEFAULT_DELIMITER);
+				
+			} catch (Exception ex) {
+				throw new RuntimeException(ex.getMessage() + " field name " + dbMetadata.getColumnName(i));
+			}
+			// set proper data type
+			fieldMetadata.setType(SQLUtil.sqlType2jetel(dbMetadata.getColumnType(i)));
+			
+			if (i == dbMetadata.getColumnCount()) {
+				fieldMetadata.setDelimiter(END_RECORD_DELIMITER);
+			}
+			
+			if (dbMetadata.isNullable(i) == ResultSetMetaData.columnNullable) {
+				fieldMetadata.setNullable(true);
+			}
+			if (dbMetadata.isCurrency(i)) {
+				fieldMetadata.setFormatStr("¤#.#");
+			}
+
+			jetelMetadata.addField(fieldMetadata);
+		}
+		return jetelMetadata;
+	}
+
 }
 
