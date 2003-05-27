315bbb1584ce1639958fb094e7cf02e25dd277be
dpavlis <dpavlis@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2003-05-27 08:17:36 +0000

    added TINYINT support into sqlType2jetel()
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@100 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/database/SQLUtil.java
+++ cloveretl.engine/src/org/jetel/database/SQLUtil.java
@@ -88,6 +88,7 @@ public class SQLUtil {
 		switch (sqlType) {
 						case Types.INTEGER:
 						case Types.SMALLINT:
+						case Types.TINYINT:
 							return DataFieldMetadata.INTEGER_FIELD;
 						case Types.BIGINT:
 						case Types.DECIMAL:
