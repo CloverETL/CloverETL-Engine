acf9c2af15ee9826494d25a67111b9a14f9f78ed
dpavlis <dpavlis@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2003-09-12 15:06:37 +0000

    initial checkin
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@140 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/src/org/jetel/main/runGraph.java
@@ -0,0 +1,63 @@
+/*
+*    jETeL/Clover - Java based ETL application framework.
+*    Copyright (C) 2002-03  David Pavlis
+*
+*    This program is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the License, or
+*    (at your option) any later version.
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package org.jetel.main;
+
+import java.io.*;
+import org.jetel.graph.*;
+
+public class runGraph{
+
+	public static void main(String args[]){
+		if (args.length<1){
+			System.err.println("Usage: runGraph <graph definition file>");
+		}
+		FileInputStream in;
+		System.out.println("***  CloverETL framework, (c) 2002-03 D.Pavlis, released under GNU Public Licence  ***\n");
+		System.out.println("Graph definition file: "+args[0]);
+		
+		try{
+			in=new FileInputStream(args[0]);
+		}
+		catch(FileNotFoundException e){
+			e.printStackTrace();
+			return;
+		}
+		
+		
+		TransformationGraphXMLReaderWriter graphReader=TransformationGraphXMLReaderWriter.getReference();
+		TransformationGraph graph= TransformationGraph.getReference();
+		
+		if (!graphReader.read(graph,in)){
+			System.err.println("Error in reading graph from XML !");
+			return;
+		}
+		
+		if(!graph.init(null)){
+		System.err.println("Graph initialization failed !");
+		return;
+		}
+		
+		if (!graph.run()){ // start all Nodes (each node is one thread)
+			System.out.println("Failed starting graph !");
+		return;		
+		}
+	
+	}
+
+}
