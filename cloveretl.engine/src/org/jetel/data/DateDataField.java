9acb6e61f8fa29b33b79b36770e94ba0eda1c0a8
dpavlis <dpavlis@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2003-12-12 13:31:25 +0000

    small improvements
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@182 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/data/DateDataField.java
+++ cloveretl.engine/src/org/jetel/data/DateDataField.java
@@ -35,10 +35,11 @@ import org.jetel.metadata.DataFieldMetadata;
 /**
  *  A class that represents ...
  *
- *@author     D.Pavlis
- *@created    January 26, 2003
- *@since      March 28, 2002
- *@see        OtherClasses
+ * @author      D.Pavlis
+ * @since       March 28, 2002
+ * @revision    $Revision$
+ * @created     January 26, 2003
+ * @see         OtherClasses
  */
 public class DateDataField extends DataField {
 
@@ -46,7 +47,7 @@ public class DateDataField extends DataField {
 	/**
 	 *  An attribute that represents ...
 	 *
-	 *@since    March 28, 2002
+	 * @since    March 28, 2002
 	 */
 	private Date value;
 	private DateFormat dateFormat;
@@ -59,8 +60,8 @@ public class DateDataField extends DataField {
 	/**
 	 *  Constructor for the DateDataField object
 	 *
-	 *@param  _metadata  Description of Parameter
-	 *@since             April 23, 2002
+	 * @param  _metadata  Description of Parameter
+	 * @since             April 23, 2002
 	 */
 	public DateDataField(DataFieldMetadata _metadata) {
 		super(_metadata);
@@ -78,9 +79,9 @@ public class DateDataField extends DataField {
 	/**
 	 *  Constructor for the DateDataField object
 	 *
-	 *@param  _metadata  Description of Parameter
-	 *@param  _value     Description of Parameter
-	 *@since             April 23, 2002
+	 * @param  _metadata  Description of Parameter
+	 * @param  _value     Description of Parameter
+	 * @since             April 23, 2002
 	 */
 	public DateDataField(DataFieldMetadata _metadata, Date _value) {
 		super(_metadata);
@@ -92,7 +93,7 @@ public class DateDataField extends DataField {
 			dateFormat = null;
 		}
 		parsePosition = new ParsePosition(0);
-		
+
 		setValue(_value);
 	}
 
@@ -100,16 +101,17 @@ public class DateDataField extends DataField {
 	/**
 	 *  Sets the date represented by DateDataField object
 	 *
-	 *@param  _value  The new Value value
-	 *@since          April 23, 2002
+	 * @param  _value                      The new Value value
+	 * @exception  BadDataFormatException  Description of the Exception
+	 * @since                              April 23, 2002
 	 */
 	public void setValue(Object _value) throws BadDataFormatException {
-		if(_value==null) {
-			if(this.metadata.isNullable()) {
+		if (_value == null) {
+			if (this.metadata.isNullable()) {
 				value = null;
 				super.setNull(true);
 			} else {
-				throw new BadDataFormatException(getMetadata().getName()+ " field can not be set to null!(nullable=false)",null);
+				throw new BadDataFormatException(getMetadata().getName() + " field can not be set to null!(nullable=false)", null);
 			}
 			return;
 		}
@@ -119,32 +121,45 @@ public class DateDataField extends DataField {
 			}else{
 				value.setTime(((Date) _value).getTime());
 			}
+
 			super.setNull(false);
 		} else {
-			if(this.metadata.isNullable()) {
+			if (this.metadata.isNullable()) {
 				value = null;
 				super.setNull(true);
-			} else
-				throw new BadDataFormatException(getMetadata().getName()+" field can not be set with this object - " +_value.toString(),_value.toString());
+			} else {
+				throw new BadDataFormatException(getMetadata().getName() + " field can not be set with this object - " + _value.toString(), _value.toString());
+			}
 		}
 	}
 
-	
+
 	/**
 	 *  Gets the date represented by DateDataField object
 	 *
-	 *@return    The Value value
-	 *@since     April 23, 2002
+	 * @return    The Value value
+	 * @since     April 23, 2002
 	 */
 	public Object getValue() {
 		return value;
 	}
 
+
+	/**
+	 *  Gets the date attribute of the DateDataField object
+	 *
+	 * @return    The date value
+	 */
+	public Date getDate() {
+		return value;
+	}
+
+
 	/**
 	 *  Sets the Null value indicator
 	 *
-	 *@param  isNull  The new Null value
-	 *@since          October 29, 2002
+	 * @param  isNull  The new Null value
+	 * @since          October 29, 2002
 	 */
 	public void setNull(boolean isNull) {
 		super.setNull(isNull);
@@ -153,11 +168,12 @@ public class DateDataField extends DataField {
 		}
 	}
 
+
 	/**
 	 *  Gets the DataField type
 	 *
-	 *@return    The Type value
-	 *@since     April 23, 2002
+	 * @return    The Type value
+	 * @since     April 23, 2002
 	 */
 	public char getType() {
 		return DataFieldMetadata.DATE_FIELD;
@@ -169,11 +185,11 @@ public class DateDataField extends DataField {
 	 *  metadata describing DateField contains formating string, that string is
 	 *  used to create output. Otherwise the standard format is used.
 	 *
-	 *@return    Description of the Returned Value
-	 *@since     April 23, 2002
+	 * @return    Description of the Returned Value
+	 * @since     April 23, 2002
 	 */
 	public String toString() {
-		if(value == null) {
+		if (value == null) {
 			return "";
 		}
 		if ((dateFormat != null)) {
@@ -187,10 +203,10 @@ public class DateDataField extends DataField {
 	/**
 	 *  Description of the Method
 	 *
-	 *@param  dataBuffer                    Description of Parameter
-	 *@param  decoder                       Description of Parameter
-	 *@exception  CharacterCodingException  Description of Exception
-	 *@since                                October 31, 2002
+	 * @param  dataBuffer                    Description of Parameter
+	 * @param  decoder                       Description of Parameter
+	 * @exception  CharacterCodingException  Description of Exception
+	 * @since                                October 31, 2002
 	 */
 	public void fromByteBuffer(ByteBuffer dataBuffer, CharsetDecoder decoder) throws CharacterCodingException {
 		fromString(decoder.decode(dataBuffer).toString());
@@ -200,10 +216,10 @@ public class DateDataField extends DataField {
 	/**
 	 *  Description of the Method
 	 *
-	 *@param  dataBuffer                    Description of Parameter
-	 *@param  encoder                       Description of Parameter
-	 *@exception  CharacterCodingException  Description of Exception
-	 *@since                                October 31, 2002
+	 * @param  dataBuffer                    Description of Parameter
+	 * @param  encoder                       Description of Parameter
+	 * @exception  CharacterCodingException  Description of Exception
+	 * @since                                October 31, 2002
 	 */
 	public void toByteBuffer(ByteBuffer dataBuffer, CharsetEncoder encoder) throws CharacterCodingException {
 		dataBuffer.put(encoder.encode(CharBuffer.wrap(toString())));
@@ -214,31 +230,32 @@ public class DateDataField extends DataField {
 	 *  Parses date value from string representation. If format string is defined,
 	 *  then it is used as expected pattern.
 	 *
-	 *@param  _valueStr  Description of Parameter
-	 *@since             April 23, 2002
+	 * @param  _valueStr  Description of Parameter
+	 * @since             April 23, 2002
 	 */
 	public void fromString(String _valueStr) {
 		parsePosition.setIndex(0);
-		if(_valueStr == null || _valueStr.equals("")) {
-			if(this.metadata.isNullable()) {
+		if (_valueStr == null || _valueStr.equals("")) {
+			if (this.metadata.isNullable()) {
 				value = null;
 				super.setNull(true);
-			} else
-				throw new BadDataFormatException(getMetadata().getName()+" field can not be set to null!(nullable=false)",_valueStr);
+			} else {
+				throw new BadDataFormatException(getMetadata().getName() + " field can not be set to null!(nullable=false)", _valueStr);
+			}
 			return;
 		}
 		try {
 			if (dateFormat != null) {
-				value = dateFormat.parse(_valueStr); //, parsePosition);
+				value = dateFormat.parse(_valueStr);//, parsePosition);
 			} else {
 				value = SimpleDateFormat.getDateInstance().parse(_valueStr);
 			}
 			super.setNull(false);
-		 }catch (ParseException e) {
+		} catch (ParseException e) {
 			super.setNull(true);
-		  	throw new BadDataFormatException("not Date",_valueStr);
-		 }
-		 
+			throw new BadDataFormatException("not Date", _valueStr);
+		}
+
 	}
 
 
@@ -246,8 +263,8 @@ public class DateDataField extends DataField {
 	 *  Performs serialization of the internal value into ByteBuffer (used when
 	 *  moving data records between components).
 	 *
-	 *@param  buffer  Description of Parameter
-	 *@since          April 23, 2002
+	 * @param  buffer  Description of Parameter
+	 * @since          April 23, 2002
 	 */
 	public void serialize(ByteBuffer buffer) {
 		if (value != null) {
@@ -261,15 +278,15 @@ public class DateDataField extends DataField {
 	/**
 	 *  Performs deserialization of data
 	 *
-	 *@param  buffer  Description of Parameter
-	 *@since          April 23, 2002
+	 * @param  buffer  Description of Parameter
+	 * @since          April 23, 2002
 	 */
 	public void deserialize(ByteBuffer buffer) {
 		long tmpl = buffer.getLong();
-		if(tmpl == 0) {
+		if (tmpl == 0) {
 			setValue(null);
 			return;
-		} 
+		}
 		if (value == null) {
 			value = new Date(tmpl);
 		} else {
@@ -282,9 +299,9 @@ public class DateDataField extends DataField {
 	/**
 	 *  Description of the Method
 	 *
-	 *@param  obj  Description of Parameter
-	 *@return      Description of the Returned Value
-	 *@since       April 23, 2002
+	 * @param  obj  Description of Parameter
+	 * @return      Description of the Returned Value
+	 * @since       April 23, 2002
 	 */
 	public boolean equals(Object obj) {
 		return (this.value.equals((((DateDataField) obj).getValue())));
@@ -294,10 +311,22 @@ public class DateDataField extends DataField {
 	/**
 	 *  Compares this object with the specified object for order
 	 *
-	 *@param  obj  Description of the Parameter
-	 *@return      Description of the Return Value
+	 * @param  obj  Description of the Parameter
+	 * @return      Description of the Return Value
 	 */
 	public int compareTo(Object obj) {
+		Date date = (Date)((DateDataField) obj).getValue();
+		return value.compareTo(date);
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @param  obj  Description of the Parameter
+	 * @return      Description of the Return Value
+	 */
+	public int compareTo(Date obj) {
 		return value.compareTo(obj);
 	}
 }
