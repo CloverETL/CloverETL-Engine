3c8fb331a53c3992123926fc64471c73248e757f
dpavlis <info@cloveretl.com>
2003-12-12 13:08:01 +0000

    added support for additional comparison operators; expression is now compiled
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@180 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/data/RecordFilter.java
+++ cloveretl.engine/src/org/jetel/data/RecordFilter.java
@@ -13,212 +13,389 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package org.jetel.data;
+import java.lang.reflect.Array;
 
-import java.util.*;
-import java.io.*;
-
-import org.jetel.exception.ComponentNotReadyException;
-import org.jetel.exception.JetelException;
-import org.jetel.metadata.DataRecordMetadata;
-import org.jetel.database.SQLUtil;
-import java.util.*;
-import java.lang.reflect.*;
-import java.util.Hashtable;
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RecordFilter{
+import org.jetel.metadata.DataFieldMetadata;
+import org.jetel.metadata.DataRecordMetadata;
+
+/**
+ *  Description of the Class
+ *
+ * @author      dpavlis
+ * @since       9. prosinec 2003
+ * @revision    $Revision$
+ */
+public class RecordFilter {
+
+	private final static int CMP_EQ = 0;// equal
+	private final static int CMP_LT = -1;// less than
+	private final static int CMP_GT = 1;// grater than
+	private final static int CMP_LTEQ = -2;// less than equal
+	private final static int CMP_GTEQ = 2;// greater than equal
+	private final static int CMP_REGEX = 3;// regular expression compare
+	private final static int CMP_N_EQL = 5;// not equal
+
+	private final static int AND = 0;
+	private final static int OR = 1;
+	private final static int NOT = 2;
+
+	private final static String S_CMP_EQ = "==";
+	private final static String S_CMP_LT = "<";
+	private final static String S_CMP_GT = ">";
+	private final static String S_CMP_LTEQ = ">=";
+	private final static String S_CMP_GTEQ = "=<";
+	private final static String S_CMP_REGEX = "~";
+	private final static String S_CMP_N_EQL = "!=";
+
+	private final static String DEFAULT_FILTER_DATE_FORMAT = "yyyy-MM-dd";
+
 	private DataRecordMetadata metadata;
-	private Vector filterSpecs;
+	private FilterItem[] filterSpecs;
+	private String filterExpression;
 
 
-	public RecordFilter(String filterExpression)
-	{
-		filterSpecs = new Vector();
-		metadata = metadata;
-		String [] filterParts=filterExpression.split(Defaults.Component.KEY_FIELDS_DELIMITER_REGEX);
-		String filterPart;
+	/**
+	 *Constructor for the RecordFilter object
+	 *
+	 * @param  filterExpression  Description of the Parameter
+	 */
+	public RecordFilter(String filterExpression) {
+		this.filterExpression = filterExpression;
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @param  recordMetadata  Description of the Parameter
+	 */
+	public void init(DataRecordMetadata recordMetadata) {
 		String filterField;
-		String filterValue;
-		String comparisonOperator = new String("");
-		for (int i=0; i<Array.getLength(filterParts); i++)
-		{
-			filterPart = filterParts[i];
+		String filterValueStr;
+		this.metadata = metadata;
+		String[] filterParts = filterExpression.split(Defaults.Component.KEY_FIELDS_DELIMITER_REGEX);
+		filterSpecs = new FilterItem[filterParts.length];
+		Map fieldNames = recordMetadata.getFieldNames();
+		int operatorIndex;
+		int cmpOperator;
+		int operatorStrLen;
 
-            //Get comparison operator
-			if (filterPart.indexOf("=")!=-1){
-				comparisonOperator=new String("=");
+		for (int i = 0; i < Array.getLength(filterParts); i++) {
+			String filterPart = filterParts[i];
+
+			//Get comparison operator
+			if ((operatorIndex = filterPart.indexOf(S_CMP_EQ)) != -1) {
+				cmpOperator = CMP_EQ;
+				operatorStrLen = S_CMP_EQ.length();
+			} else if ((operatorIndex = filterPart.indexOf(S_CMP_REGEX)) != -1) {
+				cmpOperator = CMP_REGEX;
+				operatorStrLen = S_CMP_REGEX.length();
+			} else if ((operatorIndex = filterPart.indexOf(S_CMP_LTEQ)) != -1) {
+				cmpOperator = CMP_LTEQ;
+				operatorStrLen = S_CMP_LTEQ.length();
+			} else if ((operatorIndex = filterPart.indexOf(S_CMP_GTEQ)) != -1) {
+				cmpOperator = CMP_GTEQ;
+				operatorStrLen = S_CMP_GTEQ.length();
+			} else if ((operatorIndex = filterPart.indexOf(S_CMP_N_EQL)) != -1) {
+				cmpOperator = CMP_N_EQL;
+				operatorStrLen = S_CMP_N_EQL.length();
+			} // this must go here as there otherwise <= would be reported as less not less or equal
+			else if ((operatorIndex = filterPart.indexOf(S_CMP_GT)) != -1) {
+				cmpOperator = CMP_GT;
+				operatorStrLen = S_CMP_GT.length();
+			} else if ((operatorIndex = filterPart.indexOf(S_CMP_LT)) != -1) {
+				cmpOperator = CMP_LT;
+				operatorStrLen = S_CMP_LT.length();
+			} else {
+				throw new RuntimeException("Unknown comparison operator: " + filterPart);
 			}
-			else if (filterPart.indexOf("~")!=-1){
-				comparisonOperator=new String("~");
+
+			filterField = filterPart.substring(0, operatorIndex).trim();
+			filterValueStr = filterPart.substring(operatorIndex + operatorStrLen, filterPart.length()).trim();
+
+			DataFieldMetadata fieldMetadata = recordMetadata.getField(filterField);
+			if (fieldMetadata == null) {
+				throw new RuntimeException("Unknown field name: " + filterField);
 			}
-			else if (filterPart.indexOf(">")!=-1){
-				comparisonOperator=new String(">");
+
+			Integer fieldNum = (Integer) fieldNames.get(filterField);
+			if (fieldNum == null) {
+				throw new RuntimeException("Unknown field name: " + filterField);
 			}
-			else if (filterPart.indexOf("<")!=-1){
-				comparisonOperator=new String("<");
+
+			if (cmpOperator == CMP_REGEX) {
+				try {
+					filterSpecs[i] = new FilterItem(fieldNum.intValue(), new RegexField(filterValueStr), AND);
+				} catch (Exception ex) {
+					throw new RuntimeException(ex.getMessage() + " - error when populating filter's field " + filterField + " with value " + filterValueStr);
+				}
+			} else {
+				DataField filterFieldValue = DataFieldFactory.createDataField(fieldMetadata.getType(), fieldMetadata);
+				try {
+					filterFieldValue.fromString(filterValueStr);
+				} catch (Exception ex) {
+					throw new RuntimeException(ex.getMessage() + " - error when populating filter's field " + filterField + " with value " + filterValueStr);
+				}
+
+				filterSpecs[i] = new FilterItem(fieldNum.intValue(), cmpOperator, filterFieldValue, AND);
 			}
-				
-				
-			
-			filterField = filterPart.substring(0, filterPart.indexOf(comparisonOperator));
-			filterValue = filterPart.substring(filterPart.indexOf(comparisonOperator)+1,filterPart.length());
-			Hashtable filterSpec = new Hashtable();
-			filterSpec.put("field",filterField);
-			filterSpec.put("value",filterValue);
-			filterSpec.put("comparison",comparisonOperator);
-			filterSpecs.add(filterSpec);
+
 		}
-		
-
 
 	}
-	public Vector getFilterSpecs()
-	{
+
+
+	/**
+	 *  Gets the filterSpecs attribute of the RecordFilter object
+	 *
+	 * @return    The filterSpecs value
+	 */
+	public FilterItem[] getFilterSpecs() {
 		return filterSpecs;
 	}
 
-	public boolean accepts(DataRecord record)
-	{
-		Enumeration specsEnumeration = filterSpecs.elements();
-		//iterate through each filter spec. Once a spec is verified, the record is accepted (it's an OR combination of all specs)
-		while (specsEnumeration.hasMoreElements())
-		{
-			Hashtable filterSpec = (Hashtable) specsEnumeration.nextElement();
-		    String fieldType = SQLUtil.jetelType2Str(record.getField((String) filterSpec.get("field")).getType());
-			//string comparison
-			if (fieldType.compareTo("string")==0)
-			{
-				StringBuffer fieldValue =   (StringBuffer)  record.getField((String) filterSpec.get("field")).getValue();
 
-				//logically, if we filter on a node, it's because it has a non-null value
-				if (fieldValue==null){
-					return false;
-				}
-				
-				String comparison = (String) filterSpec.get("comparison");
-				String refValue = (String) filterSpec.get("value");
-				//equality
-				if ( comparison.compareTo("=")==0)
-				{
-					if (fieldValue.toString().compareTo(refValue)==0)
-					{
-						return true;
-					}
-				}
-				//regexp
-				else if ( comparison.compareTo("~")==0)
-					if (Pattern.matches(refValue, fieldValue ))
-					{
-						return true;
-					}
-				//greater than
-				else if ( comparison.compareTo(">")==0)
-					if (fieldValue.toString().compareTo(refValue)>0)
-					{
-						return true;
-					}
-				//lower than
-				else if ( comparison.compareTo("<")==0)
-					if (fieldValue.toString().compareTo(refValue)<0)
-					{
-						return true;
-					}
-			}
-			//integer fields
-			else if (fieldType.compareTo("integer")==0)
-			{
-				//getting filter values
-				Integer fieldValue = (Integer) record.getField((String) filterSpec.get("field")).getValue();
+	/**
+	 *  Description of the Method
+	 *
+	 * @param  record  Description of the Parameter
+	 * @return         Description of the Return Value
+	 */
+	public boolean accepts(DataRecord record) {
+		int fieldNo;
+		int cmpResult;
+		DataField field2Compare;
 
-				//logically, if we filter on a node, it's because it has a non-null value
-				if (fieldValue==null){
-					return false;
-				}
-				
-				String comparison = (String) filterSpec.get("comparison");
-				Integer refValue = new Integer(Integer.parseInt((String) filterSpec.get("value")));
+		for (int i = 0; i < filterSpecs.length; i++) {
+			field2Compare = record.getField(filterSpecs[i].getFieldNo());
 
-				//check equality
-				if ( comparison.compareTo("=")==0)
-				{
-					if (fieldValue.equals(refValue))
-					{
-						return true;
-					}
-				}
-				//check lower than
-				else if ( comparison.compareTo("<")==0)
-				{
-					if (fieldValue.compareTo(refValue)<0)
-					{
-						return true;
-					}
-				}
-				//check higher than
-				else if ( comparison.compareTo(">")==0)
-				{
-					if (fieldValue.compareTo(refValue)>0)
-					{
-						return true;
-					}
-				}
-			}
-			//date fields 
-			else if (fieldType.compareTo("date")==0)
-			{
-				try{
-					
-					//getting filter value
-					DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-					Date fieldValue = (Date) record.getField((String) filterSpec.get("field")).getValue();
-
-					//logically, if we filter on a node, it's because it has a non-null value
-					if (fieldValue==null){
-						return false;
-					}
-				
-					String comparison = (String) filterSpec.get("comparison");
-					Date refValue = df.parse((String) filterSpec.get("value"));
-
-					//check equality
-					if ( comparison.compareTo("=")==0)
-					{
-						if (fieldValue.equals(refValue))
-						{
-							return true;
-						}
-					}
-					//check lower than
-					else if ( comparison.compareTo("<")==0)
-					{
-						if (fieldValue.compareTo(refValue)<0)
-						{
-							return true;
-						}
-					}
-					//check higher than
-					else if ( comparison.compareTo(">")==0)
-					{
-						if (fieldValue.compareTo(refValue)>0)
-						{
-							return true;
-						}
-				}
-
-				}
-				catch (Exception e){
-				    e.printStackTrace();
-				}
-
+			if (field2Compare == null) {
+				// this should not happen !!
+				throw new RuntimeException("Field (reference) to compare with is NULL !!");
 			}
 
-  	}
-	
+			// special case for REGEX
+			if (filterSpecs[i].getComparison() == CMP_REGEX) {
+				cmpResult = ((RegexField) filterSpecs[i].getValue()).compareTo(field2Compare);
+			} else {
+				cmpResult = ((DataField) filterSpecs[i].getValue()).compareTo(field2Compare);
+			}
+
+			switch (filterSpecs[i].getComparison()) {
+
+				case CMP_EQ:
+					if (cmpResult == 0) {
+						return true;
+					}
+					break;// equal
+				case CMP_LT:
+					if (cmpResult == -1) {
+						return true;
+					}
+					break;// less than
+				case CMP_GT:
+					if (cmpResult == 1) {
+						return true;
+					}
+					break;// grater than
+				case CMP_LTEQ:
+					if (cmpResult <= 0) {
+						return true;
+					}
+					break;// less than equal
+				case CMP_GTEQ:
+					if (cmpResult >= 0) {
+						return true;
+					}
+					break;// greater than equal
+				case CMP_N_EQL:
+					if (cmpResult != 0) {
+						return true;
+					}
+					break;
+				case CMP_REGEX:
+					if (cmpResult == 0) {
+						return true;
+					}
+					break;
+				default:
+					throw new RuntimeException("Unsupported cmparison operator !");
+			}
+
+		}
+
 		return false;
+	}
 
+
+	/**
+	 *  Description of the Class
+	 *
+	 * @author      dpavlis
+	 * @since
+	 * @revision    $Revision$
+	 */
+	private class FilterItem {
+
+		int comparison;
+		Object value;
+		int logicalOperator;
+		char fieldType;
+		int fieldNo;
+
+
+		/**
+		 *Constructor for the FilterItem object
+		 *
+		 * @param  fieldNo    Description of the Parameter
+		 * @param  cmp        Description of the Parameter
+		 * @param  value      Description of the Parameter
+		 * @param  lOperator  Description of the Parameter
+		 */
+		FilterItem(int fieldNo, int cmp, DataField value, int lOperator) {
+			this.fieldNo = fieldNo;
+			this.comparison = cmp;
+			this.value = value;
+			this.fieldType = value.getType();
+			this.logicalOperator = lOperator;
+		}
+
+
+		/**
+		 *Constructor for the FilterItem object
+		 *
+		 * @param  fieldNo    Description of the Parameter
+		 * @param  value      Description of the Parameter
+		 * @param  lOperator  Description of the Parameter
+		 */
+		FilterItem(int fieldNo, RegexField value, int lOperator) {
+			this.fieldNo = fieldNo;
+			this.comparison = CMP_REGEX;
+			this.value = value;
+			this.fieldType = 'X';// X for regeX
+			this.logicalOperator = lOperator;
+		}
+
+
+		/**
+		 *  Gets the comparison attribute of the FilterItem object
+		 *
+		 * @return    The comparison value
+		 */
+		final int getComparison() {
+			return comparison;
+		}
+
+
+		/**
+		 *  Gets the value attribute of the FilterItem object
+		 *
+		 * @return    The value value
+		 */
+		final Object getValue() {
+			return value;
+		}
+
+
+		/**
+		 *  Gets the logicalOperator attribute of the FilterItem object
+		 *
+		 * @return    The logicalOperator value
+		 */
+		final int getLogicalOperator() {
+			return logicalOperator;
+		}
+
+
+		/**
+		 *  Gets the fieldNo attribute of the FilterItem object
+		 *
+		 * @return    The fieldNo value
+		 */
+		final int getFieldNo() {
+			return fieldNo;
+		}
+
+
+		/**
+		 *  Description of the Method
+		 *
+		 * @return    Description of the Return Value
+		 */
+		public String toString() {
+			return fieldNo + ":" + comparison + ":" + value;
+		}
+	}
+
+
+	/**
+	 *  Description of the Class
+	 *
+	 * @author      dpavlis
+	 * @since
+	 * @revision    $Revision$
+	 */
+	private class RegexField {
+
+		private Pattern p;
+
+
+		/**
+		 *Constructor for the RegexField object
+		 *
+		 * @param  patternStr  Description of the Parameter
+		 */
+		RegexField(String patternStr) {
+			p = Pattern.compile(patternStr);
+		}
+
+
+		/**
+		 *  Description of the Method
+		 *
+		 * @return    Description of the Return Value
+		 */
+		public String toString() {
+			return p.toString();
+		}
+
+
+		/**
+		 *  Description of the Method
+		 *
+		 * @param  _str  Description of the Parameter
+		 */
+		public void fromString(String _str) {
+			p = Pattern.compile(_str);
+		}
+
+
+		/**
+		 *  Description of the Method
+		 *
+		 * @param  obj  Description of the Parameter
+		 * @return      Description of the Return Value
+		 */
+		public boolean equals(Object obj) {
+			Matcher m = p.matcher(((StringDataField) obj).getCharSequence());
+			return m.matches();
+		}
+
+
+		/**
+		 *  Description of the Method
+		 *
+		 * @param  obj  Description of the Parameter
+		 * @return      Description of the Return Value
+		 */
+		public int compareTo(Object obj) {
+			return equals(obj) ? 0 : -1;
+		}
 	}
 
 }
+
