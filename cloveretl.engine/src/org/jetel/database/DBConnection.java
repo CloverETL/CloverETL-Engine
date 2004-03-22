6c2c0b6679dc45b6f470cd696353b0c664e6d31d
dpavlis <info@cloveretl.com>
2004-03-22 09:20:15 +0000

    changed to support additional parameters to be specified as attributes of XML node
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@226 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/database/DBConnection.java
+++ cloveretl.engine/src/org/jetel/database/DBConnection.java
@@ -20,6 +20,7 @@ import java.io.*;
 import java.sql.*;
 import java.util.Properties;
 import org.jetel.util.ComponentXMLAttributes;
+import org.w3c.dom.NamedNodeMap;
 
 /**
  *  CloverETL class for connecting to databases.<br>
@@ -35,56 +36,56 @@ import org.jetel.util.ComponentXMLAttributes;
  *  <tr><td><b>dbURL</b></td><td>URL of the database (aka connection string)</td></tr>
  *  <tr><td><b>user</b><br><i>optional</i></td><td>username to use when connecting to DB</td></tr>
  *  <tr><td><b>password</b><br><i>optional</i></td><td>password to use when connecting to DB</td></tr>
- *  </table>  
+ *  </table>
  *
- *  <h4>Example:</h4>  
+ *  <h4>Example:</h4>
  *  <pre>&lt;DBConnection id="InterbaseDB" dbConfig="interbase.cfg"/&gt;</pre>
  *
- *@author     dpavlis
- *@created    January 15, 2003
+ * @author      dpavlis
+ * @since       21. bøezen 2004
+ * @revision    $Revision$
+ * @created     January 15, 2003
  */
 public class DBConnection {
 
 	String dbDriverName;
 	String dbURL;
-	String user;
-	String password;
 	Driver dbDriver;
 	Connection dbConnection;
+	Properties config;
 
 
 	/**
 	 *  Constructor for the DBConnection object
 	 *
-	 *@param  dbDriver  Description of the Parameter
-	 *@param  dbURL     Description of the Parameter
-	 *@param  user      Description of the Parameter
-	 *@param  password  Description of the Parameter
+	 * @param  dbDriver  Description of the Parameter
+	 * @param  dbURL     Description of the Parameter
+	 * @param  user      Description of the Parameter
+	 * @param  password  Description of the Parameter
 	 */
 	public DBConnection(String dbDriver, String dbURL, String user, String password) {
+		this.config = new Properties();
+		config.setProperty("user", user);
+		config.setProperty("password", password);
 		this.dbDriverName = dbDriver;
 		this.dbURL = dbURL;
-		this.user = user;
-		this.password = password;
 	}
 
 
 	/**
 	 *  Constructor for the DBConnection object
 	 *
-	 *@param  configFilename  properties filename containing definition of driver, dbURL, username, password
+	 * @param  configFilename  properties filename containing definition of driver, dbURL, username, password
 	 */
 	public DBConnection(String configFilename) {
-		Properties config = new Properties();
+		this.config = new Properties();
 
 		try {
 			InputStream stream = new BufferedInputStream(new FileInputStream(configFilename));
-			config.load(stream);
+			this.config.load(stream);
 			stream.close();
 			this.dbDriverName = config.getProperty("dbDriver");
 			this.dbURL = config.getProperty("dbURL");
-			this.user = config.getProperty("user");
-			this.password = config.getProperty("password");
 
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
@@ -92,21 +93,11 @@ public class DBConnection {
 	}
 
 
-	/**
-	 *  Description of the Method
-	 */
+	/**  Description of the Method */
 	public void connect() {
-		Properties property = new Properties();
-		if (user != null) {
-			property.setProperty("user", user);
-		}
-		if (password != null) {
-			property.setProperty("password", password);
-		}
-
 		try {
 			dbDriver = (Driver) Class.forName(dbDriverName).newInstance();
-			dbConnection = dbDriver.connect(dbURL, property);
+			dbConnection = dbDriver.connect(dbURL, this.config);
 		} catch (SQLException ex) {
 			throw new RuntimeException("Can't connect to DB :" + ex.getMessage());
 		} catch (Exception ex) {
@@ -121,7 +112,7 @@ public class DBConnection {
 	/**
 	 *  Description of the Method
 	 *
-	 *@exception  SQLException  Description of the Exception
+	 * @exception  SQLException  Description of the Exception
 	 */
 	public void close() throws SQLException {
 		dbConnection.close();
@@ -131,7 +122,7 @@ public class DBConnection {
 	/**
 	 *  Gets the connection attribute of the DBConnection object
 	 *
-	 *@return    The connection value
+	 * @return    The connection value
 	 */
 	public Connection getConnection() {
 		return dbConnection;
@@ -141,8 +132,8 @@ public class DBConnection {
 	/**
 	 *  Gets the statement attribute of the DBConnection object
 	 *
-	 *@return                   The statement value
-	 *@exception  SQLException  Description of the Exception
+	 * @return                   The statement value
+	 * @exception  SQLException  Description of the Exception
 	 */
 	public Statement getStatement() throws SQLException {
 		return dbConnection.createStatement();
@@ -152,9 +143,9 @@ public class DBConnection {
 	/**
 	 *  Description of the Method
 	 *
-	 *@param  sql               Description of the Parameter
-	 *@return                   Description of the Return Value
-	 *@exception  SQLException  Description of the Exception
+	 * @param  sql               Description of the Parameter
+	 * @return                   Description of the Return Value
+	 * @exception  SQLException  Description of the Exception
 	 */
 	public PreparedStatement prepareStatement(String sql) throws SQLException {
 		return dbConnection.prepareStatement(sql);
@@ -162,43 +153,74 @@ public class DBConnection {
 
 
 	/**
+	 *  Sets the property attribute of the DBConnection object
+	 *
+	 * @param  name   The new property value
+	 * @param  value  The new property value
+	 */
+	public void setProperty(String name, String value) {
+		config.setProperty(name, value);
+	}
+
+
+	/**
+	 *  Gets the property attribute of the DBConnection object
+	 *
+	 * @param  name  Description of the Parameter
+	 * @return       The property value
+	 */
+	public String getProperty(String name) {
+		return config.getProperty(name);
+	}
+
+
+	/**
 	 *  Description of the Method
 	 *
-	 *@param  nodeXML  Description of the Parameter
-	 *@return          Description of the Return Value
+	 * @param  nodeXML  Description of the Parameter
+	 * @return          Description of the Return Value
 	 */
 	public static DBConnection fromXML(org.w3c.dom.Node nodeXML) {
-		ComponentXMLAttributes xattribs=new ComponentXMLAttributes(nodeXML);
+		ComponentXMLAttributes xattribs = new ComponentXMLAttributes(nodeXML);
+		NamedNodeMap attributes = nodeXML.getAttributes();
+		DBConnection con;
 
-		try{
+		try {
 			// do we have dbConfig parameter specified ??
-			if (xattribs.exists("dbConfig")){
+			if (xattribs.exists("dbConfig")) {
 				return new DBConnection(xattribs.getString("dbConfig"));
-			}else{
-			
-			String dbDriver = xattribs.getString("dbDriver");
-			String dbURL = xattribs.getString("dbURL");
-			String user=null;
-			String password=null;
-				
-			if (xattribs.exists("user")){
-				user = xattribs.getString("user");
+			} else {
+
+				String dbDriver = xattribs.getString("dbDriver");
+				String dbURL = xattribs.getString("dbURL");
+				String user = null;
+				String password = null;
+
+				con = new DBConnection(dbDriver, dbURL, user, password);
+
+				// assign rest of attributes/parameters to connection properties so
+				// it can be retrieved by DB JDBC driver
+				for (int i = 0; i < attributes.getLength(); i++) {
+					con.setProperty(attributes.item(i).getNodeName(), attributes.item(i).getNodeValue());
+				}
+
+				return con;
 			}
-			if (xattribs.exists("password")){
-				user = xattribs.getString("password");
-			}
-			
-			return new DBConnection(dbDriver, dbURL, user, password);
-			}
-				
-		}catch(Exception ex){
+
+		} catch (Exception ex) {
 			System.err.println(ex.getMessage());
 			return null;
 		}
-		
+
 	}
-	
-	public String toString(){
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @return    Description of the Return Value
+	 */
+	public String toString() {
 		return dbURL;
 	}
 }
