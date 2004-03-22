78c5c5e8f5e2b5a4d5df03052a11a90dc0346311
dpavlis <info@cloveretl.com>
2004-03-22 09:17:48 +0000

    added calculation of checksums on files
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@224 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/FileUtils.java
+++ cloveretl.engine/src/org/jetel/util/FileUtils.java
@@ -1,31 +1,34 @@
 /*
-*    jETeL/Clover - Java based ETL application framework.
-*    Copyright (C) 2002  David Pavlis
-*
-*    This program is free software; you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation; either version 2 of the License, or
-*    (at your option) any later version.
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program; if not, write to the Free Software
-*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-
+ *  jETeL/Clover - Java based ETL application framework.
+ *  Copyright (C) 2002  David Pavlis
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package org.jetel.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.Adler32;
+import java.util.zip.Checksum;
 /**
  *  Helper class with some useful methods regarding file manipulation
  *
- * @author     dpavlis
- * @since    May 24, 2002
+ * @author      dpavlis
+ * @since       May 24, 2002
+ * @revision    $Revision$
  */
 public class FileUtils {
-
 
 	/**
 	 *  Translates fileURL into full path with all references to ENV variables resolved
@@ -39,9 +42,35 @@ public class FileUtils {
 	}
 
 
+	/**
+	 *  Calculates checksum of specified file.<br>Is suitable for short files (not very much buffered).
+	 *
+	 * @param  filename  Filename (full path) of file to calculate checksum for
+	 * @return           Calculated checksum or -1 if some error (IO) occured
+	 */
+	public static long calculateFileCheckSum(String filename) {
+		byte[] buffer = new byte[1024];
+		Checksum checksum = new Adler32(); // we use Adler - should be faster
+		int length = 0;
+		try {
+			FileInputStream dataFile = new FileInputStream(filename);
+			while (length != -1) {
+				length = dataFile.read(buffer);
+				if (length > 0) {
+					checksum.update(buffer, 0, length);
+				}
+			}
+			dataFile.close();
+
+		} catch (IOException ex) {
+			return -1;
+		}
+		return checksum.getValue();
+	}
+
 }
 
 /*
-* End class FileUtils
-*/
+ *  End class FileUtils
+ */
 
