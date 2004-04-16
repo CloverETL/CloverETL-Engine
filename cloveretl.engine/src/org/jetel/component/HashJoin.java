3da57aab54f0106c461c962f03b49e3a53bd1276
dpavlis <info@cloveretl.com>
2004-04-16 07:30:00 +0000

    added possibility to dynamically compile/execute reformat class/function
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@242 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/component/HashJoin.java
+++ cloveretl.engine/src/org/jetel/component/HashJoin.java
@@ -18,18 +18,19 @@
 package org.jetel.component;
 
 import java.io.*;
-import org.jetel.graph.*;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import org.jetel.data.DataRecord;
-import org.jetel.data.RecordKey;
-import org.jetel.data.HashKey;
 import org.jetel.data.Defaults;
+import org.jetel.data.HashKey;
+import org.jetel.data.RecordKey;
+import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.graph.*;
 import org.jetel.metadata.DataRecordMetadata;
 import org.jetel.util.ComponentXMLAttributes;
-import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.util.DynamicJavaCode;
 
 /**
  *  <h3>HashJoin Component</h3> <!-- Joins two records from two different
@@ -38,7 +39,7 @@ import org.jetel.exception.ComponentNotReadyException;
  * hash table. Then for every record from driver flow, corresponding record from
  * slave flow is looked up (if it exists) -->
  *
-  <table border="1">
+ *<table border="1">
  *
  *    <th>
  *      Component:
@@ -94,7 +95,6 @@ import org.jetel.exception.ComponentNotReadyException;
  *    </table>
  *    <h4>Example:</h4> <pre>&lt;Node id="JOIN" type="HASH_JOIN" joinKey="CustomerID" transformClass="org.jetel.test.reformatOrders"/&gt;</pre>
  *
- *
  * @author      dpavlis
  * @since       March 09, 2004
  * @revision    $Revision$
@@ -105,17 +105,16 @@ public class HashJoin extends Node {
 	/**  Description of the Field */
 	public final static String COMPONENT_TYPE = "HASH_JOIN";
 
-
 	private final static int DEFAULT_HASH_TABLE_INITIAL_CAPACITY = 512;
-	
+
 	private final static int WRITE_TO_PORT = 0;
 	private final static int DRIVER_ON_PORT = 0;
 	private final static int SLAVE_ON_PORT = 1;
 
-		
 	private String transformClassName;
 
 	private RecordTransform transformation = null;
+	private DynamicJavaCode dynamicTransformation = null;
 
 	private DataRecord[] joinedRecords;
 
@@ -128,46 +127,102 @@ public class HashJoin extends Node {
 	private RecordKey slaveKey;
 
 	private Map hashMap;
-	
+
 	// for passing data records into transform function
 	private final static DataRecord[] inRecords = new DataRecord[2];
 
 	private int hashTableInitialCapacity;
-	
+
+
+	/**
+	 *Constructor for the HashJoin object
+	 *
+	 * @param  id              Description of the Parameter
+	 * @param  joinKeys        Description of the Parameter
+	 * @param  transformClass  Description of the Parameter
+	 */
 	public HashJoin(String id, String[] joinKeys, String transformClass) {
 		super(id);
 		this.joinKeys = joinKeys;
 		this.transformClassName = transformClass;
 		this.leftOuterJoin = false;
-		this.hashTableInitialCapacity=DEFAULT_HASH_TABLE_INITIAL_CAPACITY;
+		this.hashTableInitialCapacity = DEFAULT_HASH_TABLE_INITIAL_CAPACITY;
 		// no outer join
 	}
 
-	
-	
+
+
+	/**
+	 *Constructor for the HashJoin object
+	 *
+	 * @param  id              Description of the Parameter
+	 * @param  joinKeys        Description of the Parameter
+	 * @param  transformClass  Description of the Parameter
+	 * @param  leftOuterJoin   Description of the Parameter
+	 */
 	public HashJoin(String id, String[] joinKeys, RecordTransform transformClass, boolean leftOuterJoin) {
 		super(id);
 		this.joinKeys = joinKeys;
 		this.transformation = transformClass;
 		this.leftOuterJoin = leftOuterJoin;
-		this.hashTableInitialCapacity=DEFAULT_HASH_TABLE_INITIAL_CAPACITY;
+		this.hashTableInitialCapacity = DEFAULT_HASH_TABLE_INITIAL_CAPACITY;
 	}
 
+
+	/**
+	 *Constructor for the HashJoin object
+	 *
+	 * @param  id         Description of the Parameter
+	 * @param  joinKeys   Description of the Parameter
+	 * @param  dynaTrans  Description of the Parameter
+	 */
+	public HashJoin(String id, String[] joinKeys, DynamicJavaCode dynaTrans) {
+		super(id);
+		this.joinKeys = joinKeys;
+		this.dynamicTransformation = dynaTrans;
+		this.leftOuterJoin = false;
+		this.hashTableInitialCapacity = DEFAULT_HASH_TABLE_INITIAL_CAPACITY;
+		// no outer join
+	}
+
+
+	/**
+	 *  Sets the leftOuterJoin attribute of the HashJoin object
+	 *
+	 * @param  outerJoin  The new leftOuterJoin value
+	 */
 	public void setLeftOuterJoin(boolean outerJoin) {
 		leftOuterJoin = outerJoin;
 	}
-	
+
+
+	/**
+	 *  Sets the slaveOverrideKey attribute of the HashJoin object
+	 *
+	 * @param  slaveKeys  The new slaveOverrideKey value
+	 */
 	public void setSlaveOverrideKey(String[] slaveKeys) {
 		this.slaveOverrideKeys = slaveKeys;
 	}
-	
-	public void setHashTableInitialCapacity(int capacity){
-		if (capacity > DEFAULT_HASH_TABLE_INITIAL_CAPACITY){
-			hashTableInitialCapacity=capacity;
+
+
+	/**
+	 *  Sets the hashTableInitialCapacity attribute of the HashJoin object
+	 *
+	 * @param  capacity  The new hashTableInitialCapacity value
+	 */
+	public void setHashTableInitialCapacity(int capacity) {
+		if (capacity > DEFAULT_HASH_TABLE_INITIAL_CAPACITY) {
+			hashTableInitialCapacity = capacity;
 		}
 	}
-	
 
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @exception  ComponentNotReadyException  Description of the Exception
+	 */
 	public void init() throws ComponentNotReadyException {
 		Class tClass;
 		// test that we have at least one input port and one output
@@ -185,20 +240,31 @@ public class HashJoin extends Node {
 		(slaveKey = new RecordKey(slaveOverrideKeys, getInputPort(SLAVE_ON_PORT).getMetadata())).init();
 
 		if (transformation == null) {
-			// try to load in transformation class & instantiate
-			try {
-				tClass = Class.forName(transformClassName);
-			} catch (ClassNotFoundException ex) {
-				throw new ComponentNotReadyException("Can't find specified transformation class: " + transformClassName);
-			} catch (Exception ex) {
-				throw new ComponentNotReadyException(ex.getMessage());
-			}
-			try {
-				transformation = (RecordTransform) tClass.newInstance();
-			} catch (Exception ex) {
-				throw new ComponentNotReadyException(ex.getMessage());
-			}
+			if (transformClassName != null) {
+				// try to load in transformation class & instantiate
+				try {
+					tClass = Class.forName(transformClassName);
+				} catch (ClassNotFoundException ex) {
+					throw new ComponentNotReadyException("Can't find specified transformation class: " + transformClassName);
+				} catch (Exception ex) {
+					throw new ComponentNotReadyException(ex.getMessage());
+				}
+				try {
+					transformation = (RecordTransform) tClass.newInstance();
+				} catch (Exception ex) {
+					throw new ComponentNotReadyException(ex.getMessage());
+				}
+			} else {
+				System.out.print(" (compiling dynamic source) ");
+				// use DynamicJavaCode to instantiate transformation class
+				Object transObject = dynamicTransformation.instantiate();
+				if (transObject instanceof RecordTransform) {
+					transformation = (RecordTransform) transObject;
+				} else {
+					throw new ComponentNotReadyException("Provided transformation class doesn't implement RecordTransform.");
+				}
 
+			}
 		}
 		// init transformation
 		Collection col = getInPorts();
@@ -224,39 +290,37 @@ public class HashJoin extends Node {
 		InputPort inDriverPort = getInputPort(DRIVER_ON_PORT);
 		InputPort inSlavePort = getInputPort(SLAVE_ON_PORT);
 		OutputPort outPort = getOutputPort(WRITE_TO_PORT);
-		DataRecordMetadata slaveRecordMetadata=inSlavePort.getMetadata();
+		DataRecordMetadata slaveRecordMetadata = inSlavePort.getMetadata();
 		DataRecord driverRecord;
 		DataRecord slaveRecord;
 		DataRecord outRecord;
-		
-		
-		hashMap=new HashMap(hashTableInitialCapacity);
-		if (hashMap==null){
-			resultMsg = "Can't allocate HashMap of size: "+hashTableInitialCapacity;
+
+		hashMap = new HashMap(hashTableInitialCapacity);
+		if (hashMap == null) {
+			resultMsg = "Can't allocate HashMap of size: " + hashTableInitialCapacity;
 			resultCode = Node.RESULT_FATAL_ERROR;
 			return;
 		}
-		
-		
+
 		// first read all records from SLAVE port
-		while (runIt){
-			try{
-				slaveRecord=new DataRecord(slaveRecordMetadata);
+		while (runIt) {
+			try {
+				slaveRecord = new DataRecord(slaveRecordMetadata);
 				slaveRecord.init();
-				slaveRecord=inSlavePort.readRecord(slaveRecord);
-				if (slaveRecord!=null){
+				slaveRecord = inSlavePort.readRecord(slaveRecord);
+				if (slaveRecord != null) {
 					hashMap.put(new HashKey(slaveKey, slaveRecord),
-						slaveRecord);
-				}else{
+							slaveRecord);
+				} else {
 					// we have finished reading SLAVE records
 					break;
 				}
-				
+
 			} catch (IOException ex) {
-					resultMsg = ex.getMessage();
-					resultCode = Node.RESULT_ERROR;
-					closeAllOutputPorts();
-					return;
+				resultMsg = ex.getMessage();
+				resultCode = Node.RESULT_ERROR;
+				closeAllOutputPorts();
+				return;
 			} catch (Exception ex) {
 				resultMsg = ex.getMessage();
 				resultCode = Node.RESULT_FATAL_ERROR;
@@ -268,27 +332,27 @@ public class HashJoin extends Node {
 		//	System.out.println("> "+i.next());
 		//}
 		// DEBUG END
-		
+
 		// now read all records from DRIVER port and try to look up corresponding
 		// record from SLAVE records set.
-		driverRecord=new DataRecord(inDriverPort.getMetadata());
+		driverRecord = new DataRecord(inDriverPort.getMetadata());
 		driverRecord.init();
-		outRecord=new DataRecord(outPort.getMetadata());
+		outRecord = new DataRecord(outPort.getMetadata());
 		outRecord.init();
-		HashKey driverHashKey=new HashKey(driverKey,driverRecord);
-		inRecords[0]=driverRecord;
+		HashKey driverHashKey = new HashKey(driverKey, driverRecord);
+		inRecords[0] = driverRecord;
 		//inRecords[1] - assigned individually for every slave record
-		
-		while (runIt && driverRecord !=null) {
+
+		while (runIt && driverRecord != null) {
 			try {
-				driverRecord=inDriverPort.readRecord(driverRecord);
-				if (driverRecord!=null){
+				driverRecord = inDriverPort.readRecord(driverRecord);
+				if (driverRecord != null) {
 					// let's find slave record
-					slaveRecord=(DataRecord)hashMap.get(driverHashKey);
+					slaveRecord = (DataRecord) hashMap.get(driverHashKey);
 					// do we have it or is OuterJoin enabled ?
-					if ((slaveRecord !=null)||(leftOuterJoin)){
+					if ((slaveRecord != null) || (leftOuterJoin)) {
 						// call transformation function
-						inRecords[1]=slaveRecord;
+						inRecords[1] = slaveRecord;
 						if (!transformation.transform(inRecords, outRecord)) {
 							resultCode = Node.RESULT_ERROR;
 							resultMsg = transformation.getMessage();
@@ -297,7 +361,6 @@ public class HashJoin extends Node {
 						outPort.writeRecord(outRecord);
 					}
 				}
-				
 			} catch (IOException ex) {
 				resultMsg = ex.getMessage();
 				resultCode = Node.RESULT_ERROR;
@@ -341,11 +404,25 @@ public class HashJoin extends Node {
 	public static Node fromXML(org.w3c.dom.Node nodeXML) {
 		ComponentXMLAttributes xattribs = new ComponentXMLAttributes(nodeXML);
 		HashJoin join;
+		DynamicJavaCode dynaTransCode;
 
 		try {
-			join = new HashJoin(xattribs.getString("id"),
-					xattribs.getString("joinKey").split(Defaults.Component.KEY_FIELDS_DELIMITER_REGEX),
-					xattribs.getString("transformClass"));
+			if (xattribs.exists("transformClass")) {
+				join = new HashJoin(xattribs.getString("id"),
+						xattribs.getString("joinKey").split(Defaults.Component.KEY_FIELDS_DELIMITER_REGEX),
+						xattribs.getString("transformClass"));
+			} else {
+				// do we have child node wich Java source code ?
+				dynaTransCode = DynamicJavaCode.fromXML(nodeXML);
+				if (dynaTransCode == null) {
+					throw new RuntimeException("Can't create DynamicJavaCode object - source code not found !");
+				}
+				join = new HashJoin(xattribs.getString("id"),
+						xattribs.getString("joinKey").split(Defaults.Component.KEY_FIELDS_DELIMITER_REGEX),
+						dynaTransCode);
+
+			}
+
 			if (xattribs.exists("slaveOverrideKey")) {
 				join.setSlaveOverrideKey(xattribs.getString("slaveOverrideKey").
 						split(Defaults.Component.KEY_FIELDS_DELIMITER_REGEX));
@@ -354,7 +431,7 @@ public class HashJoin extends Node {
 			if (xattribs.exists("leftOuterJoin")) {
 				join.setLeftOuterJoin(xattribs.getBoolean("leftOuterJoin"));
 			}
-			if (xattribs.exists("hashTableSize")){
+			if (xattribs.exists("hashTableSize")) {
 				join.setHashTableInitialCapacity(xattribs.getInteger("hashTableSize"));
 			}
 			return join;
