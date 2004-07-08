7135118ca37570d7848e442edb04c16af2feb8e7
dpavlis <info@cloveretl.com>
2004-07-08 12:48:01 +0000

    added method/function for checking validity of graph's object name - names for Edges/Components/Metadata are checked
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@273 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/StringUtils.java
+++ cloveretl.engine/src/org/jetel/util/StringUtils.java
@@ -1,29 +1,30 @@
 /*
-*    jETeL/Clover - Java based ETL application framework.
-*    Copyright (C) 2002-04  David Pavlis <david_pavlis@hotmail.com>
-*    
-*    This library is free software; you can redistribute it and/or
-*    modify it under the terms of the GNU Lesser General Public
-*    License as published by the Free Software Foundation; either
-*    version 2.1 of the License, or (at your option) any later version.
-*    
-*    This library is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
-*    Lesser General Public License for more details.
-*    
-*    You should have received a copy of the GNU Lesser General Public
-*    License along with this library; if not, write to the Free Software
-*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*
-*/
+ *  jETeL/Clover - Java based ETL application framework.
+ *  Copyright (C) 2002-04  David Pavlis <david_pavlis@hotmail.com>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 package org.jetel.util;
 
 /**
  *  Helper class with some useful methods regarding string/text manipulation
  *
- *@author     dpavlis
- *@since      July 25, 2002
+ * @author      dpavlis
+ * @since       July 25, 2002
+ * @revision    $Revision$
  */
 
 public class StringUtils {
@@ -32,10 +33,10 @@ public class StringUtils {
 	 *  Converts control characters into textual representation<br>
 	 *  Note: This code handles only \n, \r and \t special chars
 	 *
-	 *@param  controlString  string containing control characters
-	 *@return                string where control characters are replaced by their
+	 * @param  controlString  string containing control characters
+	 * @return                string where control characters are replaced by their
 	 *      text representation (i.e.\n -> "\n" )
-	 *@since                 July 25, 2002
+	 * @since                 July 25, 2002
 	 */
 	public static String specCharToString(String controlString) {
 		StringBuffer copy = new StringBuffer();
@@ -65,35 +66,42 @@ public class StringUtils {
 	 *  characters<br>
 	 *  Note: This code handles only \n, \r and \t special chars
 	 *
-	 *@param  controlString  Description of the Parameter
-	 *@return                String with control characters
-	 *@since                 July 25, 2002
+	 * @param  controlString  Description of the Parameter
+	 * @return                String with control characters
+	 * @since                 July 25, 2002
 	 */
 	public static String stringToSpecChar(String controlString) {
 		StringBuffer copy = new StringBuffer();
 		char character;
-		boolean isBackslash=false;
+		boolean isBackslash = false;
 		for (int i = 0; i < controlString.length(); i++) {
 			character = controlString.charAt(i);
-			if (isBackslash){
-				switch(character){
-					case '\\': copy.append('\\');
-					break;
-					case 'n' : copy.append('\n');
-					break;
-					case 't' : copy.append('\t');
-					break;
-					case 'r' : copy.append('\r');
-					break;
-					default: copy.append('\\'); copy.append(character);
-					break;
+			if (isBackslash) {
+				switch (character) {
+					case '\\':
+						copy.append('\\');
+						break;
+					case 'n':
+						copy.append('\n');
+						break;
+					case 't':
+						copy.append('\t');
+						break;
+					case 'r':
+						copy.append('\r');
+						break;
+					default:
+						copy.append('\\');
+						copy.append(character);
+						break;
 				}
-			     isBackslash=false;
-			}else{
-				if (character=='\\')
-					isBackslash=true;
-				else
+				isBackslash = false;
+			} else {
+				if (character == '\\') {
+					isBackslash = true;
+				} else {
 					copy.append(character);
+				}
 			}
 		}
 		return copy.toString();
@@ -105,9 +113,9 @@ public class StringUtils {
 	 *  Negative (&lt;0) length means justify to the left, positive (&gt;0) to the right<br>
 	 *  If message is longer than specified size, it is trimmed; if shorter, it is padded with blanks.
 	 *
-	 *@param  messages  array of objects with toString() implemented methods
-	 *@param  sizes     array of desired lengths (+-) for every message specified
-	 *@return           Formatted string
+	 * @param  messages  array of objects with toString() implemented methods
+	 * @param  sizes     array of desired lengths (+-) for every message specified
+	 * @return           Formatted string
 	 */
 	public static String formatString(Object[] messages, int[] sizes) {
 		int formatSize;
@@ -118,37 +126,69 @@ public class StringUtils {
 		for (int i = 0; i < messages.length; i++) {
 			message = messages[i].toString();
 			// left or right justified ?
-			if (sizes[i]<0){
-				formatSize=sizes[i]*(-1);
-				fillString(strBuf,message,0,formatSize);
-			}else{
-				formatSize=sizes[i];
-				if (message.length()<formatSize){
-					fillBlank(strBuf,formatSize-message.length());
-					fillString(strBuf,message,0,message.length());
-				}else{
-					fillString(strBuf,message,0,formatSize);
+			if (sizes[i] < 0) {
+				formatSize = sizes[i] * (-1);
+				fillString(strBuf, message, 0, formatSize);
+			} else {
+				formatSize = sizes[i];
+				if (message.length() < formatSize) {
+					fillBlank(strBuf, formatSize - message.length());
+					fillString(strBuf, message, 0, message.length());
+				} else {
+					fillString(strBuf, message, 0, formatSize);
 				}
 			}
 		}
 		return strBuf.toString();
 	}
-	
-	private static void fillString(StringBuffer strBuf,String source,int start,int length){
-		int srcLength=source.length();
-		for(int i=start;i<start+length;i++){
-			if (i<srcLength){
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @param  strBuf  Description of the Parameter
+	 * @param  source  Description of the Parameter
+	 * @param  start   Description of the Parameter
+	 * @param  length  Description of the Parameter
+	 */
+	private static void fillString(StringBuffer strBuf, String source, int start, int length) {
+		int srcLength = source.length();
+		for (int i = start; i < start + length; i++) {
+			if (i < srcLength) {
 				strBuf.append(source.charAt(i));
-			}else{
+			} else {
 				strBuf.append(' ');
 			}
 		}
 	}
-	
-	private static void fillBlank(StringBuffer strBuf,int length){
-		for(int i=0;i<length;i++){
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @param  strBuf  Description of the Parameter
+	 * @param  length  Description of the Parameter
+	 */
+	private static void fillBlank(StringBuffer strBuf, int length) {
+		for (int i = 0; i < length; i++) {
 			strBuf.append(' ');
 		}
+	}
+
+
+	/**
+	 *  Returns True, if the character sequence contains only valid identifier chars.<br>
+	 * [A-Za-z0-9_]
+	 * @param  seq  Description of the Parameter
+	 * @return      The validObjectName value
+	 */
+	public static boolean isValidObjectName(CharSequence seq) {
+		for (int i = 0; i < seq.length(); i++) {
+			if (!Character.isUnicodeIdentifierPart(seq.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
