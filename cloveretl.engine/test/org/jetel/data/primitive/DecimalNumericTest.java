aac6fdc50ac3d772071906b8b78139c0aec257cd
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-05-17 13:23:06 +0000

    Tests for type Decimal - check in
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@798 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/data/primitive/DecimalNumericTest.java
+++ cloveretl.engine/test/org/jetel/data/primitive/DecimalNumericTest.java
@@ -13,8 +13,8 @@ public class DecimalNumericTest extends TestCase {
 		super.setUp();
 		Defaults.init();
 		
-		 anInt=DecimalFactory.getDecimal(Integer.MIN_VALUE);
-		 aLong=DecimalFactory.getDecimal(0);
+		 anInt=DecimalFactory.getDecimal(0);
+		 aLong=DecimalFactory.getDecimal((long)Integer.MAX_VALUE*2);
 		 aFloat=DecimalFactory.getDecimal(0.00000001f);
 		 aDouble=DecimalFactory.getDecimal(0.0000001);
 		 aDefault=DecimalFactory.getDecimal();
@@ -24,58 +24,75 @@ public class DecimalNumericTest extends TestCase {
 	}
 	
 	public void test_values(){
-		assertEquals(Integer.MIN_VALUE,anInt.getInt());
-//		assertEquals(new Double(Integer.MIN_VALUE),new Double(anInt.getDouble()));
-//		assertEquals(Integer.MIN_VALUE,anInt.getLong());
-		assertEquals(DecimalFactory.getDecimal(Integer.MIN_VALUE),anInt.getDecimal());
-		assertNotSame(DecimalFactory.getDecimal(Integer.MIN_VALUE),anInt.getDecimal());
-		assertEquals(8,anInt.getPrecision());
-		assertEquals(2,anInt.getScale());
-		assertEquals(-1,anInt.compareTo(DecimalFactory.getDecimal(20)));
-		assertEquals(-1,anInt.compareTo(new Integer(20)));
-		assertFalse(anInt.isNaN());
-		anInt.setNaN(true);
-		assertTrue(anInt.isNaN());
+		for (int i=0;i<5;i++){
+			int value=0;
+			switch (i) {
+				case 1:value=Integer.MIN_VALUE;					
+					   break;
+				case 2:value=Integer.MAX_VALUE;
+					   break;
+				case 3:value=123;
+					   break;
+				case 4:value=-123;
+					  break;
+			}
+			int less=value-1;
+			int more=value+1;
+		    anInt.setValue(value);
+			assertEquals(value,anInt.getInt());
+			if (!anInt.isNaN()) assertEquals(new Double(value),new Double(anInt.getDouble()));
+			if (!anInt.isNaN()) assertEquals(value,anInt.getLong());
+			if (!anInt.isNaN()) assertEquals(DecimalFactory.getDecimal(value),anInt.getDecimal());
+			assertNotSame(DecimalFactory.getDecimal(value),anInt.getDecimal());
+			assertEquals(10,anInt.getPrecision());
+			assertEquals(0,anInt.getScale());
+			if (anInt.getInt()==Integer.MAX_VALUE) anInt.setNaN(true);
+			if (!anInt.isNaN()) assertEquals(-1,anInt.compareTo(DecimalFactory.getDecimal(more)));
+			if (!anInt.isNaN()) assertEquals(-1,anInt.compareTo(new Integer(more)));
+			if (!anInt.isNaN()) assertEquals(0,anInt.compareTo(DecimalFactory.getDecimal(value)));
+			if (!anInt.isNaN()) assertEquals(0,anInt.compareTo(new Integer(value)));
+			if (!anInt.isNaN()) assertEquals(1,anInt.compareTo(DecimalFactory.getDecimal(less)));
+			if (!anInt.isNaN()) assertEquals(1,anInt.compareTo(new Integer(less)));
+		}
 
-//		assertEquals(0,aLong.getInt());
-//		assertEquals(new Double(0),new Double(aLong.getDouble()));
-//		assertEquals(0,aLong.getLong());
-//		assertEquals(DecimalFactory.getDecimal(0),aLong.getDecimal());
-//		assertNotSame(DecimalFactory.getDecimal(0),aLong.getDecimal());
+////		assertEquals((long)Integer.MAX_VALUE*2,aLong.getInt());
+//		assertEquals(new Double((long)Integer.MAX_VALUE*2),new Double(aLong.getDouble()));
+//		assertEquals((long)Integer.MAX_VALUE*2,aLong.getLong());
+//		assertEquals(DecimalFactory.getDecimal((long)Integer.MAX_VALUE*2),aLong.getDecimal());
+//		assertNotSame(DecimalFactory.getDecimal((long)Integer.MAX_VALUE*2),aLong.getDecimal());
 //		assertEquals(8,aLong.getPrecision());
 //		assertEquals(2,aLong.getScale());
-//		assertEquals(0,aLong.compareTo(DecimalFactory.getDecimal(0)));
-//		assertEquals(0,anInt.compareTo(new Long(0)));
+////		assertEquals(1,aLong.compareTo(DecimalFactory.getDecimal(Integer.MAX_VALUE)));
+////		assertEquals(1,aLong.compareTo(new Long(Integer.MAX_VALUE)));
 //		assertFalse(aLong.isNaN());
 //		aLong.setNaN(true);
 //		assertTrue(aLong.isNaN());
-//		assertEquals(new BigDecimal(0),anInt.getBigDecimal());
-
-		assertEquals(0,aFloat.getInt());
-//		assertEquals(Double.valueOf(0.00000000001),Double.valueOf(aFloat.getDouble()));
-		assertEquals(0,aFloat.getLong());
-//		assertEquals(DecimalFactory.getDecimal(0),aFloat.getDecimal());
-		assertNotSame(DecimalFactory.getDecimal(0.00000000001f),aFloat.getDecimal());
-//		assertEquals(new BigDecimal("0.00000000001"),aFloat.getBigDecimal());
-//		assertEquals(new BigDecimal(0),aFloat.getBigDecimalOutput());
-		assertEquals(8,aFloat.getPrecision());
-		assertEquals(2,aFloat.getScale());
-//		assertEquals(0,aFloat.compareTo(DecimalFactory.getDecimal(0)));
-		assertFalse(aFloat.isNaN());
-		aFloat.setNaN(true);
-		assertTrue(aFloat.isNaN());
-
-		assertEquals(0,aDouble.getInt());
-		assertEquals(new Double(0),new Double(aDouble.getDouble()));
-		assertEquals(0,aDouble.getLong());
-//		assertEquals(DecimalFactory.getDecimal(0),aDouble.getDecimal());
-		assertNotSame(DecimalFactory.getDecimal(0),aDouble.getDecimal());
-		assertEquals(8,aDouble.getPrecision());
-		assertEquals(2,aDouble.getScale());
-//		assertEquals(0,aDouble.compareTo(DecimalFactory.getDecimal(0)));
-		assertFalse(aDouble.isNaN());
-		aDouble.setNaN(true);
-		assertTrue(aDouble.isNaN());
+//
+//		assertEquals(0,aFloat.getInt());
+////		assertEquals(Double.valueOf(0.00000000001),Double.valueOf(aFloat.getDouble()));
+//		assertEquals(0,aFloat.getLong());
+////		assertEquals(DecimalFactory.getDecimal(0),aFloat.getDecimal());
+//		assertNotSame(DecimalFactory.getDecimal(0.00000000001f),aFloat.getDecimal());
+////		assertEquals(new BigDecimal("0.00000000001"),aFloat.getBigDecimal());
+////		assertEquals(new BigDecimal(0),aFloat.getBigDecimalOutput());
+//		assertEquals(8,aFloat.getPrecision());
+//		assertEquals(2,aFloat.getScale());
+////		assertEquals(0,aFloat.compareTo(DecimalFactory.getDecimal(0)));
+//		assertFalse(aFloat.isNaN());
+//		aFloat.setNaN(true);
+//		assertTrue(aFloat.isNaN());
+//
+//		assertEquals(0,aDouble.getInt());
+//		assertEquals(new Double(0),new Double(aDouble.getDouble()));
+//		assertEquals(0,aDouble.getLong());
+////		assertEquals(DecimalFactory.getDecimal(0),aDouble.getDecimal());
+//		assertNotSame(DecimalFactory.getDecimal(0),aDouble.getDecimal());
+//		assertEquals(8,aDouble.getPrecision());
+//		assertEquals(2,aDouble.getScale());
+////		assertEquals(0,aDouble.compareTo(DecimalFactory.getDecimal(0)));
+//		assertFalse(aDouble.isNaN());
+//		aDouble.setNaN(true);
+//		assertTrue(aDouble.isNaN());
 }
 
 //	public void test_maths(){
