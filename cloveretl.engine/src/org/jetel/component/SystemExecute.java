ca8be0f228069434e7bdc5fcd50b972050627099
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-05-17 08:14:06 +0000

    "System execute" with input port
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@792 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/component/SystemExecute.java
+++ cloveretl.engine/src/org/jetel/component/SystemExecute.java
@@ -1,16 +1,14 @@
 package org.jetel.component;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.RandomAccessFile;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import org.jetel.data.DataRecord;
+import org.jetel.data.formatter.DelimitedDataFormatterNIO;
 import org.jetel.data.parser.DelimitedDataParserNIO;
 import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.graph.InputPort;
 import org.jetel.graph.Node;
 import org.jetel.util.ComponentXMLAttributes;
 import org.jetel.util.SynchronizeUtils;
@@ -26,6 +24,7 @@ public class SystemExecute extends Node{
 	private String command;
 	private int exitValue;
 	private DelimitedDataParserNIO parser;
+	private DelimitedDataFormatterNIO formatter;
 	 
 	public SystemExecute(String id,String command) {
 		super(id);
@@ -33,43 +32,69 @@ public class SystemExecute extends Node{
 	}
 
 	public void run() {
+		//Creating and initializing record from input port
+		DataRecord in_record=null;
+		InputPort inPort = getInputPort(INPUT_PORT);
+		//If there is input port read metadadata and initialize in_record
+		try{
+			in_record = new DataRecord(inPort.getMetadata());
+			in_record.init();
+		}catch(NullPointerException e){
+		}
+		formatter=new DelimitedDataFormatterNIO();
+
+		//Creating and initializing record to otput port
+		DataRecord out_record=null;
+		//If there is output port read metadadata and initialize in_record
+		try {
+			out_record= new DataRecord(getOutputPort(OUTPUT_PORT).getMetadata());
+			out_record.init();
+		}catch(NullPointerException e){
+		}
+		parser=new DelimitedDataParserNIO();
+
+		final int inPorts_size=inPorts.size();
+		final int outPorts_size=outPorts.size();
 		
 		Runtime r=Runtime.getRuntime();
-		if (runIt) 
-			try{
-				Process p=r.exec(command);
-				if (outPorts.size()>0){
-					DataRecord record = new DataRecord(getOutputPort(OUTPUT_PORT).getMetadata());
-					record.init();
-					parser=new DelimitedDataParserNIO();
-//					File tmp=File.createTempFile("in_",null);
-//					RandomAccessFile t=new RandomAccessFile(tmp,"rw");
-//					BufferedReader br = new BufferedReader( new InputStreamReader( p.getInputStream() ) );
-//					String line;
-//					while ((line=br.readLine())!=null){
-//						t.writeBytes(line+"\n");
-//					}
-//					t.close();
-					parser.open(p.getInputStream(), getOutputPort(OUTPUT_PORT).getMetadata());
-					while (((record = parser.getNext(record)) != null) && runIt) {
-						//broadcast the record to all connected Edges
-						writeRecordBroadcast(record);
-						SynchronizeUtils.cloverYield();
-					}
-				}
-				exitValue=p.waitFor();
-			}catch(IOException ex){
-				resultMsg = ex.getMessage();
-				resultCode = Node.RESULT_ERROR;
-			}catch(InterruptedException ex){
-				resultMsg = ex.getMessage();
-				resultCode = Node.RESULT_ERROR;
-			}catch(Exception ex){
-			    ex.printStackTrace();
-				resultMsg = ex.getClass().getName()+" : "+ ex.getMessage();
-				resultCode = Node.RESULT_FATAL_ERROR;
-				return;
+		
+		try{
+			Process p=r.exec(command);
+			OutputStream p_in=p.getOutputStream();
+			InputStream p_out=p.getInputStream();
+			// If there is input port read records and write them to input stream of the process
+			if (inPorts_size>0) {
+				in_record=inPort.readRecord(in_record);
+				formatter.open(p_in,getInputPort(INPUT_PORT).getMetadata());
 			}
+			while (( in_record!= null ) && runIt) {
+				formatter.write(in_record);
+				if (inPorts.size()>0) in_record=inPort.readRecord(in_record);
+				if (in_record==null) formatter.close();
+			}
+			p_in.close();
+			//If there is output port read output from process and send it to output ports
+			if (outPorts_size>0){
+				parser.open(p_out, getOutputPort(OUTPUT_PORT).getMetadata());
+				//send all out_records to output ports
+				while (((out_record = parser.getNext(out_record)) != null) && runIt) {
+					//broadcast the record to all connected Edges
+					writeRecordBroadcast(out_record);
+					SynchronizeUtils.cloverYield();
+				}
+			}
+		}catch(IOException ex){
+			resultMsg = ex.getMessage();
+			resultCode = Node.RESULT_ERROR;
+		}catch(InterruptedException ex){
+			resultMsg = ex.getMessage();
+			resultCode = Node.RESULT_ERROR;
+		}catch(Exception ex){
+		    ex.printStackTrace();
+			resultMsg = ex.getClass().getName()+" : "+ ex.getMessage();
+			resultCode = Node.RESULT_FATAL_ERROR;
+			return;
+		}
 		broadcastEOF();
 		if (runIt) {
 			resultMsg = "OK";
@@ -83,7 +108,8 @@ public class SystemExecute extends Node{
 	}
 
 	public void init() throws ComponentNotReadyException {
-		
+		if (inPorts.size()>1) 
+			throw new ComponentNotReadyException(getID() + ": too many input ports");
 	}
 
 	public String getType(){
