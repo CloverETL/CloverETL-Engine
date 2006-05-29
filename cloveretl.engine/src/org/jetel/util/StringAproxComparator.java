6acfa96f4ba77e432a39cfff6fe95ded8badbdea
agad <info@cloveretl.com>
2006-05-29 11:09:21 +0000

    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@837 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/StringAproxComparator.java
+++ cloveretl.engine/src/org/jetel/util/StringAproxComparator.java
@@ -13,17 +13,21 @@ import java.util.Locale;
 public class StringAproxComparator {
 
 	//strentgh fields
-	private final static int  IDENTICAL=4;
-	private final static int TERTIARY=3; //upper case = lower case
-	private final static int SECONDARY=2;//diacrtic letters = letters witout diacrityk (locale dependent)
-			// now done only for CZ
-	private final static int PRIMARY=1;// mistakes acceptable (Collator.PRIMARY,new Locale("en","UK"))
+	public final static int  IDENTICAL=4;
+	public final static int TERTIARY=3; //upper case = lower case
+	public final static int SECONDARY=2;//diacrtic letters = letters witout diacrityk (locale dependent)
+			// now done only for CZ and PL
+	public final static int PRIMARY=1;// mistakes acceptable (Collator.PRIMARY,new Locale("en","UK"))
 	
 	private int strentgh;
 	private int substCost=1;
 	private int delCost=1;
 	private int changCost=1;
 	private String[] rules={};
+	private int maxDiffrence=5;
+	private int identicalDiffrenceWeight=1;
+	private int tertiaryDiffrenceWeight=1;
+	private int secondaryDiffrenceWeight=1;
 
 	//arrays for method distance
 	char[] schars;
@@ -64,7 +68,7 @@ public class StringAproxComparator {
 	}
 
 	public StringAproxComparator(int changCost, int delCost, int substCost) {
-		strentgh = IDENTICAL;
+		this.setStrentgh(IDENTICAL);
 		this.changCost=changCost;
 		this.delCost=delCost;
 		this.substCost=substCost ;
@@ -120,8 +124,7 @@ public class StringAproxComparator {
         case IDENTICAL:
             return c1 == c2;
         case TERTIARY:
-            c = Math.min((int) c1 + 32, (int) c2 + 32);
-            return (c1 == c2 || Math.max((int) c1, (int) c2) == c);
+             return (Character.toLowerCase(c1)==Character.toLowerCase(c2));
         case SECONDARY:
             c = getRules(c1, rules);
             if (c < rules.length)
@@ -130,15 +133,29 @@ public class StringAproxComparator {
                 return (charEquals(c1, c2, StringAproxComparator.TERTIARY));
             }
         case PRIMARY:
-            return (col.compare(String.valueOf(c1), String.valueOf(c2)) == 0 || charEquals(
-                    c1, c2, StringAproxComparator.SECONDARY));
+            return (col.compare(String.valueOf(c1), String.valueOf(c2)) == 0 || 
+            		charEquals(c1, c2, StringAproxComparator.SECONDARY));
         }
         return false;
     }
 	
-	private int subst_cost(char c1,char c2){
-		return (charEquals(c1,c2,strentgh) ? 0 : substCost);
+	private int subst_cost(char c1,char c2,int strentgh){
+		if (strentgh==IDENTICAL)
+			return (charEquals(c1,c2,IDENTICAL) ? 0 : substCost);
+		else {
+			int weight=0;
+			switch (strentgh){
+				case TERTIARY: weight=identicalDiffrenceWeight;
+						  break;
+				case SECONDARY:weight=tertiaryDiffrenceWeight;
+						  break;
+				case PRIMARY:  weight=secondaryDiffrenceWeight;
+						  break;
+			}
+			return subst_cost(c1,c2,strentgh+1)*weight+(charEquals(c1,c2,strentgh) ? 0 : substCost);
+		}
 	}
+
 	
 	private int min(int i1,int i2,int i3){
 		int m=i1;
@@ -147,6 +164,12 @@ public class StringAproxComparator {
 		return m;
 	}
 	
+	/**
+	 * This method calculates distance between Strings s and t. If the distance is greater then maxDiffrence it returns maxDiffrence+1
+	 * @param s
+	 * @param t
+	 * @return distance between Strings s and t
+	 */
 	public int distance(String s,String t){
 		if (s.length()==0 || t.length()==0)
 			return Math.min(s.length(),t.length());
@@ -177,16 +200,21 @@ public class StringAproxComparator {
 		schars=s.toCharArray();
 		tchars=t.toCharArray();
 		
+		int cost=0;
+		
 		for (int i=1;i<slenth;i++) {
 			now[0]=slast[i];
 			tlast[0]=slast[i-1];
+			cost=now[0];
 			for (int j=1;j<tlent;j++){
-				int m=min(now[j-1]+delCost,tlast[j]+delCost,tlast[j-1]+subst_cost(schars[i-1],tchars[j-1]));
+				int m=min(now[j-1]+delCost,tlast[j]+delCost,tlast[j-1]+subst_cost(schars[i-1],tchars[j-1],strentgh));
 				if (i>1 && j>1)
 					if (schars[i-2]==tchars[j-1]&&schars[i-1]==tchars[j-2])
 						m=Math.min(m,tblast[j-2]+changCost);
 				now[j]=m;
+				if (m<cost) cost=m;
 			}
+			if (cost>maxDiffrence) return cost;
 			for (int j=0;j<tlent;j++){
 				tblast[j]=tlast[j];
 				tlast[j]=now[j];
@@ -226,6 +254,14 @@ public class StringAproxComparator {
 			col=Collator.getInstance(new Locale("US"));
 			col.setStrength(Collator.PRIMARY);
 		}
+	}
+
+	public int getMaxDiffrence() {
+		return maxDiffrence;
+	}
+
+	public void setMaxDiffrence(int maxCost) {
+		this.maxDiffrence = maxCost;
 	}
 
 }
