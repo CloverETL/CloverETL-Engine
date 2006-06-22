f18ccbb242754fca6674f2619c69fb4a6a5436ac
dpavlis <info@cloveretl.com>
2006-06-22 19:58:37 +0000

    initial check-in
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@918 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/src/org/jetel/interpreter/ExpParser.java
@@ -0,0 +1,43 @@
+/*
+ *    jETeL/Clover - Java based ETL application framework.
+ *    Copyright (C) 2002-06  David Pavlis <david.pavlis@centrum.cz> and others.
+ *    
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *    
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
+ *    Lesser General Public License for more details.
+ *    
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Created on 22.6.2006
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.jetel.interpreter;
+
+
+
+
+/**
+ * @author david
+ * @since  22.6.2006
+ *
+ * This class is only the ancestor of all Parsers in
+ * order to share Nodes
+ */
+public class ExpParser {
+
+    public ExpParser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
