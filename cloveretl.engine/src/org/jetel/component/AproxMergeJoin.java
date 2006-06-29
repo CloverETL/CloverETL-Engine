69ad5d817054735412a8cf97ca92087a046a4e66
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-06-29 13:04:58 +0000

    spelling checked and comments
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@961 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/component/AproxMergeJoin.java
+++ cloveretl.engine/src/org/jetel/component/AproxMergeJoin.java
@@ -20,11 +20,7 @@
 package org.jetel.component;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.Properties;
 
 import org.apache.commons.logging.Log;
@@ -42,7 +38,6 @@ import org.jetel.graph.OutputPort;
 import org.jetel.graph.TransformationGraph;
 import org.jetel.metadata.DataFieldMetadata;
 import org.jetel.metadata.DataRecordMetadata;
-import org.jetel.util.CodeParser;
 import org.jetel.util.ComponentXMLAttributes;
 import org.jetel.util.DynamicJavaCode;
 import org.jetel.util.StringAproxComparator;
@@ -51,9 +46,9 @@ import org.jetel.util.SynchronizeUtils;
 /**
  * <h3>Approximate Merge Join Component</h3>
  *
- * <!-- Joins records on port 0 (Master records) with records on por 1 (Slave records).
+ * <!-- Joins records on port 0 (Master records) with records on port 1 (Slave records).
  * Joining is based on specified key fields and their similarity - with defined
- * maximum disimilarity/distance - computed as edit distance - aka "Levenshtein distance"-->
+ * maximum disimilarity/distance - computed as edit distance - "Levenshtein distance"-->
  *
  * <table border="1">
  *  <th>Component:</th>
@@ -61,34 +56,83 @@ import org.jetel.util.SynchronizeUtils;
  * <tr><td><h4><i>Category:</i></h4></td>
  * <td></td></tr>
  * <tr><td><h4><i>Description:</i></h4></td>
- * <td>Joins records on port 0 (Master records) with records on por 1 (Slave records).
+ * <td>Joins records on port 0 (Master records) with records on port 1 (Slave records).
  * Joining is based on specified key fields and their similarity - with defined
- * maximum disimilarity/distance - computed as edit distance - aka "Levenshtein distance"</td></tr>
+ * maximum dissimilarity/distance - computed as edit distance - "Levenshtein distance"</td></tr>
  * <tr><td><h4><i>Inputs:</i></h4></td>
  * <td> [0] - driver records<br>
  *    [1] - slave records<br></td></tr>
  * <tr><td><h4><i>Outputs:</i></h4></td>
- * <td>[0] - records only in set A<br>
- *        [1] - records in set A&amp;B<br>
- *        [2] - records only in set B   
- *        [3] - records in set A&amp;B<br></td></tr>
+ * <td>[0] - joined records with conformity greater then given limit<br>
+ *        [1] - joined records with conformity smaller then given limit but with the same matching key<br>
+ *        [2] - driver records for which there is not matching slave  
+ *        [3] - slave records for which there is not matching driver<br></td></tr>
  * <tr><td><h4><i>Comment:</i></h4></td>
  * <td></td></tr>
  * </table>
  *  <br>
  *  <table border="1">
  *  <th>XML attributes:</th>
- *  <tr><td><b>type</b></td><td>"AGGREGATE"</td></tr>
+ *  <tr><td><b>type</b></td><td>APROX_MERGE_JOIN</td></tr>
  *  <tr><td><b>id</b></td><td>component identification</td>
- *  <tr><td><b>aggregateKey</b></td><td>field names separated by :;|  {colon, semicolon, pipe}</td>
- *  <tr><td><b>aggregateFunction</b></td><td>aggregate functions separated by :;|  {colon, semicolon, pipe} available functions are count, min, max, sum, avg, stdev</td>
- *  <tr><td><b>sorted</b></td><td>if input data flow is sorted (true)</td>
- *  <tr><td><b>equalNULL</b><br><i>optional</i></td><td>specifies whether two fields containing NULL values are considered equal. Default is FALSE.</td></tr>
+ *  <tr><td><b>joinKey</b></td><td>field names with number of letters, weight 
+ *  	and strength of comparison as four boolean values for each comparison 
+ *  	level separated by :;|  {colon, semicolon, pipe}</td>
+ *  <tr><td><b>slaveOverwriteKey</b><br><i>optional</i></td><td>can be used 
+ *  	to specify different key field names for records on slave input; 
+ *  	field names separated by :;| {colon, semicolon, pipe}</td>
+ *  <tr><td><b>matchingKey</b></td><td>field name for comparing driver and 
+ *  	slave records</td>
+ *  <tr><td><b>slaveMatchingOverwrite</b><br><i>optional</i></td><td>can be
+ *  	 used to specify different key field name for records on slave input</td></tr>
+ *  <tr><td><b>transformClass</b><br><i>optional</i></td><td>name of the class
+ *  	 to be used for transforming joined data which has conformity greater
+ *  	 then conformity limit.If no class name is specified then it is expected
+ *  	 that the transformation Java source code is embedded in XML</td></tr>
+ *  <tr><td><b>transformClassForSuspicious</b><br><i>optional</i></td><td>name
+ *  	 of the class to be used for transforming joined data which has conformity
+ *  	 smaller then conformity limit.If no class name is specified then it
+ *  	 is expected that the transformation Java source code is embedded in XML</td></tr>
+ *  <tr><td><b>libraryPath</b><br><i>optional</i></td><td>name of Java library
+ *  	 file (.jar,.zip,...) where to search for class to be used for transforming
+ *  	 joined data specified in transformClassForSuspicious parameter.</td>
+ *  <tr><td><b>libraryPathForSuspicious</b><br><i>optional</i></td><td>name
+ *  	 of Java library file (.jar,.zip,...) where to search for class to be
+ *  	 used for transforming joined data specified in transformClass parameter.<</td>
+ *  <tr><td><b>transform</b></td><td>contains definition of transformation for
+ *  	 joined records which has conformity greater then conformity limit in
+ *  	 internal clover format</td>
+ *  <tr><td><b>transformForSuspicious</b></td><td>contains definition of transformation
+ *  	 for joined records which has conformi ty smaller then conformity limit
+ *  	 in internal clover format</td>
+ *  <tr><td><b>conformity</b><br></td><td>joined records are divided to two 
+ *  	sets depending on their computed conformity is greater or smaller then
+ *  	 this parameter</td></tr>
  *  </tr>
  *  </table>
  *
  *  <h4>Example:</h4>
- *  <pre>&lt;Node id="AGGREGATE_NODE" type="AGGREGATE" aggregateKey="FirstName" aggregateFunctions="count(); min(Age); avg(Salery); min(HireDate)" sorted="false" /&gt;</pre>
+ *  <pre&gt;&lt;Node id="APROX_MERGE_JOIN0" type="APROX_MERGE_JOIN"&gt;
+ *  &lt;attr name="matchingKey"&gt;key&lt;/attr&gt;
+ *  &lt;attr name="transform"&gt;${out.0.lname} = ${in.0.lname};
+ *  ${out.0.fname} = ${in.0.fname};
+ *  ${out.0.birthdate} = ${in.0.birthdate};
+ *  ${out.0.gender} = ${in.0.gender};
+ *  ${out.0.first_name} = ${in.1.first_name};
+ *  ${out.0.last_name} = ${in.1.last_name};
+ *  ${out.0.birth_date} = ${in.1.birth_date};
+ *  ${out.0.gender1} = ${in.1.gender};
+ *  &lt;/attr&gt;
+ *  &lt;attr name="conformity"&gt;0.8&lt;/attr&gt;
+ *  &lt;attr name="slaveOverwriteKey"&gt;last_name;first_name&lt;/attr&gt;
+ *  &lt;attr name="joinKey"&gt;lname 2 0.75 false true true true;fname 4 0.25 false false false true&lt;/attr&gt;
+ *  &lt;attr name="transformForSuspicious"&gt;${out.1.lname} = ${in.0.lname};
+ *  ${out.1.fname} = ${in.0.fname};
+ *  ${out.1.first_name} = ${in.1.first_name};
+ *  ${out.1.last_name} = ${in.1.last_name};
+ *  &lt;/attr&gt;
+ *  &lt;/Node /&gt;
+ *  </pre>
  *
  * @author avackova
  *
@@ -149,7 +193,7 @@ public class AproxMergeJoin extends Node {
 	private double[] weights;
 	
 	private StringAproxComparator[] comparator;
-	private int[] maxDiffrenceLetters;
+	private int[] maxDifferenceLetters;
 
 	private double conformityLimit;
 	
@@ -159,7 +203,7 @@ public class AproxMergeJoin extends Node {
 	// for passing data records into transform function
 	private final static DataRecord[] inRecords = new DataRecord[2];
 	private DataRecord[] outConformingRecords=new DataRecord[1];
-	private DataRecord[] outSuspiciousRcords = new DataRecord[2];
+	private DataRecord[] outSuspiciousRecords = new DataRecord[2];
 
 	private Properties transformationParameters;
 	private Properties transformationParametersForSuspicious;
@@ -199,7 +243,7 @@ public class AproxMergeJoin extends Node {
 	 *  Can be used if slave record has different names
 	 *  for fields composing the key
 	 *
-	 * @param  slaveKeys  The new slaveOverrideKey value
+	 * @param  slaveKeys  The new slaveOverwriteKey value
 	 */
 	private void setSlaveOverrideKey(String[] slaveKeys) {
 		this.slaveOverwriteKeys = slaveKeys;
@@ -290,20 +334,20 @@ public class AproxMergeJoin extends Node {
 	}
 
 	/**
-	 * Calculates diffrence on given fields between two records 
+	 * Calculates difference on given fields between two records 
 	 * 
 	 * @param record1 - driver record
 	 * @param record2 - slave record
 	 * @param fieldsToCompare 
-	 * @return diffrence between two records from interval <0,1>
+	 * @return difference between two records from interval <0,1>
 	 */
 	private double[] conformity(DataRecord record1,DataRecord record2,int[][] fieldsToCompare){
 		double[] result=new double[fieldsToCompare[DRIVER_ON_PORT].length+1];
 		double totalResult=0;
 		int max=0;
 		for (int i=0;i<fieldsToCompare[DRIVER_ON_PORT].length;i++){
-			comparator[i].setMaxLettersToChange(maxDiffrenceLetters[i]);
-			max=(maxDiffrenceLetters[i]+1)*comparator[i].getMaxCostForOneLetter();
+			comparator[i].setMaxLettersToChange(maxDifferenceLetters[i]);
+			max=(maxDifferenceLetters[i]+1)*comparator[i].getMaxCostForOneLetter();
 			int distance=comparator[i].distance(
 					record1.getField(fieldsToCompare[DRIVER_ON_PORT][i]).getValue().toString(),
 					record2.getField(fieldsToCompare[SLAVE_ON_PORT][i]).getValue().toString());
@@ -339,7 +383,7 @@ public class AproxMergeJoin extends Node {
 		inRecords[0] = driver;
 		inRecords[1] = slave;
 		outConformingRecords[0] = outConforming;
-		outSuspiciousRcords[1] = outSuspicious;
+		outSuspiciousRecords[1] = outSuspicious;
 
 		while (recordBuffer.shift(dataBuffer) != null) {
 			dataBuffer.flip();
@@ -351,7 +395,7 @@ public class AproxMergeJoin extends Node {
 					resultMsg = transformation.getMessage();
 					return false;
 				}
-				//fill adidional fields
+				//fill aditional fields
 				if (conformityFieldsForConforming.length>0){
 					for (int i=0;i<conformityFieldsForConforming.length;i++){
 						if (conformityFieldsForConforming[i]>-1){
@@ -362,11 +406,11 @@ public class AproxMergeJoin extends Node {
 				conformingPort.writeRecord(outConforming);
 			}else{
 				// **** call transform function here ****
-				if (!transformationForSuspicious.transform(inRecords,outSuspiciousRcords)){
+				if (!transformationForSuspicious.transform(inRecords,outSuspiciousRecords)){
 					resultMsg = transformation.getMessage();
 					return false;
 				}
-				//fill adidional fields
+				//fill aditional fields
 				if (conformityFieldsForSuspicious.length>0){
 					for (int i=0;i<conformityFieldsForSuspicious.length;i++){
 						if (conformityFieldsForSuspicious[i]>-1){
@@ -535,7 +579,7 @@ public class AproxMergeJoin extends Node {
             }
             
         transformation.setGraph(getGraph());
-        //Checking metada on input and on  NOT_MATCH_DRIVER_OUT and NOT_MATCH_SLAVE_OUT outputs
+        //Checking metadata on input and on  NOT_MATCH_DRIVER_OUT and NOT_MATCH_SLAVE_OUT outputs
 		DataRecordMetadata[] inMetadata = new DataRecordMetadata[2];
 		inMetadata[0]=getInputPort(DRIVER_ON_PORT).getMetadata();
 		inMetadata[1]=getInputPort(SLAVE_ON_PORT).getMetadata();
@@ -552,7 +596,7 @@ public class AproxMergeJoin extends Node {
                     new DataRecordMetadata[] {getOutputPort(CONFORMING_OUT).getMetadata()})) {
 			throw new ComponentNotReadyException("Error when initializing reformat function !");
 		}
-		//checking transformation for suspicoius records
+		//checking transformation for suspicious records
 		if (transformationForSuspicious == null) {
 			if (transformClassNameForSuspicious != null) {
                 transformationForSuspicious = RecordTransformFactory.loadClass(logger,
@@ -583,17 +627,17 @@ public class AproxMergeJoin extends Node {
 		}
 		//initializing join parameters
 		joinKeys = new String[joinParameters.length];
-		maxDiffrenceLetters = new int[joinParameters.length];
-		boolean[][] strenght=new boolean[joinParameters.length][StringAproxComparator.IDENTICAL];
+		maxDifferenceLetters = new int[joinParameters.length];
+		boolean[][] strength=new boolean[joinParameters.length][StringAproxComparator.IDENTICAL];
 		weights = new double[joinParameters.length];
 		String[] tmp;
 		for (int i=0;i<joinParameters.length;i++){
 			tmp=joinParameters[i].split(" ");
 			joinKeys[i]=tmp[0];
-			maxDiffrenceLetters[i]=Integer.parseInt(tmp[1]);
+			maxDifferenceLetters[i]=Integer.parseInt(tmp[1]);
 			weights[i]=Double.parseDouble(tmp[2]);
 			for (int j=0;j<StringAproxComparator.IDENTICAL;j++){
-				strenght[i][j] = tmp[3+j].equals("true") ? true : false;
+				strength[i][j] = tmp[3+j].equals("true") ? true : false;
 			}
 		}
 		double sumOfWeights=0;
@@ -617,7 +661,7 @@ public class AproxMergeJoin extends Node {
 		for (int i=0;i<comparator.length;i++){
 			String locale=inMetadata[DRIVER_ON_PORT].getField(fieldsToCompare[DRIVER_ON_PORT][i]).getLocaleStr();
 			try {
-				comparator[i] = StringAproxComparator.createComparator(locale,strenght[i]);
+				comparator[i] = StringAproxComparator.createComparator(locale,strength[i]);
 			}catch(JetelException ex){
 				throw new ComponentNotReadyException(ex.getLocalizedMessage());
 			}
@@ -643,7 +687,7 @@ public class AproxMergeJoin extends Node {
 	 * 		computed conformities
 	 * @param metadata
 	 * @return numbers of fields where are to be conformities. When 
-	 * 		such field there is not in out metdata on coresponding position 
+	 * 		such field there is not in out metadata on corresponding position 
 	 * 		there is -1. First number in array is field number for _total_conformity
 	 */
 	private int[] findOutFields(String[] names,DataRecordMetadata metadata){
@@ -694,7 +738,7 @@ public class AproxMergeJoin extends Node {
 				if (xattribs.exists(XML_JAVA_SOURCE_ATTRIBUTE)){
 					dynaTransCode = new DynamicJavaCode(xattribs.getString(XML_JAVA_SOURCE_ATTRIBUTE));
 				}else{
-					// do we have child node wich Java source code ?
+					// do we have child node with Java source code ?
 					try {
 					    dynaTransCode = DynamicJavaCode.fromXML(graph, nodeXML);
 					} catch(Exception ex) {
@@ -726,7 +770,7 @@ public class AproxMergeJoin extends Node {
 				if (xattribs.exists(XML_JAVA_SOURCE_FOR_SUSPICIOUS_ATTRIBUTE)){
 					dynaTransCode = new DynamicJavaCode(xattribs.getString(XML_JAVA_SOURCE_FOR_SUSPICIOUS_ATTRIBUTE));
 				}else{
-					// do we have child node wich Java source code ?
+					// do we have child node with Java source code ?
 					try {
 					    dynaTransCode = DynamicJavaCode.fromXML(graph, nodeXML);
 					} catch(Exception ex) {
@@ -737,7 +781,7 @@ public class AproxMergeJoin extends Node {
 				} else { //chance to find reformat code is in transform attribute
 					if (xattribs.exists(XML_TRANSFORM_FOR_SUSPICIOUS_ATTRIBUTE)) {
 						join.setTransformSourceForSuspicious(	xattribs.getString(XML_TRANSFORM_FOR_SUSPICIOUS_ATTRIBUTE), true); 
-					}else{ //get transformation from output por 0
+					}else{ //get transformation from output port 0
 						
 					}
 				}
