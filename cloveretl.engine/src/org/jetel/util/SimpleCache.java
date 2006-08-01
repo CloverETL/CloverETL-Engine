1767d29f4dbc0db4b28e6b234733b92ef420efb0
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-08-01 09:49:26 +0000

    comments added
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1084 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/SimpleCache.java
+++ cloveretl.engine/src/org/jetel/util/SimpleCache.java
@@ -18,8 +18,6 @@
  *
  * Created on 24.11.2005
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package org.jetel.util;
 
@@ -28,10 +26,13 @@ import java.util.Map.Entry;
 
 
 /**
- * Simple cached based on LinkedHashMap.<br>
+ * Simple cache based on LinkedHashMap.<br>
  * It keeps LRU order of accessed items. When maximum capacity
  * is reached and new item is to be stored, the Eldest entry is
  * automatically removed. 
+ * When simple cache is created it can be only one entry upon one key, if you 
+ * want to have more entries upon one key (using of ArrayList) after creating 
+ * the object call method enableDuplicity()
  * 
  * @author david pavlis
  * @since  24.11.2005
@@ -39,15 +40,14 @@ import java.util.Map.Entry;
  */
 public class SimpleCache {
     
-    private static final int DEFAULT_MAX_ENTRIES = 100;
-    private static final boolean ACCESS_ORDER=true;
-    
     protected LinkedHashMap map;
     protected DuplicateKeyMap keyMap = null;
     
     protected transient int totalSize = 0;
+    protected int maxSize;
     
-    private Iterator iterator;
+    protected Iterator iterator;
+    protected Entry eldest;
 
     /**
      * Creates cache with initial size of 16 entries.
@@ -55,6 +55,7 @@ public class SimpleCache {
      */
     public SimpleCache(){
     	map = new StoreMap();
+    	maxSize = ((StoreMap)map).getMaxEntries();
     }
     
     /**
@@ -65,6 +66,7 @@ public class SimpleCache {
      */
     public SimpleCache(int initialCapacity){
     	map = new StoreMap(initialCapacity);
+       	maxSize = ((StoreMap)map).getMaxEntries();
     }
     
     /**
@@ -76,58 +78,129 @@ public class SimpleCache {
      */
     public SimpleCache(int initialCapacity,int maxCapacity){
     	map = new StoreMap(initialCapacity,maxCapacity);
-    }
+       	maxSize = ((StoreMap)map).getMaxEntries();
+   }
     
+    /**
+     * This method turns on duplicity entries upon the same key
+     * 
+     */
     public void enableDuplicity(){
     	keyMap = new DuplicateKeyMap(map);
     }
     
+    /**
+     * 
+     * @param key
+     * @return first entry upon the given key
+     */
     public Object get(Object key){
     	return (keyMap == null ? map.get(key) : keyMap.get(key) );
     }
     
+    /**
+     * @return next entry upon the key from last used method get
+     */
     public Object getNext(){
     	return (keyMap == null ? null :keyMap.getNext() );
     }
     
+    /**
+     * This method puts the value upon the given key. When there is reached 
+     * maximal capacity eldest object (when duplicity are enabled it is the
+     * eldest ArrayList) is removed. In present implementation method 
+     * StoreMap.removeEldestEntry always returns false
+     * 
+     * @param key
+     * @param value
+     * @return
+     */
     public Object put(Object key, Object value){
 		totalSize++;
-      	if (totalSize<=((StoreMap)map).getMaxEntries()){
+      	if (totalSize<=maxSize){
     		return (keyMap == null ? 
     				map.put(key,value) : keyMap.put(key,value) );
     	}else if (keyMap==null){
-    		return map.put(key,value);
+     		return map.put(key,value);
     	}
       	iterator = map.entrySet().iterator();
-      	Entry eldest = (Entry)iterator.next();
+      	eldest = (Entry)iterator.next();
       	map.remove(eldest.getKey());
     	totalSize = totalSize - ((ArrayList)eldest.getValue()).size();
     	return keyMap.put(key,value);
-    }
+
+//    	totalSize++;
+//		return (keyMap == null ? map.put(key,value) : keyMap.put(key,value) );
+   }
     
     
+    /**
+     * Class for storing limited numbers of entries
+     * 
+     * @author avackova
+     *
+     */
     class StoreMap extends LinkedHashMap{
     	
-    	int max_entries;
+        private static final int DEFAULT_MAX_ENTRIES = 100;
+        private static final boolean ACCESS_ORDER=true;
+        
+        int max_entries;
+        
+        //Following variables are not actually used, because method removeEldestEntry always returns false
+        boolean remove;
+        Object tmp;
+        int eldestSize;
 
+        /**
+         * Creates cache with initial size of 16 entries.
+         * Maximum size is defaulted to 100.
+         */
     	StoreMap(){
             super(16,(float)0.75,ACCESS_ORDER);
             max_entries=DEFAULT_MAX_ENTRIES;
     	}
     	
+        /**
+         * Creates cache with initial size equal to parameter.
+         * Maximum size is defaulted to 100.
+         * 
+         * @param initialCapacity
+         */
     	StoreMap(int initialCapacity){
             super(initialCapacity,(float)0.75,ACCESS_ORDER);
             max_entries= (initialCapacity > DEFAULT_MAX_ENTRIES ? initialCapacity : DEFAULT_MAX_ENTRIES);
         }
     	
-    	StoreMap(int initialCapacity,int maxCapacity){
-            super((initialCapacity > 16 ? initialCapacity : 16),(float)0.75,ACCESS_ORDER);
-            max_entries= maxCapacity;
-        }
+	    /**
+	     * Creates cache with initial capacity and maximum capacity
+	     * equal to specified parameters
+	     * 
+	     * @param initialCapacity
+	     * @param maxCapacity
+	     */
+	   	StoreMap(int initialCapacity,int maxCapacity){
+	        super((initialCapacity > 16 ? initialCapacity : 16),(float)0.75,ACCESS_ORDER);
+	        max_entries= maxCapacity;
+	    }
     	
+        /* (non-Javadoc)
+         * @see java.util.LinkedHashMap#removeEldestEntry(java.util.Map.Entry)
+         * 
+         * In present implementation this method always returns false, because
+         * total size is checked before calling method LinkedHashMap.put and, 
+         * when it is greater then maximal size the eldest entry is removed
+         */
         protected boolean removeEldestEntry(Map.Entry eldest) {
-//            return totalSize > max_entries;
-        	return size() > max_entries;
+         	remove = totalSize > max_entries;
+//        	if (remove) {
+//	        	tmp = eldest.getValue();
+//	        	if (tmp instanceof ArrayList) {
+//					eldestSize = ((ArrayList)tmp).size();
+//				}
+//	       		totalSize = totalSize - eldestSize;
+//        	}
+	        return remove;
          }
      	
         int getMaxEntries(){
