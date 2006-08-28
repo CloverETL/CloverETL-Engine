74781daa941001cdc69039f24580f0945e4bca4d
dpavlis <info@cloveretl.com>
2006-08-28 16:14:13 +0000

    initial check-in refactored from Partition component
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1260 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.component/src/org/jetel/component/partition/RangePartition.java
@@ -0,0 +1,72 @@
+package org.jetel.component.partition;
+
+import java.util.Arrays;
+
+import org.jetel.data.DataField;
+import org.jetel.data.DataRecord;
+import org.jetel.data.RecordKey;
+
+/**
+ * Partition algorithm which compares current key value with set of
+ * intervals (defined as a set of interval upper limits - inclusive).
+ * 
+ * @author david
+ * @since  5.3.2005
+ *
+ */
+public class RangePartition implements PartitionFunction{
+    int numPorts;
+    DataField[] boundaries;
+    int[] keyFields;
+    String[] boundariesStr;
+    
+    /**
+     * @param boundariesStr array of strings containing definitions of ranges boundaries
+     * @param record	DataRecord with the same structure as the one which will be used
+     * for determining output port.
+     */
+    public RangePartition(String[] boundariesStr){
+        this.boundariesStr=boundariesStr;
+    }
+    
+    public void init(int numPartitions, RecordKey partitionKey){
+        this.numPorts=numPartitions;
+        keyFields=partitionKey.getKeyFields();
+
+    }
+    
+    public int getOutputPort(DataRecord record){
+        // create boundaries the first time this method is called
+        if (boundaries==null){
+            boundaries = new DataField[boundariesStr.length];
+	        for (int i=0;i<boundaries.length;i++){
+	            boundaries[i]=record.getField(keyFields[0]).duplicate();
+	            boundaries[i].fromString(boundariesStr[i]);
+	        }
+	        Arrays.sort(boundaries);
+        }
+        
+        // use sequential search if number of boundries is small
+        if (boundaries.length <= 6) {
+            for (int i = 0; i < numPorts && i < boundaries.length; i++) {
+                // current boundary is upper limit inclusive
+                if (record.getField(keyFields[0]).compareTo(boundaries[i]) <= 0) {
+                    return i;
+                }
+            }
+            return numPorts - 1;
+        } else {
+            int index = Arrays.binarySearch(boundaries, record
+                    .getField(keyFields[0]));
+            // DEBUG
+            // System.out.println("Index partition: "+index+" value "+record
+            //         .getField(keyFields[0]).toString());
+            // DEBUG END
+            if (index>=0){
+                return index;
+            }else{
+                return (index*-1)-1;
+            }
+        }
+    }
+}
