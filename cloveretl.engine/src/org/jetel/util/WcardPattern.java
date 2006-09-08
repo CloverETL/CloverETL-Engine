d26209a60dd828d684b902290d6bf4159bc84316
otasanek <info@cloveretl.com>
2006-09-08 12:32:21 +0000

    proper comments
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1366 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/WcardPattern.java
+++ cloveretl.engine/src/org/jetel/util/WcardPattern.java
@@ -1,3 +1,22 @@
+/*
+*    jETeL/Clover - Java based ETL application framework.
+*    Copyright (C) 2006 Javlin Consulting <info@javlinconsulting>
+*    
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*    
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
+*    Lesser General Public License for more details.
+*    
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
 package org.jetel.util;
 
 import java.io.File;
@@ -7,7 +26,7 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 /**
- * @author Jan Hadrava
+ * @author Jan Hadrava, Javlin Consulting (www.javlinconsulting.cz)
  * 
  * Class generating collection of filenames which match
  * given wildcard patterns.
