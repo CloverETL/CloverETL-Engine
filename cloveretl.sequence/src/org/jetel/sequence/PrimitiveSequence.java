182d86c1eb3faa50393dce5b85cd33a34e8906f9
otasanek <otasanek@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-10-03 09:48:26 +0000

    New PrimitiveSequence object (1).
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1550 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.sequence/src/org/jetel/sequence/PrimitiveSequence.java
@@ -0,0 +1,186 @@
+/*
+*    jETeL/Clover - Java based ETL application framework.
+*    Copyright (C) 2005-06  Javlin Consulting <info@javlinconsulting.cz>
+*    
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*    
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
+*    Lesser General Public License for more details.
+*    
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
+package org.jetel.sequence;
+
+import org.jetel.data.sequence.Sequence;
+import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.exception.XMLConfigurationException;
+import org.jetel.graph.GraphElement;
+import org.jetel.graph.TransformationGraph;
+import org.jetel.util.ComponentXMLAttributes;
+import org.w3c.dom.Element;
+
+
+/**
+ * Simple class implementing Sequence interface. It uses internally "long" datatype to
+ * store sequence's value. The value is not persistent.<br>
+ * 
+ * @author Martin Zatopek, Javlin Consulting (www.javlinconsulting.cz)
+ *
+ */
+public class PrimitiveSequence extends GraphElement implements Sequence {
+
+    public final static String SEQUENCE_TYPE = "PRIMITIVE_SEQUENCE";
+
+    private static final String XML_NAME_ATTRIBUTE = "name";
+    private static final String XML_START_ATTRIBUTE = "start";
+    private static final String XML_STEP_ATTRIBUTE = "step";
+
+    private long value = 0;
+    
+    private long start = 0;
+    
+    private long step = 1;
+    
+    public PrimitiveSequence(String id, TransformationGraph graph, String name) {
+        super(id, graph, name);
+    }
+
+    /**
+     * @see org.jetel.graph.GraphElement#checkConfig()
+     */
+    @Override
+    public boolean checkConfig() {
+        return true;
+    }
+
+    /**
+     * @see org.jetel.graph.GraphElement#init()
+     */
+    @Override
+    public void init() throws ComponentNotReadyException {
+        //no op
+    }
+
+    /**
+     * @see org.jetel.graph.GraphElement#free()
+     */
+    @Override
+    public void free() {
+        //no op
+    }
+
+    /**
+     * @see org.jetel.data.sequence.Sequence#currentValueInt()
+     */
+    public int currentValueInt() {
+        return (int) value;
+    }
+
+    /**
+     * @see org.jetel.data.sequence.Sequence#nextValueInt()
+     */
+    public int nextValueInt() {
+        value += step;
+        return (int) value;
+    }
+
+    /**
+     * @see org.jetel.data.sequence.Sequence#currentValueLong()
+     */
+    public long currentValueLong() {
+        return value;
+    }
+
+    /**
+     * @see org.jetel.data.sequence.Sequence#nextValueLong()
+     */
+    public long nextValueLong() {
+        value += step;
+        return value;
+    }
+
+    /**
+     * @see org.jetel.data.sequence.Sequence#currentValueString()
+     */
+    public String currentValueString() {
+        return Long.toString(value);
+    }
+
+    /**
+     * @see org.jetel.data.sequence.Sequence#nextValueString()
+     */
+    public String nextValueString() {
+        value += step;
+        return Long.toString(value);
+    }
+
+    /**
+     * @see org.jetel.data.sequence.Sequence#reset()
+     */
+    public void reset() {
+        value = start;
+    }
+
+    /**
+     * @see org.jetel.data.sequence.Sequence#isPersistent()
+     */
+    public boolean isPersistent() {
+        return false;
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    /**
+     * Sets start value and resets this sequencer.
+     * @param start
+     */
+    public void setStart(long start) {
+        this.start = start;
+        reset();
+    }
+
+    public long getStep() {
+        return step;
+    }
+
+    /**
+     * Sets step value and resets this sequencer.
+     * @param step
+     */
+    public void setStep(long step) {
+        this.step = step;
+    }
+
+    static public PrimitiveSequence fromXML(TransformationGraph graph, Element nodeXML) throws XMLConfigurationException {
+        ComponentXMLAttributes xattribs = new ComponentXMLAttributes(nodeXML, graph);
+
+        try {
+            PrimitiveSequence seq =  new PrimitiveSequence(
+                xattribs.getString(XML_ID_ATTRIBUTE),
+                graph,
+                xattribs.getString(XML_NAME_ATTRIBUTE));
+            if(xattribs.exists(XML_START_ATTRIBUTE)) {
+                seq.setStart(xattribs.getInteger(XML_START_ATTRIBUTE));
+            }
+            if(xattribs.exists(XML_STEP_ATTRIBUTE)) {
+                seq.setStart(xattribs.getInteger(XML_STEP_ATTRIBUTE));
+            }
+            
+            return seq;
+        } catch(Exception ex) {
+            throw new XMLConfigurationException(SEQUENCE_TYPE + ":" + xattribs.getString(XML_ID_ATTRIBUTE," unknown ID ") + ":" + ex.getMessage(), ex);
+        }
+    }
+
+
+}
