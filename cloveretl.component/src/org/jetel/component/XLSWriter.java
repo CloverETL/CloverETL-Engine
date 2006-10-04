5b8e14733a54da6b290ad432d215881c427c9adf
agad <info@cloveretl.com>
2006-10-04 10:31:59 +0000

    new component for writing data to xls file check-in
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1564 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.component/src/org/jetel/component/XLSWriter.java
@@ -0,0 +1,133 @@
+
+/*
+*    jETeL/Clover - Java based ETL application framework.
+*    Copyright (C) 2005-06  Javlin Consulting <info@javlinconsulting.cz>
+*    
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*    
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
+*    Lesser General Public License for more details.
+*    
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
+
+package org.jetel.component;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.jetel.data.DataRecord;
+import org.jetel.data.formatter.DataFormatter;
+import org.jetel.data.formatter.XLSDataFormatter;
+import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.exception.XMLConfigurationException;
+import org.jetel.graph.InputPort;
+import org.jetel.graph.Node;
+import org.jetel.graph.TransformationGraph;
+import org.jetel.util.ComponentXMLAttributes;
+import org.jetel.util.SynchronizeUtils;
+import org.w3c.dom.Element;
+
+/**
+ * @author avackova
+ *
+ */
+public class XLSWriter extends Node {
+
+	private static final String XML_FILEURL_ATTRIBUTE = "fileURL";
+
+	public final static String COMPONENT_TYPE = "XLS_WRITER";
+	private final static int READ_FROM_PORT = 0;
+
+	private String fileURL;
+	private XLSDataFormatter formatter;
+
+	public XLSWriter(String id,String fileURL){
+		super(id);
+		this.fileURL = fileURL;
+		formatter = new XLSDataFormatter();
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.jetel.graph.Node#getType()
+	 */
+	@Override
+	public String getType() {
+		return COMPONENT_TYPE;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jetel.graph.Node#run()
+	 */
+	@Override
+	public void run() {
+		InputPort inPort = getInputPort(READ_FROM_PORT);
+		DataRecord record = new DataRecord(inPort.getMetadata());
+		record.init();
+		while (record != null && runIt) {
+			try {
+				record = inPort.readRecord(record);
+				if (record != null) {
+					formatter.write(record);
+				}
+			}
+			catch (IOException ex) {
+				resultMsg=ex.getMessage();
+				resultCode=Node.RESULT_ERROR;
+				closeAllOutputPorts();
+				return;
+			}
+			catch (Exception ex) {
+				resultMsg=ex.getClass().getName()+" : "+ ex.getMessage();
+				resultCode=Node.RESULT_FATAL_ERROR;
+				return;
+			}
+			SynchronizeUtils.cloverYield();
+		}
+		formatter.close();
+		if (runIt) resultMsg="OK"; else resultMsg="STOPPED";
+		resultCode=Node.RESULT_OK;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jetel.graph.GraphElement#checkConfig()
+	 */
+	@Override
+	public boolean checkConfig() {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jetel.graph.GraphElement#init()
+	 */
+	@Override
+	public void init() throws ComponentNotReadyException {
+		File out = new File(fileURL);
+		try {
+			if (!out.exists()){
+				out.createNewFile();
+			}
+			formatter.open(new FileOutputStream(out),getInputPort(READ_FROM_PORT).getMetadata());
+		}catch(IOException ex){
+			throw new ComponentNotReadyException(ex);
+		}
+	}
+
+	public static Node fromXML(TransformationGraph graph, Element nodeXML) throws XMLConfigurationException {
+		ComponentXMLAttributes xattribs = new ComponentXMLAttributes(nodeXML, graph);
+		try{
+			return new XLSWriter(xattribs.getString(XML_ID_ATTRIBUTE),xattribs.getString(XML_FILEURL_ATTRIBUTE));
+		} catch (Exception ex) {
+		    throw new XMLConfigurationException(COMPONENT_TYPE + ":" + xattribs.getString(XML_ID_ATTRIBUTE," unknown ID ") + ":" + ex.getMessage(),ex);
+		}
+	}
+}
