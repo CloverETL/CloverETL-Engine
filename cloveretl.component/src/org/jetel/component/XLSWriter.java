c914003c91a38214849130e7fc826ab18975bbcd
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-10-09 13:44:59 +0000

    added more attributes
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1600 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component/src/org/jetel/component/XLSWriter.java
+++ cloveretl.component/src/org/jetel/component/XLSWriter.java
@@ -22,11 +22,9 @@
 package org.jetel.component;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.jetel.data.DataRecord;
-import org.jetel.data.formatter.DataFormatter;
 import org.jetel.data.formatter.XLSDataFormatter;
 import org.jetel.exception.ComponentNotReadyException;
 import org.jetel.exception.XMLConfigurationException;
@@ -35,6 +33,7 @@ import org.jetel.graph.Node;
 import org.jetel.graph.TransformationGraph;
 import org.jetel.util.ComponentXMLAttributes;
 import org.jetel.util.SynchronizeUtils;
+import org.jetel.util.XLSUtils;
 import org.w3c.dom.Element;
 
 /**
@@ -45,18 +44,24 @@ public class XLSWriter extends Node {
 
 	private static final String XML_FILEURL_ATTRIBUTE = "fileURL";
 	private static final String XML_SAVENAMES_ATTRIBUTE = "saveNames";
+	private static final String XML_SHEETNAME_ATTRIBUTE = "sheetName";
+	private static final String XML_SHEETNUMBER_ATTRIBUTE = "sheetNumber";
+	private static final String XML_APPEND_ATTRIBUTE = "append";
+	private static final String XML_FIRSTDATAROW_ATTRIBUTE = "firstDataRow";
+	private static final String XML_FIRSTCOLUMN_ATTRIBUTE = "firstColumn";
+	private static final String XML_NAMESROW_ATTRIBUTE = "namesRow";
 
 	public final static String COMPONENT_TYPE = "XLS_WRITER";
 	private final static int READ_FROM_PORT = 0;
 
 	private String fileURL;
-	private boolean saveNames;
 	private XLSDataFormatter formatter;
+	private int namesRow;
 
-	public XLSWriter(String id,String fileURL, boolean saveNames){
+	public XLSWriter(String id,String fileURL, boolean saveNames, boolean append){
 		super(id);
 		this.fileURL = fileURL;
-		formatter = new XLSDataFormatter(saveNames);
+		formatter = new XLSDataFormatter(saveNames,append);
 	}
 	
 	/* (non-Javadoc)
@@ -118,7 +123,7 @@ public class XLSWriter extends Node {
 			if (!out.exists()){
 				out.createNewFile();
 			}
-			formatter.open(new FileOutputStream(out),getInputPort(READ_FROM_PORT).getMetadata());
+			formatter.open(out,getInputPort(READ_FROM_PORT).getMetadata());
 		}catch(IOException ex){
 			throw new ComponentNotReadyException(ex);
 		}
@@ -126,10 +131,44 @@ public class XLSWriter extends Node {
 
 	public static Node fromXML(TransformationGraph graph, Element nodeXML) throws XMLConfigurationException {
 		ComponentXMLAttributes xattribs = new ComponentXMLAttributes(nodeXML, graph);
+		XLSWriter xlsWriter;
 		try{
-			return new XLSWriter(xattribs.getString(XML_ID_ATTRIBUTE),xattribs.getString(XML_FILEURL_ATTRIBUTE),xattribs.getBoolean(XML_SAVENAMES_ATTRIBUTE,false));
+			xlsWriter = new XLSWriter(xattribs.getString(XML_ID_ATTRIBUTE),
+					xattribs.getString(XML_FILEURL_ATTRIBUTE),
+					xattribs.getBoolean(XML_SAVENAMES_ATTRIBUTE,false),
+					xattribs.getBoolean(XML_APPEND_ATTRIBUTE,false));
+			if (xattribs.exists(XML_SHEETNAME_ATTRIBUTE)){
+				xlsWriter.setSheetName(xattribs.getString(XML_SHEETNAME_ATTRIBUTE));
+			}
+			else if (xattribs.exists(XML_SHEETNUMBER_ATTRIBUTE)){
+				xlsWriter.setSheetNumber(xattribs.getInteger(XML_SHEETNUMBER_ATTRIBUTE));
+			}
+			xlsWriter.setFirstColumn(xattribs.getString(XML_FIRSTCOLUMN_ATTRIBUTE,"A"));
+			xlsWriter.setFirstRow(xattribs.getInteger(XML_FIRSTDATAROW_ATTRIBUTE,0));
+			xlsWriter.setNamesRow(xattribs.getInteger(XML_NAMESROW_ATTRIBUTE,0));
+			return xlsWriter;
 		} catch (Exception ex) {
 		    throw new XMLConfigurationException(COMPONENT_TYPE + ":" + xattribs.getString(XML_ID_ATTRIBUTE," unknown ID ") + ":" + ex.getMessage(),ex);
 		}
+	}
+
+	private void setSheetName(String sheetName) {
+		formatter.setSheetName(sheetName);
+	}
+
+	private void setSheetNumber(int sheetNumber) {
+		formatter.setSheetNumber(sheetNumber);
+	}
+	
+	private void setFirstColumn(String firstColumn){
+		formatter.setFirstColumn(firstColumn);
+	}
+	
+	private void setFirstRow(int firstRow){
+		formatter.setFirstRow(firstRow-1);
+	}
+	
+	private void setNamesRow(int namesRow){
+		formatter.setNamesRow(namesRow-1);
 	}
 }
