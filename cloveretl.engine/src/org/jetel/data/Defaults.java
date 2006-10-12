0f3cfcfed20c4f2ef608d686891f137aa76fd635
jhadrava <jhadrava@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-10-12 07:55:09 +0000

    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1635 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/data/Defaults.java
+++ cloveretl.engine/src/org/jetel/data/Defaults.java
@@ -24,10 +24,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import sun.misc.Regexp;
 
 /**
  *  Helper class which contains some framework-wide constants defitions.<br>
