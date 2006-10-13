f20f203dea0c2cd6b335012e3de8ed0a289ccfef
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-10-13 07:36:39 +0000

    added method public static String getCellCode(short cellNumber)
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1646 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/XLSUtils.java
+++ cloveretl.engine/src/org/jetel/util/XLSUtils.java
@@ -34,11 +34,11 @@ public class XLSUtils {
 	private final static int CELL_NUMBER_IN_SHEET = 'Z'-'A'+1;//number of "AA" cell in excel sheet
 	
 	/**
-	 * This method calculates xls colum number from it's code:
+	 * This method calculates xls colum number from its code:
 	 * 	A='A'-65=0,B='B'-65,....,Z='Z'-65=CELL_NUMBER_IN_SHEET-1,
 	 *  XYZ = ('X'-64)*CELL_NUMBER_IN_SHEET^2+('Y'-64)*CELL_NUMBER_IN_SHEET+('Z'-65)
 	 * @param cellCode
-	 * @return colum number from its code
+	 * @return column's number from its code
 	 * @throws InvalidNameException
 	 */
 	public static short getCellNum(String cellCode)throws InvalidNameException{
@@ -54,5 +54,23 @@ public class XLSUtils {
 		if (cellNumber<0) 
 			throw new InvalidNameException("Wrong column index: " + cellCode);
 		return (short)cellNumber;
+	}
+	
+	/**
+	 * This method finds xls column code from its number (0 based)
+	 * 
+	 * @param cellNumber
+	 * @return column's code from its number
+	 */
+	public static String getCellCode(short cellNumber){
+		//invert column number to CELL_NUMBER_IN_SHEET based form
+		String cellNumberXlsBase = Integer.toString(cellNumber,CELL_NUMBER_IN_SHEET);
+		StringBuffer cellCode = new StringBuffer(cellNumberXlsBase.length());
+		//each digit except last diminished by 1 ('A'=0) invert back to decimal form
+		for (short i=0;i<cellNumberXlsBase.length()-1;i++){
+			cellCode.append((char)(Integer.parseInt(cellNumberXlsBase.substring(i,i+1),CELL_NUMBER_IN_SHEET)-1+'A'));
+		}
+		cellCode.append((char)(Integer.parseInt(cellNumberXlsBase.substring(cellNumberXlsBase.length()-1,cellNumberXlsBase.length()),CELL_NUMBER_IN_SHEET)+'A'));
+		return cellCode.toString();
 	}
 }
