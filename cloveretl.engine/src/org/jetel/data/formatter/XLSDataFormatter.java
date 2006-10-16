6293272a35bd56939f2f3f676b7e20ada0b45cd0
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-10-16 12:39:12 +0000

    methods getCellcode a getCellNum moved from XLSUtils class
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1679 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/data/formatter/XLSDataFormatter.java
+++ cloveretl.engine/src/org/jetel/data/formatter/XLSDataFormatter.java
@@ -41,7 +41,6 @@ import org.jetel.data.primitive.Decimal;
 import org.jetel.exception.ComponentNotReadyException;
 import org.jetel.metadata.DataFieldMetadata;
 import org.jetel.metadata.DataRecordMetadata;
-import org.jetel.util.XLSUtils;
 
 /**
  * Writes records to xls sheet
@@ -53,6 +52,8 @@ import org.jetel.util.XLSUtils;
 *	@created October 10, 2006
  */
 public class XLSDataFormatter implements Formatter {
+
+	private final static int CELL_NUMBER_IN_SHEET = 'Z'-'A'+1;//number of "AA" cell in excel sheet
 
 	private HSSFWorkbook wb;
 	private HSSFSheet sheet;
@@ -119,7 +120,7 @@ public class XLSDataFormatter implements Formatter {
 			}
 		}
 		try {
-			firstColumn = XLSUtils.getCellNum(firstColumnIndex);
+			firstColumn = getCellNum(firstColumnIndex);
 		}catch(InvalidNameException ex){
 			throw new ComponentNotReadyException(ex);
 		}
@@ -279,5 +280,45 @@ public class XLSDataFormatter implements Formatter {
 
 	public int getSheetNumber() {
 		return sheetNumber;
+	}
+	/**
+	 * This method calculates xls colum number from its code:
+	 * 	A='A'-65=0,B='B'-65,....,Z='Z'-65=CELL_NUMBER_IN_SHEET-1,
+	 *  XYZ = ('X'-64)*CELL_NUMBER_IN_SHEET^2+('Y'-64)*CELL_NUMBER_IN_SHEET+('Z'-65)
+	 * @param cellCode
+	 * @return column's number from its code
+	 * @throws InvalidNameException
+	 */
+	public static short getCellNum(String cellCode)throws InvalidNameException{
+		char[] cellCodeU = cellCode.toUpperCase().toCharArray();
+		int length = cellCodeU.length;
+		int cellNumber = 0;
+		for (int j=0;j<length;j++){
+			if (cellCodeU[j]<'A' || cellCodeU[j]>'Z')
+				throw new InvalidNameException("Wrong column index: " + cellCode);
+			cellNumber = cellNumber*CELL_NUMBER_IN_SHEET + (cellCodeU[j]-64);
+		}
+		cellNumber-=1;
+		if (cellNumber<0) 
+			throw new InvalidNameException("Wrong column index: " + cellCode);
+		return (short)cellNumber;
+	}
+	
+	/**
+	 * This method finds xls column code from its number (0 based)
+	 * 
+	 * @param cellNumber
+	 * @return column's code from its number
+	 */
+	public static String getCellCode(short cellNumber){
+		//invert column number to CELL_NUMBER_IN_SHEET based form
+		String cellNumberXlsBase = Integer.toString(cellNumber,CELL_NUMBER_IN_SHEET);
+		StringBuffer cellCode = new StringBuffer(cellNumberXlsBase.length());
+		//each digit except last diminished by 1 ('A'=0) invert back to decimal form
+		for (short i=0;i<cellNumberXlsBase.length()-1;i++){
+			cellCode.append((char)(Integer.parseInt(cellNumberXlsBase.substring(i,i+1),CELL_NUMBER_IN_SHEET)-1+'A'));
+		}
+		cellCode.append((char)(Integer.parseInt(cellNumberXlsBase.substring(cellNumberXlsBase.length()-1,cellNumberXlsBase.length()),CELL_NUMBER_IN_SHEET)+'A'));
+		return cellCode.toString();
 	}
 }
