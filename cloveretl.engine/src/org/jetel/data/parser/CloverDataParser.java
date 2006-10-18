dbfe9a7e5a6453131dc5aec1dc0eaa3482f5377b
agad <info@cloveretl.com>
2006-10-18 13:00:08 +0000

    not used DataRecordTape
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1699 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/data/parser/CloverDataParser.java
+++ cloveretl.engine/src/org/jetel/data/parser/CloverDataParser.java
@@ -22,16 +22,20 @@
 package org.jetel.data.parser;
 
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.nio.LongBuffer;
+import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.jetel.data.DataRecord;
-import org.jetel.data.tape.DataRecordTape;
+import org.jetel.data.Defaults;
 import org.jetel.exception.ComponentNotReadyException;
 import org.jetel.exception.IParserExceptionHandler;
 import org.jetel.exception.JetelException;
@@ -49,21 +53,20 @@ import org.jetel.metadata.DataRecordMetadata;
 public class CloverDataParser implements Parser {
 	
 	private DataRecordMetadata metadata;
-	private DataRecordTape tape;
-	private FileChannel recordFile;
+	private InputStream in;
+	private ReadableByteChannel recordFile;
 	private ByteBuffer recordBuffer;
-	private RandomAccessFile indexFile;
-	private long index;
-	private long idx;
-	private int startRecord = 0;
-	private int counter;
+	private long index = 0;
+	private long idx = 0;
+	private int recordSize;
 	
 	private final static int LONG_SIZE_BYTES = 8;
-	private final static short BUFFER_CAPACITY = 100;
     private final static int LEN_SIZE_SPECIFIER = 4;
 
-	public int getStartRecord() {
-		return startRecord;
+	/**
+	 * @param fileurl
+	 */
+	public CloverDataParser() {
 	}
 
 	/* (non-Javadoc)
@@ -79,7 +82,6 @@ public class CloverDataParser implements Parser {
 	 * @see org.jetel.data.parser.Parser#skip(int)
 	 */
 	public int skip(int nRec) throws JetelException {
-		this.startRecord = nRec;
 		index = LONG_SIZE_BYTES * nRec;
 		return nRec;
 	}
@@ -90,29 +92,65 @@ public class CloverDataParser implements Parser {
 	public void open(Object in, DataRecordMetadata _metadata)
 			throws ComponentNotReadyException {
 		this.metadata = _metadata;
-		if (startRecord == 0) {
-			tape = new DataRecordTape((String)in,false,false);
-			try{
-				tape.open();
-			}catch(IOException ex){
-				throw new ComponentNotReadyException(ex);
+		String fileName;
+		try {
+			if (((String)in).endsWith(".zip")){
+				this.in = new ZipInputStream(new FileInputStream((String)in));
+				fileName = ((String)in).substring(((String)in).lastIndexOf(File.separator)+1,((String)in).lastIndexOf('.'));
+			}else{
+				this.in = new FileInputStream((String)in);
+				fileName  = ((String)in).substring(((String)in).lastIndexOf(File.separator)+1);
 			}
-			tape.addDataChunk();
-			tape.rewind();
-		}else{
-			try {
-				DataRecord dr = new DataRecord(metadata);
-				dr.init();
-				recordFile = new RandomAccessFile((String)in,"r").getChannel();
-				recordBuffer = ByteBuffer.allocateDirect((dr.getSizeSerialized()+LEN_SIZE_SPECIFIER)*BUFFER_CAPACITY);
-				indexFile = new RandomAccessFile((String)in + ".idx","r");
-				indexFile.seek(index);
-				idx = indexFile.readLong();
-				recordFile.position(idx);
-				counter = 0;
-			}catch(IOException ex){
-				throw new ComponentNotReadyException(ex);
+			recordFile = Channels.newChannel(this.in);
+			recordBuffer = ByteBuffer.allocateDirect(Defaults.DEFAULT_INTERNAL_IO_BUFFER_SIZE);
+			if (index > 0) {
+				if (((String)in).endsWith(".zip")){
+					ZipInputStream tmpIn = new ZipInputStream(new FileInputStream((String)in));
+		            ZipEntry entry;
+		            byte[] readBuffer = new byte[LONG_SIZE_BYTES];
+		            while((entry = tmpIn.getNextEntry()) != null) {
+		                if(entry.getName().equals("INDEX/"+fileName+".idx")) {
+		                	tmpIn.skip(index);
+		                	tmpIn.read(readBuffer);
+		                	break;
+		                }
+		            }
+		            idx = (((long)readBuffer[0] << 56) +
+		                    ((long)(readBuffer[1] & 255) << 48) +
+		                    ((long)(readBuffer[2] & 255) << 40) +
+		                    ((long)(readBuffer[3] & 255) << 32) +
+		                    ((long)(readBuffer[4] & 255) << 24) +
+		                    ((readBuffer[5] & 255) << 16) +
+		                    ((readBuffer[6] & 255) <<  8) +
+		                    ((readBuffer[7] & 255) <<  0));
+		            tmpIn.close();
+				}else{
+					File root = new File(((String)in)).getParentFile().getParentFile();
+					String filePath = root == null ? "" : root.getPath() + File.separator;
+					DataInputStream indexFile = new DataInputStream(new FileInputStream(
+							filePath + "INDEX" + File.separator + fileName + ".idx"));
+					indexFile.skip(index);
+					idx = indexFile.readLong();
+					indexFile.close();
+				}
 			}
+			if (((String)in).endsWith(".zip")) {
+	            ZipEntry entry;
+	            while((entry = ((ZipInputStream)this.in).getNextEntry()) != null) {
+	                if(entry.getName().equals("DATA/"+fileName)) {
+	                	break;
+	                }
+	            }
+			}
+			int i=0;
+			do {
+				recordBuffer.rewind();
+				recordFile.read(recordBuffer);
+				i++;
+			}while (i*Defaults.DEFAULT_INTERNAL_IO_BUFFER_SIZE < idx);
+			recordBuffer.position((int)idx%Defaults.DEFAULT_INTERNAL_IO_BUFFER_SIZE);
+		} catch (IOException ex) {
+			throw new ComponentNotReadyException(ex);
 		}
 	}
 
@@ -120,14 +158,9 @@ public class CloverDataParser implements Parser {
 	 * @see org.jetel.data.parser.Parser#close()
 	 */
 	public void close() {
-		try{
-			if (startRecord == 0) {
-				tape.close();
-			}else{
-				recordFile.close();
-				indexFile.close();
-			}
-		}catch(IOException ex){
+		try {
+			recordFile.close();
+		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 	}
@@ -135,32 +168,29 @@ public class CloverDataParser implements Parser {
 	/* (non-Javadoc)
 	 * @see org.jetel.data.parser.Parser#getNext(org.jetel.data.DataRecord)
 	 */
-	public DataRecord getNext(DataRecord record) throws JetelException {
-		try {
-			if (startRecord == 0) {
-				return getNextFromTape(record);
-			}else{
-				return getNextFromFile(record);
+	public DataRecord getNext(DataRecord record)throws JetelException{
+		if (recordBuffer.remaining() < LEN_SIZE_SPECIFIER) {
+			recordBuffer.compact();
+			try{
+				recordFile.read(recordBuffer);
+			}catch(IOException ex){
+				throw new JetelException(ex.getLocalizedMessage());
 			}
-		}catch(IOException ex){
-			throw new JetelException(ex.getLocalizedMessage());
+			recordBuffer.flip();
 		}
-	}
-	
-	private DataRecord getNextFromTape(DataRecord record) throws IOException{
-        if (tape.get(record)){
-            return record;
-        }else{
-            return null;
-        }
-	}
-	
-	private DataRecord getNextFromFile(DataRecord record)throws IOException{
-		if (counter % BUFFER_CAPACITY == 0){
-			recordFile.read(recordBuffer);
-			recordBuffer.rewind();
+		if (recordBuffer.remaining() < LEN_SIZE_SPECIFIER){
+			return null;
 		}
-		recordBuffer.position(recordBuffer.position()+LONG_SIZE_BYTES);
+		recordSize = recordBuffer.getInt();
+		if (recordSize > recordBuffer.remaining()){
+			recordBuffer.compact();
+			try{
+				recordFile.read(recordBuffer);
+			}catch(IOException ex){
+				throw new JetelException(ex.getLocalizedMessage());
+			}
+			recordBuffer.flip();
+		}
 		record.deserialize(recordBuffer);
 		return record;
 	}
