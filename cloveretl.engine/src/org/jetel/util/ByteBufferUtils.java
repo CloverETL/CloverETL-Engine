e5f0e75c3bbff5bf1ca5d164487be0cb1c820e48
agad <info@cloveretl.com>
2006-10-20 13:31:12 +0000

    added comments
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1739 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/ByteBufferUtils.java
+++ cloveretl.engine/src/org/jetel/util/ByteBufferUtils.java
@@ -1,3 +1,22 @@
+/*
+*    jETeL/Clover - Java based ETL application framework.
+*    Copyright (C) 2005-05  Javlin Consulting <info@javlinconsulting.cz>
+*    
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*    
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
+*    Lesser General Public License for more details.
+*    
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
 
 package org.jetel.util;
 
@@ -6,9 +25,29 @@ import java.nio.ByteBuffer;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
 
+/**
+ * This class provides static methods for working with ByteBuffer in association
+ *  with Channels
+ * 
+ * @author avackova (agata.vackova@javlinconsulting.cz) ; 
+ * (c) JavlinConsulting s.r.o.
+ *  www.javlinconsulting.cz
+ *
+ * @since Oct 20, 2006
+ *
+ */
 public class ByteBufferUtils {
 	
-	public static int flusch(ByteBuffer buffer, WritableByteChannel writer)
+	/**
+	 * This method flushes the buffer to the Channel and prepares it for next
+	 *  reading
+	 * 
+	 * @param buffer
+	 * @param writer
+	 * @return The number of bytes written, possibly zero
+	 * @throws IOException
+	 */
+	public static int flush(ByteBuffer buffer, WritableByteChannel writer)
 			throws IOException {
 		int write;
 		buffer.flip();
@@ -17,9 +56,19 @@ public class ByteBufferUtils {
 		return write;
 	}
 	
+	/**
+	 * This method reads new data to the buffer 
+	 * 
+	 * @param buffer
+	 * @param reader
+	 * @return The number of bytes written, possibly zero
+	 * @throws IOException
+	 */
 	public static int reload(ByteBuffer buffer, ReadableByteChannel reader) throws IOException{
 		int read;
-		buffer.compact();
+		if (buffer.position() != 0) {
+			buffer.compact();
+		}
 		read = reader.read(buffer);
 		buffer.flip();
 		return read;
