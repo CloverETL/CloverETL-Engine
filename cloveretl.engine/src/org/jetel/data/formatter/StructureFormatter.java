4d4f5c2107d14c6571bd4ad41148a8acab90337e
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-10-31 10:33:33 +0000

    changed name from StructureFormater to StructureFormatter
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1813 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/data/formatter/StructureFormatter.java
+++ cloveretl.engine/src/org/jetel/data/formatter/StructureFormatter.java
@@ -38,6 +38,9 @@ import org.jetel.util.ByteBufferUtils;
 import org.jetel.util.StringUtils;
 
 /**
+ * Outputs data record in form coherent with given mask. 
+ * Handles encoding of characters. Uses WriteableChannel.
+ * 
  * @author avackova (agata.vackova@javlinconsulting.cz) ; 
  * (c) JavlinConsulting s.r.o.
  *  www.javlinconsulting.cz
@@ -45,7 +48,7 @@ import org.jetel.util.StringUtils;
  * @since Oct 27, 2006
  *
  */
-public class StructureFormater implements Formatter {
+public class StructureFormatter implements Formatter {
 	
 	private DataRecordMetadata metadata;
 	private WritableByteChannel writer;
@@ -61,16 +64,20 @@ public class StructureFormater implements Formatter {
 	private String charSet = null;
 	
 
-	public StructureFormater(){
-		dataBuffer = ByteBuffer.allocateDirect(Defaults.DEFAULT_INTERNAL_IO_BUFFER_SIZE);
-		fieldBuffer = ByteBuffer.allocateDirect(Defaults.DataFormatter.FIELD_BUFFER_LENGTH);
+	/**
+	 * Constructor without parameters
+	 */
+	public StructureFormatter(){
 		encoder = Charset.forName(Defaults.DataFormatter.DEFAULT_CHARSET_ENCODER).newEncoder();
 		encoder.reset();
 	}
 	
-	public StructureFormater(String charEncoder){
-		dataBuffer = ByteBuffer.allocateDirect(Defaults.DEFAULT_INTERNAL_IO_BUFFER_SIZE);
-		fieldBuffer = ByteBuffer.allocateDirect(Defaults.DataFormatter.FIELD_BUFFER_LENGTH);
+	/**
+	 * Constructor
+	 * 
+	 * @param charEncoder charset for coding characters
+	 */
+	public StructureFormatter(String charEncoder){
 		charSet = charEncoder;
 		encoder = Charset.forName(charEncoder).newEncoder();
 		encoder.reset();
@@ -83,26 +90,33 @@ public class StructureFormater implements Formatter {
 			throws ComponentNotReadyException {
 		this.metadata = _metadata;
 
-		// create buffered output stream reader 
+		// create buffered output stream writer and buffers 
 		writer = (WritableByteChannel) out;
+		dataBuffer = ByteBuffer.allocateDirect(Defaults.DEFAULT_INTERNAL_IO_BUFFER_SIZE);
+		fieldBuffer = ByteBuffer.allocateDirect(Defaults.DataFormatter.FIELD_BUFFER_LENGTH);
 		
 		maskBytes = mask.getBytes();
 
+		//find places in mask where to put record field's values
 		List<DataFieldParams> maskAnalizeMap = new ArrayList<DataFieldParams>();
 		String fieldName;
 		int index ;
 		int startIndex = 0;
 		Map fields = metadata.getFieldNames();
 		do {
+			//find next '$' in mask
 			index = mask.indexOf('$',startIndex);
-			if (index > -1){
+			if (index > -1){//if found
+				//find end of field name
 				startIndex = StringUtils.findIdentifierEnd(mask,index + 1);
 				fieldName = mask.substring(index + 1,startIndex);
+				//if substring after '$' is a field name from metadata add it (with index in mask and its length) to list
 				if (fields.containsKey(fieldName)){
 					maskAnalizeMap.add(new DataFieldParams(fieldName,index, startIndex - index));
 				}
 			}
 		}while (index >-1);
+		//change list to array
 		maskAnalize = new DataFieldParams[maskAnalizeMap.size()];
 		maskAnalizeMap.toArray(maskAnalize);
 	}
@@ -124,12 +138,14 @@ public class StructureFormater implements Formatter {
 	 */
 	public void write(DataRecord record) throws IOException {
 		lastIndex = 0;
+		//for each record field which is in mask change its name to value
 		for (int i=0;i<maskAnalize.length;i++){
 			fieldName = maskAnalize[i].name;
 			index  = maskAnalize[i].index;
 			if (dataBuffer.remaining() < index - lastIndex){
 				flush();
 			}
+			//put to buffer bytes from mask from last field name to actual one
 			dataBuffer.put(maskBytes, lastIndex, index - lastIndex);
 			fieldBuffer.clear();
 			record.getField(fieldName).toByteBuffer(fieldBuffer, encoder);
@@ -161,6 +177,14 @@ public class StructureFormater implements Formatter {
 
 	public void setMask(String mask) {
 		this.mask = mask;
+	}
+
+	/**
+	 * Returns name of charset which is used by this formatter
+	 * @return Name of charset or null if none was specified
+	 */
+	public String getCharsetName() {
+		return(this.charSet);
 	}
 
 	class DataFieldParams {
