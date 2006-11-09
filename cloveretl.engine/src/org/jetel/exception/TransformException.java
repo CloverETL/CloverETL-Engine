96f41ac756ac554157b0d57078f29d92faf8852f
otasanek <info@cloveretl.com>
2006-11-09 10:57:25 +0000

    Initial commit of TransformException.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1860 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/src/org/jetel/exception/TransformException.java
@@ -0,0 +1,49 @@
+/*
+*    jETeL/Clover - Java based ETL application framework.
+*    Copyright (C) 2005-06  Javlin Consulting <info@javlinconsulting.cz>
+*    
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*    
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
+*    Lesser General Public License for more details.
+*    
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
+package org.jetel.exception;
+
+import org.jetel.component.RecordTransform;
+
+/**
+ * Exception is thrown in custom transformation function.
+ * @see RecordTransform
+ * @author Martin Zatopek (martin.zatopek@javlinconsulting.cz)
+ *         (c) Javlin Consulting (www.javlinconsulting.cz)
+ *
+ * @created 9.11.2006
+ */
+public class TransformException extends JetelException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public TransformException(String message) {
+        super(message);
+    }
+
+    public TransformException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    
+}
