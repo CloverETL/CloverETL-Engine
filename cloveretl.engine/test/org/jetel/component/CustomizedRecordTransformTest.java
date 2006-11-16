3a39a9b12479422c542188193c719bf7bea8c2ce
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-11-16 14:07:35 +0000

    new tests added
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1896 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/component/CustomizedRecordTransformTest.java
+++ cloveretl.engine/test/org/jetel/component/CustomizedRecordTransformTest.java
@@ -1,14 +1,20 @@
 
 package org.jetel.component;
 
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
 import org.jetel.data.DataRecord;
 import org.jetel.data.Defaults;
 import org.jetel.data.SetVal;
+import org.jetel.data.primitive.CloverDouble;
+import org.jetel.data.primitive.DecimalFactory;
+import org.jetel.data.primitive.Numeric;
 import org.jetel.data.sequence.Sequence;
 import org.jetel.exception.ComponentNotReadyException;
 import org.jetel.exception.TransformException;
@@ -33,6 +39,8 @@ public class CustomizedRecordTransformTest extends TestCase {
 	    Defaults.init();
 	    
         graph=new TransformationGraph();
+        
+        transform.setGraph(graph);
         
 		metadata=new DataRecordMetadata("in",DataRecordMetadata.DELIMITED_RECORD);
 		
@@ -64,6 +72,7 @@ public class CustomizedRecordTransformTest extends TestCase {
 		metaOut1.addField(new DataFieldMetadata("Age",DataFieldMetadata.NUMERIC_FIELD, "|"));
 		metaOut1.addField(new DataFieldMetadata("City",DataFieldMetadata.STRING_FIELD, "\n"));
 		metaOut1.addField(new DataFieldMetadata("Born",DataFieldMetadata.DATE_FIELD, "\n"));
+		metaOut1.getField("Born").setFormatStr("dd-MM-yyyy");
 		metaOut1.addField(new DataFieldMetadata("Value",DataFieldMetadata.INTEGER_FIELD, "\n"));
 
 		record = new DataRecord(metadata);
@@ -86,9 +95,11 @@ public class CustomizedRecordTransformTest extends TestCase {
 		SetVal.setInt(record,4,-999);
 		record1.getField("Value").setNull(true);
         
-        Sequence seq = new PrimitiveSequence("test",graph,"test");
-        graph.addSequence("test", seq);
+        Sequence seq = new PrimitiveSequence("ID",graph,"name");
+        graph.addSequence("ID", seq);
 		
+        graph.getGraphProperties().setProperty("WORKSPACE", "/home/avackova/workspace");
+        graph.getGraphProperties().setProperty("YourCity", "London");
 	}
 	
 	@Override
@@ -103,7 +114,7 @@ public class CustomizedRecordTransformTest extends TestCase {
 		transform.addFieldToFieldRule("${1.?a*}", "${1.*e}");
 		transform.addFieldToFieldRule("${out.0.Name}", "${in.0.0}");
 		transform.addFieldToFieldRule(2, "1.2");
-		transform.addFieldToFieldRule(0, 3, "${1.3}");
+		transform.addFieldToFieldRule(1, 3, "${1.3}");
 		transform.addFieldToFieldRule(0, "Value", "${in.1.Age}");
 		transform.addFieldToFieldRule("*.City", 0, 2);
 		transform.addFieldToFieldRule("{out1.3}", 0, 3);
@@ -126,12 +137,11 @@ public class CustomizedRecordTransformTest extends TestCase {
 		assertEquals(out.getField(0).getValue().toString(), record.getField(0).getValue().toString());
 		assertEquals(out.getField(1).getValue(), record.getField(1).getValue());
 		assertEquals(out.getField(2).getValue().toString(), record.getField(2).getValue().toString());
-		assertEquals(out.getField(3).getValue(), record1.getField(3).getValue());
 		assertEquals(out.getField(4).getValue(), ((Double)record1.getField(1).getValue()).intValue());
 		assertEquals(out1.getField(0).getValue().toString(), record1.getField(0).getValue().toString());
 		assertEquals(out1.getField(1).getValue(),0.0);
 		assertEquals(out1.getField(2).getValue().toString(), record.getField(2).getValue().toString());
-		assertEquals(out1.getField(3).getValue(), record.getField(3).getValue());
+		assertEquals(out1.getField(3).getValue(), record1.getField(3).getValue());
 		assertEquals(out1.getField(4).getValue(), record1.getField(4).getValue());
 		System.out.println(record.toString());
 		System.out.println(record1.toString());
@@ -139,11 +149,15 @@ public class CustomizedRecordTransformTest extends TestCase {
 		System.out.println(out1.toString());
 	}
 
-	public void test_fieldToConstant(){
+	public void test_constantToField(){
 		System.out.println("Constant to field test:");
-		transform.addFieldToFieldRule("*.Name", "Agata");
-		transform.addConstantToFieldRule(3, new GregorianCalendar(23,3,73).getTime());
-		transform.addConstantToFieldRule(0, 4, 45.55);
+		transform.addConstantToFieldRule("*.Name", "Agata");
+		transform.addConstantToFieldRule(3, new GregorianCalendar(1973,3,23).getTime());
+		transform.addConstantToFieldRule(0, 1, 45.55);
+		transform.addConstantToFieldRule("${out*.Valu?}", 100);
+		transform.addConstantToFieldRule(1, "Age", DecimalFactory.getDecimal(new BigDecimal("36474.738393")));
+		transform.addConstantToFieldRule("${out.*.2}", "Prague");
+		transform.addConstantToFieldRule(1, 3, "16-11-2006");
 		try {
 			transform.init(null, new DataRecordMetadata[]{metadata, metadata1}, 
 				new DataRecordMetadata[]{metaOut,metaOut1});
@@ -161,12 +175,81 @@ public class CustomizedRecordTransformTest extends TestCase {
 		}
 		assertEquals(out.getField(0).getValue().toString(), "Agata");
 		assertEquals(out1.getField(0).getValue().toString(), "Agata");
-		assertEquals(out.getField(3).getValue(), new GregorianCalendar(23,3,73).getTime());
-		assertEquals(out.getField(4).getValue().toString(), 45.55);
+		assertEquals(out.getField(3).getValue(), new GregorianCalendar(1973,3,23).getTime());
+		assertEquals(out.getField(1).getValue(), 45.55);
+		assertEquals(out1.getField(4).getValue(), 100);
+		assertEquals(((Double)out1.getField(1).getValue()), DecimalFactory.getDecimal(new BigDecimal("36474.738393")).getDouble());
+		assertEquals(out.getField(2).getValue().toString(), "Prague");
+		assertEquals(out1.getField(2).getValue().toString(), "Prague");
+		assertEquals(out1.getField(3).getValue(), new GregorianCalendar(2006,10,16).getTime());
 		System.out.println(record.toString());
 		System.out.println(record1.toString());
 		System.out.println(out.toString());
 		System.out.println(out1.toString());
 	}
 
+	public void test_sequenceToField(){
+		System.out.println("Sequence to field test:");
+		transform.addSequenceToFieldRule("*.Age", graph.getSequence("ID"));
+		transform.addSequenceToFieldRule(4, "ID");
+		transform.addSequenceToFieldRule("${1.Na*}", "ID.currentValueString()");
+		try {
+			transform.init(null, new DataRecordMetadata[]{metadata, metadata1}, 
+				new DataRecordMetadata[]{metaOut,metaOut1});
+		} catch (ComponentNotReadyException e) {
+			e.printStackTrace();
+		}
+		List<String> rules = transform.getResolvedRules();
+		for (Iterator<String> i = rules.iterator();i.hasNext();){
+			System.out.println(i.next());
+		}
+		try {
+			transform.transform(new DataRecord[]{record, record1}, new DataRecord[]{out,out1});
+		} catch (TransformException e) {
+			e.printStackTrace();
+		}
+		assertEquals(out.getField(1).getValue(), 1.0);
+		assertEquals(out1.getField(1).getValue(), 2.0);
+		assertEquals(out.getField(4).getValue(), 3);
+		assertEquals(out1.getField(0).getValue().toString(), "3");
+		System.out.println(record.toString());
+		System.out.println(record1.toString());
+		System.out.println(out.toString());
+		System.out.println(out1.toString());
+	}
+	
+	public void test_parameterToField(){
+		System.out.println("Parameter to field test:");
+		transform.addParameterToFieldRule("*.Age", "$ADULT");
+		transform.addParameterToFieldRule(1, 0, "${WORKSPACE}");
+		transform.addParameterToFieldRule(2, "MyCity");
+		transform.addParameterToFieldRule(1, "City", "YourCity");
+		Properties properties = new Properties();
+		properties.setProperty("$ADULT", "18");
+		properties.setProperty("$MyCity", "Prague");
+		try {
+			transform.init(properties, new DataRecordMetadata[]{metadata, metadata1}, 
+				new DataRecordMetadata[]{metaOut,metaOut1});
+		} catch (ComponentNotReadyException e) {
+			e.printStackTrace();
+		}
+		List<String> rules = transform.getResolvedRules();
+		for (Iterator<String> i = rules.iterator();i.hasNext();){
+			System.out.println(i.next());
+		}
+		try {
+			transform.transform(new DataRecord[]{record, record1}, new DataRecord[]{out,out1});
+		} catch (TransformException e) {
+			e.printStackTrace();
+		}
+		assertEquals((Double)out.getField(1).getValue(), DecimalFactory.getDecimal(properties.getProperty("$ADULT")).getDouble());
+		assertEquals((Double)out1.getField(1).getValue(), DecimalFactory.getDecimal(properties.getProperty("$ADULT")).getDouble());
+		assertEquals(out1.getField(0).getValue().toString(), graph.getGraphProperties().getStringProperty("WORKSPACE"));
+		assertEquals(out.getField(2).getValue().toString(), properties.getProperty("$MyCity"));
+		assertEquals(out1.getField(2).getValue().toString(), graph.getGraphProperties().getStringProperty("YourCity"));
+		System.out.println(record.toString());
+		System.out.println(record1.toString());
+		System.out.println(out.toString());
+		System.out.println(out1.toString());
+	}
 }
