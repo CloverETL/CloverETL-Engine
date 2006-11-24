4516a7a8efa503db1a578b909bc6e612277ce2df
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-11-24 13:26:43 +0000

    added method public void setValue(DataField _value) to DataField class
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@1976 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/data/NumericDataField.java
+++ cloveretl.engine/src/org/jetel/data/NumericDataField.java
@@ -192,9 +192,7 @@ public class NumericDataField extends DataField implements Numeric, Comparable {
             setValue((Number) _value);
         } else if (_value instanceof Decimal) {
             setValue((Decimal) _value);
-		} else if (_value instanceof DataField){
-			setValue(((DataField)_value).getValue());
-        } else {
+		} else {
 		    throw new BadDataFormatException(getMetadata().getName()+" field can not be set with this object - " +_value.toString(),_value.toString());
 		}
 	}
