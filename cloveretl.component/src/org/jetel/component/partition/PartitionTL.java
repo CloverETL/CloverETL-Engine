7f017242d0b9d8c19e433888fe3c2af009030bf7
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2006-12-01 13:41:52 +0000

    another implementation
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2024 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component/src/org/jetel/component/partition/PartitionTL.java
+++ cloveretl.component/src/org/jetel/component/partition/PartitionTL.java
@@ -27,6 +27,7 @@ import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.jetel.component.RecordTransformTL;
+import org.jetel.component.WrapperTL;
 import org.jetel.data.DataRecord;
 import org.jetel.data.RecordKey;
 import org.jetel.data.primitive.CloverInteger;
@@ -48,98 +49,38 @@ import org.jetel.metadata.DataRecordMetadata;
  */
 public class PartitionTL implements PartitionFunction {
 
+    public static final String INIT_FUNCTION_NAME="init";
     public static final String GETOUTPUTPORT_FUNCTION_NAME="getOutputPort";
+    
     
     private DataRecordMetadata metadata;
     private String srcCode;
     private Log logger;
-    private String errorMessage;
-    private TransformLangExecutor executor;
-    private CLVFFunctionDeclaration getOutputPortFunction, initFunction;
 	private Properties parameters;
+	private WrapperTL wrapper;
 
-    public PartitionTL(Log logger,String srcCode, Properties parameters) {
+    public PartitionTL(String srcCode, DataRecordMetadata metadata, 
+    		Properties parameters, Log logger) {
         this.srcCode=srcCode;
         this.logger=logger;
         this.parameters = parameters;
+        this.metadata = metadata;
+        wrapper = new WrapperTL(srcCode, metadata, parameters, logger);
     }
 
     /* (non-Javadoc)
 	 * @see org.jetel.component.partition.PartitionFunction#getOutputPort(org.jetel.data.DataRecord)
 	 */
 	public int getOutputPort(DataRecord record) {
-        executor.setInputRecords(new DataRecord[]{record});
-        
-        // execute getOutputPort function
-        executor.executeFunction(getOutputPortFunction,null);
-        
-        CloverInteger result=(CloverInteger)executor.getResult();
-            
-        return result.intValue();
+		return ((CloverInteger)wrapper.execute("getOutputPort", record)).getInt();
 	}
 
 	/* (non-Javadoc)
 	 * @see org.jetel.component.partition.PartitionFunction#init(int, org.jetel.data.RecordKey)
 	 */
 	public void init(int numPartitions, RecordKey partitionKey) throws ComponentNotReadyException{
-        CLVFStart parseTree=null;
-        TransformLangParser parser = new TransformLangParser(metadata,
-        		new ByteArrayInputStream(srcCode.getBytes()));
-        
-        try {
-            parseTree = parser.Start();
-            parseTree.init();
-        }catch(ParseException ex){
-            ex.printStackTrace();
-            logger.error(ex);
-            errorMessage=ex.getMessage();
-            throw new ComponentNotReadyException(errorMessage);
-        }catch(Exception ex){
-            ex.printStackTrace();
-            logger.error(ex);
-            errorMessage=ex.getMessage();
-            throw new ComponentNotReadyException(errorMessage);
-        }
-        
-        // log & report any parse exceptions
-        for(Iterator iter=parser.getParseExceptions().iterator();iter.hasNext();){
-            logger.error(iter.next());
-        }
-        
-        if (parser.getParseExceptions().size()>0){
-            errorMessage=((Exception)parser.getParseExceptions().get(0)).getMessage();
-            logger.error(errorMessage);
-            throw new ComponentNotReadyException(errorMessage);
-        }
-        
-        executor=new TransformLangExecutor(parameters);
-        getOutputPortFunction=(CLVFFunctionDeclaration)parser.getFunctions().get(GETOUTPUTPORT_FUNCTION_NAME);
-        initFunction=(CLVFFunctionDeclaration)parser.getFunctions().get(RecordTransformTL.INIT_FUNCTION_NAME);
-        if (getOutputPortFunction==null){
-            errorMessage="Function getOutputPort not declared/defined";
-            logger.error(errorMessage);
-            throw new ComponentNotReadyException(errorMessage);
-        }
-       
-        // execute global declarations, etc
-        try{
-            executor.visit(parseTree,null);
-        }catch (Exception ex){
-            logger.error(ex);
-            errorMessage=ex.getMessage();
-            throw new ComponentNotReadyException(errorMessage);
-        }
-        
-        //execute init transformFunction
-        if (initFunction!=null){
-            executor.executeFunction(initFunction,null); 
-        }
-        
+		wrapper.init();
+		wrapper.execute("init");
 	}
 
-    public void setMetadata(DataRecordMetadata metadata) {
-		this.metadata = metadata;
-	}
-
-	
 }
