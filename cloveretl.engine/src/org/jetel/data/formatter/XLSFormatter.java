596847a0baa23aed878339f67689497764bfa48a
agad <info@cloveretl.com>
2007-01-15 14:13:21 +0000

    MINOR:comments updated
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2299 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/data/formatter/XLSFormatter.java
+++ cloveretl.engine/src/org/jetel/data/formatter/XLSFormatter.java
@@ -1,3 +1,22 @@
+/*
+*    jETeL/Clover - Java based ETL application framework.
+*    Copyright (C) 2005-06  Javlin Consulting <info@javlinconsulting.cz>
+*    
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*    
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
+*    Lesser General Public License for more details.
+*    
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
 
 package org.jetel.data.formatter;
 
@@ -8,6 +27,25 @@ import javax.naming.InvalidNameException;
 import org.jetel.exception.ComponentNotReadyException;
 import org.jetel.metadata.DataRecordMetadata;
 
+/**
+ * Formats records for xls file. Order of method calling:
+ * <li>init(DataRecordMetadata)</li>
+ * <li>setDataTarget(File) </li>
+ * <li><i>setSheetName(String)</i> or <i>setSheetNumber(int)</i> - optional</li>
+ * <li><i>setFirstRow(int)</i> - optional</li>
+ * <li><i>setFirstColumn(int)</i> - optional</li>
+ * <li><i>setNamesRow(int)</i> - optional</li>
+ * <li>prepareSheet()</li>
+ * <li>write(DataRecord)</li>
+ * <li>close()</li>
+ * 
+ * @author avackova (agata.vackova@javlinconsulting.cz) ; 
+ * (c) JavlinConsulting s.r.o.
+ *  www.javlinconsulting.cz
+ *
+ * @since Jan 15, 2007
+ *
+ */
 public abstract class XLSFormatter implements Formatter {
 	
 	protected final static int CELL_NUMBER_IN_SHEET = 'Z'-'A'+1;//number of "AA" cell in excel sheet
@@ -24,24 +62,55 @@ public abstract class XLSFormatter implements Formatter {
 	protected int firstColumn;
 	protected boolean savedNames = false;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param append indicates if append data to existing xls sheet or replace 
+	 * them by new data 
+	 */
 	public XLSFormatter(boolean append){
 		this.append = append;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.jetel.data.formatter.Formatter#init(org.jetel.metadata.DataRecordMetadata)
+	 */
 	public void init(DataRecordMetadata _metadata) throws ComponentNotReadyException{
 		this.metadata = _metadata;
 	}
 
+	/**
+	 * Prepares sheet in xls file, which was set by method setSheetName(String sheetName)
+	 *  or setSheetNumber(int sheetNumber) for saving data. If any of above methods wasn't
+	 *  called there is created new sheet with default name.
+	 */
 	public abstract void prepareSheet();
 
+	/**
+	 * Set name of sheet, data will be written to. It has higher prioryty then
+	 * setSheetNumber method
+	 * 
+	 * @param sheetName
+	 */
 	public void setSheetName(String sheetName) {
 		this.sheetName = sheetName;
 	}
 
+	/**
+	 * Set number of sheet, data will be written to. If there was called method
+	 * setSheetName before calling prepareSheet, this number will be ignored.
+	 * 
+	 * @param sheetNumber
+	 */
 	public void setSheetNumber(int sheetNumber) {
 		this.sheetNumber = sheetNumber;
 	}
 	
+	/**
+	 * Sets the number of row (0-based), for first data record.
+	 * 
+	 * @param firstRow
+	 */
 	public void setFirstRow(int firstRow){
 		this.firstRow = firstRow;
 	}
