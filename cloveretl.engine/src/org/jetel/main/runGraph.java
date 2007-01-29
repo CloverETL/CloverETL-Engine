55de04c5a024a9c65a1c27afc3b14bb44f907b8c
dpavlis <dpavlis@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2007-01-29 14:56:27 +0000

    UPDATE:added reporting of graph Abort caused by user action
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2390 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/main/runGraph.java
+++ cloveretl.engine/src/org/jetel/main/runGraph.java
@@ -155,132 +155,150 @@ public class runGraph {
 	 * @param  args  Description of the Parameter
 	 */
 	public static void main(String args[]) {
-		boolean verbose = false;
+        boolean verbose = false;
         boolean loadFromSTDIN = false;
-		Properties properties = new Properties();
-		int trackingInterval=-1;
-		String pluginsRootDirectory = null;
+        Properties properties = new Properties();
+        int trackingInterval = -1;
+        String pluginsRootDirectory = null;
         String password = null;
         String logHost = null;
-		
-		System.out.println("***  CloverETL framework/transformation graph runner ver "+RUN_GRAPH_VERSION+", (c) 2002-06 D.Pavlis, released under GNU Lesser General Public License  ***");
-		System.out.println(" Running with framework version: "+JetelVersion.MAJOR_VERSION+"."+JetelVersion.MINOR_VERSION+" build#"+JetelVersion.BUILD_NUMBER+" compiled "+JetelVersion.LIBRARY_BUILD_DATETIME);
-		System.out.println();
-        
-		if (args.length < 1) {
-			printHelp();
-			System.exit(-1);
-		}
-        
-		// process command line arguments
-		for (int i = 0; i < args.length; i++) {
-			if (args[i].startsWith(VERBOSE_SWITCH)) {
-				verbose = true;
-			}else if (args[i].startsWith(PROPERTY_FILE_SWITCH)){
-				i++;
-				try {
-					InputStream inStream = new BufferedInputStream(new FileInputStream(args[i]));
-					properties.load(inStream);
-				} catch (IOException ex) {
-					logger.error(ex.getMessage(), ex);
-					System.exit(-1);
-				}
-			}else if (args[i].startsWith(PROPERTY_DEFINITION_SWITCH)){
-			   	//String[] nameValue=args[i].replaceFirst(PROPERTY_DEFINITION_SWITCH,"").split("=");
-				//properties.setProperty(nameValue[0],nameValue[1]);
-		    	String tmp =  args[i].replaceFirst(PROPERTY_DEFINITION_SWITCH,"");
-        	    properties.setProperty(tmp.substring(0,tmp.indexOf("=")),tmp.substring(tmp.indexOf("=") +1)); 
-			}else if (args[i].startsWith(TRACKING_INTERVAL_SWITCH)) {
-				i++;
-                try{
-                    trackingInterval = Integer.parseInt(args[i]);
-                }catch(NumberFormatException ex){
-                    System.err.println("Invalid tracking parameter: \""+args[i]+"\"");
+
+        System.out
+                .println("***  CloverETL framework/transformation graph runner ver "
+                        + RUN_GRAPH_VERSION
+                        + ", (c) 2002-06 D.Pavlis, released under GNU Lesser General Public License  ***");
+        System.out.println(" Running with framework version: "
+                + JetelVersion.MAJOR_VERSION + "." + JetelVersion.MINOR_VERSION
+                + " build#" + JetelVersion.BUILD_NUMBER + " compiled "
+                + JetelVersion.LIBRARY_BUILD_DATETIME);
+        System.out.println();
+
+        if (args.length < 1) {
+            printHelp();
+            System.exit(-1);
+        }
+
+        // process command line arguments
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith(VERBOSE_SWITCH)) {
+                verbose = true;
+            } else if (args[i].startsWith(PROPERTY_FILE_SWITCH)) {
+                i++;
+                try {
+                    InputStream inStream = new BufferedInputStream(
+                            new FileInputStream(args[i]));
+                    properties.load(inStream);
+                } catch (IOException ex) {
+                    logger.error(ex.getMessage(), ex);
                     System.exit(-1);
                 }
-			}else if (args[i].startsWith(INFO_SWITCH)){
-			    printInfo();
-			    System.exit(0);
-            }else if (args[i].startsWith(PLUGINS_SWITCH)){
+            } else if (args[i].startsWith(PROPERTY_DEFINITION_SWITCH)) {
+                // String[]
+                // nameValue=args[i].replaceFirst(PROPERTY_DEFINITION_SWITCH,"").split("=");
+                // properties.setProperty(nameValue[0],nameValue[1]);
+                String tmp = args[i].replaceFirst(PROPERTY_DEFINITION_SWITCH,
+                        "");
+                properties.setProperty(tmp.substring(0, tmp.indexOf("=")), tmp
+                        .substring(tmp.indexOf("=") + 1));
+            } else if (args[i].startsWith(TRACKING_INTERVAL_SWITCH)) {
+                i++;
+                try {
+                    trackingInterval = Integer.parseInt(args[i]);
+                } catch (NumberFormatException ex) {
+                    System.err.println("Invalid tracking parameter: \""
+                            + args[i] + "\"");
+                    System.exit(-1);
+                }
+            } else if (args[i].startsWith(INFO_SWITCH)) {
+                printInfo();
+                System.exit(0);
+            } else if (args[i].startsWith(PLUGINS_SWITCH)) {
                 i++;
                 pluginsRootDirectory = args[i];
-            }else if (args[i].startsWith(PASSWORD_SWITCH)){
+            } else if (args[i].startsWith(PASSWORD_SWITCH)) {
                 i++;
-                password = args[i]; 
-            }else if (args[i].startsWith(LOAD_FROM_STDIN_SWITCH)){
-                loadFromSTDIN=true;
-            }else if (args[i].startsWith(LOG_HOST_SWITCH)){
+                password = args[i];
+            } else if (args[i].startsWith(LOAD_FROM_STDIN_SWITCH)) {
+                loadFromSTDIN = true;
+            } else if (args[i].startsWith(LOG_HOST_SWITCH)) {
                 i++;
-                logHost = args[i]; 
-            }else if (args[i].startsWith("-")) {
-				System.err.println("Unknown option: "+args[i]);
-				System.exit(-1);
+                logHost = args[i];
+            } else if (args[i].startsWith("-")) {
+                System.err.println("Unknown option: " + args[i]);
+                System.exit(-1);
             }
-		}
+        }
 
-        //setup log4j appenders
-        if(logHost != null) {
+        // setup log4j appenders
+        if (logHost != null) {
             String[] hostAndPort = logHost.split(":");
-            if(hostAndPort[0].length() == 0 || hostAndPort.length > 2) {
-                System.err.println("Invalid log destination, i.e. -loghost localhost:4445");
+            if (hostAndPort[0].length() == 0 || hostAndPort.length > 2) {
+                System.err
+                        .println("Invalid log destination, i.e. -loghost localhost:4445");
                 System.exit(-1);
             }
             int port = 4445;
             try {
-                if(hostAndPort.length == 2) {
+                if (hostAndPort.length == 2) {
                     port = Integer.parseInt(hostAndPort[1]);
                 }
-            } catch(NumberFormatException e) {
-                System.err.println("Invalid log destination, i.e. -loghost localhost:4445");
+            } catch (NumberFormatException e) {
+                System.err
+                        .println("Invalid log destination, i.e. -loghost localhost:4445");
                 System.exit(-1);
             }
-            Logger.getRootLogger().addAppender(new SocketAppender(hostAndPort[0], port));
+            Logger.getRootLogger().addAppender(
+                    new SocketAppender(hostAndPort[0], port));
         }
 
-        //engine initialization - should be called only once
+        // engine initialization - should be called only once
         runGraph.initEngine(pluginsRootDirectory, password);
-        
-		//prapere input stream with XML graph definition
+
+        // prapere input stream with XML graph definition
         InputStream in = null;
         if (loadFromSTDIN) {
             System.out.println("Graph definition loaded from STDIN");
             in = System.in;
         } else {
-            System.out.println("Graph definition file: " + args[args.length - 1]);
-            try{
+            System.out.println("Graph definition file: "
+                    + args[args.length - 1]);
+            try {
                 URL fileURL = FileUtils.getFileURL(null, args[args.length - 1]);
                 in = fileURL.openStream();
             } catch (IOException e) {
-                System.err.println("Error - graph definition file can't be read: " + e.getMessage());
+                System.err
+                        .println("Error - graph definition file can't be read: "
+                                + e.getMessage());
                 System.exit(-1);
             }
         }
-        
-        //loading graph from the input stream
+
+        // loading graph from the input stream
         TransformationGraph graph = null;
         try {
             graph = runGraph.loadGraph(in, properties);
 
-            //check graph elements configuration
+            // check graph elements configuration
             logger.info("Checking graph configuration...");
             ConfigurationStatus status = graph.checkConfig(null);
             status.log();
 
-            if(!graph.init()) {
-                throw new GraphConfigurationException("Graph initialization failed.");
+            if (!graph.init()) {
+                throw new GraphConfigurationException(
+                        "Graph initialization failed.");
             }
 
             if (verbose) {
-                //this can be called only after graph.init()
+                // this can be called only after graph.init()
                 graph.dumpGraphConfiguration();
             }
-        }catch(XMLConfigurationException ex){
+        } catch (XMLConfigurationException ex) {
             logger.error("Error in reading graph from XML !", ex);
             if (verbose) {
                 ex.printStackTrace(System.err);
             }
             System.exit(-1);
-        }catch(GraphConfigurationException ex){
+        } catch (GraphConfigurationException ex) {
             logger.error("Error - graph's configuration invalid !", ex);
             if (verbose) {
                 ex.printStackTrace(System.err);
@@ -293,35 +311,42 @@ public class runGraph {
             }
             System.exit(-1);
         }
-        
-        // set tracking interval
-		if(trackingInterval!=-1){
-			graph.setTrackingInterval(trackingInterval*1000);
-		}
-		
-		//	start all Nodes (each node is one thread)
-		Result result=Result.N_A;
-		try {
-            result = graph.run();
-		} catch (RuntimeException ex) {
-			System.err.println("Fatal error during graph run !");
-			System.err.println(ex.getCause().getMessage());
-			if (verbose) {
-				ex.printStackTrace();
-			}
-			System.exit(-1);
-		}
-		if (result==Result.FINISHED_OK) {
-			// everything O.K.
-			System.out.println("Execution of graph successful !");
-			System.exit(0);
-		} else {
-			// something FAILED !!
-			System.err.println("Execution of graph failed !");
-			System.exit(result.code());
-		}
 
-	}
+        // set tracking interval
+        if (trackingInterval != -1) {
+            graph.setTrackingInterval(trackingInterval * 1000);
+        }
+
+        // start all Nodes (each node is one thread)
+        Result result = Result.N_A;
+        try {
+            result = graph.run();
+        } catch (RuntimeException ex) {
+            System.err.println("Fatal error during graph run !");
+            System.err.println(ex.getCause().getMessage());
+            if (verbose) {
+                ex.printStackTrace();
+            }
+            System.exit(-1);
+        }
+        switch (result) {
+
+        case FINISHED_OK:
+            // everything O.K.
+            System.out.println("Execution of graph successful !");
+            System.exit(0);
+            break;
+        case ABORTED:
+            // execution was ABORTED !!
+            System.err.println("Execution of graph aborted !");
+            System.exit(result.code());
+            break;
+        default:
+            System.err.println("Execution of graph failed !");
+            System.exit(result.code());
+        }
+
+    }
     
     
 	private static void printHelp() {
