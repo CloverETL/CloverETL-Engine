75b0957f284720b42d3b8568cc48a17163d3833e
otasanek <info@cloveretl.com>
2007-01-31 09:05:43 +0000

    UPDATE: removes implementation in checkConfig method of JmsReader and JmsWriter components.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2404 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component/src/org/jetel/component/JmsWriter.java
+++ cloveretl.component/src/org/jetel/component/JmsWriter.java
@@ -312,35 +312,35 @@ public class JmsWriter extends Node {
 		checkInputPorts(status, 1, 1);
         checkOutputPorts(status, 0, 0);
 
-        try {
+//        try {
         	
-    		if (psor == null && psorClass == null && psorCode == null) {
-    			throw new ComponentNotReadyException("Message processor not specified");
-    		}
-    		IConnection c = getGraph().getConnection(conId);
-    		if (c == null || !(c instanceof JmsConnection)) {
-    			throw new ComponentNotReadyException("Specified connection '" + conId + "' doesn't seem to be a JMS connection");
-    		}
-
-    		connection = (JmsConnection)c;
-    		inPort = getInputPort(0);
-     		try {
-    			connection.init();
-    			producer = connection.createProducer();
-    		} catch (Exception e) {
-    			throw new ComponentNotReadyException("Unable to initialize JMS consumer: " + e.getMessage());
-    		}
+//    		if (psor == null && psorClass == null && psorCode == null) {
+//    			throw new ComponentNotReadyException("Message processor not specified");
+//    		}
+//    		IConnection c = getGraph().getConnection(conId);
+//    		if (c == null || !(c instanceof JmsConnection)) {
+//    			throw new ComponentNotReadyException("Specified connection '" + conId + "' doesn't seem to be a JMS connection");
+//    		}
+//
+//    		connection = (JmsConnection)c;
+//    		inPort = getInputPort(0);
+//     		try {
+//    			connection.init();
+//    			producer = connection.createProducer();
+//    		} catch (Exception e) {
+//    			throw new ComponentNotReadyException("Unable to initialize JMS consumer: " + e.getMessage());
+//    		}
         	
         	
 //            init();
 //            free();
-        } catch (ComponentNotReadyException e) {
-            ConfigurationProblem problem = new ConfigurationProblem(e.getMessage(), ConfigurationStatus.Severity.ERROR, this, ConfigurationStatus.Priority.NORMAL);
-            if(!StringUtils.isEmpty(e.getAttributeName())) {
-                problem.setAttributeName(e.getAttributeName());
-            }
-            status.add(problem);
-        }
+//        } catch (ComponentNotReadyException e) {
+//            ConfigurationProblem problem = new ConfigurationProblem(e.getMessage(), ConfigurationStatus.Severity.ERROR, this, ConfigurationStatus.Priority.NORMAL);
+//            if(!StringUtils.isEmpty(e.getAttributeName())) {
+//                problem.setAttributeName(e.getAttributeName());
+//            }
+//            status.add(problem);
+//        }
         
         return status;
 	}
