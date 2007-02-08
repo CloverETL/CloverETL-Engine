e8056642beb262dd4b656d2d10313fe4c042e135
agad <info@cloveretl.com>
2007-02-08 08:28:24 +0000

    UPDATE:updated due to changes in engine
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2457 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.connection/test/SQLDataParserTest.java
+++ cloveretl.connection/test/SQLDataParserTest.java
@@ -1,5 +1,4 @@
 
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -8,6 +7,7 @@ import org.jetel.connection.SQLDataParser;
 import org.jetel.data.DataRecord;
 import org.jetel.exception.BadDataFormatException;
 import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.main.runGraph;
 import org.jetel.metadata.DataRecordMetadata;
 import org.jetel.metadata.DataRecordMetadataXMLReaderWriter;
 
@@ -15,80 +15,91 @@ import junit.framework.TestCase;
 
 /**
  * @author maciorowski
- *
+ * 
  */
-public class SQLDataParserTest  extends TestCase {
-private SQLDataParser aParser2 = null;
-private DataRecord record;
-	
-protected void setUp() { 
-	DataRecordMetadata metadata = null;
-	DataRecordMetadataXMLReaderWriter xmlReader = new DataRecordMetadataXMLReaderWriter();
-	DBConnection aDBConnection = null;
-				
-	try {
-//		metadata = xmlReader.read(new FileInputStream("config\\test\\rec_def\\db_null_def_rec.xml"));
-//		aDBConnection = new DBConnection("", "config\\test\\msaccess.clover_test.txt");
-		metadata = xmlReader.read(new FileInputStream("../cloveretl.engine/config/test/rec_def/db_def_rec.xml"));
-		aDBConnection = new DBConnection("conn", "../cloveretl.engine/examples/koule_postgre.cfg");
-	} catch(FileNotFoundException e){
-		e.printStackTrace();
-	}
-	
-	record = new DataRecord(metadata);
-	record.init();
+public class SQLDataParserTest extends TestCase {
+	private SQLDataParser aParser2 = null;
 
-	aParser2 = new SQLDataParser("connection","SELECT * FROM bad");
-	try {
-		aParser2.init(metadata);
-		aParser2.setDataSource(aDBConnection);
-		aParser2.initSQLDataMap(record);
-	} catch (ComponentNotReadyException e1) {
-		e1.printStackTrace();
-	}
-}
-	
-   protected void tearDown() {
-	   aParser2.close();
-	   aParser2 = null;
-	   record  = null;
-   }
+	private DataRecord record;
 
-public void test_parsing() {
-// the content of the test file
-//	  N/AStone          01/11/93
-//	-15.5          112  11/03/02
-//	 -0.7Bone Broo    99        
-	int recCount = 0;
-	try{
-		while((record=aParser2.getNext(record))!=null){
-			if(recCount==0) {
-				assertEquals(record.getField(0).toString(),"1.0");
-				assertTrue(record.getField(1).isNull());
-				assertEquals(record.getField(2).toString(),"101");
-				assertEquals(record.getField(3).toString(),"1993-01-11 00:00:00");
-			} else if(recCount==1) {
-				assertEquals(record.getField(0).toString(),"-15.5");
-				assertTrue(record.getField(3).isNull());
-				assertEquals(record.getField(2).toString(),"112");
-				assertEquals(record.getField(1).toString(),"Brook");
-			} else if(recCount==2) {
-				assertTrue(record.getField(0).isNull());
-				assertEquals(record.getField(1).toString(),"Bone Broo");
-				assertTrue(record.getField(2).isNull());
-				assertEquals(record.getField(3).toString(),"2003-01-01 00:00:00");
-			}
-			recCount++;
+	protected void setUp() {
+		DataRecordMetadata metadata = null;
+		DataRecordMetadataXMLReaderWriter xmlReader = new DataRecordMetadataXMLReaderWriter();
+		DBConnection aDBConnection = null;
+
+		runGraph.initEngine("../cloveretl.engine/plugins", null);
+		try {
+			// metadata = xmlReader.read(new
+			// FileInputStream("config\\test\\rec_def\\db_null_def_rec.xml"));
+			// aDBConnection = new DBConnection("",
+			// "config\\test\\msaccess.clover_test.txt");
+			metadata = xmlReader.read(new FileInputStream(
+					"../cloveretl.engine/config/test/rec_def/db_def_rec.xml"));
+			aDBConnection = new DBConnection("conn",
+					"../cloveretl.engine/examples/koule_postgre.cfg");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-	} catch (BadDataFormatException e){	
-		fail("Should not raise an BadDataFormatException");
-		e.printStackTrace();
-	} catch (Exception ee){
-		fail("Should not throw Exception");
-		ee.printStackTrace();
+
+		try {
+			aDBConnection.init();
+		} catch (ComponentNotReadyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		record = new DataRecord(metadata);
+		record.init();
+
+		aParser2 = new SQLDataParser("connection", "SELECT * FROM good");
+		try {
+			aParser2.init(metadata);
+			aParser2.setDataSource(aDBConnection);
+			aParser2.initSQLDataMap(record);
+		} catch (ComponentNotReadyException e1) {
+			e1.printStackTrace();
+		}
 	}
-   assertEquals(3,recCount);
-}
-	
+
+	protected void tearDown() {
+		aParser2.close();
+		aParser2 = null;
+		record = null;
+	}
+
+	public void test_parsing() {
+		int recCount = 0;
+		try {
+			while ((record = aParser2.getNext(record)) != null) {
+				if (recCount == 0) {
+					assertEquals(record.getField(0).toString(), "1.0");
+					assertEquals(record.getField(1).toString(), "Stone");
+					assertEquals(record.getField(2).toString(), "101");
+					assertEquals(record.getField(3).toString(),
+							"1993-01-11 00:00:00");
+				} else if (recCount == 2) {
+					assertEquals(record.getField(0).toString(), "-15.5");
+					assertEquals(record.getField(2).toString(), "112");
+					assertEquals(record.getField(1).toString(), "Brook");
+					assertEquals(record.getField(3).toString(),
+							"2002-11-03 00:00:00");
+				} else if (recCount == 3) {
+					assertEquals(record.getField(0).toString(), "-0.7");
+					assertEquals(record.getField(1).toString(), "Bone Broo");
+					assertEquals(record.getField(2).toString(), "99");
+					assertEquals(record.getField(3).toString(),
+							"1993-01-01 00:00:00");
+				}
+				recCount++;
+			}
+		} catch (BadDataFormatException e) {
+			fail("Should not raise an BadDataFormatException");
+			e.printStackTrace();
+		} catch (Exception ee) {
+			fail("Should not throw Exception");
+			ee.printStackTrace();
+		}
+		assertEquals(4, recCount);
+	}
 
 }
