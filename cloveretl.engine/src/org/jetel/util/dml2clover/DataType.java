dba2d0d170476cf3273f9178e1fab169ffe7468e
jausperger <jausperger@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2007-02-20 13:36:20 +0000

    INIT: create metadata from create statement
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2492 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/src/org/jetel/util/dml2clover/DataType.java
@@ -0,0 +1,19 @@
+package org.jetel.util.dml2clover;
+public final class DataType {
+
+    /** Data type, based on java.sql.Types */
+    public final char type;
+
+    /** Used when data type has fixed length */
+    public final Number length;
+
+    /** Used when data type has scale */
+    public final Number scale;
+
+    public DataType(char type, Number length, Number scale) {
+        this.type = type;
+        this.length = length;
+        this.scale = scale;
+    }
+
+}
