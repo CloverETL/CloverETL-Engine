fd091314b23acc8c00e2a0658849cd4454152333
otasanek <info@cloveretl.com>
2007-03-06 09:00:23 +0000

    UPDATE: better run scripts implementation.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2555 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/run.sh
+++ cloveretl.engine/run.sh
@@ -6,14 +6,13 @@
 # ***** set this before using script *****
 JAVA_HOME="/opt/jdk1.5"
 ENGINE_PATH="."
-LIBS_PATH="./lib"
 TRANSFORM_PATH="."
 # ****************************************
 
 ENGINE_JAR="$ENGINE_PATH/cloveretl.engine.jar"
 
-for i in $LIBS_PATH/*.jar $LIBS_PATH/*.zip; do
+for i in $ENGINE_PATH/lib/*.jar $ENGINE_PATH/lib/*.zip; do
 JARS="$JARS:$i"
 done;
 
-java -classpath "$TRANSFORM_PATH:$ENGINE_JAR:$JARS:$JAVA_HOME/lib/tools.jar" org.jetel.main.runGraph $1
+java -classpath "$TRANSFORM_PATH:$ENGINE_JAR:$JARS:$JAVA_HOME/lib/tools.jar" org.jetel.main.runGraph -plugins $ENGINE_PATH/plugins $1
