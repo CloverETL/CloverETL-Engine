3d8e2748d447066cbad5c32930afa2bafeb8c7c6
otasanek <otasanek@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2007-03-06 09:00:23 +0000

    UPDATE: better run scripts implementation.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2555 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/run.bat
+++ cloveretl.engine/run.bat
@@ -6,17 +6,16 @@ rem ********************************
 
 rem ***** set this before using script *****
 set JAVA_HOME=c:\Program Files (x86)\Java\jdk1.5.0_04
-set ENGINE_PATH=.
-set LIB_PATH=.\lib
+set ENGINE_PATH=..
 set TRANSFORM_PATH=.
 rem ****************************************
 
 set ENGINE_JAR=%ENGINE_PATH%\cloveretl.engine.jar
-set COMMONS_LOGGING_JAR=%LIB_PATH%\commons-logging.jar
-set JAVOLUTION_JAR=%LIB_PATH%\javolution.jar
-set JMS_JAR=%LIB_PATH%\jms.jar
-set JXL_JAR=%LIB_PATH%\jxl.jar
-set LOG4J_JAR=%LIB_PATH%\log4j-1.2.12.zip
-set POI_JAR=%LIB_PATH%\poi-2.5.1.jar
+set COMMONS_LOGGING_JAR=%ENGINE_PATH%\lib\commons-logging.jar
+set JAVOLUTION_JAR=%ENGINE_PATH%\lib\javolution.jar
+set JMS_JAR=%ENGINE_PATH%\lib\jms.jar
+set JXL_JAR=%ENGINE_PATH%\lib\jxl.jar
+set LOG4J_JAR=%ENGINE_PATH%\lib\log4j-1.2.12.zip
+set POI_JAR=%ENGINE_PATH%\lib\poi-2.5.1.jar
 
-java -cp "%TRANSFORM_PATH%;%ENGINE_JAR%;%COMMONS_LOGGING_JAR%;%JAVOLUTION_JAR%;%JMS_JAR%;%JXL_JAR%;%LOG4J_JAR%;%POI_JAR%;%JAVA_HOME%\lib\tools.jar" org.jetel.main.runGraph %1
+java -cp "%TRANSFORM_PATH%;%ENGINE_JAR%;%COMMONS_LOGGING_JAR%;%JAVOLUTION_JAR%;%JMS_JAR%;%JXL_JAR%;%LOG4J_JAR%;%POI_JAR%;%JAVA_HOME%\lib\tools.jar" org.jetel.main.runGraph -plugins %ENGINE_PATH%\plugins %1
