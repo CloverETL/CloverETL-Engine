5941a8bafb6e7d41519d259fd1c9ce0460f24ad8
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2007-03-27 14:06:02 +0000

    CHANGE:java examples are now in default package
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2672 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/examples/javaExamples/testDataParsing.java
+++ cloveretl.engine/examples/javaExamples/testDataParsing.java
@@ -16,7 +16,6 @@
 *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-package javaExamples;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
