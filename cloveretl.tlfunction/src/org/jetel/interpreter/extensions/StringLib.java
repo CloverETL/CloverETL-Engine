31b5e0ccbfed56202e00bf8078128c12e9628326
dpavlis <info@cloveretl.com>
2007-06-01 11:17:56 +0000

    FIX:added overriding of createContext
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2888 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.tlfunction/src/org/jetel/interpreter/extensions/StringLib.java
+++ cloveretl.tlfunction/src/org/jetel/interpreter/extensions/StringLib.java
@@ -36,23 +36,21 @@ public class StringLib extends TLFunctionLibrary {
     private static final String LIBRARY_NAME = "String";
 
     enum Function {
-        CONCAT("concat"),
-        UPPERCASE("uppercase"),
-        LOWERCASE("lowercase"),
-        LEFT("left"),
-        RIGHT("right"),
-        TRIM("trim"),
-        LENGTH("length");
-        
+        CONCAT("concat"), UPPERCASE("uppercase"), LOWERCASE("lowercase"), LEFT(
+                "left"), SUBSTRING("substring"), RIGHT("right"), TRIM("trim"), LENGTH(
+                "length"), SOUNDEX("soundex");
+
         public String name;
-        
+
         private Function(String name) {
             this.name = name;
         }
-        
+
         public static Function fromString(String s) {
-            for(Function function : Function.values()) {
-                if(s.equalsIgnoreCase(function.name) || s.equalsIgnoreCase(LIBRARY_NAME + "." + function.name)) {
+            for (Function function : Function.values()) {
+                if (s.equalsIgnoreCase(function.name)
+                        || s.equalsIgnoreCase(LIBRARY_NAME + "."
+                                + function.name)) {
                     return function;
                 }
             }
@@ -63,29 +61,40 @@ public class StringLib extends TLFunctionLibrary {
     public StringLib() {
         super();
     }
-    
-    public TLFunctionPrototype getFunction(String functionName) {
-        switch(Function.fromString(functionName)) {
-        case CONCAT: return new ConcatFunction();
-        case UPPERCASE: return new UppercaseFunction();
-        case LOWERCASE: return new LowerCaseFunction();
-        case LEFT: return new LeftFunction();
-        case RIGHT: return new RightFunction();
-        case TRIM: return new TrimFunction();
-        case LENGTH: return new LengthFunction();
-        default: return null;
-       }
-    }
-    
 
+    public TLFunctionPrototype getFunction(String functionName) {
+        switch (Function.fromString(functionName)) {
+        case CONCAT:
+            return new ConcatFunction();
+        case UPPERCASE:
+            return new UppercaseFunction();
+        case LOWERCASE:
+            return new LowerCaseFunction();
+        case SUBSTRING:
+            return new SubstringFunction();
+        case LEFT:
+            return new LeftFunction();
+        case RIGHT:
+            return new RightFunction();
+        case TRIM:
+            return new TrimFunction();
+        case LENGTH:
+            return new LengthFunction();
+        case SOUNDEX:
+            return new SoundexFunction();
+        default:
+            return null;
+        }
+    }
 
     // CONCAT
     class ConcatFunction extends TLFunctionPrototype {
 
-    	public ConcatFunction() {
-    		super("string", "concat", new TLValueType[] { TLValueType.STRING }, TLValueType.STRING);
-    	}
-    	
+        public ConcatFunction() {
+            super("string", "concat", new TLValueType[] { TLValueType.STRING },
+                    TLValueType.STRING);
+        }
+
         @Override
         public TLValue execute(TLValue[] params, TLContext context) {
             StringBuilder strBuf = (StringBuilder) context.getContext();
@@ -96,32 +105,37 @@ public class StringLib extends TLFunctionLibrary {
                         StringUtils.strBuffAppend(strBuf, params[i]
                                 .getCharSequence());
                     } else {
-                        StringUtils.strBuffAppend(strBuf, params[i]
-                                .toString());
+                        StringUtils.strBuffAppend(strBuf, params[i].toString());
                     }
                 } else {
-                    throw new TransformLangExecutorRuntimeException(
-                            params, "concat - wrong type of literal(s)");
+                    throw new TransformLangExecutorRuntimeException(params,
+                            "concat - wrong type of literal(s)");
                 }
             }
             return new TLValue(TLValueType.STRING, strBuf);
         }
-     }
+
+        @Override
+        public TLContext createContext() {
+            return TLContext.createStringContext();
+        }
+    }
 
     // UPPERCASE
     class UppercaseFunction extends TLFunctionPrototype {
-    	
-    	public UppercaseFunction(){
-    		super("string", "uppercase", new TLValueType[] { TLValueType.STRING }, TLValueType.STRING);
-    	}
-    	
+
+        public UppercaseFunction() {
+            super("string", "uppercase",
+                    new TLValueType[] { TLValueType.STRING },
+                    TLValueType.STRING);
+        }
+
         @Override
         public TLValue execute(TLValue[] params, TLContext context) {
             StringBuilder strBuf = (StringBuilder) context.getContext();
             strBuf.setLength(0);
 
-            if (!params[0].isNull()
-                    && params[0].type == TLValueType.STRING) {
+            if (!params[0].isNull() && params[0].type == TLValueType.STRING) {
                 CharSequence seq = params[0].getCharSequence();
                 strBuf.ensureCapacity(seq.length());
                 for (int i = 0; i < seq.length(); i++) {
@@ -135,52 +149,63 @@ public class StringLib extends TLFunctionLibrary {
             return new TLValue(TLValueType.STRING, strBuf);
         }
 
+        @Override
+        public TLContext createContext() {
+            return TLContext.createStringContext();
+        }
+
     }
 
     // LOWERCASE
     class LowerCaseFunction extends TLFunctionPrototype {
 
-    	public LowerCaseFunction() {
-			super("string", "lowercase",
-					new TLValueType[] { TLValueType.STRING },
-					TLValueType.STRING);
-		}
+        public LowerCaseFunction() {
+            super("string", "lowercase",
+                    new TLValueType[] { TLValueType.STRING },
+                    TLValueType.STRING);
+        }
 
-		@Override
-		public TLValue execute(TLValue[] params, TLContext context) {
-			StringBuilder strBuf = (StringBuilder) context.getContext();
-			strBuf.setLength(0);
+        @Override
+        public TLValue execute(TLValue[] params, TLContext context) {
+            StringBuilder strBuf = (StringBuilder) context.getContext();
+            strBuf.setLength(0);
 
-			if (!params[0].isNull() && params[0].type == TLValueType.STRING) {
-				CharSequence seq = params[0].getCharSequence();
-				strBuf.ensureCapacity(seq.length());
-				for (int i = 0; i < seq.length(); i++) {
-					strBuf.append(Character.toLowerCase(seq.charAt(i)));
-				}
-			} else {
-				throw new TransformLangExecutorRuntimeException(params,
-						"uppercase - wrong type of literal");
-			}
+            if (!params[0].isNull() && params[0].type == TLValueType.STRING) {
+                CharSequence seq = params[0].getCharSequence();
+                strBuf.ensureCapacity(seq.length());
+                for (int i = 0; i < seq.length(); i++) {
+                    strBuf.append(Character.toLowerCase(seq.charAt(i)));
+                }
+            } else {
+                throw new TransformLangExecutorRuntimeException(params,
+                        "uppercase - wrong type of literal");
+            }
 
-			return new TLValue(TLValueType.STRING, strBuf);
-		}
+            return new TLValue(TLValueType.STRING, strBuf);
+        }
 
-	}
-    
+        @Override
+        public TLContext createContext() {
+            return TLContext.createStringContext();
+        }
+
+    }
+
     // SUBSTRING
     class SubstringFunction extends TLFunctionPrototype {
-    	
-    	public SubstringFunction(){
-    		super("string", "substring", new TLValueType[] {TLValueType.STRING, TLValueType.INTEGER, TLValueType.INTEGER }, TLValueType.STRING);
-    	}
 
-    	@Override
+        public SubstringFunction() {
+            super("string", "substring", new TLValueType[] {
+                    TLValueType.STRING, TLValueType.INTEGER,
+                    TLValueType.INTEGER }, TLValueType.STRING);
+        }
+
+        @Override
         public TLValue execute(TLValue[] params, TLContext context) {
             StringBuilder strBuf = (StringBuilder) context.getContext();
             strBuf.setLength(0);
             int length, from;
-            if (params[0].isNull() || params[1].isNull()
-                    || params[2].isNull()) {
+            if (params[0].isNull() || params[1].isNull() || params[2].isNull()) {
                 throw new TransformLangExecutorRuntimeException(params,
                         "substring - NULL value not allowed");
             }
@@ -198,20 +223,25 @@ public class StringLib extends TLFunctionLibrary {
                         "substring - wrong type of literal(s)");
             }
 
-            return new TLValue(TLValueType.STRING, StringUtils
-                    .subString(strBuf, params[0].getCharSequence(),
-                            from, length));
+            return new TLValue(TLValueType.STRING, StringUtils.subString(
+                    strBuf, params[0].getCharSequence(), from, length));
+        }
+
+        @Override
+        public TLContext createContext() {
+            return TLContext.createStringContext();
         }
     }
 
     // LEFT
     class LeftFunction extends TLFunctionPrototype {
-    	
-    	public LeftFunction(){
-    		super("string", "left", new TLValueType[] { TLValueType.STRING, TLValueType.INTEGER }, TLValueType.STRING);
-    	}
 
-    	@Override
+        public LeftFunction() {
+            super("string", "left", new TLValueType[] { TLValueType.STRING,
+                    TLValueType.INTEGER }, TLValueType.STRING);
+        }
+
+        @Override
         public TLValue execute(TLValue[] params, TLContext context) {
             StringBuilder strBuf = (StringBuilder) context.getContext();
             strBuf.setLength(0);
@@ -233,19 +263,24 @@ public class StringLib extends TLFunctionLibrary {
                         "left - wrong type of literal(s)");
             }
 
-            return new TLValue(TLValueType.STRING, StringUtils
-                    .subString(strBuf, params[0].getCharSequence(), 0,
-                            length));
+            return new TLValue(TLValueType.STRING, StringUtils.subString(
+                    strBuf, params[0].getCharSequence(), 0, length));
+        }
+
+        @Override
+        public TLContext createContext() {
+            return TLContext.createStringContext();
         }
     }
 
     // RIGHT
     class RightFunction extends TLFunctionPrototype {
-    	
-    	public RightFunction(){
-    		super("string", "right", new TLValueType[] { TLValueType.STRING, TLValueType.INTEGER }, TLValueType.STRING);
-    	}
-    
+
+        public RightFunction() {
+            super("string", "right", new TLValueType[] { TLValueType.STRING,
+                    TLValueType.INTEGER }, TLValueType.STRING);
+        }
+
         @Override
         public TLValue execute(TLValue[] params, TLContext context) {
             StringBuilder strBuf = (StringBuilder) context.getContext();
@@ -271,19 +306,24 @@ public class StringLib extends TLFunctionLibrary {
             CharSequence src = params[0].getCharSequence();
             int from = src.length() - length;
 
-            return new TLValue(TLValueType.STRING, StringUtils
-                    .subString(strBuf, params[0].getCharSequence(),
-                            from, length));
+            return new TLValue(TLValueType.STRING, StringUtils.subString(
+                    strBuf, params[0].getCharSequence(), from, length));
+        }
+
+        @Override
+        public TLContext createContext() {
+            return TLContext.createStringContext();
         }
     }
 
     // TRIM
     class TrimFunction extends TLFunctionPrototype {
-    	
-    	public TrimFunction(){
-    		super("string", "trim", new TLValueType[] { TLValueType.STRING }, TLValueType.STRING);
-    	}
-    
+
+        public TrimFunction() {
+            super("string", "trim", new TLValueType[] { TLValueType.STRING },
+                    TLValueType.STRING);
+        }
+
         @Override
         public TLValue execute(TLValue[] params, TLContext context) {
             StringBuilder strBuf = (StringBuilder) context.getContext();
@@ -301,16 +341,21 @@ public class StringLib extends TLFunctionLibrary {
             return new TLValue(TLValueType.STRING, strBuf);
 
         }
+
+        @Override
+        public TLContext createContext() {
+            return TLContext.createStringContext();
+        }
     }
 
-    
     // LENGTH
     class LengthFunction extends TLFunctionPrototype {
-    	
-    	public LengthFunction() {
-    		super ("string", "length", new TLValueType[] { TLValueType.STRING }, TLValueType.INTEGER);
-		} 
-    
+
+        public LengthFunction() {
+            super("string", "length", new TLValueType[] { TLValueType.STRING },
+                    TLValueType.INTEGER);
+        }
+
         @Override
         public TLValue execute(TLValue[] params, TLContext context) {
             CloverInteger intBuf = (CloverInteger) context.getContext();
@@ -319,10 +364,119 @@ public class StringLib extends TLFunctionLibrary {
                         "length - wrong type of literal");
             }
             intBuf.setValue(params[0].getCharSequence().length());
-           
+
             return new TLValue(TLValueType.INTEGER, intBuf);
 
         }
+
+        @Override
+        public TLContext createContext() {
+            return TLContext.createIntegerContext();
+        }
     }
-             
+
+    // SOUNDEX
+    class SoundexFunction extends TLFunctionPrototype {
+
+        private static final int SIZE = 4;
+
+        public SoundexFunction() {
+            super("string", "soundex",
+                    new TLValueType[] { TLValueType.STRING },
+                    TLValueType.STRING);
+        }
+
+        @Override
+        public TLValue execute(TLValue[] params, TLContext context) {
+            StringBuilder strBuf = (StringBuilder) context.getContext();
+            strBuf.setLength(0);
+            if (params[0].type != TLValueType.STRING) {
+                throw new TransformLangExecutorRuntimeException(params,
+                        "soundex - wrong type of literal");
+            }
+
+            CharSequence src = params[0].getCharSequence();
+            int length = src.length();
+            char x[] = new char[length];
+            for (int i = 0; i < length; Character.toUpperCase(src.charAt(i++)))
+                ;
+            char firstLetter = x[0];
+
+            // convert letters to numeric code
+            for (int i = 0; i < x.length; i++) {
+                switch (x[i]) {
+                case 'B':
+                case 'F':
+                case 'P':
+                case 'V': {
+                    x[i] = '1';
+                    break;
+                }
+
+                case 'C':
+                case 'G':
+                case 'J':
+                case 'K':
+                case 'Q':
+                case 'S':
+                case 'X':
+                case 'Z': {
+                    x[i] = '2';
+                    break;
+                }
+
+                case 'D':
+                case 'T': {
+                    x[i] = '3';
+                    break;
+                }
+
+                case 'L': {
+                    x[i] = '4';
+                    break;
+                }
+
+                case 'M':
+                case 'N': {
+                    x[i] = '5';
+                    break;
+                }
+
+                case 'R': {
+                    x[i] = '6';
+                    break;
+                }
+
+                default: {
+                    x[i] = '0';
+                    break;
+                }
+                }
+            }
+
+            // remove duplicates
+            strBuf.append(firstLetter);
+            char last = x[0];
+            for (int i = 1; i < x.length; i++) {
+                if (x[i] != '0' && x[i] != last) {
+                    last = x[i];
+                    strBuf.append(last);
+                }
+            }
+
+            // pad with 0's or truncate
+            for (int i = strBuf.length(); i < SIZE; i++) {
+                strBuf.append('0');
+            }
+            strBuf.setLength(SIZE);
+            return new TLValue(TLValueType.STRING, strBuf);
+
+        }
+
+        @Override
+        public TLContext createContext() {
+            return TLContext.createStringContext();
+        }
+    }
+
 }
