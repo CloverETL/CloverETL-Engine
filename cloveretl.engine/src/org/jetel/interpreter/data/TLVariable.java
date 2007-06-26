b5871d6124882a59071e85b85e19fd40198bd489
dpavlis <info@cloveretl.com>
2007-06-26 15:26:14 +0000

    UPDATE:more proper fix of converting from different num type
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@2977 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/interpreter/data/TLVariable.java
+++ cloveretl.engine/src/org/jetel/interpreter/data/TLVariable.java
@@ -100,7 +100,9 @@ public class TLVariable {
     }
     
     public void setValue(TLValue value) {
+    	//small hack
         if (this.value.type!=value.type&&this.value.type.isNumeric()) {
+            this.value=TLValue.create(this.value.type);
             this.value.getNumeric().setValue(value.getNumeric());
         }else {
             this.value=value;
