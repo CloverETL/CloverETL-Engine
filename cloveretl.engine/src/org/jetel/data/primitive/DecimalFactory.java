de07727d40a6003fe162e73120f4c4fc251b3046
otasanek <info@cloveretl.com>
2007-07-19 12:17:47 +0000

    MINOR: removes obsoleted comments.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@3061 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/data/primitive/DecimalFactory.java
+++ cloveretl.engine/src/org/jetel/data/primitive/DecimalFactory.java
@@ -39,9 +39,6 @@ public class DecimalFactory {
      * How many decimal digits correspond to binary digits
      */
     private static final int BOUNDS_FOR_DECIMAL_IMPLEMENTATION = 18;
-    //FIXME in 1.5 java we can use next two line
-    //private static final double MAGIC_CONST = Math.log(10)/Math.log(2);
-    //private static final int BOUNDS_FOR_DECIMAL_IMPLEMENTATION = (int) Math.floor((Integer.SIZE - 1) / MAGIC_CONST);
 	
     public static Decimal getDecimal(int value) {
         Decimal d = getDecimal(10, 0);
