90b43c6a9fd610ba0cba8e858e6f809764a49046
agad <info@cloveretl.com>
2007-08-10 09:29:35 +0000

    BUGFIX:DBJoin - no warning during initialization
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@3130 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component/src/org/jetel/component/DBJoin.java
+++ cloveretl.component/src/org/jetel/component/DBJoin.java
@@ -304,7 +304,7 @@ public class DBJoin extends Node {
 		DataRecordMetadata inMetadata[]={ getInputPort(READ_FROM_PORT).getMetadata(),dbMetadata};
 		DataRecordMetadata outMetadata[]={getOutputPort(WRITE_TO_PORT).getMetadata()};
         lookupTable = new DBLookupTable("LOOKUP_TABLE_FROM_"+this.getId(),(DBConnection) conn,dbMetadata,query,maxCached);
-//        lookupTable.checkConfig(null);
+        lookupTable.checkConfig(null);
 		lookupTable.init();
 		try {
 			recordKey = new RecordKey(joinKey, inMetadata[0]);
