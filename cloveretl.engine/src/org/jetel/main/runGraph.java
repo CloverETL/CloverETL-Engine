6d8de16fbdbcdd0430449a12761e4ac0298a2ac5
otasanek <info@cloveretl.com>
2007-08-13 09:44:43 +0000

    CHANGE: static version information was replaced with values from JetelVersion.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@3139 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/main/runGraph.java
+++ cloveretl.engine/src/org/jetel/main/runGraph.java
@@ -95,7 +95,7 @@ public class runGraph {
     private static Log logger = LogFactory.getLog(runGraph.class);
 
     //TODO change run graph version
-	private final static String RUN_GRAPH_VERSION = "2.2";
+	private final static String RUN_GRAPH_VERSION = JetelVersion.MAJOR_VERSION+"."+JetelVersion.MINOR_VERSION;
 	public final static String VERBOSE_SWITCH = "-v";
 	public final static String PROPERTY_FILE_SWITCH = "-cfg";
 	public final static String PROPERTY_DEFINITION_SWITCH = "-P:";
