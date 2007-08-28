4f0e8203ae91ffca01b06a3051b42c5a14e31766
jurban <jurban@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2007-08-28 11:51:38 +0000

    MINOR: fixed comments.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@3205 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.bulkloader/src/org/jetel/component/InformixDataWriter.java
+++ cloveretl.bulkloader/src/org/jetel/component/InformixDataWriter.java
@@ -78,13 +78,14 @@ import org.w3c.dom.Element;
  * <tr><td><h4><i>Category:</i></h4></td>
  * <td></td></tr>
  * <tr><td><h4><i>Description:</i></h4></td>
- * <td>This component loads data to Informix database using dbload utility. 
- * There is created temporary file with dbload commands depending on input parameters. Data are read from given 
- * input file or from input port and loaded to database. On Linux/Unix system data transfer is processed by stdio.<br>
+ * <td>This component loads data to an Informix database using the dbload utility. 
+ * It creates a temporary file with dbload commands depending on input parameters. Data are read from given 
+ * input file or from the input port and loaded to database. 
+ * On Linux/Unix systems, data is transfered to the dbload utility via stdin.<br>
  * To use this component Informix client must be installed and configured on the local host.
  * </td></tr>
  * <tr><td><h4><i>Inputs:</i></h4></td>
- * <td>[0] - input records</td></tr>
+ * <td>[0] - input records, optional</td></tr>
  * <tr><td><h4><i>Outputs:</i></h4></td>
  * <td>[0] - optionally one output port defined/connected - rejected records.
  * Metadata on this port must have the same type of field as input metadata, except otput metadata has a additional field with row number.
@@ -146,10 +147,6 @@ import org.w3c.dom.Element;
  * @see         org.jetel.graph.Node
  * @see         org.jetel.graph.Edge
  * @since 		20.8.2007
- */
-/**
- * @author Miroslav Haupt (Mirek.Haupt@javlinconsulting.cz)
- *		   (c) Javlin Consulting (www.javlinconsulting.cz)
  */
 public class InformixDataWriter extends Node {
 
