e844c5d0bff633bab86bc3569cc4b91228134db6
dpavlis <dpavlis@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2007-10-24 13:20:34 +0000

    init:initial check-in
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@3419 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/src/org/jetel/util/CharSequenceReader.java
@@ -0,0 +1,84 @@
+/*
+ *    jETeL/Clover - Java based ETL application framework.
+ *    Copyright (C) 2002-2007  David Pavlis <david.pavlis@centrum.cz> and others.
+ *    
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *    
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
+ *    Lesser General Public License for more details.
+ *    
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Created on 16.10.2007 by dadik
+ *
+ */
+
+/**
+ * 
+ */
+package org.jetel.util;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * @author david pavlis <david.pavlis@opensys.eu>
+ * 
+ * This class implements Reader interface on top of CharSequence - i.e.
+ * it enables reading chars from String,StringBuffer/Builder,CharBuffer
+ *
+ */
+
+public class CharSequenceReader extends Reader {
+
+	CharSequence input;
+	int pos;
+	final int length;
+	
+	public CharSequenceReader(CharSequence input){
+		this.input=input;
+		this.length=input.length();
+		pos=0;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see java.io.Reader#close()
+	 */
+	@Override
+	public void close() throws IOException {
+		//no need to close anything
+
+	}
+
+	/* (non-Javadoc)
+	 * @see java.io.Reader#read(char[], int, int)
+	 */
+	@Override
+	public int read(char[] cbuf, int off, int len) throws IOException {
+		if (pos<length){
+			int i=0;
+			while(i<len){
+				cbuf[off++]=input.charAt(pos++);
+				i++;
+				if (pos>=this.length) break;
+			}
+			return i;
+		}		
+		return -1;
+	}
+	
+	@Override
+	public void reset() throws IOException{
+		super.reset();
+		pos=0;
+	}
+
+}
