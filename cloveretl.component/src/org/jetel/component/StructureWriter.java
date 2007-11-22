309a2a94a67839a807de1295ac9dd8b51f68392d
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2007-11-22 11:22:03 +0000

    UPDATE:StructureWriter can have 3 input ports:header, body and footer
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@3563 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component/src/org/jetel/component/StructureWriter.java
+++ cloveretl.component/src/org/jetel/component/StructureWriter.java
@@ -21,6 +21,7 @@
 
 package org.jetel.component;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.channels.Channels;
 import java.nio.channels.WritableByteChannel;
 
@@ -28,6 +29,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jetel.data.DataRecord;
 import org.jetel.data.Defaults;
+import org.jetel.data.formatter.StructureFormatter;
 import org.jetel.data.formatter.provider.StructureFormatterProvider;
 import org.jetel.data.lookup.LookupTable;
 import org.jetel.enums.PartitionFileTagType;
@@ -47,7 +49,7 @@ import org.w3c.dom.Element;
 /**
  *  <h3>StructureWriter Component</h3>
  *
- * <!-- All records from input port [0] are formatted due to given mask and written to specified file -->
+ * <!-- All records from "body input port' are formatted due to given mask and written to specified file -->
  * 
  * <table border="1">
  *  <th>Component:</th>
@@ -56,10 +58,17 @@ import org.w3c.dom.Element;
  * <tr><td><h4><i>Category:</i></h4></td>
  * <td></td></tr>
  * <tr><td><h4><i>Description:</i></h4></td>
- * <td>All records from input port [0] are formatted due to given mask and written to specified file.
- * Records can be preceded by some text (header) or be trailed by a text (footer)</td></tr>
+ * <td>All records from "body input port" are formatted due to given mask and written to specified file.
+ * There can be specified different masks for "header input port" and "footer input port". If there is specified
+ * header mask, all records from "header input port" [0] are  formatted due to this mask and written to file,
+ * then are processed records from "body input port" [1] and then is written footer, created from records read 
+ * from "footer input port" [2]. Header and footer are created in the same time and footer is stored in memory
+ * until all records from "body port" are not written to the file.</td></tr>
  * <tr><td><h4><i>Inputs:</i></h4></td>
- * <td>[0]- input records</td></tr>
+ * <td>[0]- input records - if there are not connected ports [1] and [2], this is "body port" else this is "header port"
+ * 	   [1]- input records - "body port"
+ * 	   [2]- input records - "footer port"
+ * </td></tr>
  * <tr><td><h4><i>Outputs:</i></h4></td>
  * <td></td></tr>
  * <tr><td><h4><i>Comment:</i></h4></td>
@@ -80,8 +89,8 @@ import org.w3c.dom.Element;
  *  be unchanged. If not given there is used default mask:
  *  &lt; recordName field1=$field1 field2=$field2 ... fieldn=$fieldn /&gt;
  *  where field1 ,.., fieldn are record's fields from metadata</td>
- *  <tr><td><b>header</b></td><td>text to write before records</td>
- *  <tr><td><b>footer</b></td><td>text to write after records</td>
+ *  <tr><td><b>header</b></td><td>template for formating records. Every occurrence of $fieldName will be replaced by value of the fieldName.</td>
+ *  <tr><td><b>footer</b></td><td>template for formating records. Every occurrence of $fieldName will be replaced by value of the fieldName.</td>
  *  <tr><td><b>recordsPerFile</b></td><td>max number of records in one output file</td>
  *  <tr><td><b>bytesPerFile</b></td><td>Max size of output files. To avoid splitting a record to two files, max size could be slightly overreached.</td>
  *  <tr><td><b>recordSkip</b></td><td>number of skipped records</td>
@@ -99,7 +108,16 @@ import org.w3c.dom.Element;
  *	&lt;first name = $fname&gt;
  * &lt;/Customer&gt;
  * &lt;/attr&gt;
- * &lt;attr name="footer"&gt;end of file&lt;/attr&gt;
+ * &lt;attr name="header"&gt;
+ * &lt;Header recordd&gt;
+ * 	&lt;id = $customer_id&gt;
+ * &lt;/Header record&gt;
+ * &lt;/attr&gt;
+ * &lt;attr name="footer"&gt;
+ * &lt;Footer recordd&gt;
+ * 	&lt;number = $account_no&gt;
+ * &lt;/Footer record&gt;
+ * &lt;/attr&gt;
  * &lt;/Node&gt;
  * 
  * 
@@ -129,9 +147,9 @@ public class StructureWriter extends Node {
 	private String fileURL;
 	private boolean appendData;
 	private StructureFormatterProvider formatterProvider;
-	private String header = null;
-	private String footer = null;
 	private MultiFileWriter writer;
+	private StructureFormatter headerFormatter, footerFormatter;
+	private ByteArrayOutputStream headerOutput, footerOutput;
     private int skip;
 	private int numRecords;
 	private WritableByteChannel writableByteChannel;
@@ -143,11 +161,19 @@ public class StructureWriter extends Node {
 	private String[] partitionKey;
 	private LookupTable lookupTable;
 	private PartitionFileTagType partitionFileTagType = PartitionFileTagType.NUMBER_FILE_TAG;
+	private String headerMask;
+	private String footerMask;
 
 	private static Log logger = LogFactory.getLog(StructureWriter.class);
 
 	public final static String COMPONENT_TYPE = "STRUCTURE_WRITER";
-	private final static int READ_FROM_PORT = 0;
+
+	private final static int HEADER_DEFAULT_PORT = 0;
+	private final static int BODY_DEFAULT_PORT = 1;
+	private final static int FOOTER_DEFAULT_PORT = 2;
+	
+	private int bodyPortNo;
+	private String charset;
 
 	/**
 	 * Constructor
@@ -158,15 +184,32 @@ public class StructureWriter extends Node {
 	 * @param appendData
 	 * @param mask
 	 */
+	@Deprecated
 	public StructureWriter(String id, String fileURL, String charset, 
 			boolean appendData, String mask) {
+		this(id, fileURL, charset, appendData, null, mask, null);
+	}
+
+	/**
+	 * @param id
+	 * @param fileURL
+	 * @param charset
+	 * @param appendData
+	 * @param headerMask
+	 * @param mask
+	 * @param footerMask
+	 */
+	public StructureWriter(String id, String fileURL, String charset, 
+			boolean appendData, String headerMask, String mask, String footerMask) {
 		super(id);
 		this.fileURL = fileURL;
+		this.charset = charset;
 		this.appendData = appendData;
+		this.headerMask = headerMask;
+		this.footerMask = footerMask;
 		formatterProvider = new StructureFormatterProvider(charset != null ? charset : Defaults.DataFormatter.DEFAULT_CHARSET_ENCODER);
 		formatterProvider.setMask(mask);
 	}
-
 	/* (non-Javadoc)
 	 * @see org.jetel.graph.Node#getType()
 	 */
@@ -177,12 +220,41 @@ public class StructureWriter extends Node {
 
 	@Override
 	public Result execute() throws Exception {
-		InputPort inPort = getInputPort(READ_FROM_PORT);
-		DataRecord record = new DataRecord(inPort.getMetadata());
+		
+		Producer headerProducer = null, footerProducer = null;
+		//prepare header
+		if (headerFormatter != null) {
+			headerProducer = new Producer(getInputPort(HEADER_DEFAULT_PORT), headerFormatter);
+			headerProducer.run();
+		}
+		//prepare footer
+		if (footerFormatter != null){
+			footerProducer = new Producer(getInputPort(FOOTER_DEFAULT_PORT), footerFormatter);
+			footerProducer.run();
+		}
+		//wait for header
+		if (headerProducer != null) {
+			headerProducer.join();
+		}
+		//wait for footer
+		if (footerProducer != null){
+			footerProducer.join();
+		}
+		
+		if (headerFormatter != null) {
+			formatterProvider.setHeader(headerOutput.toString(headerFormatter.getCharsetName()));
+		}
+		if (footerFormatter != null){
+			formatterProvider.setFooter(footerOutput.toString(footerFormatter.getCharsetName()));
+		}
+		//main loop: processing "body" records
+		InputPort bodyPort = getInputPort(bodyPortNo);
+		DataRecord record = new DataRecord(bodyPort.getMetadata());
 		record.init();
+        writer.init(bodyPort.getMetadata());
 		try {
 			while (record != null && runIt) {
-				record = inPort.readRecord(record);
+				record = bodyPort.readRecord(record);
 				if (record != null) {
 			        writer.write(record);
 				}
@@ -203,7 +275,7 @@ public class StructureWriter extends Node {
     public ConfigurationStatus checkConfig(ConfigurationStatus status) {
 		super.checkConfig(status);
 		 
-		checkInputPorts(status, 1, 1);
+		checkInputPorts(status, 1, 3);
         checkOutputPorts(status, 0, 0);
 
         try {
@@ -246,9 +318,34 @@ public class StructureWriter extends Node {
         if (attrPartitionKey != null) partitionKey = attrPartitionKey.split(Defaults.Component.KEY_FIELDS_DELIMITER_REGEX);
         writer.setPartitionKeyNames(partitionKey);
         writer.setPartitionFileTag(partitionFileTagType);
-        formatterProvider.setHeader(header);
-        formatterProvider.setFooter(footer);
-        writer.init(getInputPort(READ_FROM_PORT).getMetadata());
+        
+        bodyPortNo = getInPorts().size() > 1 ? BODY_DEFAULT_PORT : 0;
+		if (headerMask != null) {
+			if (bodyPortNo == 1) {
+				headerFormatter = new StructureFormatter(charset != null ? charset : Defaults.DataFormatter.DEFAULT_CHARSET_ENCODER);
+				headerFormatter.setMask(headerMask);
+			}else{
+				formatterProvider.setHeader(headerMask);
+			}
+		}
+		if (footerMask != null){
+			if (bodyPortNo == 1) {
+				footerFormatter = new StructureFormatter(charset != null ? charset : Defaults.DataFormatter.DEFAULT_CHARSET_ENCODER);
+				footerFormatter.setMask(footerMask);
+			}else{
+				formatterProvider.setFooter(footerMask);
+			}
+		}
+        if (headerFormatter != null) {
+        	headerOutput = new ByteArrayOutputStream();
+        	headerFormatter.init(getInputPort(HEADER_DEFAULT_PORT).getMetadata());
+        	headerFormatter.setDataTarget(Channels.newChannel(headerOutput));
+        }
+        if (footerFormatter != null){
+        	footerOutput = new ByteArrayOutputStream();
+        	footerFormatter.init(getInputPort(FOOTER_DEFAULT_PORT).getMetadata());
+        	footerFormatter.setDataTarget(Channels.newChannel(footerOutput));
+        }
 	}
 
 	/**
@@ -281,13 +378,9 @@ public class StructureWriter extends Node {
 									xattribs.getString(XML_FILEURL_ATTRIBUTE),
 									xattribs.getString(XML_CHARSET_ATTRIBUTE,null),
 									xattribs.getBoolean(XML_APPEND_ATTRIBUTE, false),
-									xattribs.getString(XML_MASK_ATTRIBUTE,null));
-			if (xattribs.exists(XML_HEADER_ATTRIBUTE)){
-				aDataWriter.setHeader(xattribs.getString(XML_HEADER_ATTRIBUTE));
-			}
-			if (xattribs.exists(XML_FOOTER_ATTRIBUTE)){
-				aDataWriter.setFooter(xattribs.getString(XML_FOOTER_ATTRIBUTE));
-			}
+									xattribs.getString(XML_HEADER_ATTRIBUTE,null),
+									xattribs.getString(XML_MASK_ATTRIBUTE, null),
+									xattribs.getString(XML_FOOTER_ATTRIBUTE, null));
 			if (xattribs.exists(XML_RECORD_SKIP_ATTRIBUTE)){
 				aDataWriter.setSkip(Integer.parseInt(xattribs.getString(XML_RECORD_SKIP_ATTRIBUTE)));
 			}
@@ -328,11 +421,11 @@ public class StructureWriter extends Node {
 			xmlElement.setAttribute(XML_CHARSET_ATTRIBUTE, this.formatterProvider.getCharsetName());
 		}
 		xmlElement.setAttribute(XML_APPEND_ATTRIBUTE, String.valueOf(this.appendData));
-		if (header != null){
-			xmlElement.setAttribute(XML_HEADER_ATTRIBUTE,header);
+		if (headerMask != null){
+			xmlElement.setAttribute(XML_HEADER_ATTRIBUTE,headerMask);
 		}
-		if (footer != null){
-			xmlElement.setAttribute(XML_FOOTER_ATTRIBUTE,footer);
+		if (footerMask != null){
+			xmlElement.setAttribute(XML_FOOTER_ATTRIBUTE,footerMask);
 		}
 		if (skip != 0){
 			xmlElement.setAttribute(XML_RECORD_SKIP_ATTRIBUTE, String.valueOf(skip));
@@ -357,12 +450,14 @@ public class StructureWriter extends Node {
 		xmlElement.setAttribute(XML_PARTITION_FILETAG_ATTRIBUTE, partitionFileTagType.name());
 	}
 	
+	@Deprecated
 	public void setFooter(String footer) {
-		this.footer = footer;
+		this.footerMask = footer;
 	}
 
+	@Deprecated
 	public void setHeader(String header) {
-		this.header = header;
+		this.headerMask = header;
 	}
 
     /**
@@ -460,4 +555,50 @@ public class StructureWriter extends Node {
 	public PartitionFileTagType getPartitionFileTag() {
 		return partitionFileTagType;
 	}
+
+	/**
+	 * This is class for format header/footer from input records
+	 * 
+	 * @author Agata Vackova (agata.vackova@javlinconsulting.cz) ; 
+	 * (c) JavlinConsulting s.r.o.
+	 *  www.javlinconsulting.cz
+	 *
+	 * @since Nov 22, 2007
+	 *
+	 */
+	private class Producer extends Thread{
+		
+		private InputPort inputPort;
+		private StructureFormatter formatter;
+
+		/**
+		 * @param inPort port for raeding records
+		 * @param formatter formatter to format input records
+		 */
+		Producer(InputPort inPort, StructureFormatter formatter){
+			this.inputPort = inPort;
+			this.formatter = formatter;
+		}
+
+		public void run() {
+			DataRecord record = new DataRecord(inputPort.getMetadata());
+			record.init();
+			try {
+				while (record != null && runIt) {
+					record = inputPort.readRecord(record);
+					if (record != null) {
+				        formatter.write(record);
+					}
+					SynchronizeUtils.cloverYield();
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}finally{
+				formatter.close();
+			}
+		}
+		
+	}
+
+
 }
