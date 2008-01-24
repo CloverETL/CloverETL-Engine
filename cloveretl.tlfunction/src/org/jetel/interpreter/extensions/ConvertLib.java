6eb6c15194dc77d7e864f8db38b6f543b2dbb77a
agad <info@cloveretl.com>
2008-01-24 15:11:14 +0000

    UPDATE:new tl function with test
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@3856 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.tlfunction/src/org/jetel/interpreter/extensions/ConvertLib.java
+++ cloveretl.tlfunction/src/org/jetel/interpreter/extensions/ConvertLib.java
@@ -23,22 +23,28 @@
  */
 package org.jetel.interpreter.extensions;
 
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.Format;
+import java.text.NumberFormat;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.jetel.data.primitive.CloverLong;
 import org.jetel.data.primitive.Decimal;
 import org.jetel.data.primitive.DecimalFactory;
+import org.jetel.data.primitive.Numeric;
+import org.jetel.data.primitive.NumericFormat;
+import org.jetel.data.primitive.StringFormat;
 import org.jetel.interpreter.TransformLangExecutorRuntimeException;
 import org.jetel.interpreter.data.TLBooleanValue;
 import org.jetel.interpreter.data.TLDateValue;
-import org.jetel.interpreter.data.TLNullValue;
 import org.jetel.interpreter.data.TLNumericValue;
 import org.jetel.interpreter.data.TLValue;
 import org.jetel.interpreter.data.TLValueType;
 import org.jetel.interpreter.extensions.DateLib.CalendarStore;
-import org.jetel.interpreter.extensions.StringLib.DateFormatStore;
-import org.jetel.interpreter.extensions.StringLib.Function;
 import org.jetel.metadata.DataFieldMetadata;
 import org.jetel.util.MiscUtils;
 import org.jetel.util.string.StringUtils;
@@ -46,13 +52,19 @@ import org.jetel.util.string.StringUtils;
 public class ConvertLib extends TLFunctionLibrary {
 
     private static final String LIBRARY_NAME = "Convert";
+	private static final String DEFAULT_REGEXP_TRUE_STRING = "T|TRUE|YES|Y||t|true|1|yes|y";
+	private static final String DEFAULT_REGEXP_FALSE_STRING = "F|FALSE|NO|N||f|false|0|no|n";
 
-    enum Function {
+	private static StringFormat trueFormat = StringFormat.create(DEFAULT_REGEXP_TRUE_STRING);
+	private static StringFormat falseFormat = StringFormat.create(DEFAULT_REGEXP_FALSE_STRING);
+
+	enum Function {
         NUM2STR("num2str"),
         DATE2STR("date2str"),
         STR2DATE("str2date"),
         DATE2NUM("date2num"),
-        STR2NUM("str2num");
+        STR2NUM("str2num"),
+        TRY_CONVERT("try_convert");
         
         public String name;
         
@@ -81,6 +93,7 @@ public class ConvertLib extends TLFunctionLibrary {
         case STR2DATE: return new Str2DateFunction();
         case DATE2NUM: return new Date2NumFunction();
         case STR2NUM: return new Str2NumFunction();
+        case TRY_CONVERT: return new TryConvertFunction();
         default: return null;
        }
     }
@@ -89,7 +102,7 @@ public class ConvertLib extends TLFunctionLibrary {
     class Num2StrFunction extends TLFunctionPrototype {
 
         public Num2StrFunction() {
-            super("string", "num2str", new TLValueType[] { TLValueType.DECIMAL, TLValueType.INTEGER }, 
+            super("convert", "num2str", new TLValueType[] { TLValueType.DECIMAL, TLValueType.INTEGER }, 
                     TLValueType.STRING, 2,1);
         }
 
@@ -155,7 +168,7 @@ public class ConvertLib extends TLFunctionLibrary {
         
 
         public Date2StrFunction() {
-            super("string", "date2str", new TLValueType[] { TLValueType.DATE, TLValueType.STRING }, 
+            super("convert", "date2str", new TLValueType[] { TLValueType.DATE, TLValueType.STRING }, 
                     TLValueType.STRING);
         }
 
@@ -232,7 +245,7 @@ public class ConvertLib extends TLFunctionLibrary {
         
 
         public Str2DateFunction() {
-            super("string", "str2date", new TLValueType[] { TLValueType.STRING, TLValueType.STRING, TLValueType.OBJECT, TLValueType.STRING }, 
+            super("convert", "str2date", new TLValueType[] { TLValueType.STRING, TLValueType.STRING, TLValueType.OBJECT, TLValueType.STRING }, 
                     TLValueType.DATE, 4, 2);
         }
 
@@ -310,7 +323,7 @@ public class ConvertLib extends TLFunctionLibrary {
     class Date2NumFunction extends TLFunctionPrototype {
 
         public Date2NumFunction() {
-            super("string", "date2num", new TLValueType[] { TLValueType.DATE, TLValueType.SYM_CONST }, 
+            super("convert", "date2num", new TLValueType[] { TLValueType.DATE, TLValueType.SYM_CONST }, 
                     TLValueType.INTEGER);
         }
 
@@ -343,7 +356,7 @@ public class ConvertLib extends TLFunctionLibrary {
     class Str2NumFunction extends TLFunctionPrototype {
 
         public Str2NumFunction() {
-            super("string", "str2num", new TLValueType[] { TLValueType.STRING, TLValueType.SYM_CONST, TLValueType.INTEGER }, 
+            super("convert", "str2num", new TLValueType[] { TLValueType.STRING, TLValueType.SYM_CONST, TLValueType.INTEGER }, 
                     TLValueType.INTEGER,3,1);
         }
 
@@ -412,4 +425,218 @@ public class ConvertLib extends TLFunctionLibrary {
 	}
     }
 
+    // TRY_CONVERT
+    class TryConvertFunction extends TLFunctionPrototype {
+
+        public TryConvertFunction() {
+            super("convert", "try_convert", new TLValueType[] { TLValueType.OBJECT, TLValueType.OBJECT, TLValueType.STRING }, 
+                    TLValueType.OBJECT,3,2);
+        }
+
+        @Override
+        public TLValue execute(TLValue[] params, TLContext context) {
+        	TLValueType fromtype = params[0].type;
+        	TLValueType toType = params[1].type;
+        	
+        	boolean canConvert = false;
+        	if (fromtype == toType) {
+        		if (fromtype == TLValueType.DECIMAL) {//check precision
+					TLValue candidate = TLValue.create(toType);
+					candidate.setValue(params[0].getNumeric());
+					canConvert = candidate.compareTo(params[0]) == 0;
+					if (!canConvert) {
+						return TLValue.FALSE_VAL;
+					}
+        		}
+        		params[1].setValue(params[0].getValue());
+        		return TLValue.TRUE_VAL;
+        	}
+        	if (!(fromtype.isCompatible(toType) || toType.isCompatible(fromtype))) {
+        		return TLValue.FALSE_VAL;
+        	}
+        	switch (fromtype) {
+			case BOOLEAN:
+        		canConvert = toType.isNumeric() ||	toType == TLValueType.STRING;
+        		if (canConvert) {
+					if (toType.isNumeric()) {//boolean --> numeric
+						params[1].setValue(
+								((TLBooleanValue)params[0]).getBoolean() ? 
+										TLNumericValue.NUM_ONE_VAL : 
+										TLNumericValue.NUM_ZERO_VAL);
+					}else{//boolean --> string
+						params[1].setValue(params[0].toString());
+					}
+				}
+				break;
+			case DATE:
+        		canConvert = toType == TLValueType.STRING || toType == TLValueType.LONG;
+        		//date --> string
+        		if (canConvert && toType == TLValueType.STRING) {
+        			DateFormat format;
+        			if (params.length == 3) {
+        				format = new SimpleDateFormat(params[2].toString());
+        			}else{
+        				format = DateFormat.getInstance();
+        			}
+        			params[1].setValue(format.format(((TLDateValue)params[0]).getDate()));
+        		}
+        		//date --> long
+        		if (canConvert && toType == TLValueType.LONG){
+        			((TLNumericValue<Numeric>)params[1]).setLong(((TLDateValue)params[0]).getDate().getTime());
+        		}
+				break;
+			case LONG:
+				if (toType == TLValueType.DATE) {
+					canConvert = true;
+					((TLDateValue)params[1]).setValue(new Date(params[0].getNumeric().getLong()));
+				}
+			case DECIMAL:
+			case DOUBLE:
+			case INTEGER:
+				switch (toType) {
+				case BOOLEAN://numeric --> boolean
+					TLBooleanValue result = convertToBooleanValue((TLNumericValue<Numeric>)params[0]);
+					if (result != null) {
+						canConvert = true;
+						params[1].setValue(result);
+					}
+					break;
+				case STRING://numeric --> string
+					canConvert = true;
+					NumberFormat format;
+        			if (params.length == 3) {
+        				format = new NumericFormat(params[2].toString());
+        			}else{
+        				format = new NumericFormat();
+        			}
+        			params[1].setValue(format.format(params[0].getNumeric()));
+        			break;
+				case DECIMAL:
+				case DOUBLE:
+				case INTEGER:
+				case LONG:
+					//numeric --> another numeric 
+					TLValue candidate = TLValue.create(toType);
+					candidate.setValue(params[0].getNumeric());
+					//TODO two comparison needed while can happen: cloverInt.compareTo(cloverDouble)=0 but cloverDouble.compareTo(cloverInt)=1 
+					canConvert = candidate.compareTo(params[0]) == 0 && params[0].compareTo(candidate) == 0;
+					if (canConvert) {
+	        			params[1].setValue(candidate);
+					}
+				}
+    			break;
+			case STRING:
+				Format format;
+    			Number result = null;
+				switch (toType) {
+				case BOOLEAN://string --> boolean
+					boolean isTrue = trueFormat.matches(params[0].toString());
+					boolean isFalse = falseFormat.matches(params[0].toString());
+					canConvert = isTrue || isFalse;
+					if (canConvert) {
+						params[1].setValue(isTrue ? TLValue.TRUE_VAL : TLValue.FALSE_VAL);
+					}
+					break;
+				case BYTE://string --> byte
+					canConvert = true;
+					params[1].setValue(params[0].toString().getBytes());
+					break;
+				case DATE://string --> date
+        			if (params.length == 3) {
+        				format = new SimpleDateFormat(params[2].toString());
+        			}else{
+        				format = DateFormat.getInstance();
+        			}
+        			ParsePosition pos = new ParsePosition(0);
+        			Date dateResult = ((SimpleDateFormat)format).parse(params[0].toString(), pos);
+        			if (pos.getIndex() != 0) {
+        				canConvert = true;
+        				((TLDateValue)params[1]).setValue(dateResult);
+        			}
+        			break;
+				case DECIMAL://string --> decimal
+        			if (params.length == 3) {
+        				format = new NumericFormat(params[2].toString());
+        			}else{
+        				format = new NumericFormat();
+        			}
+        			try {
+        				result = (BigDecimal)((NumericFormat)format).parse(params[0].toString());
+        				canConvert = ((BigDecimal)result).precision() <= params[1].getNumeric().getDecimal().getPrecision() &&
+        							 ((BigDecimal)result).scale() <= params[1].getNumeric().getDecimal().getScale();
+        				if (canConvert) {
+        					params[1].setValue(result);
+        				}
+        			}catch (Exception e) {
+						canConvert = false;
+					}
+        			break;
+				case DOUBLE://string --> double
+					if (params.length == 3) {
+						format = new DecimalFormat(params[2].toString());
+	        			try {
+	        				result = ((DecimalFormat)format).parse(params[0].toString());
+	        				canConvert = true;
+	        			}catch (Exception e) {
+							canConvert = false;
+						}
+					}else if (StringUtils.isNumber(params[1].toString())) {
+						try {
+							result = Double.parseDouble(params[1].toString());
+	        				canConvert = true;
+						} catch (Exception e) {
+							canConvert = false;
+						}
+					}		
+					if (canConvert) {
+						params[1].setValue(result);
+					}
+					break;
+				case LONG:
+				case INTEGER:
+					//string --> long
+					int tmp = StringUtils.isInteger(params[0].toString());
+					if (-1 < tmp && tmp < 3) {
+						try {
+							result = Long.parseLong(params[0].toString());
+							canConvert = true;
+						} catch (Exception e) {
+							canConvert = false;
+						}
+					}					
+					//if toType is integer, check if received number is in Integer range
+					if (canConvert && toType == TLValueType.INTEGER && 
+							(tmp < Integer.MIN_VALUE || Integer.MAX_VALUE < tmp)){
+						canConvert = false;
+					}
+					if (canConvert) {
+						params[1].setValue(result);
+					}
+					break;
+				}
+				break;
+			case MAP:
+			case LIST:
+			case BYTE:
+        		canConvert = toType == TLValueType.STRING;
+        		if (canConvert) {
+        			params[1].setValue(params[0].toString());
+        		}
+        		break;
+        	}
+        	
+    		return canConvert ? TLValue.TRUE_VAL : TLValue.FALSE_VAL;
+        }
+    
+    }
+
+    private static TLBooleanValue convertToBooleanValue(TLNumericValue<Numeric> value){
+    	if (value.compareTo(TLNumericValue.NUM_ONE_VAL) == 0) {
+    		return TLBooleanValue.FALSE;
+    	}
+    	if (value.compareTo(TLNumericValue.NUM_ZERO_VAL) == 0){
+    		return TLBooleanValue.TRUE;
+    	}
+    	return null;
+    }
 }
