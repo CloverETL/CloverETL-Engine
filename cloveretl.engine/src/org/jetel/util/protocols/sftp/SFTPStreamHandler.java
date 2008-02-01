247ffe45d82c37fd0fe92f9c5d9ded08dc30ce43
jausperger <jausperger@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2008-02-01 08:55:18 +0000

    FIX: sftp protocol verification added
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@3883 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/protocols/sftp/SFTPStreamHandler.java
+++ cloveretl.engine/src/org/jetel/util/protocols/sftp/SFTPStreamHandler.java
@@ -19,4 +19,13 @@ public class SFTPStreamHandler extends URLStreamHandler {
 	public URLConnection openConnection(URL url) throws IOException {
 		return new SFTPConnection(url);
 	}
+	
+    protected void parseURL(URL u, String spec, int start, int limit) {
+    	super.parseURL(u, spec, start, limit);
+    	String protocol = u.getProtocol();
+    	if (!protocol.equals("sftp") || protocol.equals("scp")) {
+    		throw new RuntimeException("Parse error: The URL protocol must be sftp or scp!");
+    	}
+    }
+
 }
