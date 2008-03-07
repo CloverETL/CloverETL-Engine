3a6d5782d2afe8b4dbaf8a4180a9ca02b169253c
agad <info@cloveretl.com>
2008-03-07 09:42:07 +0000

    CHANGE:new example structure
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@4194 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/examples/run.bat
+++ cloveretl.engine/examples/run.bat
@@ -6,15 +6,14 @@ rem ********************************
 
 rem ***** set this before using script *****
 set ENGINE_PATH=..
-set TRANSFORM_PATH=javaExamples
+set TRANSFORM_PATH=trans
 rem ****************************************
 
-set ENGINE_JAR=%ENGINE_PATH%\cloveretl.engine.jar
+set ENGINE_JAR=%ENGINE_PATH%\lib\cloveretl.engine.jar
 set COMMONS_LOGGING_JAR=%ENGINE_PATH%\lib\commons-logging.jar
 set JAVOLUTION_JAR=%ENGINE_PATH%\lib\javolution.jar
 set JMS_JAR=%ENGINE_PATH%\lib\jms.jar
 set JXL_JAR=%ENGINE_PATH%\lib\jxl.jar
 set LOG4J_JAR=%ENGINE_PATH%\lib\log4j-1.2.12.zip
-set POI_JAR=%ENGINE_PATH%\lib\poi-2.5.1.jar
 
-java -cp "%TRANSFORM_PATH%;%ENGINE_JAR%;%COMMONS_LOGGING_JAR%;%JAVOLUTION_JAR%;%JMS_JAR%;%JXL_JAR%;%LOG4J_JAR%;%POI_JAR%;%JAVA_HOME%\lib\tools.jar" org.jetel.main.runGraph -plugins %ENGINE_PATH%\plugins %1
+java -cp "%TRANSFORM_PATH%;%ENGINE_JAR%;%COMMONS_LOGGING_JAR%;%JAVOLUTION_JAR%;%JMS_JAR%;%JXL_JAR%;%LOG4J_JAR%" org.jetel.main.runGraph -plugins %ENGINE_PATH%\plugins graph\%1
--- cloveretl.engine/examples/run.sh
+++ cloveretl.engine/examples/run.sh
@@ -4,13 +4,13 @@
 # usage - ./run.sh <graph_name.grf>
 # ***** set this before using script *****
 ENGINE_PATH=".."
-TRANSFORM_PATH="javaExamples"
+TRANSFORM_PATH="trans"
 # ****************************************
 
-ENGINE_JAR="$ENGINE_PATH/cloveretl.engine.jar"
+ENGINE_JAR="$ENGINE_PATH/lib/cloveretl.engine.jar"
 
 for i in $ENGINE_PATH/lib/*.jar $ENGINE_PATH/lib/*.zip; do
 JARS="$JARS:$i"
 done;
 
-java -classpath "$TRANSFORM_PATH:$ENGINE_JAR:$JARS:$JAVA_HOME/lib/tools.jar" org.jetel.main.runGraph -plugins $ENGINE_PATH/plugins $1
+java -classpath "$TRANSFORM_PATH:$ENGINE_JAR:$JARS" org.jetel.main.runGraph -plugins $ENGINE_PATH/plugins graph/$1
