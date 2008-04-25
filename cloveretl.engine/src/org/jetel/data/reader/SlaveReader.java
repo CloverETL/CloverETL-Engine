0d7c5415c1545dd46dc7594207b32716e3cc254b
agad <info@cloveretl.com>
2008-04-25 09:30:23 +0000

    CHANGE:inner classes are stand alone
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@4397 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/src/org/jetel/data/reader/SlaveReader.java
@@ -0,0 +1,136 @@
+package org.jetel.data.reader;
+
+import java.io.IOException;
+
+import org.jetel.data.DataRecord;
+import org.jetel.data.RecordKey;
+import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.graph.InputPort;
+
+/**
+ * Slave reader without duplicates support. Pretends that all runs contain only one record.
+ * Doesn't use buffer, supports rewind operation.
+ * @author Jan Hadrava, Javlin Consulting (www.javlinconsulting.cz)
+ *
+ */
+public class SlaveReader implements InputReader {
+	private static final int CURRENT = 0;
+	private static final int NEXT = 1;
+
+	private InputPort inPort;
+	private RecordKey key;
+	private DataRecord[] rec = new DataRecord[2];
+	private boolean firstRun;
+	private boolean needsRewind;
+
+	public SlaveReader(InputPort inPort, RecordKey key) {
+		this.inPort = inPort;
+		this.key = key;
+		this.rec[CURRENT] = new DataRecord(inPort.getMetadata());
+		this.rec[NEXT] = new DataRecord(inPort.getMetadata());
+		this.rec[CURRENT].init();
+		this.rec[NEXT].init();
+		this.firstRun = true;
+		this.needsRewind = true;
+	}
+	
+	public void reset() throws ComponentNotReadyException {
+		inPort.reset();
+		this.rec[CURRENT] = new DataRecord(inPort.getMetadata());
+		this.rec[NEXT] = new DataRecord(inPort.getMetadata());
+		this.rec[CURRENT].init();
+		this.rec[NEXT].init();
+		this.firstRun = true;
+		this.needsRewind = true;
+	}
+	
+	public void free() {
+		inPort = null;
+	}
+	
+	private void swap() {
+		DataRecord tmp = rec[CURRENT];
+		rec[CURRENT] = rec[NEXT];
+		rec[NEXT] = tmp;
+	}
+
+	public boolean loadNextRun() throws InterruptedException, IOException {
+		if (inPort == null) {
+			rec[CURRENT] = rec[NEXT] = null;
+			return false;
+		} 
+		if (firstRun) {	// first call of this function
+			firstRun = false;
+			// load first record of the run
+			if (inPort.readRecord(rec[NEXT]) == null) {
+				rec[CURRENT] = rec[NEXT] = null;
+				return false;
+			}
+		}
+		swap();
+		while (true) {
+		// current record is now the first one from the run to be loaded
+		// set current record to the last one from the run to be loaded and next record to the first one
+		// from the following run
+		needsRewind = false;
+			rec[NEXT].reset();
+			if (inPort.readRecord(rec[NEXT]) == null) {
+				rec[NEXT] = null;
+				return true;
+			}
+			if (key.compare(rec[CURRENT], rec[NEXT]) != 0) {	// beginning of new run
+				return true;
+			}
+			swap();
+		}
+	}
+
+	public void rewindRun() {
+		needsRewind = false;
+	}
+
+	public DataRecord getSample() {
+		if (firstRun) {
+			return null;
+		}
+		return rec[CURRENT];
+	}
+
+	public DataRecord next() throws IOException {
+		if (firstRun || needsRewind) {
+			return null;
+		}
+		needsRewind = true;
+		return rec[CURRENT];
+	}
+
+	public RecordKey getKey() {
+		return key;
+	}
+	
+	public int compare(InputReader other) {
+		DataRecord rec1 = getSample();
+		DataRecord rec2 = other.getSample();
+//		if (rec1 == null) {
+//			return rec2 == null ? 0 : -1;
+//		} else if (rec2 == null) {
+//			return 1;
+//		}
+		if (rec1 == null) {
+			return rec2 == null ? 0 : 1;	// null is greater than any other reader (as in DriverReader)
+		} else if (rec2 == null) {
+			return -1;
+		}
+		return key.compare(other.getKey(), rec1, rec2);
+	}
+
+	public boolean hasData() {
+		return rec[NEXT] != null || rec[CURRENT] != null;
+	}		
+
+	@Override
+	public String toString() {
+		return getSample().toString();
+	}
+
+}
---
+++ cloveretl.engine/src/org/jetel/data/reader/SlaveReaderDup.java
@@ -0,0 +1,155 @@
+package org.jetel.data.reader;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.jetel.data.DataRecord;
+import org.jetel.data.Defaults;
+import org.jetel.data.FileRecordBuffer;
+import org.jetel.data.RecordKey;
+import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.graph.InputPort;
+
+/**
+ * Slave reader with duplicates support. Uses file buffer to store duplicate records. Support rewind operation.
+ * @author Jan Hadrava, Javlin Consulting (www.javlinconsulting.cz)
+ *
+ */
+public class SlaveReaderDup implements InputReader {
+	private static final int CURRENT = 0;
+	private static final int NEXT = 1;
+
+	private InputPort inPort;
+	private RecordKey key;
+	private DataRecord[] rec = new DataRecord[2];
+	private FileRecordBuffer recBuf;
+	private ByteBuffer rawRec = ByteBuffer.allocateDirect(Defaults.Record.MAX_RECORD_SIZE);
+	private boolean firstRun;
+	private boolean getFirst;
+	DataRecord deserializedRec;
+
+	public SlaveReaderDup(InputPort inPort, RecordKey key) {
+		this.inPort = inPort;
+		this.key = key;
+		this.deserializedRec = new DataRecord(inPort.getMetadata());
+		this.deserializedRec.init();
+		this.rec[CURRENT] = new DataRecord(inPort.getMetadata());
+		this.rec[NEXT] = new DataRecord(inPort.getMetadata());
+		this.rec[CURRENT].init();
+		this.rec[NEXT].init();
+		this.recBuf = new FileRecordBuffer(null);
+		this.firstRun = true;
+	}
+	
+	public void reset() throws ComponentNotReadyException {
+		inPort.reset();
+		this.rec[CURRENT] = new DataRecord(inPort.getMetadata());
+		this.rec[NEXT] = new DataRecord(inPort.getMetadata());
+		this.rec[CURRENT].init();
+		this.rec[NEXT].init();
+		recBuf.clear();
+		this.firstRun = true;
+	}
+	
+	public void free() {
+		inPort = null;
+	}
+	
+	private void swap() {
+		DataRecord tmp = rec[CURRENT];
+		rec[CURRENT] = rec[NEXT];
+		rec[NEXT] = tmp;
+	}
+
+	public boolean loadNextRun() throws InterruptedException, IOException {
+		getFirst = true;
+		if (inPort == null) {
+			rec[CURRENT] = rec[NEXT] = null;
+			return false;
+		} 
+		if (firstRun) {	// first call of this function
+			firstRun = false;
+			// load first record of the run
+			if (inPort.readRecord(rec[NEXT]) == null) {
+				rec[CURRENT] = rec[NEXT] = null;
+				return false;
+			}
+		}
+		recBuf.clear();
+		swap();
+		while (true) {
+			rec[NEXT].reset();
+			if (inPort.readRecord(rec[NEXT]) == null) {
+				rec[NEXT] = null;
+				return true;
+			}
+			if (key.compare(rec[CURRENT], rec[NEXT]) != 0) {	// beginning of new run
+				return true;
+			}
+			// move record to buffer
+			rawRec.clear();
+			rec[NEXT].serialize(rawRec);
+			rawRec.flip();
+			recBuf.push(rawRec);				
+		}
+	}
+
+	public void rewindRun() {
+		getFirst = true;
+		recBuf.rewind();
+	}
+
+	public DataRecord getSample() {
+		if (firstRun) {
+			return null;
+		}
+		return rec[CURRENT];
+	}
+
+	public DataRecord next() throws IOException {
+		if (firstRun) {
+			return null;
+		}
+		if (getFirst) {
+			getFirst = false;
+			return rec[CURRENT];
+		}
+		rawRec.clear();
+		if (recBuf.shift(rawRec) == null) {
+			return null;
+		}
+		rawRec.flip();
+		deserializedRec.deserialize(rawRec);
+		return deserializedRec;
+	}
+
+	public RecordKey getKey() {
+		return key;
+	}
+	
+	public int compare(InputReader other) {
+		DataRecord rec1 = getSample();
+		DataRecord rec2 = other.getSample();
+//		if (rec1 == null) {
+//			return rec2 == null ? 0 : -1;
+//		} else if (rec2 == null) {
+//			return 1;
+//		}
+		if (rec1 == null) {
+			return rec2 == null ? 0 : 1;	// null is greater than any other reader (as in DriverReader)
+		} else if (rec2 == null) {
+			return -1;
+		}
+		return key.compare(other.getKey(), rec1, rec2);
+	}
+	
+	public boolean hasData() {
+		return rec[NEXT] != null;
+	}		
+
+	@Override
+	public String toString() {
+		return getSample().toString();
+	}
+
+}
