a24d1c14dd70f0a82a3fe9ef83a29d3f30689c95
otasanek <info@cloveretl.com>
2008-05-19 08:14:43 +0000

    UPDATE: fromXML() method was added into GraphElement class, this should cause non-obfuscation of this method in all  descendants.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@4497 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/graph/GraphElement.java
+++ cloveretl.engine/src/org/jetel/graph/GraphElement.java
@@ -174,7 +174,8 @@ public abstract class GraphElement implements IGraphElement {
     }
     
 	/**
-	 * Only purpose of this implementation is obfuscation. Method fromXML() should not be obfuscated.
+	 * Only purpose of this implementation is obfuscation. 
+	 * Method fromXML() should not be obfuscated in all descendants.
 	 */
 	public static Node fromXML(TransformationGraph graph, Element xmlElement)throws XMLConfigurationException {
         throw new UnsupportedOperationException("not implemented in org.jetel.graph.GraphElement"); 
