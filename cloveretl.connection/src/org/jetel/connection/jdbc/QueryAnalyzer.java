24e8f208f4224018b3ba12e74d424dea63ddbfd3
agad <info@cloveretl.com>
2008-05-27 10:34:57 +0000

    BUGFIX:fix from branch 2.4 - needed for GUI
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@4557 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.connection/src/org/jetel/connection/jdbc/QueryAnalyzer.java
+++ cloveretl.connection/src/org/jetel/connection/jdbc/QueryAnalyzer.java
@@ -23,14 +23,10 @@ package org.jetel.connection.jdbc;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.jetel.data.Defaults;
-import org.jetel.util.primitive.DuplicateKeyMap;
 import org.jetel.util.string.StringUtils;
 
 /**
@@ -50,18 +46,23 @@ import org.jetel.util.string.StringUtils;
  */
 public class QueryAnalyzer {
 	
-	private final static Pattern CLOVER_DB_MAPPING_PATTERN = Pattern.compile(
-			"(\\$([\\w\\?]+)\\s*" + Defaults.ASSIGN_SIGN + "\\s*)([\\w.]+)");//$cloverField:=dbfield
+	public final static String ASSIGN_SIGN = ":=";
+	public final static char CLOVER_FIELD_PREFIX_CHAR = '$';
+	public final static String CLOVER_FIELD_PREFIX = String.valueOf(CLOVER_FIELD_PREFIX_CHAR);
+
+	private final static Pattern CLOVER_DB_MAPPING_PATTERN = Pattern.compile("(\\$([\\w\\?]+)\\s*" + ASSIGN_SIGN + "\\s*)([\\w.]+)");//$cloverField:=dbfield
 	private final static Pattern DB_CLOVER_MAPPING_PATTERN = Pattern.compile("(([\\w]+)\\s*=)\\s*[\\?\\$](\\w*)");//dbField = $cloverField or dbField = ?
 	private final static Pattern FIELDS_PATTERN = Pattern.compile("\\$?([\\w.]+)|\\?");//[$]field or ?
-	private final static Pattern CLOVER_FIELDS_PATTERN = Pattern.compile(Defaults.CLOVER_FIELD_REGEX);//$cloverField
+	private final static Pattern CLOVER_FIELDS_PATTERN = Pattern.compile("\\$\\w+");//$cloverField
 	private final static Pattern DB_FIELDS_PATTERN = Pattern.compile("\\w+");//dbField
 	private final static Pattern FIELDS_LIST_PATTERN = Pattern.compile("\\((\\s*\\$?\\w+|\\?)[\\.,\\s\\$\\w\\?]*\\)");//(dbfield1,dbField2,..) or (smth, $field1, ...)
 	
 	private String query;
 	private String source;
-	private HashMap<String, String> cloverDbFieldMap = new LinkedHashMap<String, String>();
-	private DuplicateKeyMap dbCloverFieldMap = new DuplicateKeyMap(new LinkedHashMap<String, String>());
+	private List<String[]> cloverDbFieldMap = new ArrayList<String[]>();
+	private List<String[]> dbCloverFieldMap = new ArrayList<String[]>();
+//	private HashMap<String, String> cloverDbFieldMap = new LinkedHashMap<String, String>();
+//	private DuplicateKeyMap dbCloverFieldMap = new DuplicateKeyMap(new LinkedHashMap<String, String>());
 	
 	public final static String RETURNING_KEY_WORD = "returning";
 	
@@ -95,27 +96,20 @@ public class QueryAnalyzer {
 	}
 	
 	/**
-	 * Analyze select/update/delete query
+	 * Analyze update/delete query
 	 * 
 	 * @return query in form consumable for PreparedStatement object 
 	 */
-	public String getNotInsertQuery() {
-		//find $field:=dbfield mapping
-		Matcher mappingMatcher = CLOVER_DB_MAPPING_PATTERN.matcher(query);
+	public String getUpdateDeleteQuery() {
+		Matcher mappingMatcher = DB_CLOVER_MAPPING_PATTERN.matcher(query); 
 		StringBuffer sb = new StringBuffer();
 		while(mappingMatcher.find()){
-			cloverDbFieldMap.put(mappingMatcher.group(2), mappingMatcher.group(3));
-			mappingMatcher.appendReplacement(sb, mappingMatcher.group(3));
-		}
-		mappingMatcher.appendTail(sb);
-		//find dbField = $field or dbField = ? mapping
-		mappingMatcher = DB_CLOVER_MAPPING_PATTERN.matcher(sb.toString()); 
-		sb.setLength(0);
-		while(mappingMatcher.find()){
 			if (!StringUtils.isEmpty(mappingMatcher.group(3))) {//found dbField = $field
-				dbCloverFieldMap.put(mappingMatcher.group(2), mappingMatcher.group(3));
+				dbCloverFieldMap.add(new String[]{mappingMatcher.group(2), mappingMatcher.group(3)});
+//				dbCloverFieldMap.put(mappingMatcher.group(2), mappingMatcher.group(3));
 			}else{//found dbField = ?
-				dbCloverFieldMap.put(mappingMatcher.group(2), null);
+				dbCloverFieldMap.add(new String[]{mappingMatcher.group(2), null});
+//				dbCloverFieldMap.put(mappingMatcher.group(2), null);
 			}
 			//replace whole mapping by dbField = ?
 			mappingMatcher.appendReplacement(sb, mappingMatcher.group(1) + "?");
@@ -125,9 +119,52 @@ public class QueryAnalyzer {
 	}
 		
 	/**
+	 * Analyze select query
+	 * 
+	 * @return query in form consumable for PreparedStatement object 
+	 */
+	public String getSelectQuery(){
+		Matcher mappingMatcher = CLOVER_DB_MAPPING_PATTERN.matcher(query);
+		StringBuffer sb = new StringBuffer();
+		if (mappingMatcher.find()){
+			do {
+				cloverDbFieldMap.add(new String[]{mappingMatcher.group(2), mappingMatcher.group(3)});				
+				mappingMatcher.appendReplacement(sb, mappingMatcher.group(3));
+			}while (mappingMatcher.find());
+		}else{
+			String[] fieldList = query.substring("select".length(), 
+					query.toLowerCase().indexOf("from") - 1).trim().split(",");
+			if (fieldList.length != 1 || !fieldList[0].equals("*")) {//not: select * from
+				for (int i = 0; i < fieldList.length; i++) {
+					cloverDbFieldMap.add(new String[] { null,	fieldList[i].trim() });
+				}
+			}
+		}
+		mappingMatcher.appendTail(sb);
+		//find dbField = $field or dbField = ? mapping
+		mappingMatcher = DB_CLOVER_MAPPING_PATTERN.matcher(sb.toString()); 
+		sb.setLength(0);
+		while(mappingMatcher.find()){
+			if (!StringUtils.isEmpty(mappingMatcher.group(3))) {//found dbField = $field
+				dbCloverFieldMap.add(new String[]{mappingMatcher.group(2), mappingMatcher.group(3)});
+//				dbCloverFieldMap.put(mappingMatcher.group(2), mappingMatcher.group(3));
+			}else{//found dbField = ?
+				dbCloverFieldMap.add(new String[]{mappingMatcher.group(2), null});
+//				dbCloverFieldMap.put(mappingMatcher.group(2), null);
+			}
+			//replace whole mapping by dbField = ?
+			mappingMatcher.appendReplacement(sb, mappingMatcher.group(1) + "?");
+		}
+		mappingMatcher.appendTail(sb);
+		return sb.toString();
+	}
+	
+	
+	/**
 	 * Analyze insert query
 	 * 
 	 * @return query in form consumable for PreparedStatement object 
+	 * @throws SQLException 
 	 */
 	public String getInsertQuery() throws SQLException{
 		List<String> dbFields = new ArrayList<String>();
@@ -154,10 +191,13 @@ public class QueryAnalyzer {
 			if (size > 0 && index >= size) {
 				throw new SQLException("Error in sql query: " + query);
 			}
-			if (cloverFieldsMatcher.group().startsWith(Defaults.CLOVER_FIELD_INDICATOR)) {
-				dbCloverFieldMap.put(size > 0 ? dbFields.get(index) : null, cloverFieldsMatcher.group(1));
+			if (cloverFieldsMatcher.group().startsWith(CLOVER_FIELD_PREFIX)) {
+				dbCloverFieldMap.add(new String[]{size > 0 ? dbFields.get(index) : null, 
+						cloverFieldsMatcher.group(1)});
+//				dbCloverFieldMap.put(size > 0 ? dbFields.get(index) : null, cloverFieldsMatcher.group(1));
 			}else if (index < size){
-				dbCloverFieldMap.put(dbFields.get(index), null);
+				dbCloverFieldMap.add(new String[]{dbFields.get(index), null});
+//				dbCloverFieldMap.put(dbFields.get(index), null);
 			}
 			index++;
 		}
@@ -166,7 +206,8 @@ public class QueryAnalyzer {
 		if (indexReturning > -1) {
 			Matcher mappingMatcher = CLOVER_DB_MAPPING_PATTERN.matcher(query);
 			while(mappingMatcher.find()){
-				cloverDbFieldMap.put(mappingMatcher.group(2), mappingMatcher.group(3));
+				cloverDbFieldMap.add(new String[]{mappingMatcher.group(2), mappingMatcher.group(3)});
+//				cloverDbFieldMap.put(mappingMatcher.group(2), mappingMatcher.group(3));
 			}
 		}
 		return indexReturning == -1 ? 
@@ -177,14 +218,14 @@ public class QueryAnalyzer {
 	/**
 	 * @return clover - db mapping
 	 */
-	public HashMap<String, String> getCloverDbFieldMap() {
+	public List<String[]> getCloverDbFieldMap() {
 		return cloverDbFieldMap;
 	}
 
 	/**
 	 * @return db - clover mapping (can have null as a key eg: Insert into abc values($f1, sysdate , $f2, $f3))
 	 */
-	public DuplicateKeyMap getDbCloverFieldMap() {
+	public List<String[]> getDbCloverFieldMap() {
 		return dbCloverFieldMap;
 	}
 
