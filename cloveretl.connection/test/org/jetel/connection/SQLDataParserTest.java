741e3bbbb896a66f4c9fbba6c6b36919aff0fd36
csochor <info@cloveretl.com>
2008-06-04 17:20:19 +0000

    tests added to classpath, moved to package org.jetel.connection and code style improvements
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@4615 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.connection/test/org/jetel/connection/SQLDataParserTest.java
+++ cloveretl.connection/test/org/jetel/connection/SQLDataParserTest.java
@@ -1,3 +1,4 @@
+package org.jetel.connection;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -9,6 +10,7 @@ import org.jetel.connection.jdbc.SQLDataParser;
 import org.jetel.data.DataRecord;
 import org.jetel.exception.BadDataFormatException;
 import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.exception.JetelException;
 import org.jetel.graph.runtime.EngineInitializer;
 import org.jetel.metadata.DataRecordMetadata;
 import org.jetel.metadata.DataRecordMetadataXMLReaderWriter;
@@ -22,43 +24,30 @@ public class SQLDataParserTest extends TestCase {
 
 	private DataRecord record;
 
-	protected void setUp() {
-	    EngineInitializer.initEngine("../cloveretl.engine/plugins", null, null);
+	@SuppressWarnings("deprecation")
+	protected void setUp() throws ComponentNotReadyException, JetelException, FileNotFoundException {
+		EngineInitializer.initEngine("../cloveretl.engine/plugins", null, null);
 		DataRecordMetadata metadata = null;
 		DataRecordMetadataXMLReaderWriter xmlReader = new DataRecordMetadataXMLReaderWriter();
 		DBConnection aDBConnection = null;
 
-		try {
-			// metadata = xmlReader.read(new
-			// FileInputStream("config\\test\\rec_def\\db_null_def_rec.xml"));
-			// aDBConnection = new DBConnection("",
-			// "config\\test\\msaccess.clover_test.txt");
-			metadata = xmlReader.read(new FileInputStream(
-					"../cloveretl.engine/config/test/rec_def/db_def_rec.xml"));
-			aDBConnection = new DBConnection("conn",
-					"../cloveretl.engine/examples/koule_postgre.cfg");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		// metadata = xmlReader.read(new
+		// FileInputStream("config\\test\\rec_def\\db_null_def_rec.xml"));
+		// aDBConnection = new DBConnection("",
+		// "config\\test\\msaccess.clover_test.txt");
+		metadata = xmlReader.read(new FileInputStream("../cloveretl.engine/config/test/rec_def/db_def_rec.xml"));
+		aDBConnection = new DBConnection("conn", "../cloveretl.engine/examples/koule_postgre.cfg");
 
-		try {
-			aDBConnection.init();
-		} catch (ComponentNotReadyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		aDBConnection.init();
 
 		record = new DataRecord(metadata);
 		record.init();
 
 		aParser2 = new SQLDataParser("connection", "SELECT * FROM good");
-		try {
-			aParser2.init(metadata);
-			aParser2.setDataSource(aDBConnection.getConnection(aDBConnection.getId()));
-			aParser2.initSQLDataMap(record);
-		} catch (ComponentNotReadyException e1) {
-			e1.printStackTrace();
-		}
+
+		aParser2.init(metadata);
+		aParser2.setDataSource(aDBConnection.getConnection(aDBConnection.getId()));
+		aParser2.initSQLDataMap(record);
 	}
 
 	protected void tearDown() {
@@ -75,20 +64,17 @@ public class SQLDataParserTest extends TestCase {
 					assertEquals(record.getField(0).toString(), "1.0");
 					assertEquals(record.getField(1).toString(), "Stone");
 					assertEquals(record.getField(2).toString(), "101");
-					assertEquals(record.getField(3).toString(),
-							"1993-01-11 00:00:00");
+					assertEquals(record.getField(3).toString(), "1993-01-11 00:00:00");
 				} else if (recCount == 2) {
 					assertEquals(record.getField(0).toString(), "-15.5");
 					assertEquals(record.getField(2).toString(), "112");
 					assertEquals(record.getField(1).toString(), "Brook");
-					assertEquals(record.getField(3).toString(),
-							"2002-11-03 00:00:00");
+					assertEquals(record.getField(3).toString(), "2002-11-03 00:00:00");
 				} else if (recCount == 3) {
 					assertEquals(record.getField(0).toString(), "-0.7");
 					assertEquals(record.getField(1).toString(), "Bone Broo");
 					assertEquals(record.getField(2).toString(), "99");
-					assertEquals(record.getField(3).toString(),
-							"1993-01-01 00:00:00");
+					assertEquals(record.getField(3).toString(), "1993-01-01 00:00:00");
 				}
 				recCount++;
 			}
