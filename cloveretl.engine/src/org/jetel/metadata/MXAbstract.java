e71a39d1add6064c407260512f1de739ca984cdb
jausperger <jausperger@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2008-06-18 15:14:30 +0000

    NEW: transformation from xsd to metadata and metadata to xsd
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@4698 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/metadata/MXAbstract.java
+++ cloveretl.engine/src/org/jetel/metadata/MXAbstract.java
@@ -4,17 +4,39 @@ import java.util.HashMap;
 
 abstract public class MXAbstract {
 
+	// xsd
 	protected static final String NAMESPACE = "";
 	protected static final String XMLSCHEMA = "http://www.w3.org/2001/XMLSchema";
+	protected static final String XMLNS_XSD = "xmlns:xsd";
+	protected static final String XMLNS = "xmlns";
 
-	// elements
+	// xsd elements
+	protected static final String XSD_SCHEMA = "xsd:schema";
 	protected static final String XSD_ELEMENT = "xsd:element";
 	protected static final String XSD_COMPLEX_TYPE = "xsd:complexType";
 	protected static final String XSD_SEQUENCE = "xsd:sequence";
+	protected static final String XSD_SIMPLE_TYPE = "xsd:simpleType";
+	protected static final String XSD_RESTRICTION = "xsd:restriction";
+	
+	// xsd restrictions
+	protected static final String XSD_LENGHT = "xsd:length";
+	protected static final String XSD_PATTERN = "xsd:pattern";
+	protected static final String XSD_FRACTION_DIGITS = "xsd:fractionDigits";
+	protected static final String XSD_TOTAL_DIGITS = "xsd:totalDigits";
+	
+	// xsd base data types
+	protected static final String XSD_DECIMAL = "xsd:decimal";
+	protected static final String XSD_BASE64BINARY = "xsd:base64Binary";
 
-	// record attributes
+	// xsd attributes
 	protected static final String NAME = "name";
 	protected static final String TYPE = "type";
+	protected static final String BASE = "base";
+	protected static final String VALUE = "value";
+
+	// record attributes
+	protected static final String LENGTH = "length";
+	protected static final String SCALE = "scale";
 	protected static final String FIELD_DELIMITER = "fieldDelimiterStr";
 	protected static final String FIELD_NAMES_HEADER = "fieldNamesHeader";
 	protected static final String LOCALE_STR = "localeStr";
@@ -28,24 +50,16 @@ abstract public class MXAbstract {
 	protected static final String SPECIFIED_FIELD_DELIMITER = "specifiedFieldDelimiter";
 	protected static final String SPECIFIED_RECORD_DELIMITER = "specifiedRecordDelimiter";
 	
-	// field attributes
-	protected static final String AUTO_FILLING = "autoFilling";
-	protected static final String DEFAULT_VALUE_STR = "defaultValueStr";
-	protected static final String DELIMITER_STR = "delimiterStr";
-	protected static final String FORMAT_STR = "formatStr";
-	protected static final String DEFAULT_VALUE = "defaultValue";
-	protected static final String FIELD_PROPERTIES = "fieldProperties";
-	protected static final String SHIFT = "shift";
-	protected static final String SIZE = "size";
-	
-	// properties
-	protected static final String PROPERTIES_ASSIGN = "=";
-	protected static final String PROPERTIES_DELIM = ";";
 	
 	// mapping from field type to XSD type representing the field type
 	protected static final HashMap<Character, String> typeNames = new HashMap<Character, String>();
+	
 	// mapping from field type to XSD type used as base for XSD type representing the field type
 	protected static final HashMap<Character, String> primitiveNames = new HashMap<Character, String>();
+	
+	// inverse primitiveNames mapping
+	protected static final HashMap<String, Character> namesPrimitive = new HashMap<String, Character>();
+
 	// initialize mappings
 	static {
 		typeNames.put(Character.valueOf(DataFieldMetadata.BYTE_FIELD), "CloverByte");
@@ -59,17 +73,21 @@ abstract public class MXAbstract {
 		typeNames.put(Character.valueOf(DataFieldMetadata.STRING_FIELD), "CloverString");
 		typeNames.put(Character.valueOf(DataFieldMetadata.BOOLEAN_FIELD), "CloverBoolean");
 
-		primitiveNames.put(Character.valueOf(DataFieldMetadata.BYTE_FIELD), "xsd:base64Binary");
-		primitiveNames.put(Character.valueOf(DataFieldMetadata.BYTE_FIELD_COMPRESSED), "xsd:base64Binary");
+		primitiveNames.put(Character.valueOf(DataFieldMetadata.BYTE_FIELD), XSD_BASE64BINARY);
+		primitiveNames.put(Character.valueOf(DataFieldMetadata.BYTE_FIELD_COMPRESSED), XSD_BASE64BINARY);
 		primitiveNames.put(Character.valueOf(DataFieldMetadata.DATE_FIELD), "xsd:date");
 		primitiveNames.put(Character.valueOf(DataFieldMetadata.DATETIME_FIELD), "xsd:dateTime");
-		primitiveNames.put(Character.valueOf(DataFieldMetadata.DECIMAL_FIELD), "xsd:decimal");
+		primitiveNames.put(Character.valueOf(DataFieldMetadata.DECIMAL_FIELD), XSD_DECIMAL);
 		primitiveNames.put(Character.valueOf(DataFieldMetadata.INTEGER_FIELD), "xsd:int");
 		primitiveNames.put(Character.valueOf(DataFieldMetadata.LONG_FIELD), "xsd:long");
-		primitiveNames.put(Character.valueOf(DataFieldMetadata.NUMERIC_FIELD), "xsd:decimal");
+		primitiveNames.put(Character.valueOf(DataFieldMetadata.NUMERIC_FIELD), XSD_DECIMAL);
 		primitiveNames.put(Character.valueOf(DataFieldMetadata.STRING_FIELD), "xsd:string");
 		primitiveNames.put(Character.valueOf(DataFieldMetadata.BOOLEAN_FIELD), "xsd:boolean");
+		
+		for (Character ch: primitiveNames.keySet()) {
+			if (ch == DataFieldMetadata.BYTE_FIELD_COMPRESSED || ch == DataFieldMetadata.NUMERIC_FIELD) continue;
+			namesPrimitive.put(primitiveNames.get(ch), ch);
+		}
 	}
-
 	
 }
