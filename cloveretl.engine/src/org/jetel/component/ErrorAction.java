9d29bc11a1ef686877207187f1a683411ee718f7
agad <agad@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2008-09-22 08:50:45 +0000

    UPDATE#841:reformat components- user can define error action
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@5078 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/src/org/jetel/component/ErrorAction.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package org.jetel.component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.util.joinKey.JoinKeyUtils;
+import org.jetel.util.string.StringUtils;
+
+public enum ErrorAction {
+	STOP,
+	CONTINUE;
+	
+	final static ErrorAction DEFAULT_ERROR_ACTION = STOP;
+	
+	public static Map<Integer, ErrorAction> createMap(String errorActionsString){
+	    Map<Integer, ErrorAction> errorActions = new HashMap<Integer, ErrorAction>();
+		if (errorActionsString != null){
+	        	String[] actions = StringUtils.split(errorActionsString);
+	        	if (actions.length == 1 && !actions[0].contains("=")){
+	        		errorActions.put(Integer.MIN_VALUE, ErrorAction.valueOf(actions[0].trim().toUpperCase()));
+	        	}else{
+	        	String[] action;
+		        	for (String string : actions) {
+						action = JoinKeyUtils.getMappingItemsFromMappingString(string);
+						errorActions.put(Integer.parseInt(action[0]), ErrorAction.valueOf(action[1].toUpperCase()));
+					}
+	        	}
+	        }else{
+	        	errorActions.put(-1, ErrorAction.CONTINUE);
+	        	errorActions.put(Integer.MIN_VALUE, DEFAULT_ERROR_ACTION);
+	        }
+		return errorActions;
+	}
+	
+	public static boolean checkActions(String errorActionsString) throws ComponentNotReadyException{
+    	String[] actions = StringUtils.split(errorActionsString);
+    	if (actions.length == 1 && !actions[0].contains("=")){
+			if (ErrorAction.valueOf(actions[0].trim().toUpperCase()) == null) {
+				throw new ComponentNotReadyException("Unknown error action: " + StringUtils.quote(actions[0]));
+			}
+    	}else{
+        	String[] action;
+        	for (String string : actions) {
+				action = JoinKeyUtils.getMappingItemsFromMappingString(string);
+				try{
+					Integer.parseInt(action[0]);
+				}catch(NumberFormatException e) {
+					throw new ComponentNotReadyException(e);
+				}
+				if (ErrorAction.valueOf(action[1].toUpperCase()) == null) {
+					throw new ComponentNotReadyException("Unknown error action: " + StringUtils.quote(actions[1]));
+				}
+ 			}
+    	}
+    	return true;
+	}
+}
