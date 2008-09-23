935c9b77c386a0ded86d7d0ae5623040c6cb7595
agad <info@cloveretl.com>
2008-09-23 11:38:21 +0000

    MINOR:code cleansing
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@5088 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/component/RecordTransformFactory.java
+++ cloveretl.engine/src/org/jetel/component/RecordTransformFactory.java
@@ -52,7 +52,6 @@ public class RecordTransformFactory {
     
     public static final Pattern PATTERN_CLASS = Pattern.compile("class\\s+\\w+"); 
     public static final Pattern PATTERN_TL_CODE = Pattern.compile("function\\s+transform");
-    public static final Pattern PATTERN_DENORMALIZE_CODE = Pattern.compile("function\\s+transform");
     public static final Pattern PATTERN_PARTITION_CODE = Pattern.compile("function\\s+getOutputPort"); 
     
     public static final Pattern PATTERN_PREPROCESS_1 = Pattern.compile("\\$\\{out\\."); 
@@ -241,7 +240,6 @@ public class RecordTransformFactory {
             return TRANSFORM_CLOVER_TL;
         }
         if (PATTERN_TL_CODE.matcher(transform).find() 
-        		|| PATTERN_DENORMALIZE_CODE.matcher(transform).find()
         		|| PATTERN_PARTITION_CODE.matcher(transform).find()){
             // clover internal transformation language
             return TRANSFORM_CLOVER_TL;
