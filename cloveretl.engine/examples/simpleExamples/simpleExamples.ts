64ded6fcb01907b89f0e96a24ee962814996cc10
csochor <info@cloveretl.com>
2008-11-20 16:36:16 +0000

    renamed test scenarios
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@5360 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/examples/simpleExamples/simpleExamples.ts
+++ cloveretl.engine/examples/simpleExamples/simpleExamples.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="windows-1250"?>
 <!DOCTYPE TestScenario SYSTEM "./testscenario.dtd">
-<TestScenario ident="simple-examples" description="Engine simple examples scenario" useJMX="true">    
+<TestScenario ident="simple-examples" description="Engine simple examples" useJMX="true">    
 
     <FunctionalTest ident="simpleCopy" graphFile="graph/graphSimpleCopy.grf">
       <FlatFile outputFile="data-out/friends-country+town-age.dat" supposedFile="supposed-out/friends-country+town-age.dat"/>
