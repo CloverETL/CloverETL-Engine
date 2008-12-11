e3195bae8252c76a52f7dda07ca7164f2c1df189
mpetrik <mpetrik@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2008-12-11 12:20:52 +0000

    update: test scenarios : advanced and ext examples scenarios idents changed to appropriate name
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@5526 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/examples/advancedExamples/advancedExamples.ts
+++ cloveretl.engine/examples/advancedExamples/advancedExamples.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="windows-1250"?>
 <!DOCTYPE TestScenario SYSTEM "testscenario.dtd">
-<TestScenario ident="simple-examples" description="Engine advanced examples" useJMX="true">    
+<TestScenario ident="advanced-examples" description="Engine advanced examples" useJMX="true">
 
 
 </TestScenario>
--- cloveretl.engine/examples/extExamples/extExamples.ts
+++ cloveretl.engine/examples/extExamples/extExamples.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="windows-1250"?>
 <!DOCTYPE TestScenario SYSTEM "testscenario.dtd">
-<TestScenario ident="simple-examples" description="Engine extended examples" useJMX="true">    
+<TestScenario ident="ext-examples" description="Engine extended examples" useJMX="true">
 
 
 </TestScenario>
