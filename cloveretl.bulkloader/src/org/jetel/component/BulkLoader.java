212cdb8669e6ecc24d0ff783dfefb0a1412d1117
mhaupt <info@cloveretl.com>
2009-02-17 13:09:08 +0000

    UPDATE: add SPACE_CHAR constant
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@5885 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.bulkloader/src/org/jetel/component/BulkLoader.java
+++ cloveretl.bulkloader/src/org/jetel/component/BulkLoader.java
@@ -43,6 +43,7 @@ public abstract class BulkLoader extends Node {
 	protected final static String XML_FILE_URL_ATTRIBUTE = "fileURL";
 	protected final static String XML_PARAMETERS_ATTRIBUTE = "parameters";
 	
+	protected final static char SPACE_CHAR = ' ';
 	protected final static String EQUAL_CHAR = "=";
 	protected final static int UNUSED_INT = -1;
 	protected final static File TMP_DIR = new File(".");
