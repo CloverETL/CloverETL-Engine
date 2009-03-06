478987f01844343f7dcbf60998a827ca3f6c46b8
avackova <info@cloveretl.com>
2009-03-06 11:39:31 +0000

    UPDATE:tests for bulk loaders
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@6033 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/examples/extExamples/extExamples.ts
+++ cloveretl.engine/examples/extExamples/extExamples.ts
@@ -3,6 +3,8 @@
 <TestScenario ident="ext-examples" description="Engine extended examples" useJMX="true">
 
 	<DBConnection ident="postgre" type="POSTGRE" user="test" password="test" URL="jdbc:postgresql://koule/test" driver="org.postgresql.Driver" />
+	<DBConnection ident="oracle" type="ORACLE" user="test" password="test" URL="jdbc:oracle:thin:@koule:1521:xe" driver="oracle.jdbc.OracleDriver" />
+	<DBConnection ident="mysql" type="MYSQL" user="test" password="" URL="jdbc:mysql://koule/test" driver="org.gjt.mm.mysql.Driver" />
 
 	<FunctionalTest ident="LDAPReaderWriter" graphFile="graph/graphLdapReaderWriter.grf">
 	</FunctionalTest>
@@ -18,6 +20,30 @@
 	</FunctionalTest>
 	
 	<FunctionalTest ident="OracleDataWriter" graphFile="graph/graphOracleDataWriter.grf">
+	      	<SQLStatement connection="oracle">DELETE FROM test</SQLStatement>
+	      <DBTableToTable
+	      	 outputTable="test" 
+	      	 outputTableConnection="oracle"
+	      	 supposedTable="test_supposed"
+	      	 supposedTableConnection="oracle"
+	      />
+	 	  <FlatFile outputFile="data-out/bad0Port.bad" supposedFile="supposed-out/bad1.OracleDataWriter.kkk"/>	                                                                    
+	 	  <FlatFile outputFile="data-out/bad1.kkk" supposedFile="supposed-out/bad1.OracleDataWriter.kkk"/>	                                                                    
+	 	  <FlatFile outputFile="data-out/bad1Port.bad" supposedFile="supposed-out/bad1.OracleDataWriter.kkk"/>	                                                                    
+	 	  <FlatFile outputFile="data-out/bad2.kkk" supposedFile="supposed-out/bad2.OracleDataWriter.kkk"/>	                                                                    
+	 	  <FlatFile outputFile="data-out/bad2Port.bad" supposedFile="supposed-out/bad2.OracleDataWriter.kkk"/>	                                                                    
+	 	  <FlatFile outputFile="data-out/bad3Port.bad" supposedFile="supposed-out/bad2.OracleDataWriter.kkk"/>	                                                                    
+	</FunctionalTest>
+
+	<FunctionalTest ident="MySqlDataWriter" graphFile="graph/graphMysqlDataWriter.grf">
+	      	<SQLStatement connection="mysql">DELETE FROM test</SQLStatement>
+	      <DBTableToTable
+	      	 outputTable="test" 
+	      	 outputTableConnection="mysql"
+	      	 supposedTable="test_supposed"
+	      	 supposedTableConnection="mysql"
+	      />
+	 	  <FlatFile outputFile="data-out/out.dat" supposedFile="supposed-out/out.MysqlDataWriter.dat"/>	                                                                    
 	</FunctionalTest>
 
 </TestScenario>
