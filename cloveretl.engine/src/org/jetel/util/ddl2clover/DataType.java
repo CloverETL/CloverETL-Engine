4845c1a51dd821b4b85f85bab41b37c615070cae
jausperger <jausperger@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2009-03-25 10:00:23 +0000

    FIX: issue 1659; ddl2metadata - wrong boolean type, date formats and sizes;
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@6198 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/ddl2clover/DataType.java
+++ cloveretl.engine/src/org/jetel/util/ddl2clover/DataType.java
@@ -1,4 +1,5 @@
 package org.jetel.util.ddl2clover;
+
 public final class DataType {
 
     /** Data type, based on java.sql.Types */
@@ -10,10 +11,14 @@ public final class DataType {
     /** Used when data type has scale */
     public final Number scale;
 
-    public DataType(char type, Number length, Number scale) {
+    /** Used when data type has format */
+    public String format;
+
+    public DataType(char type, Number length, Number scale, String format) {
         this.type = type;
         this.length = length;
         this.scale = scale;
+        this.format = format;
     }
 
 }
