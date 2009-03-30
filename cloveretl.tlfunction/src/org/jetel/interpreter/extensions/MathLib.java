6e21428a23bf836dd02791aaca650c292e3d299f
dpavlis <info@cloveretl.com>
2009-03-30 15:49:15 +0000

    INIT:added implementation of bit operations - Issue 0001698
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@6265 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.tlfunction/src/org/jetel/interpreter/extensions/MathLib.java
+++ cloveretl.tlfunction/src/org/jetel/interpreter/extensions/MathLib.java
@@ -28,6 +28,8 @@ import java.util.Collection;
 import java.util.List;
 
 import org.jetel.interpreter.TransformLangExecutorRuntimeException;
+import org.jetel.interpreter.data.TLBooleanValue;
+import org.jetel.interpreter.data.TLListValue;
 import org.jetel.interpreter.data.TLNumericValue;
 import org.jetel.interpreter.data.TLValue;
 import org.jetel.interpreter.data.TLValueType;
@@ -46,7 +48,12 @@ public class MathLib extends TLFunctionLibrary {
         PI("pi"),
         E("e"),
         RANDOM("random"),
-        ABS("abs");
+        ABS("abs"),
+        BIT_AND("bit_and"),
+        BIT_OR("bit_or"),
+        BIT_XOR("bit_xor"),
+        BIT_SET("bit_set"),
+        BIT_IS_SET("bit_is_set");
         
         public String name;
         
@@ -91,6 +98,11 @@ public class MathLib extends TLFunctionLibrary {
         case E: return new EFunction();
         case RANDOM: return new RandomFunction();
         case ABS: return new AbsFunction();
+        case BIT_AND: return new BitAndFunction();
+        case BIT_OR: return new BitOrFunction();
+        case BIT_XOR: return new BitXorFunction();
+        case BIT_SET: return new BitSetFunction();
+        case BIT_IS_SET: return new BitIsSetFunction();
         default: return null;
         }
     }
@@ -338,5 +350,145 @@ public class MathLib extends TLFunctionLibrary {
         }
     }                        
 
+    // BIT_AND
+    class BitAndFunction extends TLFunctionPrototype { 
+        public BitAndFunction() {
+            super("math", "bit_and", "Bit wise AND on parameters ", new TLValueType[] { TLValueType.NUMBER, TLValueType.NUMBER}, TLValueType.LONG);
+        }
 
+        @Override
+        public TLValue execute(TLValue[] params, TLContext context) {
+            if (params[0].type.isNumeric() && params[1].type.isNumeric()) {
+                TLNumericValue retVal=(TLNumericValue)context.getContext();
+                try {
+                   retVal.setValue(((TLNumericValue)params[0]).getLong() & ((TLNumericValue)params[1]).getLong());
+                    return retVal;
+                } catch (Exception ex) {
+                    throw new TransformLangExecutorRuntimeException(
+                            "Error when executing BIT_AND function", ex);
+                }
+            }
+            throw new TransformLangExecutorRuntimeException(null,
+                    params, "bit_and - wrong type of literal(s)");
+        }
+        @Override
+        public TLContext createContext() {
+            return TLContext.createLongContext();
+        }
+    }                     
+
+ // BIT_OR
+    class BitOrFunction extends TLFunctionPrototype { 
+        public BitOrFunction() {
+            super("math", "bit_or", "Bit wise OR on parameters ", new TLValueType[] { TLValueType.NUMBER, TLValueType.NUMBER}, TLValueType.LONG);
+        }
+
+        @Override
+        public TLValue execute(TLValue[] params, TLContext context) {
+            if (params[0].type.isNumeric() && params[1].type.isNumeric()) {
+                TLNumericValue retVal=(TLNumericValue)context.getContext();
+                try {
+                   retVal.setValue(((TLNumericValue)params[0]).getLong() | ((TLNumericValue)params[1]).getLong());
+                    return retVal;
+                } catch (Exception ex) {
+                    throw new TransformLangExecutorRuntimeException(
+                            "Error when executing BIT_OR function", ex);
+                }
+            }
+            throw new TransformLangExecutorRuntimeException(null,
+                    params, "bit_or - wrong type of literal(s)");
+        }
+        @Override
+        public TLContext createContext() {
+            return TLContext.createLongContext();
+        }
+    }              
+    
+ // BIT_XOR
+    class BitXorFunction extends TLFunctionPrototype { 
+        public BitXorFunction() {
+            super("math", "bit_and", "Bit wise AND on parameters ", new TLValueType[] { TLValueType.NUMBER, TLValueType.NUMBER}, TLValueType.LONG);
+        }
+
+        @Override
+        public TLValue execute(TLValue[] params, TLContext context) {
+            if (params[0].type.isNumeric() && params[1].type.isNumeric()) {
+                TLNumericValue retVal=(TLNumericValue)context.getContext();
+                try {
+                   retVal.setValue(((TLNumericValue)params[0]).getLong() ^ ((TLNumericValue)params[1]).getLong());
+                    return retVal;
+                } catch (Exception ex) {
+                    throw new TransformLangExecutorRuntimeException(
+                            "Error when executing BIT_XOR function", ex);
+                }
+            }
+            throw new TransformLangExecutorRuntimeException(null,
+                    params, "bit_xor - wrong type of literal(s)");
+        }
+        @Override
+        public TLContext createContext() {
+            return TLContext.createLongContext();
+        }
+    }              
+    
+ // BIT_IS_SET
+    class BitIsSetFunction extends TLFunctionPrototype { 
+        public BitIsSetFunction() {
+            super("math", "bit_is_set", "Tests if n-th bit of 1st argument is set", new TLValueType[] { TLValueType.NUMBER, TLValueType.NUMBER}, TLValueType.BOOLEAN);
+        }
+
+        @Override
+        public TLValue execute(TLValue[] params, TLContext context) {
+            if (params[0].type.isNumeric() && params[1].type.isNumeric()) {               
+                try {
+                	if ((((TLNumericValue)params[0]).getLong() & ( 1 << ((TLNumericValue)params[1]).getLong())) != 0)
+                		return TLBooleanValue.TRUE;
+                	else
+                		return TLBooleanValue.FALSE;
+                	
+                } catch (Exception ex) {
+                    throw new TransformLangExecutorRuntimeException(
+                            "Error when executing BIT_IS_SET function", ex);
+                }
+            }
+            throw new TransformLangExecutorRuntimeException(null,
+                    params, "bit_is_set - wrong type of literal(s)");
+        }
+    }           
+    
+ // BIT_IS_SET
+    class BitSetFunction extends TLFunctionPrototype { 
+        public BitSetFunction() {
+            super("math", "bit_set", "Tests if n-th bit of 1st argument is set", new TLValueType[] { TLValueType.NUMBER, TLValueType.NUMBER, TLValueType.BOOLEAN}, TLValueType.BOOLEAN);
+        }
+
+        @Override
+        public TLValue execute(TLValue[] params, TLContext context) {
+            if (params[0].type.isNumeric() && params[1].type.isNumeric() && params[2].type==TLValueType.BOOLEAN) {               
+                try {
+                	TLNumericValue retVal=(TLNumericValue)context.getContext();
+                	 
+                	if (((TLBooleanValue)params[2])==TLBooleanValue.TRUE){
+                		retVal.setValue((((TLNumericValue)params[0]).getLong() | ( 1 << ((TLNumericValue)params[1]).getLong()) )); 
+                	}else{
+                		retVal.setValue((((TLNumericValue)params[0]).getLong() & (~( 1 << ((TLNumericValue)params[1]).getLong()))));
+                	}
+                	
+                	return retVal;
+                
+                	
+                } catch (Exception ex) {
+                    throw new TransformLangExecutorRuntimeException(
+                            "Error when executing BIT_SET function", ex);
+                }
+            }
+            throw new TransformLangExecutorRuntimeException(null,
+                    params, "bit_set - wrong type of literal(s)");
+        }
+        @Override
+        public TLContext createContext() {
+            return TLContext.createLongContext();
+        }
+    }         
+    
 }
