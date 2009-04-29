9fc026d988df46f46a48d45d8b6b4c5c0b685fa5
jausperger <info@cloveretl.com>
2009-04-29 11:28:04 +0000

    NEW: issue 1827; dedup order verification
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@6490 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/src/org/jetel/enums/OrderEnum.java
@@ -0,0 +1,15 @@
+package org.jetel.enums;
+
+/**
+ * Order Enum.
+ * 
+ * @author Jan Ausperger (jan.ausperger@javlin.eu)
+ * (c) OpenSys (www.opensys.eu)
+ */
+public enum OrderEnum {
+	ASC,	// ascending order
+	DESC,	// descending order
+	IGNORE,	// don't check order of records
+	AUTO,	// check the input to be either in ascending or descending order
+
+}
