2deb690c737bc0690e4860c54c87f3eba4e6db06
mhaupt <info@cloveretl.com>
2009-06-12 15:00:58 +0000

    #2088 add logging graph to out file
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@6875 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/graph/runtime/PrimitiveAuthorityProxy.java
+++ cloveretl.engine/src/org/jetel/graph/runtime/PrimitiveAuthorityProxy.java
@@ -22,10 +22,17 @@ package org.jetel.graph.runtime;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.channels.Channels;
+import java.util.Random;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
 import org.jetel.data.sequence.Sequence;
 import org.jetel.exception.ComponentNotReadyException;
 import org.jetel.exception.GraphConfigurationException;
@@ -65,7 +72,7 @@ public class PrimitiveAuthorityProxy implements IAuthorityProxy {
 	 * 
 	 * @see org.jetel.graph.runtime.IAuthorityProxy#executeGraph(long, java.lang.String)
 	 */
-	public RunResult executeGraph(long runId, String graphFileName, Properties props) {
+	public RunResult executeGraph(long runId, String graphFileName, Properties props, String logFile) {
         RunResult rr = new RunResult();
 
 		InputStream in = null;		
@@ -79,6 +86,12 @@ public class PrimitiveAuthorityProxy implements IAuthorityProxy {
         }
         
         GraphRuntimeContext runtimeContext = new GraphRuntimeContext();
+        runtimeContext.setRunId(getUniqueRunId(runId));
+        runtimeContext.setLogLevel(Level.ALL);
+        runtimeContext.setLogLocation(logFile);
+        if (logFile != null) {
+        	prepareLogger(runtimeContext);
+        }
         if (props != null)
         	runtimeContext.addAdditionalProperties(props);
         Future<Result> futureResult = null;                
@@ -138,5 +151,69 @@ public class PrimitiveAuthorityProxy implements IAuthorityProxy {
         rr.duration = totalTime;
         
 		return rr;
+	}
+	
+	private static long getUniqueRunId(long parentRunId) {
+		Random random = new Random();
+		long runId = Math.abs((random.nextLong() % 999));
+		return (runId != parentRunId) ? runId : runId + 1;
+		// TODO returned runId mustn't be unique
+	}
+	
+	/**
+	 * Set file that will be used for logging graph.
+	 * Information about graph, log level and log location are in graph runtime context
+	 * 
+	 * @param runtimeContext
+	 */
+	private static void prepareLogger(GraphRuntimeContext runtimeContext) {
+		FileAppender logAppender = null;
+		try {
+			logAppender = new FileAppender(new PatternLayout("%d %-5p %-3X{runId} [%t] %m%n"), runtimeContext.getLogLocation());
+			Filter f = new GraphLogFilter(runtimeContext.getRunId(), runtimeContext.getLogLevel());
+			logAppender.addFilter(f);
+			Logger.getRootLogger().addAppender(logAppender);
+		} catch (Exception e) {
+			Logger.getRootLogger().error("logger initialization", e);
+		}
+	}
+	
+	/**
+	 * Note: This class is copied from Clover server.
+	 * 
+	 * Use instance of this filter for log appender which should collect messages only from specified graph execution.
+	 * Execution is specified by runId.
+	 * We expect, that this value is preset in MDC for each related thread.
+	 * 
+	 * @author Martin Varecha <martin.varecha@javlinconsulting.cz>
+	 * (c) JavlinConsulting s.r.o.
+	 * www.javlinconsulting.cz
+	 * @created Mar 5, 2008
+	 */
+	private static class GraphLogFilter extends Filter{
+		private long runId;
+		private Level logLevel;
+	 
+		public GraphLogFilter(long runId, Level logLevel) {
+			super();
+			this.runId = runId;
+			this.logLevel = logLevel;
+		}
+
+		@Override
+		public int decide(LoggingEvent event) {
+			Object oRunId = event.getMDC("runId");
+			if (!(oRunId instanceof Long))
+				return Filter.DENY;
+			Long rId = (Long)oRunId;
+			if (this.runId != rId.longValue())
+				return Filter.DENY;
+			else {
+				if (event.getLevel().isGreaterOrEqual(logLevel))
+					return Filter.ACCEPT;
+				else
+					return Filter.DENY;
+			}
+		}
 	}
 }
