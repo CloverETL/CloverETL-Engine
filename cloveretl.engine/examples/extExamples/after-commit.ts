5e6289144e30562231f697aa5f90235ed4f11c55
avackova <avackova@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2009-11-24 11:51:46 +0000

    UPDATE:test for LDAPReader/Writer
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@7713 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/examples/extExamples/after-commit.ts
+++ cloveretl.engine/examples/extExamples/after-commit.ts
@@ -4,10 +4,10 @@
 
 	<DBConnection ident="postgre_foodmart" type="POSTGRE" user="test" password="test" URL="jdbc:postgresql://koule/foodmart" driver="org.postgresql.Driver" />
 
-<!--
 	<FunctionalTest ident="LDAPReaderWriter" graphFile="graph/graphLdapReaderWriter.grf">
+          <Property name="CONN_DIR" value="../../../cloveretl.test.scenarios/conn" />
+	 	  <FlatFile outputFile="data-out/persons.txt" supposedFile="data-out/ldap_persons.txt"/>	                                                                 
 	</FunctionalTest>
--->
 
 	<FunctionalTest ident="ApproximativeJoin" graphFile="graph/graphApproximativeJoin.grf">
         <Property name="CONN_DIR" value="../../../cloveretl.test.scenarios/conn" />
