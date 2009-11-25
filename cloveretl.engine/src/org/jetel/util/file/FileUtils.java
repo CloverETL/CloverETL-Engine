361bdd92e76964dce810a08a9290ca6803961ee3
mzatopek <info@cloveretl.com>
2009-11-25 10:35:08 +0000

    UPDATE: AuthorityProxy changed return value of some methods from streams to channels. This commit updates all related classes.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@7724 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/file/FileUtils.java
+++ cloveretl.engine/src/org/jetel/util/file/FileUtils.java
@@ -297,7 +297,7 @@ public class FileUtils {
     			TransformationGraph graph = ContextProvider.getGraph();
         		if (graph == null)
         			throw new NullPointerException("Graph reference cannot be null when \""+SandboxStreamHandler.SANDBOX_PROTOCOL+"\" protocol is used.");
-            	return graph.getAuthorityProxy().getSandboxResourceInput(graph.getRuntimeContext().getRunId(), url.getHost(), url.getPath());
+            	return Channels.newInputStream(graph.getAuthorityProxy().getSandboxResourceInput(graph.getRuntimeContext().getRunId(), url.getHost(), url.getPath()));
         	}
         	
         	try {
@@ -670,7 +670,7 @@ public class FileUtils {
         		if (graph == null)
         			throw new NullPointerException("Graph reference cannot be null when \""+SandboxStreamHandler.SANDBOX_PROTOCOL+"\" protocol is used.");
     			URL url = FileUtils.getFileURL(contextURL, input);
-            	return graph.getAuthorityProxy().getSandboxResourceOutput(graph.getRuntimeContext().getRunId(), url.getHost(), url.getPath());
+            	return Channels.newOutputStream(graph.getAuthorityProxy().getSandboxResourceOutput(graph.getRuntimeContext().getRunId(), url.getHost(), url.getPath()));
     		} else {
     			// file input stream 
     			String filePath = FileUtils.getFile(contextURL, input);
