b9ce0aa0339926b201e2c33c2ae3c0b4ef9d9796
csochor <info@cloveretl.com>
2010-02-26 12:40:48 +0000

    FIX: added quickbase and compiler.commercial
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@8049 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/hudson/build.sh
+++ cloveretl.engine/hudson/build.sh
@@ -7,7 +7,7 @@ set
 cd cloveretl.engine
 
 /opt/apache-ant/bin/ant clean build reports-hudson dist \
-	-Dadditional.plugin.list=cloveretl.component.commercial,cloveretl.lookup.commercial \
+	-Dadditional.plugin.list=cloveretl.component.commercial,cloveretl.lookup.commercial,cloveretl.compiler.commercial,cloveretl.quickbase.commercial \
 	-Dcte.environment.config=trunk_java-1.5-Sun \
 	-Dcte.logpath=/data/cte-logs \
 	-Dcte.hudson.link=job/$JOB_NAME/$BUILD_NUMBER
