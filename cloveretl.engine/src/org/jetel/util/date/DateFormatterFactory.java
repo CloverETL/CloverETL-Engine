37a060664a40c852458661ca2c073a63ae484c7d
mjanik <mjanik@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2010-04-16 10:02:11 +0000

    MINOR: Comments fix.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@8330 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/date/DateFormatterFactory.java
+++ cloveretl.engine/src/org/jetel/util/date/DateFormatterFactory.java
@@ -4,22 +4,29 @@ import java.util.Locale;
 
 import org.jetel.util.MiscUtils;
 
-public class DateFormatterFactory {
-	
+/**
+ * Factory for internally used date formatters.
+ *
+ * @author Martin Janik, Javlin a.s. &lt;martin.janik@javlin.eu&gt;
+ *
+ * @version 16th April 2010
+ * @created 17th August 2009
+ */
+public final class DateFormatterFactory {
+
 	/** the Java prefix specifying date format strings used by the Java's DateFormat class */
 	public static final String JAVA_FORMAT_PREFIX = "java:";
 	/** the Joda-Time prefix specifying date format strings used by the Joda-Time's DateTimeFormatter class */
 	public static final String JODA_FORMAT_PREFIX = "joda:";
 
-
 	public static DateFormatter createFormatter(String formatString, Locale locale) {
+		if (locale == null) {
+			locale = Locale.getDefault();
+		}
 
-		if (locale==null)
-			locale=Locale.getDefault();
-		
-		if (formatString==null){
-				return new JavaDateFormatter(locale);
-		}else if (formatString.startsWith(JODA_FORMAT_PREFIX)) {
+		if (formatString == null) {
+			return new JavaDateFormatter(locale);
+		} else if (formatString.startsWith(JODA_FORMAT_PREFIX)) {
 			return new JodaDateFormatter(formatString.substring(JODA_FORMAT_PREFIX.length()), locale);
 		} else {
 			if (formatString.startsWith(JAVA_FORMAT_PREFIX)) {
@@ -28,23 +35,26 @@ public class DateFormatterFactory {
 
 			return new JavaDateFormatter(formatString, locale);
 		}
-
 	}
 
 	public static DateFormatter createFormatter(String formatString, String localeString) {
 		return createFormatter(formatString, MiscUtils.createLocale(localeString));
 	}
-	
+
 	public static DateFormatter createFormatter(String formatString) {
 		return createFormatter(formatString, Locale.getDefault());
 	}
-	
-	public static DateFormatter createFormatter(Locale locale){
+
+	public static DateFormatter createFormatter(Locale locale) {
 		return new JavaDateFormatter(locale);
 	}
-	
-	public static DateFormatter createFormatter(){
+
+	public static DateFormatter createFormatter() {
 		return new JavaDateFormatter();
+	}
+
+	private DateFormatterFactory() {
+		throw new UnsupportedOperationException();
 	}
 
 }
