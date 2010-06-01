6229f940943ffdeb9d5c51eedcc3c7f0ca313fbb
csochor <info@cloveretl.com>
2010-06-01 12:42:22 +0000

    UPDATE: clover version parsing (version after last -); cte.environment.config always starts with engine
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@8655 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/hudson/build.sh
+++ cloveretl.engine/hudson/build.sh
@@ -5,7 +5,7 @@ set -u
 
 export ANT_OPTS="-Xmx500m"
 
-CLOVER_VERSION_X_X=`echo $JOB_NAME | sed 's/^cloveretl.engine-\([0-9]\+\.[0-9]\+\).*$/\1/'`
+CLOVER_VERSION_X_X=`echo $JOB_NAME | sed 's/^\(.*\)-\([^-]*\)$/\2/'`
 
 set
 
@@ -13,7 +13,7 @@ cd cloveretl.engine
 
 /opt/apache-ant/bin/ant clean build reports-hudson dist \
 	-Dadditional.plugin.list=cloveretl.component.commercial,cloveretl.lookup.commercial,cloveretl.compiler.commercial,cloveretl.quickbase.commercial,cloveretl.tlfunction.commercial,cloveretl.ctlfunction.commercial\
-	-Dcte.environment.config=${CLOVER_VERSION_X_X}_java-1.5-Sun \
+	-Dcte.environment.config=engine-${CLOVER_VERSION_X_X}_java-1.6-Sun \
 	-Dcte.logpath=/data/cte-logs \
 	-Dcte.hudson.link=job/$JOB_NAME/$BUILD_NUMBER \
 	-Ddir.examples=../cloveretl.examples
