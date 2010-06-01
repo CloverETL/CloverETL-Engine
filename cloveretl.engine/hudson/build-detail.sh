3c035daa11adbc994db68bcc2cb8a898cc66c3d6
csochor <csochor@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2010-06-01 12:42:22 +0000

    UPDATE: clover version parsing (version after last -); cte.environment.config always starts with engine
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@8655 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/hudson/build-detail.sh
+++ cloveretl.engine/hudson/build-detail.sh
@@ -2,13 +2,15 @@
 
 export ANT_OPTS="-Xmx500m"
 
+CLOVER_VERSION_X_X=`echo $JOB_NAME | sed 's/^\(.*\)-\([^-]*\)$/\2/'`
+
 set
 
 cd cloveretl.engine
 
 /opt/apache-ant/bin/ant clean reports-hudson-detail \
 	-Dadditional.plugin.list=cloveretl.lookup.commercial,cloveretl.component.commercial,cloveretl.compiler.commercial,cloveretl.quickbase.commercial,cloveretl.tlfunction.commercial,cloveretl.ctlfunction.commercial \
-	-Dcte.environment.config=trunk_detail \
+	-Dcte.environment.config=engine-${CLOVER_VERSION_X_X}_java-1.6-Sun_detail \
 	-Dcte.logpath=/data/cte-logs \
 	-Dcte.hudson.link=job/$JOB_NAME/$BUILD_NUMBER \
 	-Dtest.exclude=org/jetel/graph/ResetTest.java \
