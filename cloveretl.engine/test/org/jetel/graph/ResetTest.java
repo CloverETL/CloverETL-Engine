5c4a9234f8126a3e205fe31f602072fe960e7bc0
twaller <twaller@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2010-06-21 11:41:53 +0000

    UPDATE: Fix, semicolons removed.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@8944 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/graph/ResetTest.java
+++ cloveretl.engine/test/org/jetel/graph/ResetTest.java
@@ -83,9 +83,9 @@ public class ResetTest extends CloverTestCase {
 							&& !pathname.getName().equals("graphSequenceChecker.grf") // ok, is to fail
 							&& !pathname.getName().equals("FixedData.grf") // ok, is to fail
 							&& !pathname.getName().equals("xpathReaderStates.grf") // ok, is to fail
-							&& !pathname.getName().equals("graphDataPolicy.grf"); // ok, is to fail
-							&& !pathname.getName().equals("conversionDecimal2integer.grf"); // ok, is to fail
-							&& !pathname.getName().equals("conversionDouble2integer.grf"); // ok, is to fail							
+							&& !pathname.getName().equals("graphDataPolicy.grf") // ok, is to fail
+							&& !pathname.getName().equals("conversionDecimal2integer.grf") // ok, is to fail
+							&& !pathname.getName().equals("conversionDouble2integer.grf") // ok, is to fail							
 							&& !pathname.getName().equals("conversionLong2integer.grf"); // ok, is to fail
 							
 //TODO these graphs should work in the future:
