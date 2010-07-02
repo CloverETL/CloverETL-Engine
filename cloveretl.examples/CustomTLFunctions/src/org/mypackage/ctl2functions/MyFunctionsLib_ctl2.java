baa35af619fa578b9e1c793b45b24fb836b9b618
avackova <info@cloveretl.com>
2010-07-02 12:07:06 +0000

    UPDATE:double_metaphone function can accept one or two parameters
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@9037 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.examples/CustomTLFunctions/src/org/mypackage/ctl2functions/MyFunctionsLib_ctl2.java
+++ cloveretl.examples/CustomTLFunctions/src/org/mypackage/ctl2functions/MyFunctionsLib_ctl2.java
@@ -22,12 +22,26 @@ import java.util.Date;
 
 import org.apache.commons.codec.language.DoubleMetaphone;
 import org.jetel.ctl.Stack;
+import org.jetel.ctl.extensions.TLCache;
 import org.jetel.ctl.extensions.TLFunctionAnnotation;
 import org.jetel.ctl.extensions.TLFunctionCallContext;
 import org.jetel.ctl.extensions.TLFunctionLibrary;
 import org.jetel.ctl.extensions.TLFunctionPrototype;
 
-
+ /**
+ * This class represents user's CTL2 functions library.
+ * The library contains two functions and one cache (really simple - 
+ * just for illustration how to use cache). 
+ * It must implement <i>getExecutable(String)</i> method, that
+ * defines custom functions. This is the only place, the functions
+ * names are defined (in contrast to CTL1, where the names of functions 
+ * must correspond with the names in <i>plugin.xml</i> file).
+ * 
+ * @author Agata Vackova (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *
+ * @created 2010-07-01
+ */
 public class MyFunctionsLib_ctl2 extends TLFunctionLibrary {
 	
 	@Override
@@ -62,16 +76,27 @@ public class MyFunctionsLib_ctl2 extends TLFunctionLibrary {
 		}
 
 		public void execute(Stack stack, TLFunctionCallContext context) {
-			Date arg1 = stack.popDate();
+			//arguments must be pop from stack in inverted order, that there were pushed on stack:
+			//calling function(arg1, ..., argN) pushes arguments on stack: arg1, then arg2, ..., argN
+			//so poping them must be in inverted order: argN, ..., arg1
 			String arg2 = stack.popString();
+			Date arg1 = stack.popDate();
+			//pushing function result to executor stack
 			stack.push(myFunction(context, arg1, arg2));
 		}
 	}
 
+	private static DoubleMetaphone doubleMetaphone = new DoubleMetaphone();
+
+	//Double metaphone with one argument
 	@TLFunctionAnnotation("Encodes a given word using the Double Metaphone algorithm.")
 	public static final String doubleMetaphone(TLFunctionCallContext context, String word) {
-		DoubleMetaphone doubleMetaphone = new DoubleMetaphone();
-		doubleMetaphone.setMaxCodeLen(word.length());
+		return doubleMetaphone.doubleMetaphone(word);
+	}
+
+	//Double metaphone with two arguments
+	@TLFunctionAnnotation("Encodes a given word using the Double Metaphone algorithm with defined max length.")
+	public static final String doubleMetaphone(TLFunctionCallContext context, String word, int length) {
 		return doubleMetaphone.doubleMetaphone(word);
 	}
 
@@ -79,12 +104,43 @@ public class MyFunctionsLib_ctl2 extends TLFunctionLibrary {
 	class DoubleMetaphoneFunction implements TLFunctionPrototype {
 
 		public void init(TLFunctionCallContext context) {
+			//cache for some values that can be reused
+			context.setCache(new DoubleMetaphoneCache());
 		}
 
 		public void execute(Stack stack, TLFunctionCallContext context) {
+			int length = -1;
+			//get second parameter if exist
+			if (context.getParams().length > 1) {
+				length = stack.popInt();
+			}
+			//get first parameter
 			String word = stack.popString();
+			//check and eventually change length
+			if (length == -1) {
+				length = word.length();
+			}
+			if (length != ((DoubleMetaphoneCache)context.getCache()).length) {
+				doubleMetaphone.setMaxCodeLen(length);
+				((DoubleMetaphoneCache)context.getCache()).length = length;
+			}
+			//push function result to executor stack
 			stack.push(doubleMetaphone(context, word));
 		}
 	}
 
+}
+/**
+ * Cache for the values, that can be reused,
+ * when executing the same function many times.
+ *  
+ * @author Agata Vackova (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *
+ * @created 2 Jul 2010
+ */
+class DoubleMetaphoneCache extends TLCache {
+	
+	int length = -1;
+	
 }
