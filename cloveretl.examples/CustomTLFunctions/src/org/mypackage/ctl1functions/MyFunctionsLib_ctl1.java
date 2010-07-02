e71f2cecd4bc816c2fbc642fd4fa8ad429b50ca2
avackova <avackova@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2010-07-02 12:07:06 +0000

    UPDATE:double_metaphone function can accept one or two parameters
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@9037 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.examples/CustomTLFunctions/src/org/mypackage/ctl1functions/MyFunctionsLib_ctl1.java
+++ cloveretl.examples/CustomTLFunctions/src/org/mypackage/ctl1functions/MyFunctionsLib_ctl1.java
@@ -1,3 +1,21 @@
+/*
+ * jETeL/CloverETL - Java based ETL application framework.
+ * Copyright (c) Javlin, a.s. (info@cloveretl.com)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package org.mypackage.ctl1functions;
  
  
@@ -15,10 +33,28 @@ import org.jetel.interpreter.extensions.TLContext;
 import org.jetel.interpreter.extensions.TLFunctionLibrary;
 import org.jetel.interpreter.extensions.TLFunctionPrototype;
  
+/**
+ * This class represents user's CTL1 functions library.
+ * The library contains two functions and one context. 
+ * It must contain enum with functions listing and implement 
+ * <i>getAllFunctions()</i> and <i>getFunction(String)</i> methods.
+ * 
+ * The names of functions must correspond with the names in 
+ * <i>plugin.xml</i> file (see <i>ctl1_plugin.xml</i>, that is renamed 
+ * to <i>plugin.xml</i> during installation - {@link readme.html})
+ * 
+ * @author Agata Vackova (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *
+ * @created 2010-07-01
+ */
 public class MyFunctionsLib_ctl1 extends TLFunctionLibrary {
  
 	private static final String LIBRARY_NAME = "MyCTL1Functions";
  
+	/**
+	 * List of functions in the library
+	 */
 	enum Function {
         MY_FUNCTION("my_function"),
         DOUBLE_METAPHONE("double_metaphone");
@@ -64,12 +100,15 @@ public class MyFunctionsLib_ctl1 extends TLFunctionLibrary {
     	return ret;
 	}
  
+    /**
+     * Implementation of my_function function
+     */
     class MyFunction extends TLFunctionPrototype {
  
         public MyFunction() {
             super(LIBRARY_NAME, "my_function", "Description of my function",
-            		new TLValueType[] { TLValueType.DATE, TLValueType.STRING }, //MyFunction1 has 2 input parameters: 1st is date type,2nd is string type
-                    TLValueType.STRING);//MyFunction1 returns string
+            		new TLValueType[] { TLValueType.DATE, TLValueType.STRING }, //my_function has 2 input parameters: 1st is date type,2nd is string type
+                    TLValueType.STRING);//my_function returns string
         }
  
         @Override
@@ -111,18 +150,25 @@ public class MyFunctionsLib_ctl1 extends TLFunctionLibrary {
         }
     }
  
+	 
+    /**
+     * Implementation of double_metaphone function
+     */
     class DoubleMetaphoneFunction extends TLFunctionPrototype {
- 
+    	
         public DoubleMetaphoneFunction() {
             super(LIBRARY_NAME, "double_metaphone", "Encodes a given word using the Double Metaphone algorithm", 
-            		new TLValueType[] { TLValueType.STRING}, 
-                    TLValueType.STRING);
+            		new TLValueType[] { TLValueType.STRING, TLValueType.INTEGER}, //first argument is of the type string, the second one - integer
+                    TLValueType.STRING,//double_metaphone returns string value
+                    2, 1);//accepts maximally 2 parameters, minimum one parameter must be present
         }
  
         @Override
         public TLValue execute(TLValue[] params, TLContext context) {
-        	TLStringValue val = (TLStringValue)context.context;
-            StringBuilder strBuf = (StringBuilder)val.getValue();
+        	//usage of custom context (see below)
+        	DoubleMetaphoneContext con = (DoubleMetaphoneContext)context.context;
+			TLStringValue val = (TLStringValue) con.value;
+			StringBuilder strBuf = (StringBuilder)val .getValue();
             strBuf.setLength(0);
   
             //checking input parameters
@@ -133,20 +179,54 @@ public class MyFunctionsLib_ctl1 extends TLFunctionLibrary {
             if (params[0].type!=TLValueType.STRING)
                 throw new TransformLangExecutorRuntimeException(params,
                 		Function.DOUBLE_METAPHONE.name() + " - wrong type of literal");
+            if (params.length > 1 && params[1].type!=TLValueType.INTEGER)
+                throw new TransformLangExecutorRuntimeException(params,
+                		Function.DOUBLE_METAPHONE.name() + " - wrong type of literal");
  
             String word = params[0].toString();
-    		DoubleMetaphone doubleMetaphone = new DoubleMetaphone();
-    		doubleMetaphone.setMaxCodeLen(word.length());
-    		strBuf.append(doubleMetaphone.doubleMetaphone(word));
+            //if exists 2nd parameter, use it as max code length
+            int length = params.length > 1 ? params[1].getNumeric().getInt() : word.length();
+            //checking context settings
+    		if (length != con.maxCodeLength) {
+				DoubleMetaphoneContext.doubleMetaphone.setMaxCodeLen(length);
+			}
+    		//"body" of the function
+			strBuf.append(DoubleMetaphoneContext.doubleMetaphone.doubleMetaphone(word));
 
     		return val;
         }
  
         @Override
         public TLContext createContext() {
-            return TLContext.createStringContext();
+            return DoubleMetaphoneContext.createContex();
         }
     }
+    
 }
  
+/**
+ * This class demonstrates how to create your own context.
+ * In context you should store objects that can be reused in every execution of your function.
+ * This saves time when executing the same function many times.
+ * 
+ * @author Agata Vackova (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *
+ * @created 2010-07-01
+ */
+class DoubleMetaphoneContext {
+	
+	final static DoubleMetaphone doubleMetaphone = new DoubleMetaphone();
+	int maxCodeLength = -1;
+	TLValue value;
+	
+    static TLContext createContex(){
+    	DoubleMetaphoneContext con=new DoubleMetaphoneContext();
+        con.value=TLValue.create(TLValueType.STRING);
+        TLContext<DoubleMetaphoneContext> context=new TLContext<DoubleMetaphoneContext>();
+        context.setContext(con);
  
+        return context;        	
+    }
+}
+
