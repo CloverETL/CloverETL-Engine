df21b50a9db5de13ce45039acd1fffa77e8a922e
csochor <info@cloveretl.com>
2010-12-14 17:21:27 +0000

    NEW: issue 4453: test optimalized engine
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@9509 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/hudson/build-optimalized.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+set -u
+
+export ANT_OPTS="-Xmx500m"
+
+CLOVER_VERSION_X_X=`echo $JOB_NAME | sed 's/^\(.*\)-\([^-]*\)$/\2/'`
+
+set
+
+cd cloveretl.engine
+
+/opt/apache-ant/bin/ant clean build reports-hudson dist \
+	-Dadditional.plugin.list=cloveretl.component.commercial,cloveretl.lookup.commercial,cloveretl.compiler.commercial,cloveretl.quickbase.commercial,cloveretl.tlfunction.commercial,cloveretl.ctlfunction.commercial\
+	-Dcte.environment.config=engine-${CLOVER_VERSION_X_X}_java-1.6-Sun \
+	-Dcte.logpath=/data/cte-logs \
+	-Dcte.hudson.link=job/$JOB_NAME/$BUILD_NUMBER \
+	-Dtest.exclude=org/jetel/graph/ResetTest.java \
+	-Ddir.examples=../cloveretl.examples \
+	-Dobfuscate.plugin.pattern=cloveretl.*
+	
+if  [ "$(hostname)" != "klara" ] ; then
+	rsync -rv --remove-source-files /data/cte-logs/ klara:/data/cte-logs
+fi
