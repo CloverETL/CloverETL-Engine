0dac9473cf25d338f641adac8c3fbc41fb1e2bf8
lkrejci <lkrejci@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2011-05-02 16:23:41 +0000

    NEW: CLD-2268 Pervasive specific introduced, fixes types compatibility and schema loading
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@10527 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.jdbc/src/org/jetel/connection/jdbc/specific/impl/PervasiveSpecific.java
@@ -0,0 +1,108 @@
+/*
+ * jETeL/CloverETL - Java based ETL application framework.
+ * Copyright (c) Javlin, a.s. (info@cloveretl.com)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jetel.connection.jdbc.specific.impl;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+
+import org.jetel.connection.jdbc.DBConnection;
+import org.jetel.connection.jdbc.specific.conn.DefaultConnection;
+import org.jetel.exception.JetelException;
+import org.jetel.metadata.DataFieldMetadata;
+import org.jetel.util.string.StringUtils;
+
+/**
+ * @author lkrejci (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *
+ * @created 29 Apr 2011
+ */
+public class PervasiveSpecific extends AbstractJdbcSpecific {
+	
+	private static final PervasiveSpecific INSTANCE = new PervasiveSpecific();
+	
+	public static PervasiveSpecific getInstance() {
+		return INSTANCE;
+	}
+
+	@Override
+	public Connection createSQLConnection(DBConnection dbConnection, OperationType operationType) throws JetelException {
+		return new DefaultConnection(dbConnection, operationType, getAutoKeyType());
+	}
+
+	@Override
+	public ArrayList<String> getSchemas(Connection connection) throws SQLException {
+		return getMetaCatalogs(connection.getMetaData());
+	}
+	
+	@Override
+	public String jetelType2sqlDDL(DataFieldMetadata field) {
+		int sqlType = jetelType2sql(field);
+		
+		switch(sqlType) {
+		case Types.BINARY:
+		case Types.VARBINARY:
+			return sqlType2str(sqlType);
+		default :
+			return super.jetelType2sqlDDL(field);
+		}
+	}
+	
+	@Override
+	public String sqlType2str(int sqlType) {
+		switch(sqlType) {
+		case Types.BOOLEAN:
+			return "BIT";
+		case Types.VARBINARY:
+			return "LONGVARBINARY";
+		}
+		return super.sqlType2str(sqlType);
+	}
+	
+	@Override
+	public int jetelType2sql(DataFieldMetadata field) {
+		switch (field.getType()) {
+		case DataFieldMetadata.BOOLEAN_FIELD:
+			return Types.BIT;
+        case DataFieldMetadata.BYTE_FIELD:
+        case DataFieldMetadata.BYTE_FIELD_COMPRESSED:
+        	if (!StringUtils.isEmpty(field.getFormatStr())
+					&& field.getFormatStr().equalsIgnoreCase(DataFieldMetadata.BLOB_FORMAT_STRING)) {
+        		return Types.BLOB;
+        	}
+            return field.isFixed() ? Types.BINARY : Types.LONGVARBINARY;
+        case DataFieldMetadata.NUMERIC_FIELD:
+			return Types.DOUBLE;
+		default:
+			return super.jetelType2sql(field);
+		}
+	}
+	
+	public char sqlType2jetel(int sqlType) {
+		switch (sqlType) {
+			case Types.BIT:
+				return DataFieldMetadata.BOOLEAN_FIELD;
+			default:
+				return super.sqlType2jetel(sqlType);
+		}
+	}
+
+}
