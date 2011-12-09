6c72c4988a52df41a640f90b1956f49c6dbb0651
raszykj <raszykj@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2011-12-09 16:45:26 +0000

    FIX: XlsxZip test fixed
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-3-2@11182 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/data/formatter/XLSXDataFormatter.java
+++ cloveretl.engine/src/org/jetel/data/formatter/XLSXDataFormatter.java
@@ -106,8 +106,13 @@ public class XLSXDataFormatter extends XLSFormatter {
 
 		try {
     		if (dataTarget instanceof Object[]) {
+    			Object[] args = (Object[])dataTarget;
     			URL url = (URL) ((Object[])dataTarget)[0];
         		String fName = (String) ((Object[])dataTarget)[1];
+        		if (args.length >= 3 && args[2] instanceof de.schlichtherle.io.FileOutputStream) {
+        			((de.schlichtherle.io.FileOutputStream) args[2]).close();
+        		}
+
         		// input stream
         		try {
             		InputStream inputStream = FileUtils.getInputStream(url, fName);
