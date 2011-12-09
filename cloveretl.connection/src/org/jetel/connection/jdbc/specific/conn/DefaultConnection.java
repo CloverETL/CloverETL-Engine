be290bc11306a0f8626467c1c2a82c3751108db7
najvarp <najvarp@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2011-12-09 13:11:03 +0000

    FIX: reverting r.11173 - Java 7 fix causes trouble in Java 6 - not needed now
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-3-2@11177 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.connection/src/org/jetel/connection/jdbc/specific/conn/DefaultConnection.java
+++ cloveretl.connection/src/org/jetel/connection/jdbc/specific/conn/DefaultConnection.java
@@ -625,35 +625,6 @@ public class DefaultConnection implements Connection {
 		connection.setClientInfo(name, value);
 	}
 
-	@Override
-	public void setSchema(String schema) throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public String getSchema() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void abort(Executor executor) throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setNetworkTimeout(Executor executor, int milliseconds) throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getNetworkTimeout() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-  
 	/* JDBC_4_ANT_KEY_END */
 }
