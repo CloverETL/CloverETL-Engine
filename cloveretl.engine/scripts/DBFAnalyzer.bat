1274b4afeb7f2c5fd766266f72a019e1d3028b3d
zatopekm <zatopekm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-01-05 15:22:19 +0000

    FIX: CL-2058 Update 'analyzeDB.bat' and 'DBFAnalyzer.bat' to be stable to libraries updates
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk@11283 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/scripts/DBFAnalyzer.bat
+++ cloveretl.engine/scripts/DBFAnalyzer.bat
@@ -5,31 +5,32 @@ REM this script was inspired by practices gained from ant run scripts (http://an
 REM usage 
 REM DBFAnalyzer.bat [-v(erbose)] <DBF filename> [<metadata output filename>]
 
-REM prepare CLOVER_HOME environmental variable
+REM prepare DERIVED_CLOVER_HOME environmental variable
 REM %~dp0 is expanded pathname of the current script under NT
 set DEFAULT_CLOVER_HOME=%~dp0..
-if "%CLOVER_HOME%"=="" set CLOVER_HOME=%DEFAULT_CLOVER_HOME%
+
+if "%CLOVER_HOME%"=="" (set DERIVED_CLOVER_HOME=%DEFAULT_CLOVER_HOME%) else (set DERIVED_CLOVER_HOME=%CLOVER_HOME%)
 set DEFAULT_CLOVER_HOME=
 
 REM find CLOVER_HOME if it does not exist due to either an invalid value passed
 REM by the user or the %0 problem on Windows 9x
-if exist "%CLOVER_HOME%\lib\cloveretl.engine.jar" goto checkJava
+if exist "%DERIVED_CLOVER_HOME%\lib\cloveretl.engine.jar" goto checkJava
 
 REM check for clover in Program Files
 if not exist "%ProgramFiles%\cloverETL" goto checkSystemDrive
-set CLOVER_HOME=%ProgramFiles%\cloverETL
+set DERIVED_CLOVER_HOME=%ProgramFiles%\cloverETL
 goto checkJava
 
 :checkSystemDrive
 REM check for clover in root directory of system drive
 if not exist %SystemDrive%\cloverETL\lib\cloveretl.engine.jar goto checkCDrive
-set CLOVER_HOME=%SystemDrive%\cloverETL
+set DERIVED_CLOVER_HOME=%SystemDrive%\cloverETL
 goto checkJava
 
 :checkCDrive
 REM check for clover in C:\cloverETL for Win9X users
 if not exist C:\cloverETL\lib\cloveretl.engine.jar goto noCloverHome
-set CLOVER_HOME=C:\cloverETL
+set DERIVED_CLOVER_HOME=C:\cloverETL
 goto checkJava
 
 :noCloverHome
@@ -41,7 +42,7 @@ set JAVA_CMD=%JAVACMD%
 if "%JAVA_HOME%" == "" goto noJavaHome
 if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
 if "%JAVA_CMD%" == "" set JAVA_CMD=%JAVA_HOME%\bin\java.exe
-goto DBFAnalyzer
+goto parseArgs
 
 :noJavaHome
 if "%JAVA_CMD%" == "" set JAVA_CMD=java.exe
@@ -49,32 +50,35 @@ if "%JAVA_CMD%" == "" set JAVA_CMD=java.exe
 REM Prepare the command line arguments. This loop allows for an unlimited number
 REM of arguments (up to the command line limit, anyway).
 
-if ""%1""=="""" goto DBFAnalyzer
+:parseArgs
+if ""%1""=="""" goto createClasspath
 set CMD_LINE_ARGS=%1
 shift
 
 :cmdLineArgs
-if ""%1""=="""" goto DBFAnalyzer
+if ""%1""=="""" goto createClasspath
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
 shift
 goto cmdLineArgs
 
+:createClasspath
+FOR /f "tokens=*" %%G IN ('dir /b "%DERIVED_CLOVER_HOME%/lib"') DO (call :collectClasspath %%G)
+GOTO DBFAnalyzer
+
+:collectClasspath
+ set ENGINE_CLASSPATH=%ENGINE_CLASSPATH%;%DERIVED_CLOVER_HOME%/lib/%1
+GOTO :eof
+
 :DBFAnalyzer
 
-set CLOVER_ENGINE_JAR=%CLOVER_HOME%\lib\cloveretl.engine.jar
-set COMMONS_LOGGING_JAR=%CLOVER_HOME%\lib\commons-logging.jar
-set LOG4J_JAR=%CLOVER_HOME%\lib\log4j-1.2.12.jar
-
-echo "%JAVA_CMD%" -classpath "%CLOVER_ENGINE_JAR%;%COMMONS_LOGGING_JAR%;%LOG4J_JAR%" org.jetel.database.dbf.DBFAnalyzer %CMD_LINE_ARGS%
-"%JAVA_CMD%" -classpath "%CLOVER_ENGINE_JAR%;%COMMONS_LOGGING_JAR%;%LOG4J_JAR%" org.jetel.database.dbf.DBFAnalyzer %CMD_LINE_ARGS%
+echo "%JAVA_CMD%" -classpath "%ENGINE_CLASSPATH%" org.jetel.database.dbf.DBFAnalyzer %CMD_LINE_ARGS%
+"%JAVA_CMD%" -classpath "%ENGINE_CLASSPATH%" org.jetel.database.dbf.DBFAnalyzer %CMD_LINE_ARGS%
 set RETURN_CODE=%ERRORLEVEL%
 
 set JAVA_CMD=
 set CMD_LINE_ARGS=
-
-set CLOVER_ENGINE_JAR=
-set COMMONS_LOGGING_JAR=
-set LOG4J_JAR=
+set ENGINE_CLASSPATH=
+set DERIVED_CLOVER_HOME=
 
 exit /B %RETURN_CODE%
 
