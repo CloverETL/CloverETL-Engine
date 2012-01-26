851a4741ce95d1cc447291aea555038ef6bcf931
zatopekm <zatopekm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-01-26 17:07:53 +0000

    BACKPORT: backport from trunk (rel-3-3-M1) CL-2051 AnalyzeDB utility does not work on Windows
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-3-2@11419 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/scripts/analyzeDB.bat
+++ cloveretl.engine/scripts/analyzeDB.bat
@@ -7,31 +7,32 @@ REM analyzeDB.bat <analyze_arguments>
 REM example:
 REM analyzeDB.bat -config interbase.cfg -o employee.fmt -q "select * from EMPLOYEE"
 
-REM prepare CLOVER_HOME environmental variable
+REM prepare DERIVED_CLOVER_HOME environmental variable
 REM %~dp0 is expanded pathname of the current script under NT
 set DEFAULT_CLOVER_HOME=%~dp0..
-if "%CLOVER_HOME%"=="" set CLOVER_HOME=%DEFAULT_CLOVER_HOME%
+
+if "%CLOVER_HOME%"=="" (set DERIVED_CLOVER_HOME=%DEFAULT_CLOVER_HOME%) else (set DERIVED_CLOVER_HOME=%CLOVER_HOME%)
 set DEFAULT_CLOVER_HOME=
 
 REM find CLOVER_HOME if it does not exist due to either an invalid value passed
 REM by the user or the %0 problem on Windows 9x
-if exist "%CLOVER_HOME%\lib\cloveretl.engine.jar" goto checkJava
+if exist "%DERIVED_CLOVER_HOME%\lib\cloveretl.engine.jar" goto checkJava
 
 REM check for clover in Program Files
 if not exist "%ProgramFiles%\cloverETL" goto checkSystemDrive
-set CLOVER_HOME=%ProgramFiles%\cloverETL
+set DERIVED_CLOVER_HOME=%ProgramFiles%\cloverETL
 goto checkJava
 
 :checkSystemDrive
 REM check for clover in root directory of system drive
 if not exist %SystemDrive%\cloverETL\lib\cloveretl.engine.jar goto checkCDrive
-set CLOVER_HOME=%SystemDrive%\cloverETL
+set DERIVED_CLOVER_HOME=%SystemDrive%\cloverETL
 goto checkJava
 
 :checkCDrive
 REM check for clover in C:\cloverETL for Win9X users
 if not exist C:\cloverETL\lib\cloveretl.engine.jar goto noCloverHome
-set CLOVER_HOME=C:\cloverETL
+set DERIVED_CLOVER_HOME=C:\cloverETL
 goto checkJava
 
 :noCloverHome
@@ -43,7 +44,7 @@ set JAVA_CMD=%JAVACMD%
 if "%JAVA_HOME%" == "" goto noJavaHome
 if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
 if "%JAVA_CMD%" == "" set JAVA_CMD=%JAVA_HOME%\bin\java.exe
-goto analyzeDB
+goto parseArgs
 
 :noJavaHome
 if "%JAVA_CMD%" == "" set JAVA_CMD=java.exe
@@ -51,37 +52,38 @@ if "%JAVA_CMD%" == "" set JAVA_CMD=java.exe
 REM Prepare the command line arguments. This loop allows for an unlimited number
 REM of arguments (up to the command line limit, anyway).
 
-if ""%1""=="""" goto analyzeDB
+:parseArgs
+if ""%1""=="""" goto createClasspath
 set CMD_LINE_ARGS=%1
 shift
 
 :cmdLineArgs
-if ""%1""=="""" goto analyzeDB
+if ""%1""=="""" goto createClasspath
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
 shift
 goto cmdLineArgs
 
+:createClasspath
+FOR /f "tokens=*" %%G IN ('dir /b "%DERIVED_CLOVER_HOME%/lib"') DO (call :collectClasspath %%G)
+GOTO analyzeDB
+
+:collectClasspath
+ set ENGINE_CLASSPATH=%ENGINE_CLASSPATH%;%DERIVED_CLOVER_HOME%/lib/%1
+GOTO :eof
+
 :analyzeDB
+set CLOVER_CONNECTION_JAR=%DERIVED_CLOVER_HOME%\plugins\org.jetel.connection\cloveretl.connection.jar
+set CLOVER_PLUGINS_DIR=%DERIVED_CLOVER_HOME%\plugins
 
-set CLOVER_ENGINE_JAR=%CLOVER_HOME%\lib\cloveretl.engine.jar
-set CLOVER_CONNECTION_JAR=%CLOVER_HOME%\plugins\org.jetel.connection\cloveretl.connection.jar
-set COMMONS_LOGGING_JAR=%CLOVER_HOME%\lib\commons-logging-1.1.1.jar
-set LOG4J_JAR=%CLOVER_HOME%\lib\log4j-1.2.15.jar
-
-set CLOVER_PLUGINS_DIR=%CLOVER_HOME%\plugins
-
-echo "%JAVA_CMD%" -classpath "%CLOVER_ENGINE_JAR%;%CLOVER_CONNECTION_JAR%;%COMMONS_LOGGING_JAR%;%LOG4J_JAR%" org.jetel.connection.jdbc.AnalyzeDB %CMD_LINE_ARGS% -plugins %CLOVER_PLUGINS_DIR%
-"%JAVA_CMD%" -classpath "%CLOVER_ENGINE_JAR%;%CLOVER_CONNECTION_JAR%;%COMMONS_LOGGING_JAR%;%LOG4J_JAR%" org.jetel.connection.jdbc.AnalyzeDB %CMD_LINE_ARGS% -plugins %CLOVER_PLUGINS_DIR%
+echo "%JAVA_CMD%" -classpath "%ENGINE_CLASSPATH%;%CLOVER_CONNECTION_JAR%" org.jetel.connection.jdbc.AnalyzeDB %CMD_LINE_ARGS% -plugins %CLOVER_PLUGINS_DIR%
+"%JAVA_CMD%" -classpath "%ENGINE_CLASSPATH%;%CLOVER_CONNECTION_JAR%" org.jetel.connection.jdbc.AnalyzeDB %CMD_LINE_ARGS% -plugins %CLOVER_PLUGINS_DIR%
 set RETURN_CODE=%ERRORLEVEL%
 
 set JAVA_CMD=
 set CMD_LINE_ARGS=
-
-set CLOVER_ENGINE_JAR=
+set ENGINE_CLASSPATH=
+set DERIVED_CLOVER_HOME=
 set CLOVER_CONNECTION_JAR=
-set COMMONS_LOGGING_JAR=
-set LOG4J_JAR=
-
 set CLOVER_PLUGINS_DIR=
 
 exit /B %RETURN_CODE%
