abb8fbe87ffb04efb6c4087174b6e1fbbe014a3c
kramolist <kramolist@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-03-01 14:14:14 +0000

    FIX: CLD-3435 - DnD of XLS(X) file to graph creates UDR instead of XLSReader
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-3-3-0-M1@11727 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component/src/org/jetel/component/AdditionalComponentAttributes.java
+++ cloveretl.component/src/org/jetel/component/AdditionalComponentAttributes.java
@@ -19,6 +19,8 @@
 package org.jetel.component;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -26,113 +28,111 @@ import java.util.Set;
  * An auxiliary class used as a dirty fix solving non-visibility of commercial components from
  * a package containing FileComponentFactory 
  * 
+ * See issues CLD-3314, CLD-3435 
+ * 
  * @author Pavel Simecek (info@cloveretl.com)
  *         (c) Javlin, a.s. (www.cloveretl.com)
  *
  * @created 21.2.2012
  */
 public enum AdditionalComponentAttributes {
-	SPREADSHEET_DATA_READER,
-	SPREADSHEET_DATA_WRITER;
+	
+	// Components are listed with descending priority (for common file extension(s))
+	SPREADSHEET_DATA_READER("SPREADSHEET_READER", true, "xls", "xlsx"),
+	SPREADSHEET_DATA_WRITER("SPREADSHEET_WRITER", false, "xls", "xlsx"),
+	XLS_READER("XLS_READER", true, "xls", "xlsx"),
+	XLS_WRITER("XLS_WRITER", false, "xls", "xlsx");
 	
 	static public enum ComponentOperation {
 		READER, WRITER;
 	}
 	
-	private static final String SPREADSHEET_DATA_READER_COMPONENT_TYPE = "SPREADSHEET_READER";
-	private static final String SPREADSHEET_DATA_WRITER_COMPONENT_TYPE = "SPREADSHEET_WRITER";
-	
-	private static final String [] SPREADSHEET_DATA_READER_EXTENSIONS = { "xls", "xlsx"};
-	private static final String [] SPREADSHEET_DATA_WRITER_EXTENSIONS = { "xls", "xlsx"};
-	
+	private final String componentType;
+	private boolean reader;
+	private final String[] fileExtentions;
+
+	private AdditionalComponentAttributes(String componentType, boolean reader, String... fileExtentions) {
+		this.componentType = componentType;
+		this.reader = reader;
+		this.fileExtentions = fileExtentions;
+	}
+
 	private static Map<String, String []> componentTypeToExtensions;
-	private static Map<String, AdditionalComponentAttributes> extensionToReader;
-	private static Map<String, AdditionalComponentAttributes> extensionToWriter;
+	private static Map<String, List<AdditionalComponentAttributes>> extensionToReader;
+	private static Map<String, List<AdditionalComponentAttributes>> extensionToWriter;
 	
 	static {
 		componentTypeToExtensions = new HashMap<String, String []>();
-		extensionToReader = new HashMap<String, AdditionalComponentAttributes>();
-		extensionToWriter = new HashMap<String, AdditionalComponentAttributes>();
+		extensionToReader = new HashMap<String, List<AdditionalComponentAttributes>>();
+		extensionToWriter = new HashMap<String, List<AdditionalComponentAttributes>>();
 		for (AdditionalComponentAttributes additionalComponentAttributes : AdditionalComponentAttributes.values()) {
 			componentTypeToExtensions.put(getComponentType(additionalComponentAttributes), getFileExtensions(additionalComponentAttributes));
+			
 			if (isReader(additionalComponentAttributes)) {
-				for (String extension : getFileExtensions(additionalComponentAttributes)) {
-					extensionToReader.put(extension, additionalComponentAttributes);
-				}
+				registerComonentAttribute(additionalComponentAttributes, extensionToReader);
 			}
 			if (isWriter(additionalComponentAttributes)) {
-				for (String extension : getFileExtensions(additionalComponentAttributes)) {
-					extensionToWriter.put(extension, additionalComponentAttributes);
-				}
+				registerComonentAttribute(additionalComponentAttributes, extensionToWriter);
 			}
+		}
+	}
+
+	private static void registerComonentAttribute(AdditionalComponentAttributes additionalComponentAttributes, Map<String, List<AdditionalComponentAttributes>> register) {
+		for (String extension : getFileExtensions(additionalComponentAttributes)) {
+			List<AdditionalComponentAttributes> componentAttributes = register.get(extension);
+			if (componentAttributes == null) {
+				componentAttributes = new LinkedList<AdditionalComponentAttributes>();
+				register.put(extension, componentAttributes);
+			}
+			componentAttributes.add(additionalComponentAttributes);
 		}
 	}
 	
 	public static String getComponentType(AdditionalComponentAttributes additionalComponentAttributes) {
-		if (additionalComponentAttributes == SPREADSHEET_DATA_READER) {
-			return SPREADSHEET_DATA_READER_COMPONENT_TYPE;
-		} else if (additionalComponentAttributes == SPREADSHEET_DATA_WRITER){
-			return  SPREADSHEET_DATA_WRITER_COMPONENT_TYPE;
-		} else {
-			return null;
-		}
+		return additionalComponentAttributes.getComponentType();
 	}
 	
 	public String getComponentType() {
-		return getComponentType(this);
+		return componentType;
 	}
 	
 	public static String [] getFileExtensions(AdditionalComponentAttributes additionalComponentAttributes) {
-		if (additionalComponentAttributes == SPREADSHEET_DATA_READER) {
-			return SPREADSHEET_DATA_READER_EXTENSIONS;
-		} else if (additionalComponentAttributes == SPREADSHEET_DATA_WRITER){
-			return  SPREADSHEET_DATA_WRITER_EXTENSIONS;
-		} else {
-			return null;
-		}
+		return additionalComponentAttributes.getFileExtensions();
 	}
 	
 	public String [] getFileExtensions() {
-		return getFileExtensions(this);
+		return fileExtentions;
 	}
 	
 	public static boolean isReader(AdditionalComponentAttributes additionalComponentAttributes) {
-		if (additionalComponentAttributes == SPREADSHEET_DATA_READER) {
-			return true;
-		} else {
-			return false;
-		}
+		return additionalComponentAttributes.isReader();
 	}
 	
 	public boolean isReader() {
-		return isReader(this);
+		return reader;
 	}
 	
 	public static boolean isWriter(AdditionalComponentAttributes additionalComponentAttributes) {
-		if (additionalComponentAttributes == SPREADSHEET_DATA_WRITER) {
-			return true;
-		} else {
-			return false;
-		}
+		return additionalComponentAttributes.isWriter();
 	}
 	
 	public boolean isWriter() {
-		return isWriter(this);
+		return !reader;
 	}
 	
 	public static String [] getFileExtensionsByComponentType(String componentType) {
 		return componentTypeToExtensions.get(componentType);
 	}
 	
-	public static AdditionalComponentAttributes getReaderByFileExtension(String extension) {
+	public static List<AdditionalComponentAttributes> getReaderByFileExtension(String extension) {
 		return extensionToReader.get(extension);
 	}
 	
-	public static AdditionalComponentAttributes getWriterByFileExtension(String extension) {
+	public static List<AdditionalComponentAttributes> getWriterByFileExtension(String extension) {
 		return extensionToWriter.get(extension);
 	}
 	
-	public static AdditionalComponentAttributes getComponentByFileExtension(String extension, ComponentOperation componentOperation) {
+	public static List<AdditionalComponentAttributes> getComponentByFileExtension(String extension, ComponentOperation componentOperation) {
 		if (componentOperation == ComponentOperation.READER) {
 			return getReaderByFileExtension(extension);
 		} else {
