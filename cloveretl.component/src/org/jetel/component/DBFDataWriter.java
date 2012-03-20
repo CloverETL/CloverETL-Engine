46dc65cdd258cb1fc78c8924fc1f2b8ad69e3733
pavlisd <pavlisd@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-03-20 09:43:15 +0000

    INIT:initial check-in
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/issue-clo-118@11913 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.component/src/org/jetel/component/DBFDataWriter.java
@@ -0,0 +1,489 @@
+/*
+ * jETeL/CloverETL - Java based ETL application framework.
+ * Copyright (c) Javlin, a.s. (info@cloveretl.com)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jetel.component;
+
+import java.io.IOException;
+import java.nio.channels.WritableByteChannel;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jetel.data.DataRecord;
+import org.jetel.data.Defaults;
+import org.jetel.data.lookup.LookupTable;
+import org.jetel.database.dbf.DBFDataFormatter;
+import org.jetel.enums.PartitionFileTagType;
+import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.exception.ConfigurationStatus;
+import org.jetel.graph.InputPort;
+import org.jetel.graph.Node;
+import org.jetel.graph.Result;
+import org.jetel.graph.TransformationGraph;
+import org.jetel.util.MultiFileWriter;
+import org.jetel.util.SynchronizeUtils;
+import org.jetel.util.bytes.SystemOutByteChannel;
+import org.jetel.util.bytes.WritableByteChannelIterator;
+import org.jetel.util.file.FileUtils;
+import org.jetel.util.property.ComponentXMLAttributes;
+import org.w3c.dom.Element;
+
+/**
+ *  <h3>DBFDataWriter Component</h3>
+ *
+ * <!-- All records from input port [0] are formatted to dBase/FoxBase style file-->
+ * 
+ * <table border="1">
+ *  <th>Component:</th>
+ * <tr><td><h4><i>Name:</i></h4></td>
+ * <td>DBFDataWriter</td></tr>
+ * <tr><td><h4><i>Category:</i></h4></td>
+ * <td></td></tr>
+ * <tr><td><h4><i>Description:</i></h4></td>
+ * <td>All records from input port [0] are formatted to dBase/FoxBase style table and written to specified file.</td></tr>
+ * <tr><td><h4><i>Inputs:</i></h4></td>
+ * <td>[0]- input records</td></tr>
+ * <tr><td><h4><i>Outputs:</i></h4></td>
+ * <td></td></tr>
+ * <tr><td><h4><i>Comment:</i></h4></td>
+ * <td></td></tr>
+ * </table>
+ *  <br>  
+ *  <table border="1">
+ *  <th>XML attributes:</th>
+ *  <tr><td><b>type</b></td><td>"DBF_DATA_WRITER"</td></tr>
+ *  <tr><td><b>id</b></td><td>component identification</td>
+ *  <tr><td><b>fileURL</b></td><td>Output files mask.
+ *  Use wildcard '#' to specify where to insert sequential number of file. Number of consecutive wildcards specifies
+ *  minimal length of the number. Name without wildcard specifies only one file.</td>
+ *  <tr><td><b>charset</b></td><td>character encoding of the output file (if not specified, then ISO-8859-1 is used)</td>
+ *  <tr><td><b>append</b></td><td>whether to append data at the end if output file exists or replace it (values: true/false)</td>
+ *  <tr><td><b>recordsPerFile</b></td><td>max number of records in one output file</td>
+ *  <tr><td><b>bytesPerFile</b></td><td>Max size of output files. To avoid splitting a record to two files, max size could be slightly overreached.</td>
+ *  <tr><td><b>recordSkip</b></td><td>number of skipped records</td>
+ *  <tr><td><b>recordCount</b></td><td>number of written records</td>
+ *  </tr>
+ *  </table>  
+ *
+ * <h4>Example:</h4>
+ * <pre>&lt;Node append="true" fileURL="${WORKSPACE}/output/customers.dbf"
+ *  id="DBF_DATA_WRITER" type="DBF_DATA_WRITER"&gt;
+ * &lt;/Node&gt;
+ * 
+ * 
+ *  @author dpavlis (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *         
+ * @since Mar, 13, 2012
+ *
+ */
+public class DBFDataWriter extends Node {
+
+	private static final String XML_APPEND_ATTRIBUTE = "append";
+	private static final String XML_FILEURL_ATTRIBUTE = "fileURL";
+	private static final String XML_CHARSET_ATTRIBUTE = "charset";
+	private static final String XML_RECORD_SKIP_ATTRIBUTE = "recordSkip";
+	private static final String XML_RECORD_COUNT_ATTRIBUTE = "recordCount";
+	private static final String XML_RECORDS_PER_FILE = "recordsPerFile";
+	private static final String XML_BYTES_PER_FILE = "bytesPerFile";
+	private static final String XML_PARTITIONKEY_ATTRIBUTE = "partitionKey";
+	private static final String XML_PARTITION_ATTRIBUTE = "partition";
+	private static final String XML_PARTITION_OUTFIELDS_ATTRIBUTE = "partitionOutFields";
+	private static final String XML_PARTITION_FILETAG_ATTRIBUTE = "partitionFileTag";
+
+	private String fileURL;
+	private boolean appendData;
+	private DBFDataFormatter formatter;
+	private MultiFileWriter writer;
+    private int skip;
+	private int numRecords;
+	private WritableByteChannel writableByteChannel;
+	private int recordsPerFile;
+	private int bytesPerFile;
+	private String charsetName;
+	
+	private String partition;
+	private String attrPartitionKey;
+	private LookupTable lookupTable;
+	private String attrPartitionOutFields;
+	private PartitionFileTagType partitionFileTagType = PartitionFileTagType.NUMBER_FILE_TAG;
+
+	public final static String COMPONENT_TYPE = "DBF_DATA_WRITER";
+	private final static int READ_FROM_PORT = 0;
+	private final static int OUTPUT_PORT = 0;
+
+	private static Log logger = LogFactory.getLog(DBFDataWriter.class);
+
+	/**
+	 * Constructor
+	 * 
+	 * @param id
+	 * @param fileURL
+	 * @param charset
+	 * @param appendData
+	 * @param fields
+	 */
+	public DBFDataWriter(String id, String fileURL, String charset, 
+			boolean appendData) {
+		super(id);
+		this.fileURL = fileURL;
+		this.appendData = appendData;
+		this.charsetName = charset == null ? Defaults.DataFormatter.DEFAULT_CHARSET_ENCODER : charset;
+		formatter =  new DBFDataFormatter(charsetName);
+	}
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param id
+	 * @param writableByteChannel
+	 * @param charset
+	 * @param appendData
+	 * @param fields
+	 */
+	public DBFDataWriter(String id, WritableByteChannel writableByteChannel, String charset, 
+			boolean appendData, String[] fields) {
+		super(id);
+		this.writableByteChannel = writableByteChannel;
+		this.appendData = appendData;
+		this.charsetName = charset;
+		throw new UnsupportedOperationException("Can't work with WritableByteChannel !");
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jetel.graph.Node#getType()
+	 */
+	@Override
+	public String getType() {
+		return COMPONENT_TYPE;
+	}
+
+	@Override
+	public void preExecute() throws ComponentNotReadyException {
+		super.preExecute();
+		
+		if (firstRun()) {
+	        writer.init(getInputPort(READ_FROM_PORT).getMetadata());
+		}
+		else {
+			writer.reset();
+		}
+	}
+
+	@Override
+	public Result execute() throws Exception {
+		InputPort inPort = getInputPort(READ_FROM_PORT);
+		DataRecord record = new DataRecord(inPort.getMetadata());
+		record.init();
+		while (record != null && runIt) {
+			record = inPort.readRecord(record);
+			if (record != null) {
+		        writer.write(record);
+			}
+			SynchronizeUtils.cloverYield();
+		}
+		writer.finish();
+        return runIt ? Result.FINISHED_OK : Result.ABORTED;
+	}
+
+	@Override
+	public void postExecute() throws ComponentNotReadyException {
+		super.postExecute();
+		try {
+			writer.close();
+		}
+		catch (IOException e) {
+			throw new ComponentNotReadyException(COMPONENT_TYPE + ": " + e.getMessage(),e);
+		}
+	}
+
+	@Override
+	public synchronized void free() {
+		super.free();
+		if (writer != null)
+			try {
+				writer.close();
+			} catch(Throwable t) {
+				logger.warn("Resource releasing failed for '" + getId() + "'. " + t.getMessage(), t);
+			}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jetel.graph.GraphElement#checkConfig()
+	 */
+    @Override
+    public ConfigurationStatus checkConfig(ConfigurationStatus status) {
+		super.checkConfig(status);
+		
+		if(!checkInputPorts(status, 1, 1)
+				|| !checkOutputPorts(status, 0, 1)) {
+			return status;
+		}
+
+        try {
+        	FileUtils.canWrite(getGraph() != null ? getGraph().getRuntimeContext().getContextURL() 
+        			: null, fileURL);
+        } catch (ComponentNotReadyException e) {
+            status.add(e,ConfigurationStatus.Severity.ERROR,this,
+            		ConfigurationStatus.Priority.NORMAL,XML_FILEURL_ATTRIBUTE);
+        }
+        
+        return status;
+    }
+	
+	/* (non-Javadoc)
+	 * @see org.jetel.graph.GraphElement#init()
+	 */
+	@Override
+	public void init() throws ComponentNotReadyException {
+        if(isInitialized()) return;
+		super.init();
+		TransformationGraph graph = getGraph();
+		
+		initLookupTable();
+
+		// based on file mask, create/open output file
+		if (fileURL != null) {
+	        writer = new MultiFileWriter(formatter, graph != null ? graph.getRuntimeContext().getContextURL() : null, fileURL);
+		} else {
+			if (writableByteChannel == null) {
+		        writableByteChannel =  new SystemOutByteChannel();
+			}
+	        writer = new MultiFileWriter(formatter, new WritableByteChannelIterator(writableByteChannel));
+		}
+        writer.setLogger(logger);
+        writer.setBytesPerFile(bytesPerFile);
+        writer.setRecordsPerFile(recordsPerFile);
+        writer.setAppendData(appendData);
+        writer.setSkip(skip);
+        writer.setNumRecords(numRecords);
+        writer.setDictionary(graph.getDictionary());
+        if (attrPartitionKey != null) {
+            writer.setLookupTable(lookupTable);
+            writer.setPartitionKeyNames(attrPartitionKey.split(Defaults.Component.KEY_FIELDS_DELIMITER_REGEX));
+            writer.setPartitionFileTag(partitionFileTagType);
+        	if (attrPartitionOutFields != null) {
+        		writer.setPartitionOutFields(attrPartitionOutFields.split(Defaults.Component.KEY_FIELDS_DELIMITER_REGEX));
+        	}
+        }
+        writer.setOutputPort(getOutputPort(OUTPUT_PORT)); //for port protocol: target file writes data
+	}
+
+	/**
+	 * Creates and initializes lookup table.
+	 * 
+	 * @throws ComponentNotReadyException
+	 */
+	private void initLookupTable() throws ComponentNotReadyException {
+		if (partition == null) return;
+		
+		// Initializing lookup table
+		lookupTable = getGraph().getLookupTable(partition);
+		if (lookupTable == null) {
+			throw new ComponentNotReadyException("Lookup table \"" + partition + "\" not found.");
+		}
+		if (!lookupTable.isInitialized()) {
+			lookupTable.init();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jetel.graph.Node#fromXML(org.jetel.graph.TransformationGraph, org.w3c.dom.Element)
+	 */
+	public static Node fromXML(TransformationGraph graph, Element nodeXML) {
+		ComponentXMLAttributes xattribs=new ComponentXMLAttributes(nodeXML, graph);
+		DBFDataWriter aDataWriter = null;
+		
+		try{
+			aDataWriter = new DBFDataWriter(xattribs.getString(Node.XML_ID_ATTRIBUTE),
+									xattribs.getString(XML_FILEURL_ATTRIBUTE),
+									xattribs.getString(XML_CHARSET_ATTRIBUTE,null),
+									xattribs.getBoolean(XML_APPEND_ATTRIBUTE, false));
+			if (xattribs.exists(XML_RECORD_SKIP_ATTRIBUTE)){
+				aDataWriter.setSkip(Integer.parseInt(xattribs.getString(XML_RECORD_SKIP_ATTRIBUTE)));
+			}
+			if (xattribs.exists(XML_RECORD_COUNT_ATTRIBUTE)){
+				aDataWriter.setNumRecords(Integer.parseInt(xattribs.getString(XML_RECORD_COUNT_ATTRIBUTE)));
+			}
+            if(xattribs.exists(XML_RECORDS_PER_FILE)) {
+            	aDataWriter.setRecordsPerFile(xattribs.getInteger(XML_RECORDS_PER_FILE));
+            }
+            if(xattribs.exists(XML_BYTES_PER_FILE)) {
+            	aDataWriter.setBytesPerFile(xattribs.getInteger(XML_BYTES_PER_FILE));
+            }
+			if(xattribs.exists(XML_PARTITIONKEY_ATTRIBUTE)) {
+				aDataWriter.setPartitionKey(xattribs.getString(XML_PARTITIONKEY_ATTRIBUTE));
+            }
+			if(xattribs.exists(XML_PARTITION_ATTRIBUTE)) {
+				aDataWriter.setPartition(xattribs.getString(XML_PARTITION_ATTRIBUTE));
+            }
+			if(xattribs.exists(XML_PARTITION_FILETAG_ATTRIBUTE)) {
+				aDataWriter.setPartitionFileTag(xattribs.getString(XML_PARTITION_FILETAG_ATTRIBUTE));
+            }
+			if(xattribs.exists(XML_PARTITION_OUTFIELDS_ATTRIBUTE)) {
+				aDataWriter.setPartitionOutFields(xattribs.getString(XML_PARTITION_OUTFIELDS_ATTRIBUTE));
+            }
+		}catch(Exception ex){
+			System.err.println(COMPONENT_TYPE + ":" + xattribs.getString(Node.XML_ID_ATTRIBUTE,"unknown ID") + ":" + ex.getMessage());
+			return null;
+		}
+		
+		return aDataWriter;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jetel.graph.Node#toXML(org.w3c.dom.Element)
+	 */
+	@Override
+	public void toXML(org.w3c.dom.Element xmlElement) {
+		super.toXML(xmlElement);
+		xmlElement.setAttribute(XML_FILEURL_ATTRIBUTE,this.fileURL);
+		if (charsetName != null) {
+			xmlElement.setAttribute(XML_CHARSET_ATTRIBUTE, this.charsetName);
+		}
+		xmlElement.setAttribute(XML_APPEND_ATTRIBUTE, String.valueOf(this.appendData));
+		if (skip != 0){
+			xmlElement.setAttribute(XML_RECORD_SKIP_ATTRIBUTE, String.valueOf(skip));
+		}
+		if (numRecords != 0){
+			xmlElement.setAttribute(XML_RECORD_COUNT_ATTRIBUTE,String.valueOf(numRecords));
+		}
+		if (recordsPerFile > 0) {
+			xmlElement.setAttribute(XML_RECORDS_PER_FILE, Integer.toString(recordsPerFile));
+		}
+		if (bytesPerFile > 0) {
+			xmlElement.setAttribute(XML_BYTES_PER_FILE, Integer.toString(bytesPerFile));
+		}
+		if (partition != null) {
+			xmlElement.setAttribute(XML_PARTITION_ATTRIBUTE, partition);
+		} else if (lookupTable != null) {
+			xmlElement.setAttribute(XML_PARTITION_ATTRIBUTE, lookupTable.getId());
+		}
+		if (attrPartitionKey != null) {
+			xmlElement.setAttribute(XML_PARTITIONKEY_ATTRIBUTE, attrPartitionKey);
+		}
+		if (attrPartitionOutFields != null) {
+			xmlElement.setAttribute(XML_PARTITION_OUTFIELDS_ATTRIBUTE, attrPartitionOutFields);
+		}
+		xmlElement.setAttribute(XML_PARTITION_FILETAG_ATTRIBUTE, partitionFileTagType.name());
+	}
+	
+    /**
+     * Sets number of skipped records in next call of getNext() method.
+     * @param skip
+     */
+    public void setSkip(int skip) {
+        this.skip = skip;
+    }
+
+    /**
+     * Sets number of written records.
+     * @param numRecords
+     */
+    public void setNumRecords(int numRecords) {
+        this.numRecords = numRecords;
+    }
+
+    public void setBytesPerFile(int bytesPerFile) {
+        this.bytesPerFile = bytesPerFile;
+    }
+
+    public void setRecordsPerFile(int recordsPerFile) {
+        this.recordsPerFile = recordsPerFile;
+    }
+
+    /**
+     * Sets lookup table for data partition.
+     * 
+     * @param lookupTable
+     */
+	public void setLookupTable(LookupTable lookupTable) {
+		this.lookupTable = lookupTable;
+	}
+
+	/**
+	 * Gets lookup table for data partition.
+	 * 
+	 * @return
+	 */
+	public LookupTable getLookupTable() {
+		return lookupTable;
+	}
+
+	/**
+	 * Gets partition (lookup table id) for data partition.
+	 * 
+	 * @param partition
+	 */
+	public void setPartition(String partition) {
+		this.partition = partition;
+	}
+
+	/**
+	 * Gets partition (lookup table id) for data partition.
+	 * 
+	 * @return
+	 */
+	public String getPartition() {
+		return partition;
+	}
+
+	/**
+	 * Sets partition key for data partition.
+	 * 
+	 * @param partitionKey
+	 */
+	public void setPartitionKey(String partitionKey) {
+		this.attrPartitionKey = partitionKey;
+	}
+
+	/**
+	 * Gets partition key for data partition.
+	 * 
+	 * @return
+	 */
+	public String getPartitionKey() {
+		return attrPartitionKey;
+	}
+	
+	/**
+	 * Sets fields which are used for file output name.
+	 * 
+	 * @param partitionOutFields
+	 */
+	public void setPartitionOutFields(String partitionOutFields) {
+		attrPartitionOutFields = partitionOutFields;
+	}
+
+	/**
+	 * Sets number file tag for data partition.
+	 * 
+	 * @param partitionKey
+	 */
+	public void setPartitionFileTag(String partitionFileTagType) {
+		this.partitionFileTagType = PartitionFileTagType.valueOfIgnoreCase(partitionFileTagType);
+	}
+
+	/**
+	 * Gets number file tag for data partition.
+	 * 
+	 * @return
+	 */
+	public PartitionFileTagType getPartitionFileTag() {
+		return partitionFileTagType;
+	}
+}
