261588da14078b96c8063f40a8c73c688fffab27
slamam <slamam@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-03-21 10:10:02 +0000

    FIX: graph tests failing because of fix CL-2197
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-3-3-0-M2@11932 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.examples/SimpleExamples/after-commit.ts
+++ cloveretl.examples/SimpleExamples/after-commit.ts
@@ -65,9 +65,8 @@
 	 <FlatFile outputFile="data-out/correctCustomersControlled.txt" supposedFile="supposed-out/correctCustomers.DataPolicy.Controlled.txt"/>
 	 <FlatFile outputFile="data-out/correctCustomersLenient.txt" supposedFile="supposed-out/correctCustomers.DataPolicy.Lenient.txt"/>
 	 <FlatFile outputFile="data-out/allCustomersStrict.txt" supposedFile="supposed-out/allCustomers.DataPolicy.Strict.txt"/>
-		<RegEx expression="org.jetel.exception.BadDataFormatException: account_num \(long\) cannot be set to &quot;8747577056D&quot; - doesn't match defined format &quot;&quot; with reason &quot;Incomplete parsing&quot; in record 3, field 2 \(&quot;account_num&quot;\), metadata &quot;customer&quot;; value: '8747577056D'" occurences="1"/>
+		<RegEx expression="org.jetel.exception.BadDataFormatException: Field account_num\(long\) cannot be set to value &quot;8747577056D&quot;; doesn't match the specified format &quot;&quot; with reason &quot;Incomplete parsing&quot;; in record 3, field 2" occurences="1"/>
 </FunctionalTest>
-
 
 <FunctionalTest ident="DBFJoin" graphFile="graph/graphDBFJoin.grf">
 	<FlatFile outputFile="data-out/joinedDBForders.out" supposedFile="supposed-out/joinedDBForders.DBFJoin.out"/>
