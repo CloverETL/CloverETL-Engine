ba5f5441e78d35df189788f3d89ebb6cda5174aa
pavlisd <pavlisd@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-04-19 09:10:53 +0000

    inserted by mistake
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/issue-CLO-126@12119 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component.hadoop/build/src-improved/org/jetel/hadoop/component/IHadoopSequenceFileFormatter.java
+++
@@ -1,9 +0,0 @@
-package org.jetel.hadoop.component;
-
-import org.jetel.data.formatter.Formatter;
-
-public interface IHadoopSequenceFileFormatter extends Formatter {
-
-	public void setKeyValueFields(String keyFieldName, String valueFieldName);
-	
-}
