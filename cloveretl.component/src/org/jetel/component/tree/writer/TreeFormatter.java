2bc46a8ada61bd8e09d5d64552361810dc8cb75d
slamam <slamam@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-04-27 14:56:39 +0000

    UPDATE: trunk merged
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/issue-clo-118@12195 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component/src/org/jetel/component/tree/writer/TreeFormatter.java
+++ cloveretl.component/src/org/jetel/component/tree/writer/TreeFormatter.java
@@ -24,6 +24,7 @@ import java.util.Arrays;
 import org.jetel.component.tree.writer.model.runtime.WritableMapping;
 import org.jetel.component.tree.writer.model.runtime.WritableMapping.MappingWriteState;
 import org.jetel.data.DataRecord;
+import org.jetel.data.formatter.AbstractFormatter;
 import org.jetel.data.formatter.Formatter;
 import org.jetel.exception.ComponentNotReadyException;
 import org.jetel.exception.JetelException;
@@ -36,7 +37,7 @@ import org.jetel.metadata.DataRecordMetadata;
  * 
  * @created 18 Jan 2011
  */
-public abstract class TreeFormatter implements Formatter {
+public abstract class TreeFormatter extends AbstractFormatter {
 
 	private WritableMapping mapping;
 	private final int availableDataArrayLength;
