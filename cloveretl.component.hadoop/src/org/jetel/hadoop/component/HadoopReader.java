3e5f8983a0f2e4bc4048cafe5cb1d1368b4d5852
pavlisd <pavlisd@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-06-22 20:53:37 +0000

    UPDATE:minor clean up
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/issue-CLO-126@12464 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component.hadoop/src/org/jetel/hadoop/component/HadoopReader.java
+++ cloveretl.component.hadoop/src/org/jetel/hadoop/component/HadoopReader.java
@@ -1,3 +1,22 @@
+/*
+ * jETeL/CloverETL - Java based ETL application framework.
+ * Copyright (c) Javlin, a.s. (info@cloveretl.com)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 package org.jetel.hadoop.component;
 
 import java.io.IOException;
@@ -28,6 +47,14 @@ import org.jetel.util.property.ComponentXMLAttributes;
 import org.jetel.util.property.RefResFlag;
 import org.jetel.util.string.StringUtils;
 import org.w3c.dom.Element;
+
+/**
+* @author David Pavlis (info@cloveretl.com)
+*         (c) Javlin a.s. (www.javlin.eu)
+* 
+* @since 	
+* @revision    $Revision: $ 
+*/
 
 public class HadoopReader extends Node {
 
