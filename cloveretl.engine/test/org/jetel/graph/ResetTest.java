d8035871ee0330894806de936b4cb312c9e9a410
hajekt <hajekt@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-08-06 08:51:32 +0000

    MINOR: updated list of graphs that are not executed
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-3@12678 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/graph/ResetTest.java
+++ cloveretl.engine/test/org/jetel/graph/ResetTest.java
@@ -160,7 +160,8 @@ public class ResetTest extends CloverTestCase {
 							&& !pathname.getName().equals("XMLExtract_charset_invalid.grf") // ok, is to fail
 							&& !pathname.getName().equals("XMLExtract_mappingURL_missing.grf") // ok, is to fail
 							&& !pathname.getName().equals("RunGraph_differentOutputMetadataFail.grf") // ok, is to fail
-							&& !pathname.getName().equals("SandboxOperationHandlerTest.grf"); // runs only on server
+							&& !pathname.getName().equals("SandboxOperationHandlerTest.grf") // runs only on server
+							&& !pathname.getName().equals("WebDAV.grf"); // remove after CL-2434 solved
 					
 				}
 			});
