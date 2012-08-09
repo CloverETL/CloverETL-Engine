6f48b5a458ff635eda848d4e8af07df1609790e7
hajekt <info@cloveretl.com>
2012-08-09 14:41:06 +0000

    MINOR: paths to nonexistent tmp dirs are commented out
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-3@12714 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.examples/CompanyTransactionsTutorial/night-functional.ts
+++ cloveretl.examples/CompanyTransactionsTutorial/night-functional.ts
@@ -4,7 +4,7 @@
 <TestScenario ident="Company Transactions Tutorial" description="Company Transactions Tutorial examples" useJMX="true">    
 
 	<FunctionalTest ident="A01" graphFile="graph/A01_SplittingTransactions.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp/Amounts.dat" supposedFile="supposed-out/A01.Amounts.dat"/>
 	 	<FlatFile outputFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp/Customers.dat" supposedFile="supposed-out/A01.Customers.dat"/>
 	 	<FlatFile outputFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp/Employees.dat" supposedFile="supposed-out/A01.Employees.dat"/>
@@ -12,17 +12,17 @@
 	</FunctionalTest>
 	
 	<FunctionalTest ident="A02" graphFile="graph/A02_CreatingXLSEmployeesWithFamily.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp/EmployeesWithFamily.xls" supposedFile="supposed-out/A02.EmployeesWithFamily.xls"/>
 	</FunctionalTest>
 
 	<FunctionalTest ident="A03" graphFile="graph/A03_ConvertingCustomersFromDelimitedToFixed.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp/CustomersFixed.txt" supposedFile="supposed-out/A03.CustomersFixed.txt"/>
 	</FunctionalTest>
 
 	<FunctionalTest ident="A04" graphFile="graph/A04_SortingTransactions.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="data-out/CustomersForEmployees.txt" supposedFile="supposed-out/A04.CustomersForEmployees.txt"/>
 	 	<FlatFile outputFile="data-out/CustomersForStates.txt" supposedFile="supposed-out/A04.CustomersForStates.txt"/>
 	 	<FlatFile outputFile="data-out/EmployeesForCustomers.txt" supposedFile="supposed-out/A04.EmployeesForCustomers.txt"/>
@@ -33,27 +33,27 @@
 	</FunctionalTest>
 	
 	<FunctionalTest ident="A05" graphFile="graph/A05_CreatingXMLEmplFamCustAm.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 		<Property name="MAPPINGS" value="0"/>
 	</FunctionalTest>
 
 	<FunctionalTest ident="A06" graphFile="graph/A06_CreatingXMLCustEmplFamAm.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 		<Property name="MAPPINGS" value="0"/>
 	</FunctionalTest>
 
 	<FunctionalTest ident="A07" graphFile="graph/A07_CreatingXMLAmCustEmplFam.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 		<Property name="MAPPINGS" value="0"/>
 	</FunctionalTest>
 
 	<FunctionalTest ident="A08" graphFile="graph/A08_CreatingXMLTransactionsFamily.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 		<Property name="MAPPINGS" value="0"/>
 	</FunctionalTest>
 
 	<FunctionalTest ident="A09" graphFile="graph/A09_XMLExtractEmplFamCustAm.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="data-out/amountsXMLExtractWithEmplID_WithCustID.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A09.amountsXMLExtractWithEmplID_WithCustID.txt"/>
 	 	<FlatFile outputFile="data-out/childrenXMLExtract.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A09.childrenXMLExtract.txt"/>
 	 	<FlatFile outputFile="data-out/customersXMLExtract.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A09.customersXMLExtract.txt"/>
@@ -62,7 +62,7 @@
 	</FunctionalTest>
 	
 	<FunctionalTest ident="A10" graphFile="graph/A10_XMLExtractCustEmplFamAm.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="data-out/amountsXMLExtractWithCustID_WithEmplID.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A10.amountsXMLExtractWithCustID_WithEmplID.txt"/>
 	 	<FlatFile outputFile="data-out/childrenXMLExtract.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A10.childrenXMLExtract.txt"/>
 	 	<FlatFile outputFile="data-out/customersXMLExtract.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A10.customersXMLExtract.txt"/>
@@ -71,7 +71,7 @@
 	</FunctionalTest>
 	
 	<FunctionalTest ident="A11" graphFile="graph/A11_XMLExtractAmCustEmplFam.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="data-out/amountsXMLExtract.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A11.amountsXMLExtract.txt"/>
 	 	<FlatFile outputFile="data-out/childrenXMLExtract.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A11.childrenXMLExtract.txt"/>
 	 	<FlatFile outputFile="data-out/customersXMLExtractWithAmID.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A11.customersXMLExtractWithAmID.txt"/>
@@ -80,14 +80,14 @@
 	</FunctionalTest>
 	
 	<FunctionalTest ident="A12" graphFile="graph/A12_XMLExtractTransactionsFamily.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="data-out/childrenXMLExtract.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A12.childrenXMLExtract.txt"/>
 	 	<FlatFile outputFile="data-out/transactionsXMLExtract.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A12.transactionsXMLExtract.txt"/>
 	 	<FlatFile outputFile="data-out/spousesXMLExtract.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A12.spousesXMLExtract.txt"/>
 	</FunctionalTest>
 	
 	<FunctionalTest ident="A13" graphFile="graph/A13_XMLXPathEmplFamCustAm.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="data-out/amountsXMLXPathWithEmplID_WithCustID.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A13.amountsXMLXPathWithEmplID_WithCustID.txt"/>
 	 	<FlatFile outputFile="data-out/childrenXMLXPath.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A13.childrenXMLXPath.txt"/>
 	 	<FlatFile outputFile="data-out/customersXMLXPath.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A13.customersXMLXPath.txt"/>
@@ -96,7 +96,7 @@
 	</FunctionalTest>
 	
 	<FunctionalTest ident="A14" graphFile="graph/A14_XMLXPathCustEmplFamAm.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="data-out/amountsXMLXPathWithCustID_WithEmplID.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A14.amountsXMLXPathWithCustID_WithEmplID.txt"/>
 	 	<FlatFile outputFile="data-out/childrenXMLXPath.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A14.childrenXMLXPath.txt"/>
 	 	<FlatFile outputFile="data-out/customersXMLXPath.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A14.customersXMLXPath.txt"/>
@@ -105,7 +105,7 @@
 	</FunctionalTest>
 	
 	<FunctionalTest ident="A15" graphFile="graph/A15_XMLXPathAmCustEmplFam.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="data-out/customersXMLXPathWithAmID.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A15.customersXMLXPathWithAmID.txt"/>
 	 	<FlatFile outputFile="data-out/childrenXMLXPath.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A15.childrenXMLXPath.txt"/>
 	 	<FlatFile outputFile="data-out/amountsXMLXPath.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A15.amountsXMLXPath.txt"/>
@@ -114,7 +114,7 @@
 	</FunctionalTest>
 	
 	<FunctionalTest ident="A16" graphFile="graph/A16_XMLXPathTransactionsFamily.grf">
-		<Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/>
+		<!-- <Property name="DATATMP_DIR" value="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/data-tmp"/> -->
 	 	<FlatFile outputFile="data-out/transactionsXMLXPath.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A16.transactionsXMLXPath.txt"/>
 	 	<FlatFile outputFile="data-out/childrenXMLXPath.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A16.childrenXMLXPath.txt"/>
 	 	<FlatFile outputFile="data-out/spousesXMLXPath.txt" supposedFile="/data/bigfiles/cloveretl-engine-2.8/cloveretl.engine.examples.CompanyTransactionsTutorial.bigdata/supposed-out/A16.spousesXMLXPath.txt"/>
