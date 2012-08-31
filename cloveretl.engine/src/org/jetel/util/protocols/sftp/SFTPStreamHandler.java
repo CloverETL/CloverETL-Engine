a6ebfbb13ab1674ed7f1e5e97c487900c87a4485
slamam <slamam@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-08-31 13:30:34 +0000

    UPDATE: merge of release-3.2 branch
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-3@12834 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/protocols/sftp/SFTPStreamHandler.java
+++ cloveretl.engine/src/org/jetel/util/protocols/sftp/SFTPStreamHandler.java
@@ -23,32 +23,94 @@ import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLStreamHandler;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jetel.graph.ContextProvider;
+import org.jetel.graph.Node;
 
 /**
  * URLStreamHandler for sftp connection.
  * 
- * @author Jan Ausperger (jan.ausperger@javlinconsulting.cz)
- *         (c) Javlin Consulting (www.javlinconsulting.cz)
+ * @author Jan Ausperger, Martin Slama (jan.ausperger@javlinconsulting.cz) (c) Javlin Consulting
+ *         (www.javlinconsulting.cz)
  */
 public class SFTPStreamHandler extends URLStreamHandler {
 
+	/*
+	 * Opened connection are cached for each component (component id is the key for the connection). Cached connection
+	 * is re-used if possible. New connection is created just if it's necessary.
+	 */
+	private final Map<String, SFTPConnection> pool = new HashMap<String, SFTPConnection>();
+
 	@Override
 	public URLConnection openConnection(URL url) throws IOException {
-		return new SFTPConnection(url);
+		return openConnection(url, null);
 	}
 
 	@Override
 	public URLConnection openConnection(URL url, Proxy proxy) throws IOException {
-		return new SFTPConnection(url, proxy);
+		String nodeId = getCurrentNodeId();
+		SFTPConnection connection = null;
+		if (!connectionExists(url)) {
+			connection = new SFTPConnection(url, proxy, this);
+			pool.put(nodeId, connection);
+		} else {
+			connection = pool.get(nodeId);
+		}
+		connection.setURL(url);
+		return connection;
 	}
 
-    @Override
+	@Override
 	protected void parseURL(URL u, String spec, int start, int limit) {
-    	super.parseURL(u, spec, start, limit);
-    	String protocol = u.getProtocol();
-    	if (!(protocol.equals("sftp") || protocol.equals("scp"))) {
-    		throw new RuntimeException("Parse error: The URL protocol must be sftp or scp!");
-    	}
-    }
+		super.parseURL(u, spec, start, limit);
+		String protocol = u.getProtocol();
+		if (!(protocol.equals("sftp") || protocol.equals("scp"))) {
+			throw new RuntimeException("Parse error: The URL protocol must be sftp or scp!");
+		}
+	}
 
+	/**
+	 * Removes given connection from the pool.
+	 * 
+	 * @param connection
+	 *            Connection to be removed.
+	 * @return True if connection was removed, false otherwise.
+	 */
+	public boolean removeFromPool(SFTPConnection connection) {
+		for (Map.Entry<String, SFTPConnection> entries : pool.entrySet()) {
+			if (connection == entries.getValue()) {
+				return pool.remove(entries.getKey()) != null;
+			}
+		}
+		return false;
+	}
+
+	private boolean connectionExists(URL url) {
+		SFTPConnection connection = pool.get(getCurrentNodeId());
+		if (connection != null) {
+			URL connectionURL = connection.getURL();
+			boolean connectionExists = connectionURL.getPort() == url.getPort();
+			if (connectionExists && connectionURL.getHost() != null) {
+				connectionExists = connectionURL.getHost().equals(url.getHost());
+			}
+			if (connectionExists && connectionURL.getProtocol() != null) {
+				connectionExists = connectionURL.getProtocol().equals(url.getProtocol());
+			}
+			if (connectionExists && connectionURL.getAuthority() != null) {
+				connectionExists = connectionURL.getAuthority().equals(url.getAuthority());
+			}
+			if (connectionExists && connectionURL.getUserInfo() != null) {
+				connectionExists = connectionURL.getUserInfo().equals(url.getUserInfo());
+			}
+			return connectionExists;
+		}
+		return false;
+	}
+
+	private String getCurrentNodeId() {
+		Node node = ContextProvider.getNode();
+		return node == null ? null : node.getId();
+	}
 }
