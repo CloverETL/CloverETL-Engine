c5e96c1805dbf466a5c15b9bcb9c0614c97cd550
kuglerm <kuglerm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-09-19 09:28:43 +0000

    FIX: CLD-4065, CLD-4072 - fixed naming of unnamed columns in select query
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-3@12930 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.connection/src/org/jetel/connection/jdbc/SQLUtil.java
+++ cloveretl.connection/src/org/jetel/connection/jdbc/SQLUtil.java
@@ -598,14 +598,44 @@ public class SQLUtil {
 	}
 
 	/**
-	 * Searches select clause for function calls or other unnamed fields and generates
-	 * names for them
-	 * @param select
-	 * @return
+	 * method checks if text contains only closed pairs of bars.
+	 * for example: "(select 1)), (select " return false;
+	 * @param text
+	 * @return true if only closed pairs are contained in text
 	 */
+	private static boolean isAllBarPairsClosed(String text) {
+		int currentOpen = 0;
+		for (int i = 0; i < text.length(); i++) {
+			char current = text.charAt(i);
+			switch (current) {
+			case '(': {
+				currentOpen++;
+				break;
+			}
+			case ')': {
+				if (currentOpen > 0) {
+					currentOpen --;
+					break;
+				} else {
+					return false;
+				}
+			}
+			}
+		}
+		
+		return currentOpen == 0;
+	}
+
 	static String SELECT_KW = "select";
 	static Pattern FROM_KW = Pattern.compile("\\s+from\\s+");
 	static String selectDelim = ",";
+	/**
+	 * Searches select clause for function calls or other unnamed fields and generates
+	 * names for them
+	 * @param select
+	 * @param specific
+	 * @return
+	 */
 	public static String removeUnnamedFields(String select, JdbcSpecific specific) {
 		if (select == null) {
 			return null;
@@ -625,16 +655,47 @@ public class SQLUtil {
 			contentOffset = selectKwOffset + SELECT_KW.length();
 			newQuery.append(select.substring(starti, contentOffset));
 			selectPart = select.substring(contentOffset);
-			Matcher m = FROM_KW.matcher(selectPart);
-			if (m.find()) {
-				selectPart = selectPart.substring(0, m.start());
+			Matcher m = FROM_KW.matcher(selectPart.toLowerCase());
+			boolean founded = false;
+			while (m.find()) {
+				// we can't just cut it there, it breaks up this case: select a, (select b from t2 ) from t1;
+				if (isAllBarPairsClosed(selectPart.substring(0, m.start()))) {	
+					selectPart = selectPart.substring(0, m.start());
+					founded = true;
+					break;
+				}
+			}
+			
+			// select without from ? could be something like: select (select 1) from ... - inner select 
+			if (!founded) {
+				int inPosition;
+				inPosition = selectPart.indexOf(')');
+				while (inPosition > -1 ) {
+					
+					if (isAllBarPairsClosed(selectPart.substring(0, inPosition))) {	
+						selectPart = selectPart.substring(0, inPosition);	
+						break;
+					}			
+					inPosition = selectPart.indexOf(')',inPosition+1);	
+				}
+			}
+			
+			// from original string - select what from ... - what contains another select !
+			String innerSelectPart;
+			if (selectPart.indexOf(SELECT_KW) != -1) {
+				innerSelectPart = removeUnnamedFields(selectPart, specific);
+			} else {
+				innerSelectPart = selectPart;
 			}
 
-			parts = selectPart.split(selectDelim);
+			parts = innerSelectPart.split(selectDelim);
 			StringBuilder newSelectPart = new StringBuilder();
 			for(int i = 0; parts != null && i < parts.length; i++) {
 				
-				if (parts[i].trim().endsWith(")")) {
+				// we can't just add it on ')', it breaks up this case: func1(func2(aaa),bbb,ccc);
+				// ignoring delimiter as it is not important for bars
+				if (parts[i].trim().endsWith(")") 
+						&& isAllBarPairsClosed(newSelectPart.toString()+parts[i])) {
 					parts[i] += " as AUTOCOLUMN" + String.valueOf(Math.round(Math.random() * 100000));
 				}
 				
@@ -647,8 +708,8 @@ public class SQLUtil {
 
 			newQuery.append(newSelectPart);
 			
-			selectKwOffset++;
 			starti = contentOffset + selectPart.length();
+			selectKwOffset = starti;
 		}
 		newQuery.append(select.substring(starti));
 		
