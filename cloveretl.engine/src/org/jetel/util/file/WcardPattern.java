0fe9686a8c19cf2edc0e69b44a9c775ed87d236f
slamam <slamam@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-11-13 15:16:54 +0000

    FIX: CL-2585 Read problem when downloading zip from S3
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-3-2@13173 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/file/WcardPattern.java
+++ cloveretl.engine/src/org/jetel/util/file/WcardPattern.java
@@ -269,10 +269,17 @@ public class WcardPattern {
 		List<String> newFileNames = resolveAndSetFileNames(fileName); // returns simple file names
         if (fileStreamNames.size() == 0) {
         	for (String newFileName: newFileNames) {
-                URL url = FileUtils.getFileURL(parent, newFileName);
-            	if ((outherPathNeedsInputStream || (anchor.contains("" + WCARD_CHAR[0]) || anchor.contains("" + WCARD_CHAR[1]))))
-            		fileStreamNames.add(new FileStreamName(newFileName, FileUtils.getAuthorizedConnection(url).getInputStream()));
-            	else fileStreamNames.add(new FileStreamName(newFileName));
+        		
+        		InputStream is = null;
+            	if ((outherPathNeedsInputStream || (anchor.contains("" + WCARD_CHAR[0]) || anchor.contains("" + WCARD_CHAR[1])))) {
+            		URL url = FileUtils.getFileURL(parent, newFileName);
+            		if (S3InputStream.isS3File(url)) {
+            			is = FileUtils.getInputStream(parent, newFileName);
+            		} else {
+            			is = FileUtils.getAuthorizedConnection(url).getInputStream();
+            		}
+            	}
+            	fileStreamNames.add(new FileStreamName(newFileName, is));
         	}
         }
         
