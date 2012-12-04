5a85ad420bafbbdd192cd88e681bec83b6785510
michalicaj <michalicaj@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-12-04 09:15:23 +0000

    MINOR: licence header fixed
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-3@13311 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/TargetFile.java
+++ cloveretl.engine/src/org/jetel/util/TargetFile.java
@@ -1,7 +1,7 @@
 /*
  * jETeL/CloverETL - Java based ETL application framework.
  * Copyright (c) Javlin, a.s. (info@cloveretl.com)
- *
+ *  
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
