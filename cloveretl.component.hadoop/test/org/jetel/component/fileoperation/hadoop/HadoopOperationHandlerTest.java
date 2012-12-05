e6cc0b12070562f020eb134b25c8b3711b1375bb
krivanekm <krivanekm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-12-05 10:40:44 +0000

    MINOR: CLO-126 - File operations: disabled HadoopOperationHandlerTest.testCreateDated() - synchronization issues.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-3-4-0-M1@13319 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component.hadoop/test/org/jetel/component/fileoperation/hadoop/HadoopOperationHandlerTest.java
+++ cloveretl.component.hadoop/test/org/jetel/component/fileoperation/hadoop/HadoopOperationHandlerTest.java
@@ -152,91 +152,92 @@ public class HadoopOperationHandlerTest extends OperationHandlerTestTemplate {
 	}
 
 	/*
+	 * FIXME re-enable when Hadoop servers are properly synchronized 
 	 * Overridden - setting last modification date is not supported for directories
 	 */
-	@Override
-	public void testCreateDated() throws Exception {
-		CloverURI uri;
-		Date modifiedDate = new Date(10000);
-		
-		{ // does not work very well on FTP, as the timezone knowledge is required
-			uri = relativeURI("datedFile.tmp");
-			long tolerance = 2 * 60 * 1000; // 2 minutes 
-			Date beforeFileWasCreated = new Date(System.currentTimeMillis() - tolerance);
-			assertTrue(manager.create(uri).success());
-			Date afterFileWasCreated = new Date(System.currentTimeMillis() + tolerance);
-			InfoResult info = manager.info(uri);
-			assertTrue(info.isFile());
-			Date fileCreatedDate = info.getLastModified();
-			if (fileCreatedDate != null) {
-				assertTrue(fileCreatedDate.after(beforeFileWasCreated));
-				assertTrue(afterFileWasCreated.after(fileCreatedDate));
-			}
-		}
-
-		uri = relativeURI("topdir1/subdir/subsubdir/file");
-		System.out.println(uri.getAbsoluteURI());
-		assertFalse(String.format("%s already exists", uri), manager.exists(uri));
-		assertTrue(manager.create(uri, new CreateParameters().setMakeParents(true)).success());
-		assertTrue(String.format("%s is a not file", uri), manager.isFile(uri));
-		assertTrue(manager.create(uri, new CreateParameters().setLastModified(modifiedDate)).success());
-		assertEquals("Dates are different", modifiedDate, manager.info(uri).getLastModified());
-		
-//		uri = relativeURI("topdir2/subdir/subsubdir/dir2/");
+//	@Override
+//	public void testCreateDated() throws Exception {
+//		CloverURI uri;
+//		Date modifiedDate = new Date(10000);
+//		
+//		{ // does not work very well on FTP, as the timezone knowledge is required
+//			uri = relativeURI("datedFile.tmp");
+//			long tolerance = 2 * 60 * 1000; // 2 minutes 
+//			Date beforeFileWasCreated = new Date(System.currentTimeMillis() - tolerance);
+//			assertTrue(manager.create(uri).success());
+//			Date afterFileWasCreated = new Date(System.currentTimeMillis() + tolerance);
+//			InfoResult info = manager.info(uri);
+//			assertTrue(info.isFile());
+//			Date fileCreatedDate = info.getLastModified();
+//			if (fileCreatedDate != null) {
+//				assertTrue(fileCreatedDate.after(beforeFileWasCreated));
+//				assertTrue(afterFileWasCreated.after(fileCreatedDate));
+//			}
+//		}
+//
+//		uri = relativeURI("topdir1/subdir/subsubdir/file");
 //		System.out.println(uri.getAbsoluteURI());
 //		assertFalse(String.format("%s already exists", uri), manager.exists(uri));
 //		assertTrue(manager.create(uri, new CreateParameters().setMakeParents(true)).success());
-//		assertTrue(String.format("%s is not a directory", uri), manager.isDirectory(uri));
+//		assertTrue(String.format("%s is a not file", uri), manager.isFile(uri));
 //		assertTrue(manager.create(uri, new CreateParameters().setLastModified(modifiedDate)).success());
 //		assertEquals("Dates are different", modifiedDate, manager.info(uri).getLastModified());
-		
-		uri = relativeURI("file");
-		uri = relativeURI("datedFile");
-		System.out.println(uri.getAbsoluteURI());
-		assertFalse(String.format("%s already exists", uri), manager.exists(uri));
-		assertTrue(manager.create(uri, new CreateParameters().setMakeParents(true)).success());
-		assertTrue(String.format("%s is not a file", uri), manager.isFile(uri));
-		assertTrue(manager.create(uri, new CreateParameters().setLastModified(modifiedDate)).success());
-		assertEquals("Dates are different", modifiedDate, manager.info(uri).getLastModified());
-		
-//		uri = relativeURI("datedDir1");
+//		
+////		uri = relativeURI("topdir2/subdir/subsubdir/dir2/");
+////		System.out.println(uri.getAbsoluteURI());
+////		assertFalse(String.format("%s already exists", uri), manager.exists(uri));
+////		assertTrue(manager.create(uri, new CreateParameters().setMakeParents(true)).success());
+////		assertTrue(String.format("%s is not a directory", uri), manager.isDirectory(uri));
+////		assertTrue(manager.create(uri, new CreateParameters().setLastModified(modifiedDate)).success());
+////		assertEquals("Dates are different", modifiedDate, manager.info(uri).getLastModified());
+//		
+//		uri = relativeURI("file");
+//		uri = relativeURI("datedFile");
 //		System.out.println(uri.getAbsoluteURI());
 //		assertFalse(String.format("%s already exists", uri), manager.exists(uri));
-//		assertTrue(manager.create(uri, new CreateParameters().setDirectory(true).setLastModified(modifiedDate)).success());
-//		assertTrue(String.format("%s is not a directory", uri), manager.isDirectory(uri));
-//		assertEquals("Dates are different", modifiedDate, manager.info(uri).getLastModified());
-
-//		uri = relativeURI("datedDir2/");
-//		System.out.println(uri.getAbsoluteURI());
-//		assertFalse(String.format("%s already exists", uri), manager.exists(uri));
+//		assertTrue(manager.create(uri, new CreateParameters().setMakeParents(true)).success());
+//		assertTrue(String.format("%s is not a file", uri), manager.isFile(uri));
 //		assertTrue(manager.create(uri, new CreateParameters().setLastModified(modifiedDate)).success());
-//		assertTrue(String.format("%s is not a directory", uri), manager.isDirectory(uri));
 //		assertEquals("Dates are different", modifiedDate, manager.info(uri).getLastModified());
-		
-		{
-			String dirName = "touch";
-			modifiedDate = null; 
-
-			uri = relativeURI(dirName);
-			assertFalse(String.format("%s already exists", uri), manager.exists(uri));
-			
-			uri = relativeURI(dirName + "/file.tmp");
-			System.out.println(uri.getAbsoluteURI());
-			assertTrue(manager.create(uri, new CreateParameters().setMakeParents(true)).success());
-			modifiedDate = manager.info(uri).getLastModified();
-			Thread.sleep(1000);
-			assertTrue(manager.create(uri).success());
-			assertTrue(manager.info(uri).getLastModified().after(modifiedDate));
-			
-//			uri = relativeURI(dirName + "/dir/");
+//		
+////		uri = relativeURI("datedDir1");
+////		System.out.println(uri.getAbsoluteURI());
+////		assertFalse(String.format("%s already exists", uri), manager.exists(uri));
+////		assertTrue(manager.create(uri, new CreateParameters().setDirectory(true).setLastModified(modifiedDate)).success());
+////		assertTrue(String.format("%s is not a directory", uri), manager.isDirectory(uri));
+////		assertEquals("Dates are different", modifiedDate, manager.info(uri).getLastModified());
+//
+////		uri = relativeURI("datedDir2/");
+////		System.out.println(uri.getAbsoluteURI());
+////		assertFalse(String.format("%s already exists", uri), manager.exists(uri));
+////		assertTrue(manager.create(uri, new CreateParameters().setLastModified(modifiedDate)).success());
+////		assertTrue(String.format("%s is not a directory", uri), manager.isDirectory(uri));
+////		assertEquals("Dates are different", modifiedDate, manager.info(uri).getLastModified());
+//		
+//		{
+//			String dirName = "touch";
+//			modifiedDate = null; 
+//
+//			uri = relativeURI(dirName);
+//			assertFalse(String.format("%s already exists", uri), manager.exists(uri));
+//			
+//			uri = relativeURI(dirName + "/file.tmp");
 //			System.out.println(uri.getAbsoluteURI());
 //			assertTrue(manager.create(uri, new CreateParameters().setMakeParents(true)).success());
 //			modifiedDate = manager.info(uri).getLastModified();
 //			Thread.sleep(1000);
 //			assertTrue(manager.create(uri).success());
 //			assertTrue(manager.info(uri).getLastModified().after(modifiedDate));
-		}
-	}
+//			
+////			uri = relativeURI(dirName + "/dir/");
+////			System.out.println(uri.getAbsoluteURI());
+////			assertTrue(manager.create(uri, new CreateParameters().setMakeParents(true)).success());
+////			modifiedDate = manager.info(uri).getLastModified();
+////			Thread.sleep(1000);
+////			assertTrue(manager.create(uri).success());
+////			assertTrue(manager.info(uri).getLastModified().after(modifiedDate));
+//		}
+//	}
 
 //	@Override
 //	public void testResolve() throws Exception {
