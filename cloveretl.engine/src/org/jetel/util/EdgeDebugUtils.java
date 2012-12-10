9ccded034963b5f057afec9fedb691abf12dde7b
zatopekm <zatopekm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2012-12-10 12:24:14 +0000

    UDPDATE: edge debug view is working even for clustered edges (edges attached to cluster components)
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-3-4-0-M1@13346 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/EdgeDebugUtils.java
+++ cloveretl.engine/src/org/jetel/util/EdgeDebugUtils.java
@@ -22,8 +22,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.jetel.exception.JetelRuntimeException;
+import org.jetel.util.string.StringUtils;
 
 /**
+ * Collection of small utilities for file name manipulation for edge debug files.
+ * 
+ * Edge debug file name has following pattern: ${runId}-${edgeId}[-${index}].dbg
+ * 
+ * Where "index" is used to distinguish multiplied edges connected to cluster components. 
+ * 
+ * Naming convention: "unique edge id" is unambiguous identification of all edges even
+ * for multiplied edges connected with cluster partitioners and cluster gathers. For example
+ * output edge from cluster partitioner with id="edge1" is actually duplicated to ensure cluster
+ * partitioning, so several edges is in fact connected edge1, edge1__1, edge1__2, edge1__3, ...
+ * 
+ * There is small 'brainfuck' in the code, delimiter for edgeId and index for edge identification is
+ * double underscore, but debug file used '-' character as delimiter.
+ *  
  * @author Kokon (info@cloveretl.com)
  *         (c) Javlin, a.s. (www.cloveretl.com)
  *
@@ -31,45 +46,150 @@ import org.jetel.exception.JetelRuntimeException;
  */
 public class EdgeDebugUtils {
 
-	private static final Pattern pattern = Pattern.compile("(.*)-(.*)\\.dbg");
+	/**
+	 * Pattern for debug file name "${runId}-${edgeId}[-${index}].dbg"
+	 */
+	private static final String DEBUG_FILE_NAME_PATTERN = "(\\d*)-(" + StringUtils.OBJECT_NAME_PATTERN + ")(-(\\d+))?\\.dbg";
 	
+	private static final Pattern PATTERN = Pattern.compile(DEBUG_FILE_NAME_PATTERN);
+	
+	/** Delimiter between edge id and index of cluster usage */
+	private static final String UNIQUE_EDGE_ID_DELIMITER = "__";
+	
+	/**
+	 * Returns edge debug file name for given runId and edgeId. EdgeId can be unique edge identifier.
+	 */
 	public static String getDebugFileName(long runId, String edgeId) {
-		return runId + "-" + edgeId + ".dbg";
-	}
-	
-	public static boolean isDebugFileName(String fileName) {
-		return pattern.matcher(fileName).matches();
-	}
-	
-	public static boolean isDebugFileName(String fileName, long runId, String edgeId) {
-		if (isDebugFileName(fileName)) {
-			if (extractRunId(fileName).equals(Long.toString(runId))) {
-				if (extractEdgeId(fileName).startsWith(edgeId)) { //that could be done in better way
-					return true;
+		if (runId < 0 || !StringUtils.isValidObjectId(edgeId)) {
+			throw new JetelRuntimeException("invalid arguments");
+		}
+		if (edgeId.contains(UNIQUE_EDGE_ID_DELIMITER)) {
+			String[] parts = edgeId.split(UNIQUE_EDGE_ID_DELIMITER);
+			if (parts.length == 2) {
+				try {
+					int index = Integer.parseInt(parts[1]);
+					return runId + "-" + parts[0] + "-" + index + ".dbg";
+				} catch (NumberFormatException e) {
+					//DO NOTHING
 				}
 			}
 		}
-		return false;
+		return runId + "-" + edgeId + ".dbg";
+	}
+	
+	/**
+	 * Constructs unique edge identification for given edge id and cluster index.
+	 * So for values "Edge1" and 2 returns "Edge1__2".
+	 */
+	public static String assembleUniqueEdgeId(String edgeId, Integer index) {
+		return edgeId + ((index != null && index >= 0) ? (UNIQUE_EDGE_ID_DELIMITER + index) : "");
 	}
 
-	public static String extractEdgeId(String fileName) {
-		if (isDebugFileName(fileName)) {
-			Matcher m = pattern.matcher(fileName);
-			m.matches();
-			return m.group(2);
-		} else {
-			throw new JetelRuntimeException("Given filename '" + fileName + "' does not match edge debug file name pattern (<runId>-<edgeId>.dbg).");
+	/**
+	 * @return true if the given fileName is correct edge debug file name; false otherwise
+	 */
+	public static boolean isDebugFileName(String fileName) {
+		try {
+			parseDebugFileName(fileName);
+			return true;
+		} catch (JetelRuntimeException e) {
+			return false;
 		}
 	}
 	
-	public static String extractRunId(String fileName) {
-		if (isDebugFileName(fileName)) {
-			Matcher m = pattern.matcher(fileName);
-			m.matches();
-			return m.group(1);
-		} else {
-			throw new JetelRuntimeException("Given filename '" + fileName + "' does not match edge debug file name pattern (<runId>-<edgeId>.dbg).");
+	/**
+	 * @return true if the given fileName is correct edge debug file name and moreover
+	 * if the given runId and edgeId is encoded in the given fileName, cluster index is ignored
+	 */
+	public static boolean isDebugFileName(String fileName, long runId, String edgeId) {
+		try {
+			ParsingResult parsingResult = parseDebugFileName(fileName);
+			return parsingResult.runtId == runId && parsingResult.edgeId.equals(edgeId);
+		} catch (JetelRuntimeException e) {
+			return false;
 		}
+	}
+
+	/**
+	 * @return unique edgeId encoded in the given fileName (so for "1270-Edge1__2" returns "Edge1__2")
+	 */
+	public static String extractUniqueEdgeId(String fileName) {
+		ParsingResult parsingResult = parseDebugFileName(fileName);
+		return assembleUniqueEdgeId(parsingResult.edgeId, parsingResult.index);
+	}
+	
+	/**
+	 * @return runId encoded in the given fileName (so for "1270-Edge1__2" returns 1270)
+	 */
+	public static long extractRunId(String fileName) {
+		return parseDebugFileName(fileName).runtId;
+	}
+
+	/**
+	 * @return edgeId from given uniqueEdgeId (so for "Edge1__2" returns "Edge1")
+	 */
+	public static String extractEdgeIdFromUniqueEdgeId(String uniqueEdgeId) {
+		if (uniqueEdgeId.contains(UNIQUE_EDGE_ID_DELIMITER)) {
+			String[] parts = uniqueEdgeId.split(UNIQUE_EDGE_ID_DELIMITER);
+			if (parts.length == 2) {
+				return parts[0];
+			}
+		}
+		return uniqueEdgeId;
+	}
+
+	private static ParsingResult parseDebugFileName(String fileName) {
+		ParsingResult result = new ParsingResult();
+		
+		Matcher m = PATTERN.matcher(fileName);
+		if (!m.matches()) {
+			throw new JetelRuntimeException("Given filename '" + fileName + "' does not match edge debug file name pattern (${runId}-${edgeId}[-${index}].dbg)");
+		}			
+
+		//parse runId
+		String runIdStr = m.group(1);
+		try {
+			long runId = Long.parseLong(runIdStr);
+			if (runId < 0) {
+				throw new JetelRuntimeException("Debug filename '" + fileName + "' is illegal. Detected runId '" + runIdStr + "' is not valid run identifier.");
+			}
+			result.runtId = runId;
+		} catch (NumberFormatException e) {
+			throw new JetelRuntimeException("Debug filename '" + fileName + "' is illegal. Detected runId '" + runIdStr + "' is not valid run identifier.", e);
+		}
+
+		//parse edgeId
+		String edgeId = m.group(2);
+		if (StringUtils.isEmpty(edgeId)) {
+			throw new JetelRuntimeException("Given debug filename has empty edgeId.");
+		}
+		if (!StringUtils.isValidObjectId(edgeId)) {
+			throw new JetelRuntimeException("Given debug filename has edgeId '" + edgeId + "', which is not valid graph element identifier.");
+		}
+		result.edgeId = edgeId;
+		
+		//parse index
+		String indexStr = m.group(4);
+		if (indexStr != null) {
+			try {
+				int index = Integer.parseInt(indexStr);
+				if (index < 0) {
+					throw new JetelRuntimeException("Debug filename '" + fileName + "' is illegal. Detected index '" + indexStr + "' is not valid.");
+				}
+				result.index = index;
+			} catch (NumberFormatException e) {
+				throw new JetelRuntimeException("Debug filename '" + fileName + "' is illegal. Detected index '" + indexStr + "' is not valid number.", e);
+			}
+		}
+		
+		
+		return result;
+	}
+	
+	private static class ParsingResult {
+		public long runtId;
+		public String edgeId;
+		public Integer index; //can be null
 	}
 	
 }
---
+++ cloveretl.engine/test/org/jetel/util/EdgeDebugUtilsTest.java
@@ -0,0 +1,191 @@
+/*
+ * jETeL/CloverETL - Java based ETL application framework.
+ * Copyright (c) Javlin, a.s. (info@cloveretl.com)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jetel.util;
+
+import org.jetel.test.CloverTestCase;
+
+/**
+ * @author Kokon (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *
+ * @created 7.12.2012
+ */
+public class EdgeDebugUtilsTest extends CloverTestCase {
+	
+	public void testGetDebugFileName() {
+		assertEquals("0-edge.dbg", EdgeDebugUtils.getDebugFileName(0, "edge"));
+		assertEquals("1234-eee.dbg", EdgeDebugUtils.getDebugFileName(1234, "eee"));
+		assertEquals("1234-eee-1.dbg", EdgeDebugUtils.getDebugFileName(1234, "eee__1"));
+		assertEquals("12-edsaee-1435.dbg", EdgeDebugUtils.getDebugFileName(12, "edsaee__1435"));
+		assertEquals("1234-eee__.dbg", EdgeDebugUtils.getDebugFileName(1234, "eee__"));
+
+		try {
+			assertEquals("1234-e.e-e.dbg", EdgeDebugUtils.getDebugFileName(1234, "e.e-e"));
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			assertEquals("-1-.dbg", EdgeDebugUtils.getDebugFileName(-1, ""));
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			assertEquals("1234-null.dbg", EdgeDebugUtils.getDebugFileName(1234, null));
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			assertEquals("1234-eee--1.dbg", EdgeDebugUtils.getDebugFileName(1234, "eee__-1"));
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+	}
+
+	public void testAssembleUniqueEdgeId() {
+		assertEquals("edge1__0", EdgeDebugUtils.assembleUniqueEdgeId("edge1", 0));	
+		assertEquals("edge1__123", EdgeDebugUtils.assembleUniqueEdgeId("edge1", 123));	
+		assertEquals("edge1", EdgeDebugUtils.assembleUniqueEdgeId("edge1", -1));	
+		assertEquals("edge1", EdgeDebugUtils.assembleUniqueEdgeId("edge1", -123));	
+		assertEquals("edge1", EdgeDebugUtils.assembleUniqueEdgeId("edge1", null));	
+	}
+	
+	public void testIsDebugFileName() {
+		assertTrue(EdgeDebugUtils.isDebugFileName("0-edge.dbg") == true);
+		assertTrue(EdgeDebugUtils.isDebugFileName("-123-edge.dbg") == false);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge.dbg") == true);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-.dbg") == false);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-eee.dbg") == true);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-e.ee.dbg") == false);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-1.dbg") == true);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-123.dbg") == true);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-0.dbg") == true);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge--1.dbg") == false);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-.dbg") == false);
+	}
+	
+	public void testIsDebugFileName1() {
+		assertTrue(EdgeDebugUtils.isDebugFileName("0-edge.dbg", 0, "edge") == true);
+		assertTrue(EdgeDebugUtils.isDebugFileName("0-edge.dbg", 1, "edge") == false);
+		assertTrue(EdgeDebugUtils.isDebugFileName("0-edge.dbg", 0, "ege") == false);
+		
+		assertTrue(EdgeDebugUtils.isDebugFileName("-123-edge.dbg", -123, "edge") == false);
+		assertTrue(EdgeDebugUtils.isDebugFileName("-123-edge.dbg", 123, null) == false);
+
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge.dbg", 123, "edge") == true);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge.dbg", 1, "edge") == false);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge.dbg", 123, "ege") == false);
+
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-1.dbg", 123, "edge") == true);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-1.dbg", 1, "edge") == false);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-1.dbg", 123, "edge-1") == false);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-1.dbg", 123, "edge__1") == false);
+		
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-0.dbg", 123, "edge") == true);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-0.dbg", 1, "edge") == false);
+		assertTrue(EdgeDebugUtils.isDebugFileName("123-edge-0.dbg", 123, "edge0") == false);
+	}
+
+	public void testExtractUniqueEdgeId() {
+		assertEquals("edge", EdgeDebugUtils.extractUniqueEdgeId("0-edge.dbg"));
+		assertEquals("eee", EdgeDebugUtils.extractUniqueEdgeId("123-eee.dbg"));
+		assertEquals("eee__1", EdgeDebugUtils.extractUniqueEdgeId("123-eee-1.dbg"));
+		assertEquals("eee__0", EdgeDebugUtils.extractUniqueEdgeId("123-eee-0.dbg"));
+		assertEquals("eee__1", EdgeDebugUtils.extractUniqueEdgeId("123-eee__1.dbg"));
+
+		try {
+			EdgeDebugUtils.extractUniqueEdgeId("abc");
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			EdgeDebugUtils.extractUniqueEdgeId("-abc.dbg");
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			EdgeDebugUtils.extractUniqueEdgeId("-1-abc.dbg");
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			EdgeDebugUtils.extractUniqueEdgeId("1-ab-c.dbg");
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			EdgeDebugUtils.extractUniqueEdgeId("1-ab-.dbg");
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			EdgeDebugUtils.extractUniqueEdgeId("1-ab--1.dbg");
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+	}
+
+	public void testExtractRunId() {
+		assertEquals(0, EdgeDebugUtils.extractRunId("0-edge.dbg"));
+		assertEquals(123, EdgeDebugUtils.extractRunId("123-eee.dbg"));
+		assertEquals(123, EdgeDebugUtils.extractRunId("123-eee-0.dbg"));
+		assertEquals(123, EdgeDebugUtils.extractRunId("123-eee-123.dbg"));
+
+		try {
+			EdgeDebugUtils.extractRunId("abc");
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			EdgeDebugUtils.extractRunId("-abc.dbg");
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			EdgeDebugUtils.extractRunId("-1-abc.dbg");
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+		try {
+			EdgeDebugUtils.extractRunId("1-ab-c.dbg");
+			assertTrue(false);
+		} catch (Exception e) {
+			
+		}
+	}
+
+	public void testExtractEdgeIdFromUniqueEdgeId() {
+		assertEquals("edge1", EdgeDebugUtils.extractEdgeIdFromUniqueEdgeId("edge1"));
+		assertEquals("edge1", EdgeDebugUtils.extractEdgeIdFromUniqueEdgeId("edge1__0"));
+		assertEquals("edge1", EdgeDebugUtils.extractEdgeIdFromUniqueEdgeId("edge1__123"));
+	}
+	
+}
