0f4c37c92ace9170e98e70cc03422980d366a635
krivanekm <krivanekm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2013-01-24 12:25:02 +0000

    UPDATE: ProxyConfiguration moved to org.jetel.util.protocols.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-3@13512 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/protocols/ProxyConfiguration.java
+++ cloveretl.engine/src/org/jetel/util/protocols/ProxyConfiguration.java
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-package org.jetel.util.protocols.webdav;
+package org.jetel.util.protocols;
 
 import java.io.IOException;
 import java.net.Proxy;
