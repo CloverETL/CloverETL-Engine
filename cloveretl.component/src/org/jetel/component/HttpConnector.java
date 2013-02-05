7f875da8d6747407284df7c234bab97fceff922a
hajekt <hajekt@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2013-02-05 14:28:14 +0000

    UPDATE: preserve original headers order on component output and replace HashMap with LinkedHashMap (merged from trunk-3.3)
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-3-3-0@13589 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.component/src/org/jetel/component/HttpConnector.java
+++ cloveretl.component/src/org/jetel/component/HttpConnector.java
@@ -39,7 +39,6 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -1103,8 +1102,8 @@ public class HttpConnector extends Node {
 	protected static class HTTPRequestConfiguration {
 		private String target;
 		private String proxy;
-		private Map<String, String> parameters = new HashMap<String, String>();
-		private Map<String, String> multipartEntities = new HashMap<String, String>();
+		private Map<String, String> parameters = new LinkedHashMap<String, String>();
+		private Map<String, String> multipartEntities = new LinkedHashMap<String, String>();
 		private String content;
 		
 		private URL targetURL;
@@ -1193,8 +1192,8 @@ public class HttpConnector extends Node {
 			configuration.setTarget(target);
 			configuration.setProxy(proxy);
 			configuration.setContent(content);
-			configuration.setParameters(new HashMap<String, String>(parameters));
-			configuration.setMultipartEntities(new HashMap<String, String>(multipartEntities));
+			configuration.setParameters(new LinkedHashMap<String, String>(parameters));
+			configuration.setMultipartEntities(new LinkedHashMap<String, String>(multipartEntities));
 			
 			configuration.setProxyURL(proxyURL);
 			configuration.setTargetURL(targetURL);
@@ -1817,7 +1816,7 @@ public class HttpConnector extends Node {
 			resultRecord.getField(RP_STATUS_CODE_INDEX).setValue(response.getStatusLine().getStatusCode());
 			
 			Header[] headers = response.getAllHeaders();
-			Map<String, String> headersMap = new HashMap<String, String>(headers.length);
+			Map<String, String> headersMap = new LinkedHashMap<String, String>(headers.length);
 			List<String> rawHeaders = new ArrayList<String>(headers.length);
 			for (Header header: headers) {
 				headersMap.put(header.getName(), header.getValue());
@@ -2837,7 +2836,7 @@ public class HttpConnector extends Node {
 	 * @return a map representing request parameters.
 	 */
 	private Map<String, String> prepareRequestParameters(Set<String> fieldsToIgnore) {
-		Map<String, String> parameters = new HashMap<String, String>();
+		Map<String, String> parameters = new LinkedHashMap<String, String>();
 		
 		//there are some input fields which should be added to the request
 		if (addInputFieldsAsParametersToUse) {
@@ -2864,7 +2863,7 @@ public class HttpConnector extends Node {
 	 * @return a map representing multi-part entities.
 	 */
 	private Map<String, String> prepareMultipartEntities() {
-		Map<String, String> multipartEntitiesMap = new HashMap<String, String>();
+		Map<String, String> multipartEntitiesMap = new LinkedHashMap<String, String>();
 
 		//parse multipart entities
 		if (multipartEntities != null) {
