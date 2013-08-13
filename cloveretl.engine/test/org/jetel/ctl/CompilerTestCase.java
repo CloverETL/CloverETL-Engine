fd3d4ac9762e25b2ee0bd366294411cd40d0bdc1
tomcanyiv <info@cloveretl.com>
2013-08-13 15:49:01 +0000

    UPDATE: test cases for function decimal2integer - function returns null for null input instead of throwing exception.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-5@14794 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/ctl/CompilerTestCase.java
+++ cloveretl.engine/test/org/jetel/ctl/CompilerTestCase.java
@@ -6941,16 +6941,8 @@ public abstract class CompilerTestCase extends CloverTestCase {
 		check("toInteger", 0);
 		check("toInteger2", -500);
 		check("toInteger3", 1000000);
-	}
-	
-	public void test_convertlib_decimal2integer_expect_error(){
-		//this test should be expected to success in future
-		try {
-			doCompile("function integer transform(){integer i = decimal2integer(null); return 0;}","test_convertlib_decimal2integer_expect_error");
-			fail();
-		} catch (Exception e) {
-			// do nothing
-		}
+		check("nullRet1", null);
+		check("nullRet2", null);
 	}
 	
 	public void test_convertlib_decimal2long() {
