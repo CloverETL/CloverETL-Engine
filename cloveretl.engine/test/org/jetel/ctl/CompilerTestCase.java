0ae094207ae2e1dd25b620de803adb5a8fbd9f34
tomcanyiv <info@cloveretl.com>
2013-08-14 11:18:38 +0000

    UPDATE: test cases for function dobule2long() - function returns null for null input, instead of throwing exception.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-5@14807 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/ctl/CompilerTestCase.java
+++ cloveretl.engine/test/org/jetel/ctl/CompilerTestCase.java
@@ -6984,18 +6984,10 @@ public abstract class CompilerTestCase extends CloverTestCase {
 		check("toLong", 0l);
 		check("toLong2", -500l);
 		check("toLong3", 10000000000l);
+		check("nullRet1", null);
+		check("nullRet2", null);
 	}
 
-	public void test_convertlib_double2long_expect_error(){
-		//this test should be expected to success in future
-		try {
-			doCompile("function integer transform(){long l = double2long(null); return 0;}","test_convertlib_doublel2long_expect_error");
-			fail();
-		} catch (Exception e) {
-			// do nothing
-		}
-	}
-	
 	public void test_convertlib_getFieldName() {
 		doCompile("test_convertlib_getFieldName");
 		check("fieldNames",Arrays.asList("Name", "Age", "City", "Born", "BornMillisec", "Value", "Flag", "ByteArray", "Currency"));
