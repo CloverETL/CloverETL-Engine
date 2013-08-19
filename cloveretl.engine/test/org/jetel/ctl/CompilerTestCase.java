9ae1503f69030af00306a41c7903a16110235080
tomcanyiv <info@cloveretl.com>
2013-08-19 14:27:00 +0000

    UPDATE: clean up
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-5@14887 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/ctl/CompilerTestCase.java
+++ cloveretl.engine/test/org/jetel/ctl/CompilerTestCase.java
@@ -6921,12 +6921,6 @@ public abstract class CompilerTestCase extends CloverTestCase {
 		} catch (Exception e) {
 			// do nothing
 		}
-		try {
-			doCompile("function integer transform(){string s = byte2base64(str2byte('a', 'UTF-8'), null);return 0;}","test_convertlib_byte2base64_expect_error");
-			fail();
-		} catch (Exception e) {
-			// do nothing
-		}
 	}
 	
 	public void test_convertlib_byte2hex() {
