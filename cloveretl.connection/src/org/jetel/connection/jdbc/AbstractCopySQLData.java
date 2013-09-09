22d24f8ec21117b0b7e2806ec55b07d17a3bcd21
krivanekm <krivanekm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2013-09-09 09:18:47 +0000

    FIX: CLO-953 - DBOutputTable: TIME column null values fixed (PostgreSQL).
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-4@15043 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.connection/src/org/jetel/connection/jdbc/AbstractCopySQLData.java
+++ cloveretl.connection/src/org/jetel/connection/jdbc/AbstractCopySQLData.java
@@ -1298,7 +1298,7 @@ public abstract class AbstractCopySQLData implements CopySQLData {
 			        pStatement.setTime(fieldSQL, timeValue);
 			    }
 			} else {
-				pStatement.setNull(fieldSQL, java.sql.Types.DATE);
+				pStatement.setNull(fieldSQL, java.sql.Types.TIME);
 			}
 		}
 		
