89a019fb611d94b287bfa6be7134b077c70c9b84
krivanekm <info@cloveretl.com>
2013-09-16 09:51:03 +0000

    FIX: CLO-1731 - CTL2: End position of a variable declaration fixed.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-5@15082 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/ctl/TransformLangParser.java
+++ cloveretl.engine/src/org/jetel/ctl/TransformLangParser.java
@@ -1219,7 +1219,7 @@ public class TransformLangParser extends ExpParser/*@bgen(jjtree)*/implements Tr
         jjtn000.begin(n.getLine(),n.getColumn());
       jj_consume_token(IDENTIFIER);
         jjtn000.setName(token.image);
-        jjtn000.end(token.beginLine,token.beginColumn);
+        jjtn000.end(token.endLine,token.endColumn);
       switch (jj_nt.kind) {
       case ASSIGN:
         jj_consume_token(ASSIGN);
