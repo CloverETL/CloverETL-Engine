3cbc8a1e9be68e05aeb8ef9794d1ab8b38e9a166
raszykj <info@cloveretl.com>
2013-10-09 11:06:48 +0000

    SVN: merge branches/release-3-5-0-M2 to trunk-3-5
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-5@15202 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/test/org/jetel/util/property/PropertiesRefResolverTest.java
@@ -0,0 +1,48 @@
+/*
+ * jETeL/CloverETL - Java based ETL application framework.
+ * Copyright (c) Javlin, a.s. (info@cloveretl.com)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jetel.util.property;
+
+import java.util.Properties;
+
+import org.jetel.test.CloverTestCase;
+
+/**
+ * @author reichmanf (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *
+ * @created 08.10.2013
+ */
+public class PropertiesRefResolverTest extends CloverTestCase {
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	/**
+	 * issue CLO-2154
+	 */
+	public void testResolveRef_CLO2154() {
+		String path = "C:\\Users\\user/CloverETL/sandboxes/test";
+		Properties properties = new Properties();
+
+		PropertyRefResolver propertyRefResolver = new PropertyRefResolver(properties);
+		assertEquals("C:\\Users\\user/CloverETL/sandboxes/test", propertyRefResolver.resolveRef(path));
+	}
+}
