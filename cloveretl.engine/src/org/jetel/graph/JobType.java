ea8b6f5c9ac864678614c2029ae216f97e93bab3
raszykj <info@cloveretl.com>
2013-10-17 11:12:01 +0000

    SVN: merge branches/release-3-5-0-M2 to trunk-3-5
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-5@15228 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/graph/JobType.java
+++ cloveretl.engine/src/org/jetel/graph/JobType.java
@@ -53,6 +53,15 @@ public enum JobType {
 		this.fileType = fileType;
 		
 	}
+
+	/** 
+	 * Getter which returns response of name() method. 
+	 * This getter is useful when the JobType is used as placeholder 
+	 * and we must explicitly specify that it should return name() not toString() 
+	 * - e.g. JSF may sometimes call toString() in some cases instead of name(). */ 
+	public String getName() {
+		return this.name();
+	}
 	
 	@Override
 	public String toString() {
