541976ec02ae1f07c49913c684bf1510e7112ba3
hajekt <hajekt@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2013-11-08 08:09:51 +0000

    FIX: invalid header.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-5@15318 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/graph/LRemoteEdge.java
+++ cloveretl.engine/src/org/jetel/graph/LRemoteEdge.java
@@ -1,12 +1,21 @@
 /*
- * Copyright 2006-2009 Opensys TM by Javlin, a.s. All rights reserved.
- * Opensys TM by Javlin PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * jETeL/CloverETL - Java based ETL application framework.
+ * Copyright (c) Javlin, a.s. (info@cloveretl.com)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * Opensys TM by Javlin a.s.; Kremencova 18; Prague; Czech Republic
- * www.cloveretl.com; info@cloveretl.com
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-
 package org.jetel.graph;
 
 import java.io.IOException;
