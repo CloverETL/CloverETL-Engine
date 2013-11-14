0561c9afe6675ec7d860a9f87dcda5c07f4a3079
raszykj <raszykj@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2013-11-14 13:14:19 +0000

    SVN: merge branches/release-3-4-3 to trunk-3-4
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-4@15339 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/graph/ResetTest.java
+++ cloveretl.engine/test/org/jetel/graph/ResetTest.java
@@ -178,7 +178,9 @@ public class ResetTest extends CloverTestCase {
 							&& !file.getName().equals("DenormalizerWithoutInputFile.grf") // probably subgraph not supposed to be executed separately
 							&& !file.getName().equals("SimpleSequence_longValue.grf") // needs the sequence to be reset on start
 							&& !file.getName().equals("BeanWriterReader_employees.grf") // remove after CL-2474 solved
-							&& !file.getName().equals("EmptyGraph.grf"); // ok, is to fail
+							&& !file.getName().equals("EmptyGraph.grf") // ok, is to fail
+							&& !file.getName().equals("graphHTTPConnector.grf") // external service is unstable
+							&& !file.getName().equals("EmailReader_BadDataFormatException.grf"); // ok, is to fail
 					
 				}
 			};
