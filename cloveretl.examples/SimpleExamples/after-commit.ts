bb4a46e995ce8adb32bfc3c31815383b336ff042
krygielovam <krygielovam@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2014-01-30 15:57:29 +0000

    FIX: CLO-2874 - fix of some tests
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-4-0@15564 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.examples/SimpleExamples/after-commit.ts
+++ cloveretl.examples/SimpleExamples/after-commit.ts
@@ -65,7 +65,7 @@
 	 <FlatFile outputFile="data-out/correctCustomersControlled.txt" supposedFile="supposed-out/correctCustomers.DataPolicy.Controlled.txt"/>
 	 <FlatFile outputFile="data-out/correctCustomersLenient.txt" supposedFile="supposed-out/correctCustomers.DataPolicy.Lenient.txt"/>
 	 <!-- This output is unstable. FlatFile outputFile="data-out/allCustomersStrict.txt" supposedFile="supposed-out/allCustomers.DataPolicy.Strict.txt"/-->
-		<RegEx expression="org.jetel.exception.BadDataFormatException: Field account_num\(long\) cannot be set to value &quot;8747577056D&quot;; doesn't match the specified format &quot;&quot; with reason &quot;Incomplete parsing&quot;; in record 3, field 2" occurences="1"/>
+		<RegEx expression="org.jetel.exception.BadDataFormatException: Field account_num\(long\) cannot be set to value &quot;8747577056D&quot;; doesn't match the specified format &quot;&quot; with reason &quot;Extraneous characters &quot;D&quot;&quot;; in record 3, field 2" occurences="1"/>
 </FunctionalTest>
 
 <FunctionalTest ident="DBFJoin" graphFile="graph/graphDBFJoin.grf">
