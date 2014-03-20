5b1c51c25855a228f70bca7f4845a87b937830e8
zatopekm <info@cloveretl.com>
2014-03-20 10:19:56 +0000

    FIX: CLO-3176 Graph runs forever in case phases are in wrong order
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-4-0-0-M1@15759 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/graph/TransformationGraphAnalyzer.java
+++ cloveretl.engine/src/org/jetel/graph/TransformationGraphAnalyzer.java
@@ -472,7 +472,7 @@ public class TransformationGraphAnalyzer {
 			Node writer = edge.getWriter(); //can be null for remote edges
 			readerPhase = reader != null ? reader.getPhase() : null;
 			writerPhase = writer != null ? writer.getPhase() : null;
-			if (readerPhase != writerPhase) {
+			if (readerPhase.getPhaseNum() > writerPhase.getPhaseNum()) {
 				// edge connecting two nodes belonging to different phases
 				// has to be buffered
 				edge.setEdgeType(EdgeTypeEnum.PHASE_CONNECTION);
