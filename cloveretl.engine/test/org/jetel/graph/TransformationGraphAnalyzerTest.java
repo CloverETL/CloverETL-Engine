4b9d55922ff7694eb285660e358d648e484ac2e7
zatopekm <zatopekm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2014-04-08 08:37:32 +0000

    FIX: test TransformationGraphAnalyzerTest fixed
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-4-0@15830 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/graph/TransformationGraphAnalyzerTest.java
+++ cloveretl.engine/test/org/jetel/graph/TransformationGraphAnalyzerTest.java
@@ -94,6 +94,7 @@ public class TransformationGraphAnalyzerTest extends CloverTestCase {
 	public void testNodesTopologicalSorting_06() throws FileNotFoundException, XMLConfigurationException, GraphConfigurationException, MalformedURLException {
 		GraphRuntimeContext runtimeContext = new GraphRuntimeContext();
 		runtimeContext.setContextURL(new File("data").toURI().toURL());
+		runtimeContext.setJobType(JobType.JOBFLOW);
 		TransformationGraph graph = TransformationGraphXMLReaderWriter.loadGraph(new FileInputStream("data/graph/TopologicalSorting_06.jbf"), runtimeContext);
 		List<Node> sortedNodes = TransformationGraphAnalyzer.nodesTopologicalSorting(new ArrayList<Node>(graph.getPhase(0).getNodes().values()));
 		checkSortedNodes(sortedNodes, "DATA_GENERATOR", "LOOP", "SIMPLE_COPY", "TRASH");
@@ -102,6 +103,7 @@ public class TransformationGraphAnalyzerTest extends CloverTestCase {
 	public void testNodesTopologicalSorting_07() throws FileNotFoundException, XMLConfigurationException, GraphConfigurationException, MalformedURLException {
 		GraphRuntimeContext runtimeContext = new GraphRuntimeContext();
 		runtimeContext.setContextURL(new File("data").toURI().toURL());
+		runtimeContext.setJobType(JobType.JOBFLOW);
 		TransformationGraph graph = TransformationGraphXMLReaderWriter.loadGraph(new FileInputStream("data/graph/TopologicalSorting_07.jbf"), runtimeContext);
 		List<Node> sortedNodes = TransformationGraphAnalyzer.nodesTopologicalSorting(new ArrayList<Node>(graph.getPhase(0).getNodes().values()));
 		checkSortedNodes(sortedNodes, "LOOP", "SIMPLE_COPY");
@@ -110,6 +112,7 @@ public class TransformationGraphAnalyzerTest extends CloverTestCase {
 	public void testNodesTopologicalSorting_08() throws FileNotFoundException, XMLConfigurationException, GraphConfigurationException, MalformedURLException {
 		GraphRuntimeContext runtimeContext = new GraphRuntimeContext();
 		runtimeContext.setContextURL(new File("data").toURI().toURL());
+		runtimeContext.setJobType(JobType.JOBFLOW);
 		TransformationGraph graph = TransformationGraphXMLReaderWriter.loadGraph(new FileInputStream("data/graph/TopologicalSorting_08.jbf"), runtimeContext);
 		List<Node> sortedNodes = TransformationGraphAnalyzer.nodesTopologicalSorting(new ArrayList<Node>(graph.getPhase(0).getNodes().values()));
 		checkSortedNodes(sortedNodes, "DATA_GENERATOR", "LOOP", "SIMPLE_COPY", "SIMPLE_COPY1", "TRASH");
@@ -118,6 +121,7 @@ public class TransformationGraphAnalyzerTest extends CloverTestCase {
 	public void testNodesTopologicalSorting_09() throws FileNotFoundException, XMLConfigurationException, GraphConfigurationException, MalformedURLException {
 		GraphRuntimeContext runtimeContext = new GraphRuntimeContext();
 		runtimeContext.setContextURL(new File("data").toURI().toURL());
+		runtimeContext.setJobType(JobType.JOBFLOW);
 		TransformationGraph graph = TransformationGraphXMLReaderWriter.loadGraph(new FileInputStream("data/graph/TopologicalSorting_09.jbf"), runtimeContext);
 		List<Node> sortedNodes = TransformationGraphAnalyzer.nodesTopologicalSorting(new ArrayList<Node>(graph.getPhase(0).getNodes().values()));
 		checkSortedNodes(sortedNodes, "DATA_GENERATOR", "LOOP1", "SIMPLE_COPY2", "SIMPLE_COPY1", "LOOP", "SIMPLE_COPY", "TRASH");
@@ -126,6 +130,7 @@ public class TransformationGraphAnalyzerTest extends CloverTestCase {
 	public void testNodesTopologicalSorting_10() throws FileNotFoundException, XMLConfigurationException, GraphConfigurationException, MalformedURLException {
 		GraphRuntimeContext runtimeContext = new GraphRuntimeContext();
 		runtimeContext.setContextURL(new File("data").toURI().toURL());
+		runtimeContext.setJobType(JobType.JOBFLOW);
 		TransformationGraphXMLReaderWriter graphReaderWriter = new TransformationGraphXMLReaderWriter(runtimeContext);
 		graphReaderWriter.setStrictParsing(false);
 		TransformationGraph graph = graphReaderWriter.read(new FileInputStream("data/graph/TopologicalSorting_10.jbf"));
