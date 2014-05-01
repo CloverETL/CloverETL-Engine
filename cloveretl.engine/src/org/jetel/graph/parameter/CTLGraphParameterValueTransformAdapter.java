0832163b0c9b933b2183476809afc4a596aead1b
raszykj <info@cloveretl.com>
2014-05-01 13:33:16 +0000

    NEW: CLO-3600 - Graph parameter dynamic CTL value
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-4-0@15901 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/src/org/jetel/graph/parameter/CTLGraphParameterValueTransformAdapter.java
@@ -0,0 +1,71 @@
+/*
+ * jETeL/CloverETL - Java based ETL application framework.
+ * Copyright (c) Javlin, a.s. (info@cloveretl.com)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jetel.graph.parameter;
+
+import org.apache.log4j.Logger;
+import org.jetel.ctl.CTLAbstractTransformAdapter;
+import org.jetel.ctl.TransformLangExecutor;
+import org.jetel.ctl.TransformLangExecutorRuntimeException;
+import org.jetel.ctl.ASTnode.CLVFFunctionDeclaration;
+import org.jetel.exception.ComponentNotReadyException;
+
+/**
+ * Class for executing graph parameter value function written in CTL
+ * 
+ * @author Raszyk (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *
+ * @created 29. 4. 2014
+ */
+public class CTLGraphParameterValueTransformAdapter extends CTLAbstractTransformAdapter implements GraphParameterValueFunction {
+
+	private CLVFFunctionDeclaration getParameterValueFunction;
+	
+	public CTLGraphParameterValueTransformAdapter(TransformLangExecutor executor, Logger logger) {
+		super(executor, logger);
+	}
+	
+	@Override
+	public void init() throws ComponentNotReadyException {
+		getParameterValueFunction = executor.getFunction(GET_PARAMETER_VALUE_FUNCTION_NAME);
+		
+		if (getParameterValueFunction == null) {
+			throw new ComponentNotReadyException(GET_PARAMETER_VALUE_FUNCTION_NAME + " is not defined");
+		}
+		
+		CLVFFunctionDeclaration initFunction = executor.getFunction(INIT_FUNCTION_NAME);
+		if (initFunction != null) {
+			executor.executeFunction(initFunction, NO_ARGUMENTS, NO_DATA_RECORDS, NO_DATA_RECORDS);
+		}
+	}
+
+	@Override
+	public String getParameterValue() {
+		Object parameterValue = executor.executeFunction(getParameterValueFunction, NO_ARGUMENTS, NO_DATA_RECORDS, NO_DATA_RECORDS);
+		
+		if (parameterValue == null || !(parameterValue instanceof String)) {
+			throw new TransformLangExecutorRuntimeException(getParameterValueFunction.getName() + "() function must return 'string'");
+		}
+		
+		String stringParameterValue = (String) parameterValue;
+		
+		return stringParameterValue;
+	}
+
+}
---
+++ cloveretl.engine/src/org/jetel/graph/parameter/GraphParameterValueFunction.java
@@ -0,0 +1,50 @@
+/*
+ * jETeL/CloverETL - Java based ETL application framework.
+ * Copyright (c) Javlin, a.s. (info@cloveretl.com)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jetel.graph.parameter;
+
+import org.jetel.component.Transform;
+import org.jetel.exception.ComponentNotReadyException;
+import org.jetel.exception.TransformException;
+
+/**
+ * @author Raszyk (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *
+ * @created 29. 4. 2014
+ */
+public interface GraphParameterValueFunction extends Transform {
+
+	public static final String GET_PARAMETER_VALUE_FUNCTION_NAME = "getParameterValue";
+	public static final String INIT_FUNCTION_NAME = "init";
+	
+	/**
+	 * Called before {@link #getParameterValue()} is called for the first time.
+	 * @throws ComponentNotReadyException 
+	 */
+	public void init() throws ComponentNotReadyException;
+	
+	/**
+	 * Graph parameter dynamic value as a string.
+	 * 
+	 * @return
+	 * @throws TransformException 
+	 */
+	public String getParameterValue() throws TransformException;
+
+}
