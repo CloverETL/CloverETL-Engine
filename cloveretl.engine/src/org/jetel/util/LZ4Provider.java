9f398ca54fc189a3c253e94078377d5baf23eccf
krivanekm <info@cloveretl.com>
2014-06-17 13:21:39 +0000

    FIX: CLO-3962 - Fixed JVM crash on Linux IBM 64bit Java 7 with LZ4 compression library.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-4-0@16107 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

---
+++ cloveretl.engine/src/org/jetel/util/LZ4Provider.java
@@ -0,0 +1,47 @@
+/*
+ * jETeL/CloverETL - Java based ETL application framework.
+ * Copyright (c) Javlin, a.s. (info@cloveretl.com)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jetel.util;
+
+import net.jpountz.lz4.LZ4Factory;
+
+import org.jetel.util.exec.PlatformUtils;
+
+/**
+ * Workaround for CLO-3962.
+ * Linux 64bit IBM Java crashes from version 7.0.7.0 on.
+ * To be tested with IBM Java 8.
+ * 
+ * In this environment, enforce the usage of {@link LZ4Factory#unsafeInstance()}.
+ * 
+ * @author krivanekm (info@cloveretl.com)
+ *         (c) Javlin, a.s. (www.cloveretl.com)
+ *
+ * @created Jun 17, 2014
+ * 
+ * @see <a href="https://bug.javlin.eu/browse/CLO-3962">CLO-3962</a>
+ */
+public class LZ4Provider {
+
+	private static boolean IBM_FIX = PlatformUtils.getJavaVendor().contains("IBM") && PlatformUtils.isLinuxPlatform() && PlatformUtils.getOSArch().contains("amd64") && PlatformUtils.getJavaMajorVersion() >= 7;
+	
+	public static LZ4Factory fastestInstance() {
+		return IBM_FIX ? LZ4Factory.unsafeInstance() : LZ4Factory.fastestInstance();
+	}
+
+}
