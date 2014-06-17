8253942919262dfbefadbad5f66e8baa6ab7b3c1
zatopekm <zatopekm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2014-06-17 14:21:02 +0000

    FIX: CLO-3957 Server does not show error component id and type if graph initialization failed
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-4-0@16108 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/exception/JetelRuntimeException.java
+++ cloveretl.engine/src/org/jetel/exception/JetelRuntimeException.java
@@ -43,7 +43,7 @@ public class JetelRuntimeException extends RuntimeException {
     }
 
     public JetelRuntimeException(Throwable cause) {
-        super(cause);
+        super(null, cause);
     }
 
 }
