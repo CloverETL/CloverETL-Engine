2a024539dac7663fb2e471dcb6e03f0d6f7f1185
krivanekm <info@cloveretl.com>
2014-06-18 09:54:15 +0000

    UPDATE: CLO-3769 - IBM Initiate tests: Hadoop tests disabled.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-3-5@16110 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/build-scripts/hudson.groovy
+++ cloveretl.engine/build-scripts/hudson.groovy
@@ -156,7 +156,7 @@ if( !runTests ){
 	}
 	if (testName == "after-commit" && testJVM.endsWith("IBM")) {
 		// disable Hadoop tests
-		antArgs += "-Dexcludedtestidentpattern=Hadoop.*|HDFS.*|Hive.*"
+		antArgs += "-Dtestenv.excludedtestidentpattern=Hadoop.*|HDFS.*|Hive.*"
 		// prevent OutOfMemoryError and Segmentation error on IBM Java
 		antArgs += "-Drunscenarios.Xmx=-Xmx1024m"
 	}
