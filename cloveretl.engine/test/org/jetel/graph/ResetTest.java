e02897608f61811b1db575908a79f5b02ee26d9f
urbanj <urbanj@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2014-07-23 13:33:35 +0000

    FIX: excluded ReadTweets.grf from reset test (uses subgraphs).
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-4-0-0-M2@16224 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/graph/ResetTest.java
+++ cloveretl.engine/test/org/jetel/graph/ResetTest.java
@@ -219,6 +219,7 @@ public class ResetTest extends CloverTestCase {
 							&& !file.getName().equals("EmailReader_BadDataFormatException.grf") // ok, is to fail
 							&& !file.getName().equals("PhaseOrderCheck.grf") // ok, is to fail
 							&& !file.getName().equals("graphCloverData.grf") // remove after CLO-4360 fixed
+							&& !file.getName().equals("ReadTweets.grf") // server test
 							&& !file.getName().equals("MetadataWriter.grf"); // server test
 					
 				}
