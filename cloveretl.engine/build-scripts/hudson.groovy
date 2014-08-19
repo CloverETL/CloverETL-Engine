71bcd8aa80cdcd42d3e43d30c6921abbff11ab12
krivanekm <info@cloveretl.com>
2014-08-19 08:13:28 +0000

    SVN: Revert r16274 (heap size increase for IBM Java tests).
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-3-5-2@16282 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/build-scripts/hudson.groovy
+++ cloveretl.engine/build-scripts/hudson.groovy
@@ -158,7 +158,7 @@ if( !runTests ){
 		// disable Hadoop tests
 		antArgs += "-Dtestenv.excludedtestidentpattern=Hadoop.*|HDFS.*|Hive.*|AddressDoctor*"
 		// prevent OutOfMemoryError and Segmentation error on IBM Java
-		antArgs += "-Drunscenarios.Xmx=-Xmx2048m"
+		antArgs += "-Drunscenarios.Xmx=-Xmx1024m"
 	}
 
 	
