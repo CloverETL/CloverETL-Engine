4cdb376ad73f49b165dd92f0f404baa96b0d4f00
krivanekm <info@cloveretl.com>
2014-09-09 09:02:54 +0000

    UPDATE: CLO-4730 - Attempt to switch IBM Java tests to virt-orange.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-4-0@16371 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/build-scripts/hudson.groovy
+++ cloveretl.engine/build-scripts/hudson.groovy
@@ -46,8 +46,8 @@ println "versionSuffix = " + versionSuffix
 println "buildNumber   = " + buildNumber 
 println "====================================================="
 
-//println "Environment variables:"
-//System.getenv().each{ println "\t${it}" }
+println "Environment variables:"
+System.getenv().each{ println "\t${it}" }
 
 baseD = new File( new File('').absolutePath )
 engineD = new File( baseD, "cloveretl.engine" ) 
