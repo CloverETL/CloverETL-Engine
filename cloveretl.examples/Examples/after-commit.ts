37c3314ab99658791759e6fbc3cfd94a6b57f169
urbanj <urbanj@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2014-11-18 12:40:40 +0000

    FIX: removed jobflow tests for jobflows that were deleted.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/release-4-0-1@16634 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.examples/Examples/after-commit.ts
+++ cloveretl.examples/Examples/after-commit.ts
@@ -86,23 +86,7 @@
 	<FunctionalTest ident="OrdersLookup" graphFile="graph/OrdersLookup.grf">
 		<FlatFile outputFile="data-out/orders_late.txt" supposedFile="supposed-out/orders_late.txt"/>
 	</FunctionalTest>
-	
-	<FunctionalTest ident="DataQualityFirewall" graphFile="jobflow/DataQualityFirewall.jbf" excludedEtlEnvironment="engine">
-		<RegEx expression="Moved &quot;.*employees\.csv&quot; to &quot;.*data-out\/incoming_processed\/.*\/employees\.csv&quot;" occurences="1"/>
-	</FunctionalTest>
-	
-	<FunctionalTest ident="ExecuteExternalUtility" graphFile="jobflow/ExecuteExternalUtility.jbf" excludedEtlEnvironment="engine" />
-	
-	<FunctionalTest ident="FileOperations" graphFile="jobflow/FileOperations.jbf" excludedEtlEnvironment="engine">
-		<FlatFile outputFile="data-out/selected_customers.dat" supposedFile="supposed-out/selected_customers.dat"/>
-	</FunctionalTest>
-	
-	<FunctionalTest ident="SalesforceWebService" graphFile="jobflow/SalesforceWebService.jbf" excludedEtlEnvironment="engine,cluster">
-		<Property name="FORCE_COM_PASSWORD" value="8dmJHbfHl9" />
-		<Property name="FORCE_COM_SECURITY_TOKEN" value="zUYYJ3wgydhrVlAVTLE4Aten" />
-		<Property name="FORCE_COM_USER" value="support@javlin.eu" />
-	</FunctionalTest>
-	
+			
 	<FunctionalTest ident="MetadataWriting" graphFile="graph/MetadataWriting.grf" excludedEtlEnvironment="engine">
 		<FlatFile outputFile="data-out/employees_txt.fmt" supposedFile="supposed-out/employees_txt.fmt"/>
 		<FlatFile outputFile="data-out/departments_txt.fmt" supposedFile="supposed-out/departments_txt.fmt"/>
