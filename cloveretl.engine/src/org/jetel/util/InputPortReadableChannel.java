ac130424fca84173d40e7fbd29e894996c2aefd0
salamonp <info@cloveretl.com>
2015-01-06 15:55:43 +0000

    FIX: CLO-5524 - JSONReader/XMLReader - input->output mapping in stream mode
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-4-1@16830 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/InputPortReadableChannel.java
+++ cloveretl.engine/src/org/jetel/util/InputPortReadableChannel.java
@@ -33,7 +33,7 @@ import org.jetel.graph.InputPort;
 import org.jetel.util.bytes.CloverBuffer;
 
 /**
- * Implementation of <code>java.nio.channels.ReadableByteChannel</code> for reading data from input port (thread-safe).
+ * Implementation of <code>java.nio.channels.ReadableByteChannel</code> for streamed reading data from input port (thread-safe).
  * 
  * @author Martin Slama (martin.slama@javlin.eu) (c) Javlin, a.s. (www.cloveretl.com)
  * 
@@ -204,6 +204,16 @@ public class InputPortReadableChannel implements ReadableByteChannel {
 
 	    	buffer.flip();
 		}
+	}
+	
+	/**
+	 * Fix of CLO-5524. This class basically reads records and throws away all theirs
+	 * data except one field. In some cases we need to access data in the other fields as well.
+	 * This method lets us access the whole DataRecord. 
+	 * @return
+	 */
+	protected DataRecord getCurrentRecord() {
+		return record.duplicate();
 	}
 	
 }
