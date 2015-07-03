c520cbdf1f08537b5fa140268ab200b37d3cb428
salamonp <salamonp@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2015-07-03 07:42:06 +0000

    fixing trunk-4-0 tests after backport of CLO-6697
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-4-0@17790 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/data/DataRecordWithLazyLoadingTest.java
+++ cloveretl.engine/test/org/jetel/data/DataRecordWithLazyLoadingTest.java
@@ -47,6 +47,7 @@ public class DataRecordWithLazyLoadingTest extends CloverTestCase {
 	public void testLazyLoading() {
 		DataRecordWithLazyLoading record = DataRecordFactory.newRecordWithLazyLoading(metadata);
 		record.init();
+		record.reset();
 		assertNull(record.getField("field1").getValue());
 		
 		record.getField("field2").setSourceData(11);
