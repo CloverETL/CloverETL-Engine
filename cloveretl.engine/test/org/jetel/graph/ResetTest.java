3451a3297397002f468998d292610d5bb894fd20
krivanekm <krivanekm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2015-09-04 09:01:39 +0000

    SVN: merge branches/release-4-1-0 to trunk-4-1
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-4-1@18020 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/test/org/jetel/graph/ResetTest.java
+++ cloveretl.engine/test/org/jetel/graph/ResetTest.java
@@ -327,8 +327,10 @@ public class ResetTest extends CloverTestCase {
 							&& !file.getName().equals("UDW_escapeSequences_CLO-5660.grf") // server test
 							&& !file.getName().equals("SetJobOutput_dictionary_CLO-3007.grf") // server test
 							&& !file.getName().equals("ValidationDefaultLanguageSettings.grf") // server test
+							&& !file.getName().equals("DB_rollback_CLO-4878.grf") // server test
 							&& !file.getName().equals("ValidationTransformLifeCycle.grf") // have to be run only once
-							&& !file.getName().equals("Tableau-ThreadSafe.grf"); // disabled test
+							&& !file.getName().equals("Tableau-ThreadSafe.grf") // disabled test
+							&& !file.getName().startsWith("MultiFileWriter_emptyFile"); // excluded until CLO-7184 resolved
 				}
 			};
 			
@@ -344,6 +346,8 @@ public class ResetTest extends CloverTestCase {
 							&& !file.getName().equals("dataPolicy") // negative tests 
 							&& !file.getName().equals("metadataPropagation") // mostly server tests
 							&& !file.getName().equals("ExecuteGraph") // jobflows
+							&& !file.getName().equals("RecordToKeyValues") // CLO-7086: temporarily removed tests
+							&& !file.getName().equals("KeyValuesToRecord") // CLO-7086: temporarily removed tests
 							&& !file.getName().equals("windows"); // wokna only tests
 				}
 			};
@@ -408,6 +412,7 @@ public class ResetTest extends CloverTestCase {
 		runtimeContext.setUseJMX(false);
 	
 		runtimeContext.setContextURL(FileUtils.getFileURL(FileUtils.appendSlash(baseAbsolutePath))); // context URL should be absolute
+		runtimeContext.setJobUrl(FileUtils.getFileURL(runtimeContext.getContextURL(), graphFile.getPath()).toString());
 		// absolute path in PROJECT parameter is required for graphs using Derby database
 		runtimeContext.addAdditionalProperty("PROJECT", baseAbsolutePath);
 		if (StringUtils.findString(graphFile.getName(), NEEDS_SCENARIOS_CONNECTION) != -1) {
