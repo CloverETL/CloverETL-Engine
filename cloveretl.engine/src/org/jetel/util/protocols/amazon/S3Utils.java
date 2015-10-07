502c62a39dde28b2715431efd3ab137c7a73eb65
krivanekm <krivanekm@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2015-10-07 13:09:34 +0000

    UPDATE: CLO-7170/CLO-7293 - S3: Added backward-compatibility support for server-side encryption.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/issue-clo-7170@18162 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/util/protocols/amazon/S3Utils.java
+++ cloveretl.engine/src/org/jetel/util/protocols/amazon/S3Utils.java
@@ -20,11 +20,16 @@ package org.jetel.util.protocols.amazon;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Properties;
 
 import org.jetel.util.ExceptionUtils;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.Upload;
 
@@ -36,14 +41,57 @@ import com.amazonaws.services.s3.transfer.Upload;
  */
 public class S3Utils {
 	
+	private static String JETS3T_PROPERTIES_FILENAME = "jets3t.properties";
+	
+	private static String JETS3T_SERVER_SIDE_ENCRYPTION_PROPERTY = "s3service.server-side-encryption";
+	
+	private static Boolean sse = null;
+	
 	private S3Utils() {
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * Thread-safe lazy initialization.
+	 * 
+	 * @author krivanekm (info@cloveretl.com)
+	 *         (c) Javlin, a.s. (www.cloveretl.com)
+	 *
+	 * @created 7. 10. 2015
+	 */
+	private static class PropertiesSingletonHolder {
+		private static final Properties INSTANCE = new Properties();
+		
+		static {
+			try (InputStream cpIS = S3Utils.class.getResourceAsStream("/" + JETS3T_PROPERTIES_FILENAME);) {
+				if (cpIS != null) {
+					INSTANCE.load(cpIS);
+				}
+			} catch (IOException ioe) {}
+		}
+	}
+	
+	/**
+	 * Backward compatibility: load SSE setting from old jets3t.properties
+	 * file on classpath.
+	 */
+	private static boolean isSSE() {
+		if (sse == null) {
+			sse = Objects.equals("AES256", PropertiesSingletonHolder.INSTANCE.getProperty(JETS3T_SERVER_SIDE_ENCRYPTION_PROPERTY));
+		}
+		return sse;
+	}
+
 	public static void uploadFile(TransferManager tm, File file, String targetBucket, String targetKey) throws IOException {
 		Upload upload = null;
 		try {
-			upload = tm.upload(targetBucket, targetKey, file);
+			PutObjectRequest request = new PutObjectRequest(targetBucket, targetKey, file);
+			if (isSSE()) {
+				ObjectMetadata metadata = new ObjectMetadata();
+				metadata.setSSEAlgorithm(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
+				request.withMetadata(metadata);
+			}
+			upload = tm.upload(request);
 			upload.waitForCompletion();
 //			if (file.length() <= MULTIPART_UPLOAD_THRESHOLD) {
 //				try {
