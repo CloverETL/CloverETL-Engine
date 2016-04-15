ced5ec034188074a9c1dc2d9b1b7bf1dac73e606
krygielovam <krygielovam@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2016-04-15 09:46:38 +0000

    UPDATE: CLO-8366 - Open proper CTL editor on BP (in validator)
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/branches/issue-clo-5406@18634 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.engine/src/org/jetel/component/TransformUtils.java
+++ cloveretl.engine/src/org/jetel/component/TransformUtils.java
@@ -36,9 +36,7 @@ public class TransformUtils {
 	public static final String COMPONENT_ID_PARAM = "componentId"; //$NON-NLS-1$
 	public static final String PROPERTY_NAME_PARAM = "propertyName"; //$NON-NLS-1$
 	public static final String GRAPH_PARAMETER_NAME_PARAM = "graphParameterName"; //$NON-NLS-1$
-	public static final String TYPE_PARAM = "type"; //$NON-NLS-1$
-	public static final String VALIDATOR_TYPE = "validator"; //$NON-NLS-1$
-	public static final String VALIDATOR_CUSTOM_RULE_NAME_PARAM = "customRuleName"; //$NON-NLS-1$
+	public static final String PLUGIN_PARAM = "plugin"; //$NON-NLS-1$
 	
 	/**
 	 * Returns the error message passed to "onError" functions.
