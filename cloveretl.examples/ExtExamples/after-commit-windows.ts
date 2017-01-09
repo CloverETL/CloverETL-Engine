f6b0759701300691010cb036308284ebe6f7bc7c
hajekt <hajekt@a09ad3ba-1a0f-0410-b1b9-c67202f10d70>
2017-01-09 16:56:50 +0000

    UPDATE: CLO-6569 - Create basic graph tests for MSSQLDataWriter - set correct username and password.
    git-svn-id: https://svn.javlin.eu/svn/cloveretl.opensource/trunk-4-5@19473 a09ad3ba-1a0f-0410-b1b9-c67202f10d70

--- cloveretl.examples/ExtExamples/after-commit-windows.ts
+++ cloveretl.examples/ExtExamples/after-commit-windows.ts
@@ -7,6 +7,8 @@
 	<FunctionalTest ident="MsSqlDataWriter" graphFile="graph/graphMsSqlDataWriter.grf">
 		<Property name="BCP_UTILITY_PATH" value="C:/Program Files/Microsoft SQL Server/100/Tools/Binn/bcp.exe"/>
 		<Property name="PARAMETERS" value="characterType|errFile=${DATAOUT_DIR}\err.bcp"/>
+		<Property name="USER" value="sa"/>
+		<Property name="PASS" value="semafor"/>
 		<FlatFile outputFile="data-out/err.bcp" supposedFile="supposed-out/err.MsSqlWriter.bcp"/>	                                                                    
 		<FlatFile outputFile="data-out/exchange.bcp" supposedFile="supposed-out/exchange.MsSqlWriter.bcp"/>	                                                                    
 		<FlatFile outputFile="data-out/out.txt" supposedFile="supposed-out/out.MsSqlWriter.txt"/>	                                                                    
