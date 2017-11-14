14c507fb83ad558437080f5e65812227c6e7a950
Lukas Adamek <lukas.adamek@cloveretl.com>
2017-11-14 09:15:38 +0100

    UPDATE: CLO-12061 - Implement REST Job writer mapping

--- cloveretl.engine/src/org/jetel/util/RestJobMappingProvider.java
+++ cloveretl.engine/src/org/jetel/util/RestJobMappingProvider.java
@@ -18,7 +18,12 @@
  */
 package org.jetel.util;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.jetel.graph.InputPort;
 import org.jetel.graph.Node;
+import org.jetel.metadata.DataFieldMetadata;
 
 /**
  * @author jedlickad (info@cloveretl.com)
@@ -29,7 +34,59 @@ import org.jetel.graph.Node;
 public class RestJobMappingProvider {
 
 	public static String createMapping(Node outputComponent, RestJobOutputType outputType) {
-		//TODO implement com.cloveretl.gui.dataservice.utils.MappingProvider.createMapping
-		return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><root xmlns:clover=\"http://www.cloveretl.com/ns/xmlmapping\"><clover:collection clover:inPort=\"0\" clover:name=\"requestEcho\"><element0><clover:elements clover:include=\"$0.*\"/></element0></clover:collection></root>";
+		StringBuilder result = new StringBuilder();
+		
+		Map<Integer, InputPort> inputPorts = outputComponent.getInputPorts();
+		
+		boolean metadataName = outputType != RestJobOutputType.JSON || Boolean.parseBoolean(outputComponent.getAttributes().getProperty("metadataName"));
+		boolean topLevelArray = outputType == RestJobOutputType.JSON && (Boolean.parseBoolean(outputComponent.getAttributes().getProperty("topLevelArray")) || metadataName);
+	
+		if (inputPorts.isEmpty() || (outputType != RestJobOutputType.JSON && outputType != RestJobOutputType.XML)) {
+			return result.toString();
+		}
+
+		result.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"); //$NON-NLS-1$
+		
+		if (outputType == RestJobOutputType.XML) {
+			result.append("<root xmlns:clover=\"http://www.cloveretl.com/ns/xmlmapping\">"); //$NON-NLS-1$
+			for (Entry<Integer, InputPort> entrySet : inputPorts.entrySet()) {
+				int portNumber = entrySet.getKey();
+					
+				result.append("<clover:element clover:inPort=\"").append(portNumber).append("\" clover:name=\"") //$NON-NLS-1$ //$NON-NLS-2$
+					.append(entrySet.getValue().getEdge().getMetadata().getName()).append("\">"); //$NON-NLS-1$
+				result.append("<clover:elements clover:include=\"$").append(portNumber).append(".*\"/>"); //$NON-NLS-1$ //$NON-NLS-2$
+				result.append("</clover:element>"); //$NON-NLS-1$
+			}			
+			result.append("</root>"); //$NON-NLS-1$
+			
+		} else if (metadataName) {
+			result.append("<root xmlns:clover=\"http://www.cloveretl.com/ns/xmlmapping\">"); //$NON-NLS-1$
+			for (Entry<Integer, InputPort> entrySet : inputPorts.entrySet()) {
+				int portNumber = entrySet.getKey();
+					
+				result.append("<clover:collection clover:inPort=\"").append(portNumber).append("\" clover:name=\"") //$NON-NLS-1$ //$NON-NLS-2$
+					.append(entrySet.getValue().getEdge().getMetadata().getName()).append("\">"); //$NON-NLS-1$
+				result.append("<element").append(portNumber).append("><clover:elements clover:include=\"$").append(portNumber).append(".*\"/></element") //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+					.append(portNumber).append(">");
+				result.append("</clover:collection>"); //$NON-NLS-1$
+			}
+			result.append("</root>"); //$NON-NLS-1$
+			
+		} else if (topLevelArray) {
+			result.append("<clover:collection xmlns:clover=\"http://www.cloveretl.com/ns/xmlmapping\" clover:inPort=\"0\" clover:name=\"root\">"); //$NON-NLS-1$
+			result.append("<element0><clover:elements clover:include=\"$0.*\"/></element0>"); //$NON-NLS-1$
+			result.append("</clover:collection>"); //$NON-NLS-1$
+			
+		} else {
+			result.append("<root xmlns:clover=\"http://www.cloveretl.com/ns/xmlmapping\" clover:inPort=\"0\">"); //$NON-NLS-1$
+			for (DataFieldMetadata field : inputPorts.values().iterator().next().getEdge().getMetadata().getFields()) {
+				result.append("<").append(field.getName()).append(">"); //$NON-NLS-1$ //$NON-NLS-2$
+				result.append("$0.").append(field.getName()); //$NON-NLS-1$
+				result.append("</").append(field.getName()).append(">"); //$NON-NLS-1$ //$NON-NLS-2$
+			}
+			result.append("</root>"); //$NON-NLS-1$
+		}
+
+		return result.toString();
 	}
 }
