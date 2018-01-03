7cf25f808d7a3150c4c54626017902db95f16912
Dominik Janura <dominik.janura@cloveretl.com>
2018-01-03 13:45:47 +0100

    UPDATE: CLO-843 - Split engine job into "engine build" and "engine tests" jobs

--- cloveretl.engine/build-scripts/hudson.groovy
+++ cloveretl.engine/build-scripts/hudson.groovy
@@ -18,127 +18,91 @@ def javaVersion = System.getProperty("java.specification.version", "")
 
 def testName
 
-// get paths for bouncy castle jars
-def getBouncyPath() {
-	bouncyPath = new GroovyShell().parse(new File('./cloveretl.test.environment/test-scripts/bouncyCastle.groovy')).with {
-		main()
-	}
-	println "We got path $bouncyPath"
-}
-
-jobNameM = jobName =~ /^(cloveretl\.engine)-((tests-after-commit-mac-java-1.8-Sun|tests-after-commit-windows-java-1.7-Sun|tests-after-commit-windows-java-1.7-IBM|tests-after-commit-windows-java-1.7-IBM|tests-after-commit-proxy-java-1.7-Sun|tests-after-commit-java-8-Sun|tests-after-commit-java-1.7-IBM|tests-night-java-1.6-IBM|tests-night-java-1.6-JRockit|tests-night-functional-java-1.7-Sun|tests-after-commit|tests-reset|tests-performance-java-1.7-Sun|detail)-)?(.+)$/
-assert jobNameM.matches() 
+jobNameM = jobName =~ /^(cloveretl\.engine)-((tests-after-commit-mac-java-1.8-Sun|tests-after-commit-windows-java-1.7-Sun|tests-after-commit-windows-java-1.7-IBM|tests-after-commit-windows-java-1.7-IBM|tests-after-commit-proxy-java-1.7-Sun|tests-after-commit-java-8-Sun|tests-after-commit-java-1.7-IBM|tests-night-java-1.6-IBM|tests-night-java-1.6-JRockit|tests-night-functional-java-1.7-Sun|tests-after-commit|tests-reset|tests-performance-java-1.7-Sun|detail|tests-after-commit)-)?(.+)$/
+assert jobNameM.matches()
 jobBasename = jobNameM[0][1]
 jobGoal = jobNameM[0][3]
 versionSuffix = jobNameM[0][4]
 
-if( !jobGoal ) jobGoal = "after-commit"
-runTests = jobGoal.startsWith("tests") && jobGoal.contains("java") 
+if( !jobGoal ) jobGoal = "engine"
+if( jobGoal == "tests-after-commit" ) jobGoal = "after-commit"
+runTests = jobGoal.startsWith("tests") && jobGoal.contains("java")
 if( runTests ) {
 	testNameM = jobGoal =~ /^tests-(.+)-(java-[^-]+-[^-]+)(-(.*))?$/
-	assert testNameM.matches() 
+	assert testNameM.matches()
 	testName = testNameM[0][1]
 	testJVM = testNameM[0][2]
 	testOption = testNameM[0][4]
 	testConfiguration = "engine-${versionSuffix}_${testJVM}"
 	if( testOption ) {
 		testConfiguration += "-" + testOption
-	}   
+	}
 	scenarios = testName + ".ts"
 }
 
 baseD = new File( new File('').absolutePath )
 engineD = new File( baseD, "cloveretl.engine" )
-testEnvironmentD = new File( baseD, "cloveretl.test.environment" )
-trustStoreF = new File(baseD, "cloveretl.test.scenarios/truststore/certs") //set default truststore - some tests might use different one
 
 def jobIdent = generateJobIdent(testName ? testName : jobGoal)
-new File(baseD, "cloveretl.test.scenarios/jobIdent.prm").write("JOB_IDENT=" + jobIdent)
-new File(baseD, "cloveretl.examples/ExtExamples/jobIdent.prm").write("JOB_IDENT=" + jobIdent)
- 
+
 def startTime = new Date();
 println "======================= " + startTime
 println "====================================================="
 println "======= Running CloverETL Engine tests =============="
-println "jobBasename   = " + jobBasename 
+println "jobBasename   = " + jobBasename
 println "jobGoal   = " + jobGoal
-if( runTests ){ 
-	println "testName   = " + testName 
+if( runTests ){
+	println "testName   = " + testName
 	println "testJVM   = " + testJVM
 	println "testOption   = " + testOption
-} 
-println "versionSuffix = " + versionSuffix 
-println "buildNumber   = " + buildNumber 
-println "javaVersion   = " + javaVersion 
+}
+println "versionSuffix = " + versionSuffix
+println "buildNumber   = " + buildNumber
+println "javaVersion   = " + javaVersion
 println "jobIdent      = " + jobIdent
 println "====================================================="
 
 println "Environment variables:"
 System.getenv().each{ println "\t${it}" }
 
-getBouncyPath()
-
 antCustomEnv = ["ANT_OPTS":"-Xmx2048m -XX:MaxPermSize=256m"]
+antBaseD = engineD
 if( !runTests ){
-	// compile engine and run some tests
-	antBaseD = engineD
+	// only compile engine, do not run tests
 	antArgs = [
 		"-Dadditional.plugin.list=cloveretl.license.engine,cloveretl.component.hadoop,cloveretl.component.commercial,cloveretl.lookup.commercial,cloveretl.compiler.commercial,cloveretl.quickbase.commercial,cloveretl.ctlfunction.commercial,cloveretl.addressdoctor.commercial,cloveretl.profiler.commercial,cloveretl.mongodb.commercial,cloveretl.validator.commercial,cloveretl.initiate.engine,cloveretl.spreadsheet.commercial,cloveretl.oem.example.component,cloveretl.subgraph.commercial,cloveretl.smb2,cloveretl.tableau",
-		"-Dcte.logpath=${workspace}/cte-logs",
-		"-Dcteguiloglink=${jenkinsBuildUrl}/artifact/cte-logs/",
-		"-Dcte.hudson.link=job/${jobName}/${buildNumber}",
-		"-Ddir.examples=../cloveretl.examples",
 		"-Djavaversion=${javaVersion}",
-		"-Dcloveretl.smb2.bouncycastle.jar.file=${bouncyPath}/bcprov-jdk15on-1.57.jar",
-		"-Drunscenarios.trustStore=-Djavax.net.ssl.trustStore=${trustStoreF}"
+		"-Ddir.examples=../cloveretl.examples"
 	]
-	if( jobGoal == "after-commit" ) {
-		antTarget = "reports-hudson"
-		antArgs += "-Dcte.environment.config=engine-${versionSuffix}_java-1.7-Sun"
-		antArgs += "-Dtest.exclude=org/jetel/graph/ResetTest.java,org/jetel/component/fileoperation/SFTPOperationHandlerTest.java,org/jetel/component/fileoperation/FTPOperationHandlerTest.java,com/opensys/cloveretl/component/EmailFilterTest.java,org/jetel/component/fileoperation/hadoop/Hadoop511OperationHandlerTest.java"
-		antArgs += "-Druntests-target=runtests-scenario-after-commit"
-	} else if( jobGoal == "optimalized"){
-		antTarget = "reports-hudson-optimalized"
-		antArgs += "-Dcte.environment.config=engine-${versionSuffix}_java-1.6-Sun_optimalized"
-		antArgs += "-Dobfuscate.plugin.pattern=cloveretl\\.(?!ctlfunction).*"
-		antArgs += "-Druntests-dontrun=true"
-		antArgs += "-Druntests-target=runtests-scenario-after-commit-with-engine-classes"
-	} else if( jobGoal == "detail"){
-		antTarget = "reports-hudson-detail"
-		antArgs += "-Dcte.environment.config=engine-${versionSuffix}_java-1.7-Sun_detail"
-		antArgs += "-Dtest.exclude=org/jetel/graph/ResetTest.java,org/jetel/component/fileoperation/hadoop/Hadoop511OperationHandlerTest.java"
-		antArgs += "-Drun.coverage=true"
-		antArgs += "-Druntests-target=runtests-scenario-after-commit"
-	} else if( jobGoal == "tests-reset"){
-		antTarget = "runtests-tests-reset"
-		antArgs += "-Druntests-plugins-dontrun=true"	
-		antArgs += "-Dtest.include=org/jetel/graph/ResetTest.java"
-		antArgs += "-Druntests.engine.Xmx=-Xmx3072m"
-		antArgs += "-Dadditional.jvmargs=-Djavax.net.ssl.trustStore=${trustStoreF}"
-		//antArgs += "-Dadditional.jvmargs=-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=33333 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=172.22.2.28"
+	if( jobGoal == "engine" ) {
+		antTarget = "dist"
 	} else {
 		println "ERROR: Unknown goal '${jobGoal}'"
 		exit 1
 	}
-	
 } else {
 	// download engine and run tests only
+	testEnvironmentD = new File( baseD, "cloveretl.test.environment" )
+	trustStoreF = new File(baseD, "cloveretl.test.scenarios/truststore/certs") //set default truststore - some tests might use different one
+	new File(baseD, "cloveretl.test.scenarios/jobIdent.prm").write("JOB_IDENT=" + jobIdent)
+	new File(baseD, "cloveretl.examples/ExtExamples/jobIdent.prm").write("JOB_IDENT=" + jobIdent)
+	getBouncyPath()
 	antBaseD = testEnvironmentD
-	
+
 	engineJobName = "cloveretl.engine-" + versionSuffix
 	engineBuildNumber = new URL( env['JENKINS_URL'] + "job/${engineJobName}/lastSuccessfulBuild/buildNumber").text
 	println "engineBuildNumber   = " + engineBuildNumber
 
-        cloverVersionPropertiesURL = env['JENKINS_URL'] + "job/${engineJobName}/${engineBuildNumber}/artifact/cloveretl.engine/version.properties"
-        println "cloverVersionPropertiesURL ${cloverVersionPropertiesURL}"
+	cloverVersionPropertiesURL = env['JENKINS_URL'] + "job/${engineJobName}/${engineBuildNumber}/artifact/cloveretl.engine/version.properties"
+	println "cloverVersionPropertiesURL ${cloverVersionPropertiesURL}"
 
 	cloverVersionProperties = new URL( cloverVersionPropertiesURL ).text
-        cloverVersionPropertiesM = cloverVersionProperties =~ /version.product=([^\n]*)/
+	cloverVersionPropertiesM = cloverVersionProperties =~ /version.product=([^\n]*)/
 
 	cloverVersion = cloverVersionPropertiesM[0][1]
 	cloverVersionDash = cloverVersion.replaceAll("\\.","-")
 	println "cloverVersion   = " + cloverVersion
-	
+
 	antArgs = [
 		"-Ddir.engine.build=../cloverETL/lib",
 		"-Ddir.plugins=../cloverETL/plugins",
@@ -151,10 +115,30 @@ if( !runTests ){
 		"-Ddir.examples=../cloveretl.examples",
 		"-Dtestenv.etlenvironment=engine",
 		"-Dcloveretl.smb2.bouncycastle.jar.file=${bouncyPath}/bcprov-jdk15on-1.57.jar",
-		"-Djavaversion=${javaVersion}"
+		"-Djavaversion=${javaVersion}",
+		"-Drunscenarios.trustStore=\"-Djavax.net.ssl.trustStore=${trustStoreF}\""
 	]
 
 	antTarget = "run-scenarios-with-engine-build-with-testdb"
+	if( jobGoal == "after-commit" ){
+		antTarget = "runtests-with-testdb"
+		antArgs += "-Dcte.environment.config=engine-${versionSuffix}_java-1.7-Sun"
+		antArgs += "-Dtest.exclude=org/jetel/graph/ResetTest.java,org/jetel/component/fileoperation/SFTPOperationHandlerTest.java,org/jetel/component/fileoperation/FTPOperationHandlerTest.java,com/opensys/cloveretl/component/EmailFilterTest.java,org/jetel/component/fileoperation/hadoop/Hadoop511OperationHandlerTest.java"
+		antArgs += "-Druntests-target=runtests-scenario-after-commit"
+	} else if( jobGoal == "detail"){
+		antTarget = "reports-hudson-detail"
+		antArgs += "-Dcte.environment.config=engine-${versionSuffix}_java-1.7-Sun_detail"
+		antArgs += "-Dtest.exclude=org/jetel/graph/ResetTest.java,org/jetel/component/fileoperation/hadoop/Hadoop511OperationHandlerTest.java"
+		antArgs += "-Drun.coverage=true"
+		antArgs += "-Druntests-target=runtests-scenario-after-commit"
+	} else if( jobGoal == "tests-reset"){
+		antTarget = "runtests-tests-reset"
+		antArgs += "-Druntests-plugins-dontrun=true"
+		antArgs += "-Dtest.include=org/jetel/graph/ResetTest.java"
+		antArgs += "-Druntests.engine.Xmx=-Xmx3072m"
+		antArgs += "-Dadditional.jvmargs=-Djavax.net.ssl.trustStore=${trustStoreF}"
+		//antArgs += "-Dadditional.jvmargs=-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=33333 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=172.22.2.28"
+	}
 	if( testOption == "profile" ){
 		antTarget = "run-scenarios-with-profiler"
 		antArgs += "-Dprofiler.settings=CPURecording;MonitorRecording;ThreadProfiling;VMTelemetryRecording"
@@ -190,10 +174,8 @@ if( !runTests ){
 		antArgs += "-Drunscenarios.Xmso=-Xmso2048k" // CLO-4730, CLO-4567
 	}
 
-	antArgs += "-Drunscenarios.trustStore=\"-Djavax.net.ssl.trustStore=${trustStoreF}\""
-	
 	cloverD = new File(baseD, "cloverETL")
-	// removing files from previous build 
+	// removing files from previous build
 	ant.delete( dir:cloverD )
 	ant.delete(failonerror:false){ fileset( dir:"/data/bigfiles/tmp" , includes:"**")}
 
@@ -202,9 +184,6 @@ if( !runTests ){
 	engineURL.download( engineFile )
 	ant.unzip( src:engineFile, dest:baseD )
 	ant.delete( file:engineFile)
-
-	//"svn up svn+ssh://klara/svn/cloveretl.bigdata/branches/release-${CLOVER_VERSION_X_X_DASH} /data/bigfiles/cloveretl-engine-${CLOVER_VERSION_X_X}".execute()
-			
 }
 
 assert antTarget
@@ -238,7 +217,7 @@ if ( env['testlevel'] ) {
 
 antArgs.each{arg-> antC += arg}
 antC.executeSave(subEnv(antCustomEnv), antBaseD)
-	
+
 /* some common Groovy extensions */
 
 void init(){
@@ -248,40 +227,40 @@ void init(){
 		p.waitForProcessOutput( System.out, System.err )
 		assert p.exitValue() == 0
 	}
-	
+
 	ArrayList.metaClass.executeSave = {
 		delegate.executeSave(null, null)
 	}
-	
+
 	ArrayList.metaClass.executeSave = { procEnv, dir ->
 		print "starting ant command: "; delegate.each{ print "'"+it+"' "}; println ""
 		def p = delegate.execute(procEnv, dir)
 		p.waitForProcessOutput( System.out, System.err )
 		assert p.exitValue() == 0
 	}
-	
+
 	ArrayList.metaClass.executeRsync = {
 		print "starting command: "; delegate.each{ print "'"+it+"' "}; println ""
 		def p = delegate.execute()
 		p.waitForProcessOutput( System.out, System.err )
 		assert (p.exitValue() == 0 || p.exitValue() == 24) // rsync exits with 24 if some files vanish - usually ok
 	}
-	
+
 	URL.metaClass.download = { File toFile ->
 		println "downloading ${delegate} to ${toFile}"
-	    def out = new BufferedOutputStream(new FileOutputStream(toFile))
-	    try {
-		    out << delegate.openStream()
+		def out = new BufferedOutputStream(new FileOutputStream(toFile))
+		try {
+			out << delegate.openStream()
 		} finally {
-    		out.close()
+			out.close()
 		}
 	}
 }
 
-def String[] subEnv(m) { 
+def String[] subEnv(m) {
 	n = [:]
-	System.getenv().collect {k,v->n[k]=v} 
-	m.collect {k,v->n[k]=v} 
+	System.getenv().collect {k,v->n[k]=v}
+	m.collect {k,v->n[k]=v}
 	n.collect { k, v -> "$k=$v" }
 }
 
@@ -295,4 +274,12 @@ String generateJobIdent(String name) {
 		jobIdent = name.take(3)
 	}
 	return jobIdent + String.format('%05d', Math.abs(new Random().nextInt(100000)))
+}
+
+// get paths for bouncy castle jars
+def getBouncyPath() {
+	bouncyPath = new GroovyShell().parse(new File('./cloveretl.test.environment/test-scripts/bouncyCastle.groovy')).with {
+		main()
+	}
+	println "We got path $bouncyPath"
 }
