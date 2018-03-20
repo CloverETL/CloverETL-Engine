d5aa5177417d662f479ebcf098e6e63e2e6c5408
Lukas Adamek <lukas.adamek@cloveretl.com>
2018-03-20 13:13:42 +0100

    UPDATE: CLO-12918 - Support tcServer with Tomcat 8.5 - Reviewer: hajekt

--- cloveretl.examples/BasicExamples/after-commit.ts
+++ cloveretl.examples/BasicExamples/after-commit.ts
@@ -83,7 +83,7 @@
 		<FlatFile outputFile="data-out/orders_late.txt" supposedFile="supposed-out/orders_late.txt"/>
 	</FunctionalTest>
 			
-	<FunctionalTest ident="MetadataWriting" graphFile="graph/MetadataWriting.grf" excludedEtlEnvironment="engine" excludedJavaVersions="1.7" excludedContainers="tomcat7,tomcat8,tomcat9,jetty9,glassfish3,websphere85,jboss6,jboss7,jboss7-eap-6-4,tcserver3,tcserver319">
+	<FunctionalTest ident="MetadataWriting" graphFile="graph/MetadataWriting.grf" excludedEtlEnvironment="engine" excludedJavaVersions="1.7" excludedContainers="tomcat7,tomcat8,tomcat9,jetty9,glassfish3,websphere85,jboss6,jboss7,jboss7-eap-6-4,tcserver329">
 		<FlatFile outputFile="data-out/employees_txt.fmt" supposedFile="supposed-out/employees_txt.fmt"/>
 		<FlatFile outputFile="data-out/departments_txt.fmt" supposedFile="supposed-out/departments_txt.fmt"/>
 	</FunctionalTest>
@@ -93,7 +93,7 @@
 		<FlatFile outputFile="data-out/departments_txt.fmt" supposedFile="supposed-out/departments_txt_version2.fmt"/>
 	</FunctionalTest>
 	
-	<FunctionalTest ident="MetadataWriting_version3" graphFile="graph/MetadataWriting.grf" excludedEtlEnvironment="engine" excludedJavaVersions="1.7" excludedContainers="tomcat7,tomcat8,tomcat9,jetty9,weblogic10,weblogic12,weblogic-12-1-3,glassfish3,websphere85,tcserver3,tcserver319">
+	<FunctionalTest ident="MetadataWriting_version3" graphFile="graph/MetadataWriting.grf" excludedEtlEnvironment="engine" excludedJavaVersions="1.7" excludedContainers="tomcat7,tomcat8,tomcat9,jetty9,weblogic10,weblogic12,weblogic-12-1-3,glassfish3,websphere85,tcserver329">
 		<FlatFile outputFile="data-out/employees_txt.fmt" supposedFile="supposed-out/employees_txt_version3.fmt"/>
 		<FlatFile outputFile="data-out/departments_txt.fmt" supposedFile="supposed-out/departments_txt_version3.fmt"/>
 	</FunctionalTest>
